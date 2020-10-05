17befa307d0edf358b2597ffc8f2cdb212cdecf5
James Talton <jtalton@redhat.com>
2020-10-05 15:58:49 -0400

    Fix css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import { AcmHeader, NavItemE } from '@open-cluster-management/ui-components'
 import '@patternfly/react-core/dist/styles/base.css'
+import { AcmHeader, NavItemE } from '@open-cluster-management/ui-components'
 import React from 'react'
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 import { ClusterManagement } from './Pages/ClusterManagement/ClusterManagement'
