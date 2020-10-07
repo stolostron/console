b66e4745db37d96e0ad47cb396b3a42cfe72642e
James Talton <jtalton@redhat.com>
2020-10-07 12:40:39 -0400

    e2e tests

---
+++ e2e/setup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+echo > ./.env
+
+CLUSTER_API_URL=`oc get infrastructure cluster -o jsonpath={.status.apiServerURL}`
+echo CLUSTER_API_URL=$CLUSTER_API_URL >> ./.env
+
+CLUSTER_API_TOKEN=`oc whoami -t`
+echo CLUSTER_API_TOKEN=$CLUSTER_API_TOKEN >> ./.env
+
+OAUTH2_CLIENT_ID=multicloudingress
+echo OAUTH2_CLIENT_ID=$OAUTH2_CLIENT_ID >> ./.env
+
+OAUTH2_CLIENT_SECRET=multicloudingresssecret
+echo OAUTH2_CLIENT_SECRET=$OAUTH2_CLIENT_SECRET >> ./.env
+
+OAUTH2_REDIRECT_URL=http://localhost:4000/login/callback
+echo OAUTH2_REDIRECT_URL=$OAUTH2_REDIRECT_URL >> ./.env
+
+NODE_TLS_REJECT_UNAUTHORIZED=0
+echo NODE_TLS_REJECT_UNAUTHORIZED=$NODE_TLS_REJECT_UNAUTHORIZED >> ./.env
+
+BACKEND_URL=http://localhost:4000
+echo BACKEND_URL=$BACKEND_URL >> ./.env
+
+FRONTEND_URL=http://localhost:3000
+echo FRONTEND_URL=$FRONTEND_URL >> ./.env
+
+REDIRECT_URIS=$(oc get OAuthClient $OAUTH2_CLIENT_ID -o json | jq -c "[.redirectURIs[], \"$OAUTH2_REDIRECT_URL\"] | unique")
+oc patch OAuthClient multicloudingress --type json -p "[{\"op\": \"add\", \"path\": \"/redirectURIs\", \"value\": ${REDIRECT_URIS}}]"
--- e2e/setup.ts
+++ e2e/setup.ts
@@ -1,3 +1,6 @@
+import { config } from 'dotenv'
+config()
+
 import * as axios from 'axios'
 
 export let request: axios.AxiosInstance
@@ -5,9 +8,9 @@ export let request: axios.AxiosInstance
 // eslint-disable-next-line @typescript-eslint/require-await
 async function setupBeforeAll(): Promise<void> {
     request = axios.default.create({
-        baseURL: process.env.CONSOLE_API,
+        baseURL: process.env.BACKEND_URL,
         validateStatus: () => true,
-        headers: { cookie: 'acm-access-token-cookie=123' },
+        headers: { cookie: `acm-access-token-cookie=${process.env.CLUSTER_API_TOKEN}` },
     })
 }
 
