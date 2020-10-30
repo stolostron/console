8eb3b30f3990d0f02d2353fedb9c6b05fee0a261
Han Zhang <hanzhang@redhat.com>
2020-10-30 14:31:18 -0400

    cleanup non used code, and add basic tests (#24)

--- frontend/src/Pages/ClusterManagement/Clusters/ClusterDetails.tsx
+++ frontend/src/Pages/ClusterManagement/Clusters/ClusterDetails.tsx
@@ -1,10 +1,7 @@
 import {
     AcmEmptyState,
-    AcmLoadingPage,
-    AcmPageCard,
     AcmPageHeader,
-    AcmTable,
-    IAcmTableColumn
+    AcmPageCard
 } from '@open-cluster-management/ui-components'
 import {
     AcmSecondaryNav,
@@ -12,18 +9,10 @@ import {
 } from '@open-cluster-management/ui-components/lib/AcmSecondaryNav/AcmSecondaryNav'
 
 import { Page } from '@patternfly/react-core'
-import CheckIcon from '@patternfly/react-icons/dist/js/icons/check-circle-icon'
-import MinusCircleIcon from '@patternfly/react-icons/dist/js/icons/minus-circle-icon'
-import InProgressIcon from '@patternfly/react-icons/dist/js/icons/in-progress-icon'
-import UnknownIcon from '@patternfly/react-icons/dist/js/icons/unknown-icon'
-
-//import from '@patternfly/icons/in-progress'
-import { ClusterManagementAddons, ClusterManagementAddOn} from '../../../lib/ClusterManagementAddOn'
-import { ManagedClusterAddOn, ManagedClusterAddOns as GetManagedClusterAddOns} from '../../../lib/ManagedClusterAddOn'
-import React, { ReactNode, useEffect, Fragment } from 'react'
-import { ErrorPage } from '../../../components/ErrorPage'
-import { RouteComponentProps, Link, useLocation, Route, BrowserRouter as Router,Switch,useRouteMatch } from 'react-router-dom'
+import React from 'react'
+import { RouteComponentProps, Link, useLocation, Route, Switch, useRouteMatch } from 'react-router-dom'
 import {NodePoolsPageContent} from './ClusterDetailsPages/NodesPools'
+import {ClustersSettingsPageContent} from './ClusterDetailsPages/ClusterSettings'
 
 type ClusterDetailsParams =  { id: string };
 export function ClusterDetailsPage({match}: RouteComponentProps<ClusterDetailsParams>) {
@@ -66,7 +55,7 @@ export function ClustersDeatilsPageContent(props: {
         <React.Fragment>
                 <Switch>
                     <Route path={`${match.path}`} exact>
-                        <AcmEmptyState title="No cluster found." message="Your cluster does not exist." />
+                    <AcmPageCard><AcmEmptyState title="No cluster found." message="Your cluster does not exist." /></AcmPageCard>
                     </Route>
                     <Route path={`${match.path}/nodespools`} >
                         <NodePoolsPageContent name={props.name} namespace={props.namespace} />
@@ -78,208 +67,3 @@ export function ClustersDeatilsPageContent(props: {
         </React.Fragment>
     )
 }
-
-export function ClustersSettingsPageContent(props: {
-    name: string;
-    namespace: string;
-}) {
-    const { loading, error, data, startPolling, stopPolling, refresh } = ClusterManagementAddons()
-
-    const MCARes = GetManagedClusterAddOns(props.namespace)
-    useEffect(() => {
-        startPolling(5 * 1000)
-        MCARes.startPolling(5*1000)
-        return stopPolling
-    }, [startPolling, stopPolling, refresh])
-
-    if (loading || MCARes.loading) {
-        return <AcmLoadingPage />
-    } else if (error) {
-        return <ErrorPage error={error} />
-    } else if (!data || data.length === 0 || !MCARes.data || MCARes.data.length==0) {
-        return (
-            <AcmEmptyState
-                title="No addons found."
-                message="Your cluster does not contain any addons."
-            />
-        )
-    }
-
-    return (
-        <ClusterSettingsTable 
-        clusterManagementAddOns={data} 
-        managedClusterAddOns={MCARes.data}
-        refresh={refresh}
-        />
-    )
-}
-
-export function ClusterSettingsTable(props: {
-    clusterManagementAddOns: ClusterManagementAddOn[]
-    refresh: () => void
-    managedClusterAddOns: ManagedClusterAddOn[] | undefined
-   // deleteConnection: (name?: string, namespace?: string) => Promise<unknown>
-}) {
-    const columns: IAcmTableColumn<ClusterManagementAddOn>[] = [
-        {
-            header: 'Name',
-            sort: 'metadata.name',
-            search: 'metadata.name',
-            cell: 'metadata.name',
-        },
-        {
-            header: 'Status',
-            sort: 'metadata.name',
-            search: 'metadata.name',
-            cell: getDisplayStatus,
-        },
-        {
-            header: 'Message',
-            sort: 'metadata.name',
-            search: 'metadata.name',
-            cell: getDisplayMessage,
-        },
-    ]
-    function keyFn(clusterManagementAddOn: ClusterManagementAddOn) {
-        return clusterManagementAddOn.metadata?.uid as string
-    }
-    function getDisplayStatus(cma: ClusterManagementAddOn): ReactNode {
-        const mcaStatus = props.managedClusterAddOns?.find(mca => mca.metadata.name === cma.metadata.name)
-       
-        if (mcaStatus?.status?.conditions === undefined) {
-            return <span style={{ whiteSpace: 'nowrap' }} key="2">  
-                        <MinusCircleIcon color="grey" key="disabled-icon" />
-                        <span key="status">&nbsp; Disabled</span>
-                </span>
-        }
-        const managedClusterAddOnConditionDegraded = mcaStatus?.status.conditions.find(
-            (condition) => condition.type === 'Degraded'
-        )
-        if (managedClusterAddOnConditionDegraded?.status === 'True') {
-            return <span style={{ whiteSpace: 'nowrap' }} key="2">  
-                        <MinusCircleIcon color="red" key="degraded-icon" />
-                    <span key="status">&nbsp; Degraded</span>
-                    </span>
-        } 
-        const managedClusterAddOnConditionProgressing = mcaStatus?.status.conditions.find(
-            (condition) => condition.type === 'Progressing'
-        )
-        if (managedClusterAddOnConditionProgressing?.status === 'True') {
-            return <span style={{ whiteSpace: 'nowrap' }} key="2">  
-                    <InProgressIcon color="grey" />
-                    <span key="status">&nbsp; Progressing</span>
-                </span>
-        }
-        const  managedClusterAddOnConditionAvailable = mcaStatus?.status.conditions.find(
-            (condition) => condition.type === 'Available'
-        )   
-        if (managedClusterAddOnConditionAvailable?.status === 'True') {
-            return <span style={{ whiteSpace: 'nowrap' }} key="2">  
-                        <CheckIcon color="green" key="available-icon" />
-                        <span key="status">&nbsp; Available</span>
-                    </span>
-        }
-        if ((managedClusterAddOnConditionAvailable?.status === 'False') || (managedClusterAddOnConditionProgressing?.status === 'False') || (managedClusterAddOnConditionDegraded?.status === 'False')) {
-            return <span style={{ whiteSpace: 'nowrap' }} key="2">  
-                        <InProgressIcon color="grey" />
-                        <span key="status">&nbsp; Progressing</span>
-                    </span>
-        }
-        
-        return <span style={{ whiteSpace: 'nowrap' }} key="2">  
-                    <UnknownIcon color="grey" />
-                <span key="status">&nbsp; Unknown</span>
-                </span>
-    } 
-
-    function getDisplayMessage(cma: ClusterManagementAddOn): ReactNode {
-        const mcaStatus = props.managedClusterAddOns?.find(mca => mca.metadata.name === cma.metadata.name)
-        console.log("mcaStatus: ", mcaStatus)
-        if (mcaStatus?.status?.conditions === undefined) {
-            return <span key="status">&nbsp; - </span>
-        }
-        const managedClusterAddOnConditionDegraded = mcaStatus?.status.conditions.find(
-            (condition) => condition.type === 'Degraded'
-        )
-        if (managedClusterAddOnConditionDegraded?.status === 'True') {
-            return managedClusterAddOnConditionDegraded.message
-        } 
-        const managedClusterAddOnConditionProgressing = mcaStatus?.status.conditions.find(
-            (condition) => condition.type === 'Progressing'
-        )
-        if (managedClusterAddOnConditionProgressing?.status === 'True') {
-            return managedClusterAddOnConditionProgressing.message
-        }
-        const  managedClusterAddOnConditionAvailable = mcaStatus?.status.conditions.find(
-            (condition) => condition.type === 'Available'
-        )   
-        if (managedClusterAddOnConditionAvailable?.status === 'True') {
-            return managedClusterAddOnConditionAvailable.message
-        }
-        if ((managedClusterAddOnConditionAvailable?.status === 'False') || (managedClusterAddOnConditionProgressing?.status === 'False') || (managedClusterAddOnConditionDegraded?.status === 'False')) {
-            return ""
-        }
-        
-        return <span>Unknown</span>
-    } 
-
-    // const [deleteProviderConnection] = useDeleteProviderConnectionMutation({ client })
-    //const [confirm, setConfirm] = useState<IConfirmModalProps>(ClosedConfirmModalProps)
-   // const history = useHistory()
-
-    return (
-        <AcmPageCard>
-            <AcmTable<ClusterManagementAddOn>
-                plural="clustermanagementaddons"
-                items={props.clusterManagementAddOns}
-                columns={columns}
-                keyFn={keyFn}
-                tableActions={[
-                
-                ]}
-                bulkActions={[
-                   
-                ]}
-                rowActions={[
-              
-                ]}
-            />
-        </AcmPageCard>
-    )
-}
-
-// export function ClusterDetailsTable(props: { 
-//     clusterManagementAddOns: ClusterManagementAddOn[]
-//     refresh: () => void
-// )} {
-//     const columns: IAcmTableColumn<ClusterManagementAddOn>[] = [
-//         {
-//             header: 'Name',
-//             sort: 'metadata.name',
-//             search: 'metadata.name',
-//             cell: 'metadata.name',
-//         },
-//     ]
-
-//     function keyFn(clusterManagementAddOn: ClusterManagementAddOn) {
-//         return clusterManagementAddOn.metadata?.uid as string
-//     }
-  
-//     return (
-//         <AcmTable<ClusterManagementAddOn>
-//             plural="clustermanagementaddons"
-//             items={props.clusterManagementAddOns}
-//             columns={columns}
-//             keyFn={keyFn}
-//             tableActions={[
-                
-//             ]}
-//             bulkActions={[
-               
-//             ]}
-//             rowActions={[
-               
-//             ]}
-//         />
-//     )
-// }
--- frontend/src/lib/ManagedClusterAddOn.tsx
+++ frontend/src/lib/ManagedClusterAddOn.tsx
@@ -36,35 +36,3 @@ export function ManagedClusterAddOns(namespace: string) {
     return GetWrapper<ManagedClusterAddOn[]>(restFunc)
 }
 
-export function GetMCA(namespace: string) {
-    const originalList = managedClusterAddOns.listNamespace(namespace)
-    console.log("originallist:",originalList)
-
-
-
-    // originalList = originalList.items.map((addon) => {
-    //     const { metadata, status: { conditions, relatedObjects, addOnMeta } } = addon;
-    //     const crd = _.get(relatedObjects, '[0]', {});
-    
-    //     // Order of precedence:
-    //     // degraded=true
-    //     // progressing=true
-    //     // available=true
-    //     // all conditions are false = progressing
-    //     // available=false = unavailable
-    //     // default = unknown
-    //     const isDegraded = conditions.find(({ type, status }) => type === 'Degraded' && status === 'True') || false;
-    //     const isProgressing = conditions.find(({ type, status }) => type === 'Progressing' && status === 'True') || false;
-    //     const isAvailable = conditions.find(({ type, status }) => type === 'Available' && status === 'True') || false;
-    //     const allFalseCondition = conditions.every(({ status }) => status !== 'True') ? { type: 'Progressing' } : false;
-    //     const isNotAvailable = conditions.find(({ type, status }) => type === 'Available' && status === 'False') || false;
-    //     if (isNotAvailable) {
-    //       isNotAvailable.type = 'Unavailable';
-    //     }
-    //     const status = isDegraded || isProgressing || isAvailable || allFalseCondition || isNotAvailable || { type: 'Unknown' };
-    //     const description = _.get(addOnMeta, 'description', '');
-    //     return { metadata, status, addOnResource: { ...crd, description } };
-    //   });
-}
-
-
