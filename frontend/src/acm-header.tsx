7e3443fd1e27814c0f73e96e93f29f4afbdf3476
Sho Weimer <sweimer@redhat.com>
2020-11-10 12:07:05 -0500

    save progress

--- frontend/src/acm-header.tsx
+++ frontend/src/acm-header.tsx
@@ -1,3 +1,4 @@
+// CONSOLE-HEADER
 import Axios, { AxiosResponse } from 'axios'
 
 declare global {
@@ -10,44 +11,50 @@ const fetchHeader = async () => {
     let headerResponse: AxiosResponse
     try {
         headerResponse = await Axios.request({
-            url: `${process.env.REACT_APP_BACKEND}/cluster-management/header`,
+            url: '/cluster-management/header',
             method: 'GET',
             responseType: 'json',
             withCredentials: true
         })
-
-        console.log('headerResponse', headerResponse)
 
         if (headerResponse.status === 200) {
             const { headerHtml, files, props, state } = headerResponse.data
 
             const head = document.querySelector('head')
             const body = document.querySelector('body')
-    
-            const headerContainer = document.createElement('div')
-            headerContainer.id = 'header'
-            headerContainer.innerHTML = headerHtml
-            body?.prepend(headerContainer)
             
             const propScript = document.createElement('script')
             propScript.id = 'props'
             propScript.type ='application/json'
-            propScript.innerHTML = props
+            propScript.innerHTML = JSON.stringify(props)
+            body?.appendChild(propScript)
     
             window.__PRELOADED_STATE__ = state
-    
-            Object.values(files as object).forEach(({ path }) => {
-                if (path.endsWith('css')) {
-                    const link = document.createElement('link')
-                    link.rel = 'stylesheet'
-                    link.href = `${process.env.REACT_APP_BACKEND}${path}`
-                    head?.appendChild(link)
-                } else {
-                    const script = document.createElement('script')
-                    script.src = `${process.env.REACT_APP_BACKEND}${path}`
-                    body?.appendChild(script)
+
+            const link = document.createElement('link')
+            link.rel = 'stylesheet'
+            link.href = files.css.path
+            link.onload = function() {
+                const headerContainer = document.querySelector('#header')
+                if (headerContainer) {
+                    headerContainer.innerHTML = headerHtml
                 }
-            })
+
+                const vendorScript = document.createElement('script')
+                vendorScript.src = files.dll.path
+                vendorScript.onload = function () {
+                    const nlsScript = document.createElement('script')
+                    nlsScript.src = files.nls.path
+                    nlsScript.onload = function() {
+                        const jsScript = document.createElement('script')
+                        jsScript.src = files.js.path
+                        body?.appendChild(jsScript)
+                    }
+                    body?.appendChild(nlsScript)
+                }
+                body?.appendChild(vendorScript)
+            }
+            head?.appendChild(link)
         }
     } catch(err) {
         console.error(err)
