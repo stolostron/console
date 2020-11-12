0887274ebcf2b86509503313f634f6159e432794
randybrunopiverger <rbrunopi@redhat.com>
2020-11-12 14:49:45 -0500

    table, delete, testing

--- frontend/src/Pages/ClusterManagement/BareMetalAssets/BaremetalAssets.tsx
+++ frontend/src/Pages/ClusterManagement/BareMetalAssets/BaremetalAssets.tsx
@@ -1,10 +1,11 @@
-import { AcmEmptyState, AcmLoadingPage, AcmPageCard, AcmTable } from '@open-cluster-management/ui-components'
+import { AcmLabels, AcmLoadingPage, AcmPageCard, AcmTable, AcmEmptyState } from '@open-cluster-management/ui-components'
 import { Page } from '@patternfly/react-core'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { ClosedConfirmModalProps, ConfirmModal, IConfirmModalProps } from '../../../components/ConfirmModal'
 import { ErrorPage } from '../../../components/ErrorPage'
-import { client } from '../../../lib/apollo-client'
-import { BareMetalAsset, useBareMetalAssetsQuery } from '../../../sdk'
-import { ClusterManagementPageHeader } from '../ClusterManagement'
+import { BareMetalAssets as GetBareMetalAsset, BareMetalAsset, BMAStatusMessage, GetLabels, bareMetalAssets } from '../../../library/resources/bare-metal-asset'
+import { ClusterManagementPageHeader, NavigationPath } from '../ClusterManagement'
 
 export function BareMetalAssetsPage() {
     return (
@@ -16,29 +17,71 @@ export function BareMetalAssetsPage() {
 }
 
 export function BareMetalAssets() {
-    const { data, loading, error, startPolling, stopPolling, refetch } = useBareMetalAssetsQuery({
-        client,
-    })
+    const { data, loading, error, startPolling, stopPolling, refresh } = GetBareMetalAsset()
+    useEffect(refresh, [refresh])
     useEffect(() => {
-        refetch()
-        startPolling(10 * 1000)
-        return () => {
-            stopPolling()
-        }
-    }, [refetch, startPolling, stopPolling])
+        startPolling(5 * 1000)
+        return stopPolling
+    }, [startPolling, stopPolling, refresh])
+
     if (loading) {
         return <AcmLoadingPage />
     } else if (error) {
         return <ErrorPage error={error} />
-    } else if (!data?.bareMetalAssets || data.bareMetalAssets.length === 0) {
-        return <AcmPageCard><AcmEmptyState title="No bare metal assets found" message="No bare metal assets found" /></AcmPageCard>
+    } else if (!data?.items || data.items.length === 0) {
+        return (
+            <AcmPageCard>
+                <AcmEmptyState title="No bare metal assets found" message="No bare metal assets found" />
+            </AcmPageCard>
+        )
     }
-    return <BareMetalAssetsTable bareMetalAssets={data.bareMetalAssets as BareMetalAsset[]}></BareMetalAssetsTable>
+
+    return (
+        <BareMetalAssetsTable
+            bareMetalAssets={data.items}
+            refresh={refresh}
+            deleteBareMetalAsset={bareMetalAssets.delete}
+        ></BareMetalAssetsTable>
+    )
 }
 
-export function BareMetalAssetsTable(props: { bareMetalAssets: BareMetalAsset[] }) {
+export function deleteBareMetalAssets(bareMetalAssets: BareMetalAsset[], deleteBareMetalAsset: (name: string, namespace?: string) => Promise<unknown>, refresh: () => void) {
+    const promises: Array<Promise<any>> = []
+
+    bareMetalAssets.forEach( bareMetalAsset => {
+        promises.push(
+            deleteBareMetalAsset(
+            bareMetalAsset.metadata.name!,
+            bareMetalAsset.metadata?.namespace
+        ))
+   })
+   Promise.all(promises)        
+   .then(() => {
+       refresh()
+   })
+}
+
+export function BareMetalAssetsTable(props: {
+    bareMetalAssets: BareMetalAsset[]
+    refresh: () => void
+    deleteBareMetalAsset: (name: string, namespace?: string) => Promise<unknown>}) {
+
+    const [confirm, setConfirm] = useState<IConfirmModalProps>(ClosedConfirmModalProps)
+    const history = useHistory()
+
+    function keyFn(bareMetalAsset: BareMetalAsset) {
+        return bareMetalAsset.metadata.uid as string
+    }
+
     return (
         <AcmPageCard>
+            <ConfirmModal
+                open={confirm.open}
+                confirm={confirm.confirm}
+                cancel={confirm.cancel}
+                title={confirm.title}
+                message={confirm.message}
+            ></ConfirmModal>
             <AcmTable<BareMetalAsset>
                 emptyState={<AcmEmptyState title="No bare metal assets found" />}
                 plural="bare metal assets"
@@ -51,16 +94,108 @@ export function BareMetalAssetsTable(props: { bareMetalAssets: BareMetalAsset[] 
                         search: 'metadata.name',
                     },
                     {
-                        header: 'namespace',
+                        header: 'Namespace',
                         cell: 'metadata.namespace',
                         search: 'metadata.namespace',
                     },
+                    {
+                        header: 'Cluster',
+                        cell: 'metal3.io/cluster-deployment-name',
+                        search: 'metal3.io/cluster-deployment-name',
+                    },
+                    {
+                        header: 'Role',
+                        cell: 'metadata.labels.metal3.io/role',
+                        search: 'metadata.labels.metal3.io/role',
+                    },
+                    {
+                        header: 'Status',
+                        cell: (bareMetalAssets) => {
+                            return BMAStatusMessage(bareMetalAssets)
+                        },
+                    },
+                    {
+                        header: 'Labels',
+                        cell: (bareMetalAssets) => {
+                            const labels = GetLabels(bareMetalAssets)
+                            return <AcmLabels labels={labels} />
+                        },
+                    },
                 ]}
-                keyFn={(item) => item.metadata.uid}
-                tableActions={[]}
-                rowActions={[]}
-                bulkActions={[]}
+                keyFn={keyFn}
+                tableActions={[
+                    {
+                        id: 'createAsset',
+                        title: 'Create Asset',
+                        click: () => {
+                            //history.push(NavigationPath.createBaremetalAssets)
+                        },
+                    },
+                ]}
+                bulkActions={[
+                    {
+                        id: 'destroyBareMetalAsset',
+                        title: 'Destroy',
+                        click: (bareMetalAssets: BareMetalAsset[]) => {
+                            setConfirm({
+                                title: 'Delete bare metal assets',
+                                message: `You are about to delete ${bareMetalAssets.length} bare metal assets. The bare metal assets will no longer be available. This action is irreversible.`,
+                                open: true,
+                                confirm: () => {
+                                    deleteBareMetalAssets(bareMetalAssets, props.deleteBareMetalAsset, props.refresh)
+                                    setConfirm(ClosedConfirmModalProps)
+                                },
+                                cancel: () => {
+                                    setConfirm(ClosedConfirmModalProps)
+                                },
+                            })
+                        },
+                    },
+                    { id: 'createBareMetalAssetCluster', title: 'Create Cluster', click: (items) => {} },
+                ]}
+                rowActions={[
+                    { id: 'editLabels', title: 'Edit labels', click: (item) => {} },
+                    { id: 'editAsset', title: 'Edit Asset', click: (item) => {} },
+                    { id: 'deleteAsset', title: 'Delete Asset', click: (bareMetalAsset: BareMetalAsset) => {
+                        setConfirm({
+                            title: 'Delete bare metal asset',
+                            message: `You are about to delete ${bareMetalAsset.metadata?.name}. The bare metal asset will no longer be available. This action is irreversible.`,
+                            open: true,
+                            confirm: () => {
+                                props
+                                    .deleteBareMetalAsset(
+                                        bareMetalAsset.metadata.name!,
+                                        bareMetalAsset.metadata?.namespace
+                                    )
+                                    .then(() => {
+                                        props.refresh()
+                                    })
+                                setConfirm(ClosedConfirmModalProps)
+                            },
+                            cancel: () => {
+                                setConfirm(ClosedConfirmModalProps)
+                            },
+                        })
+                    } },
+                ]}
             />
         </AcmPageCard>
     )
+}
+
+function GetStatusMessage(status: string) {
+    switch (status) {
+        case 'CredentialsFound':
+            return 'No credentials'
+        case 'AssetSyncStarted':
+            return 'Asset syncing'
+        case 'ClusterDeploymentFound':
+            return 'No cluster deployment'
+        case 'AssetSyncCompleted':
+            return 'Asset sync failed'
+        case 'Ready':
+            return 'Ready'
+        default:
+            return ''
+    }
 }
---
+++ frontend/src/library/resources/bare-metal-asset.ts
@@ -0,0 +1,140 @@
+import { V1ObjectMeta, V1Secret } from '@kubernetes/client-node'
+import { ResourceList } from './resource'
+import { resourceMethods } from '../utils/resource-methods'
+//import { ProviderID } from '../../lib/providers'
+import { useQuery } from '../../lib/useQuery'
+
+export const BareMetalAssetApiVersion = 'inventory.open-cluster-management.io/v1alpha1'
+export type BareMetalAssetApiVersionType = 'inventory.open-cluster-management.io/v1alpha1'
+
+export const BareMetalAssetKind = 'BareMetalAsset'
+export type BareMetalAssetKindType = 'BareMetalAsset'
+
+export interface BareMetalAsset {
+    apiVersion: BareMetalAssetApiVersionType
+    kind: BareMetalAssetKindType
+    metadata: V1ObjectMeta
+    spec: {
+        bmc: {
+            address: string
+            credentialsName: string
+        }
+        bootMac: string
+    }
+    status?: {
+        conditions: Array<{
+            lastTransitionTime: Date
+            message: string
+            reason: string
+            status: string
+            type: string
+        }>
+    }
+}
+
+export const bareMetalAssets = resourceMethods<BareMetalAsset>({
+    apiVersion: BareMetalAssetApiVersion,
+    kind: BareMetalAssetKind
+})
+// TODO: generate logic for listing BMA in edge cases, where fields are missing
+const originalList = bareMetalAssets.list
+
+bareMetalAssets.list = async (labels?: string[]) => {
+    const result = await originalList(labels)
+    return result
+}
+
+const originalCreate = bareMetalAssets.create
+
+bareMetalAssets.create = async (bareMetalAsset: BareMetalAsset) => {
+    return originalCreate(bareMetalAsset)
+}
+
+export function BareMetalAssets() {
+    return useQuery<ResourceList<BareMetalAsset>>(bareMetalAssets.list)
+}
+
+export function BMAStatusMessage(bareMetalAssets: BareMetalAsset) {
+    const KNOWN_STATUSES = [
+        'CredentialsFound',
+        'AssetSyncStarted',
+        'ClusterDeploymentFound',
+        'AssetSyncCompleted',
+        'Ready',
+    ]
+    GetLabels(bareMetalAssets)
+    if (bareMetalAssets.status) {
+        let mostCurrentStatusTime = bareMetalAssets.status!.conditions[0].lastTransitionTime
+        let mostCurrentStatus = bareMetalAssets.status!.conditions[0].type
+        for (let conditions of bareMetalAssets.status!.conditions) {
+            if (conditions.lastTransitionTime > mostCurrentStatusTime!) {
+                mostCurrentStatusTime = conditions.lastTransitionTime
+                mostCurrentStatus = conditions.type
+            }
+            // if status time is equivalent, take the status at that was added last
+            else if (conditions.lastTransitionTime === mostCurrentStatusTime) {
+                mostCurrentStatusTime = conditions.lastTransitionTime
+                mostCurrentStatus = conditions.type
+            }
+        }
+        return GetStatusMessage(mostCurrentStatus)
+    }
+    return ''
+}
+function GetStatusMessage(status: string) {
+    switch (status) {
+        case 'CredentialsFound':
+            return 'No credentials'
+        case 'AssetSyncStarted':
+            return 'Asset syncing'
+        case 'ClusterDeploymentFound':
+            return 'No cluster deployment'
+        case 'AssetSyncCompleted':
+            return 'Asset sync failed'
+        case 'Ready':
+            return 'Ready'
+        default:
+            return ''
+    }
+}
+
+export function GetLabels(bareMetalAssets: BareMetalAsset) {
+    const labels = []
+    const labelDict = bareMetalAssets.metadata.labels
+    for (let key in labelDict) {
+        labels.push(key + '=' + labelDict[key])
+    }
+    return labels
+}
+// TODO - should this be moved to or combined with ./Secrets.tsx ?
+export interface BMASecret extends V1Secret {
+    apiVersion: 'v1',
+    kind: 'secret',
+    metadata: V1ObjectMeta
+    data: {
+        password: string,
+        username: string,
+    }
+}
+
+export const bmaSecrets = resourceMethods<BMASecret>({ apiVersion: 'v1', kind: 'Secret' })
+
+const originalSecretCreate = bmaSecrets.create
+
+bmaSecrets.create = async (bmaSecrets: BMASecret) => {
+    return originalSecretCreate(bmaSecrets)
+}
+
+export function MakeId(customID?: string) {
+    if(customID){
+        return customID
+    }
+
+    let result           = ''
+    const characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length
+    for ( var i = 0; i < 5; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result
+ }
