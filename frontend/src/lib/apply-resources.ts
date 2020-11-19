d0270e406e3985cce4a3749d0aa3a226d932d929
James Talton <jtalton@redhat.com>
2020-11-19 13:19:30 -0500

    Add functions for deleting clusters

--- frontend/src/lib/apply-resources.ts
+++
@@ -1,8 +0,0 @@
-import { IResource } from '../resources/resource'
-import { createResource } from './resource-request'
-
-export async function applyResources(resources: IResource[]) {
-    for (const resource of resources) {
-        createResource(resource)
-    }
-}
