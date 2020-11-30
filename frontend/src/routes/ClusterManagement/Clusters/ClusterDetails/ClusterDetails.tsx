fd5ecf6ebdb128ab04b9963af4b07265efa0d5d4
Sho Weimer <sweimer@redhat.com>
2020-11-30 17:12:45 -0500

    Cluster details overview page details card (#81)
    * details page changes
    
    * run prettier fix
    
    * add spacing
    
    * fix lint errors
    
    * small change

--- frontend/src/routes/ClusterManagement/Clusters/ClusterDetails/ClusterDetails.tsx
+++ frontend/src/routes/ClusterManagement/Clusters/ClusterDetails/ClusterDetails.tsx
@@ -1,59 +1,92 @@
 import {
-    AcmEmptyState,
-    AcmPageCard,
     AcmPageHeader,
     AcmSecondaryNav,
     AcmSecondaryNavItem,
 } from '@open-cluster-management/ui-components'
 import { Page } from '@patternfly/react-core'
-import React, { Fragment, Suspense } from 'react'
+import React, { Fragment, Suspense, useEffect, useCallback, useState } from 'react'
 import { Link, Redirect, Route, RouteComponentProps, Switch, useLocation } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { NavigationPath } from '../../../../NavigationPath'
+import { ClusterOverviewPageContent } from './ClusterOverview/ClusterOverview'
 import { NodePoolsPageContent } from './ClusterNodes/ClusterNodes'
 import { ClustersSettingsPageContent } from './ClusterSettings/ClusterSettings'
+import { useQuery } from '../../../../lib/useQuery'
+import { getSingleCluster, getCluster, Cluster } from '../../../../lib/get-cluster'
+import { ClusterDeployment } from '../../../../resources/cluster-deployment'
+import { ManagedClusterInfo } from '../../../../resources/managed-cluster-info'
+import { CertificateSigningRequest } from '../../../../resources/certificate-signing-requests'
+
+export const ClusterContext = React.createContext<{
+    readonly cluster: Cluster | undefined
+}>({
+    cluster: undefined
+})
 
 export default function ClusterDetailsPage({ match }: RouteComponentProps<{ id: string }>) {
     const location = useLocation()
     const { t } = useTranslation(['cluster'])
+    const queryCluster = () => getSingleCluster(match.params.id, match.params.id)
+    const { data, startPolling } = useQuery(useCallback(queryCluster, [match.params.id]))
+    const [cluster, setCluster] = useState<Cluster | undefined>(undefined)
+
+    useEffect(startPolling, [startPolling])
+    useEffect(() => {
+        const items = data?.map((d) => {
+            if (d.status === 'fulfilled') {
+                return d.value
+            } else {
+                console.error(d.reason)
+                return undefined
+            }
+        })
+
+        let singleCluster: Cluster | undefined
+        if (items) {
+            singleCluster = getCluster(items[1] as ManagedClusterInfo, items[0] as ClusterDeployment, items[2] as CertificateSigningRequest[])
+        }
+
+        setCluster(singleCluster)
+    }, [data])
+
     return (
         <Page>
-            <AcmPageHeader title={match.params.id} breadcrumb={[{ text: t('clusters'), to: NavigationPath.clusters }, { text: t('cluster.details'), to: '' }]} />
-            <AcmSecondaryNav>
-                <AcmSecondaryNavItem
-                    isActive={location.pathname === NavigationPath.clusterOverview.replace(':id', match.params.id)}
-                >
-                    <Link to={NavigationPath.clusterOverview.replace(':id', match.params.id)}>Overview</Link>
-                </AcmSecondaryNavItem>
-                <AcmSecondaryNavItem
-                    isActive={location.pathname === NavigationPath.clusterNodes.replace(':id', match.params.id)}
-                >
-                    <Link to={NavigationPath.clusterNodes.replace(':id', match.params.id)}>Nodes</Link>
-                </AcmSecondaryNavItem>
-                <AcmSecondaryNavItem
-                    isActive={location.pathname === NavigationPath.clusterSettings.replace(':id', match.params.id)}
-                >
-                    <Link to={NavigationPath.clusterSettings.replace(':id', match.params.id)}>Settings</Link>
-                </AcmSecondaryNavItem>
-            </AcmSecondaryNav>
-            <Suspense fallback={<Fragment />}>
-                <Switch>
-                    <Route exact path={NavigationPath.clusterOverview}>
-                        <AcmPageCard>
-                            <AcmEmptyState title="No cluster found." message="Your cluster does not exist." />
-                        </AcmPageCard>
-                    </Route>
-                    <Route exact path={NavigationPath.clusterNodes}>
-                        <NodePoolsPageContent name={match.params.id} namespace={match.params.id} />
-                    </Route>
-                    <Route exact path={NavigationPath.clusterSettings}>
-                        <ClustersSettingsPageContent name={match.params.id} namespace={match.params.id} />
-                    </Route>
-                    <Route exact path={NavigationPath.clusterDetails}>
-                        <Redirect to={NavigationPath.clusterOverview.replace(':id', match.params.id)} />
-                    </Route>
-                </Switch>
-            </Suspense>
+            <ClusterContext.Provider value={{ cluster }}>
+                <AcmPageHeader title={match.params.id} breadcrumb={[{ text: t('clusters'), to: NavigationPath.clusters }, { text: t('cluster.details'), to: '' }]} />
+                <AcmSecondaryNav>
+                    <AcmSecondaryNavItem
+                        isActive={location.pathname === NavigationPath.clusterOverview.replace(':id', match.params.id)}
+                    >
+                        <Link to={NavigationPath.clusterOverview.replace(':id', match.params.id)}>Overview</Link>
+                    </AcmSecondaryNavItem>
+                    <AcmSecondaryNavItem
+                        isActive={location.pathname === NavigationPath.clusterNodes.replace(':id', match.params.id)}
+                    >
+                        <Link to={NavigationPath.clusterNodes.replace(':id', match.params.id)}>Nodes</Link>
+                    </AcmSecondaryNavItem>
+                    <AcmSecondaryNavItem
+                        isActive={location.pathname === NavigationPath.clusterSettings.replace(':id', match.params.id)}
+                    >
+                        <Link to={NavigationPath.clusterSettings.replace(':id', match.params.id)}>Settings</Link>
+                    </AcmSecondaryNavItem>
+                </AcmSecondaryNav>
+                <Suspense fallback={<Fragment />}>
+                    <Switch>
+                        <Route exact path={NavigationPath.clusterOverview}>
+                            <ClusterOverviewPageContent />
+                        </Route>
+                        <Route exact path={NavigationPath.clusterNodes}>
+                            <NodePoolsPageContent name={match.params.id} namespace={match.params.id} />
+                        </Route>
+                        <Route exact path={NavigationPath.clusterSettings}>
+                            <ClustersSettingsPageContent name={match.params.id} namespace={match.params.id} />
+                        </Route>
+                        <Route exact path={NavigationPath.clusterDetails}>
+                            <Redirect to={NavigationPath.clusterOverview.replace(':id', match.params.id)} />
+                        </Route>
+                    </Switch>
+                </Suspense>
+            </ClusterContext.Provider>
         </Page>
     )
 }
---
+++ frontend/src/routes/ClusterManagement/Clusters/ClusterDetails/ClusterOverview/ClusterOverview.tsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react'
+import { AcmDescriptionList, AcmLabels } from '@open-cluster-management/ui-components'
+import { PageSection } from '@patternfly/react-core'
+import { useTranslation } from 'react-i18next'
+import { ClusterContext } from '../ClusterDetails'
+import { StatusField, DistributionField } from '../../../../../components/ClusterCommon'
+
+export function ClusterOverviewPageContent() {
+    const { cluster } = useContext(ClusterContext)
+    const { t } = useTranslation(['cluster']) 
+    return (
+        <PageSection>
+            <AcmDescriptionList
+                title={t('table.details')}
+                leftItems={[
+                    { key: t('table.name'), value: cluster?.name },
+                    { key: t('table.status'), value: cluster?.status && <StatusField status={cluster?.status} /> },
+                    { key: t('table.distribution'), value: cluster?.distribution?.displayVersion && <DistributionField data={cluster?.distribution} /> },
+                    { key: t('table.labels'), value: cluster?.labels && <AcmLabels labels={cluster?.labels} /> },
+                ]}
+                rightItems={[
+                    { key: t('table.kubeApiServer'), value: cluster?.kubeApiServer },
+                    { key: t('table.consoleUrl'), value: cluster?.consoleURL && <a href={cluster?.consoleURL} target="_blank" rel="noreferrer">{cluster?.consoleURL}</a> }
+                ]} />
+        </PageSection>
+    )
+}
