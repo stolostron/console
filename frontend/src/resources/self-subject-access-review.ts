5f0ce282d60876f4cd963f5dc63b2ce26d69c966
randybrunopiverger <rbrunopi@redhat.com>
2020-12-08 13:39:08 -0500

    RBAC resource file
    resource file for self subject access review

---
+++ frontend/src/resources/self-subject-access-review.ts
@@ -0,0 +1,52 @@
+import { V1ObjectMeta } from '@kubernetes/client-node'
+import { IResource } from './resource'
+import { ClusterLabels } from './managed-cluster'
+import { createResource } from '../lib/resource-request'
+
+export const SelfSubjectAccessReviewApiVersion = 'authorization.k8s.io/v1'
+export type SelfSubjectAccessReviewApiVersionType = 'authorization.k8s.io/v1'
+
+export const SelfSubjectAccessReviewKind = 'SelfSubjectAccessReview'
+export type SelfSubjectAccessReviewType = 'SelfSubjectAccessReview'
+
+export interface SelfSubjectAccessReview extends IResource {
+    apiVersion: SelfSubjectAccessReviewApiVersionType
+    kind: SelfSubjectAccessReviewType
+    metadata: V1ObjectMeta
+    spec: {
+        resourceAttributes: ResourceAttributes,
+        user?: string
+    }
+    status?:{
+        allowed: boolean
+        denied: boolean
+        evaluationError: string
+        reason: string
+    }
+}
+export type ResourceAttributes = {
+    group: string
+            name: string
+            namespace: string
+            resource: string
+            subresource: string
+            verb: string
+            version: string
+}
+
+export function createSubjectAccessReview(resourceAttributes:ResourceAttributes){
+    const result = createResource<SelfSubjectAccessReview>({
+        apiVersion: SelfSubjectAccessReviewApiVersion,
+        kind: SelfSubjectAccessReviewKind,
+        metadata:{},
+        spec:{
+            resourceAttributes
+        }
+    })
+    return {
+        promise: result.promise.then((selfSubjectAccessReview) => {
+            return selfSubjectAccessReview
+        }),
+        abort: result.abort,
+    }
+}
