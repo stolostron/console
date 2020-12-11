db6a473db31607a6494415c2a511c89d09970b0b
Sho Weimer <sweimer@redhat.com>
2020-12-11 17:02:26 -0500

    save

--- frontend/src/routes/ProviderConnections/ProviderConnections/ProviderConnections.tsx
+++ frontend/src/routes/ProviderConnections/ProviderConnections/ProviderConnections.tsx
@@ -5,6 +5,8 @@ import {
     AcmEmptyState,
     AcmPageCard,
     AcmTable,
+    AcmInlineProvider,
+    Provider,
     compareStrings,
 } from '@open-cluster-management/ui-components'
 import { AlertActionCloseButton, AlertVariant, Page } from '@patternfly/react-core'
@@ -111,7 +113,32 @@ export function ProviderConnectionsTable(props: { providerConnections?: Provider
                             return compareStrings(getProvider(a.metadata?.labels), getProvider(b.metadata?.labels))
                         },
                         cell: (item: ProviderConnection) => {
-                            return getProvider(item.metadata?.labels)
+                            const label = item.metadata.labels?.['cluster.open-cluster-management.io/provider']
+                            let provider
+                            switch(label) {
+                            case ProviderID.GCP:
+                                provider = Provider.gcp
+                                break
+                            case ProviderID.AWS:
+                                provider = Provider.aws
+                                break
+                            case ProviderID.AZR:
+                                provider = Provider.azure
+                                break
+                            case ProviderID.VMW:
+                                provider = Provider.vmware
+                                break
+                            case ProviderID.BMC:
+                                provider = Provider.baremetal
+                                break
+                            case ProviderID.CRH:
+                                provider = Provider.redhatcloud
+                                break
+                            case ProviderID.UKN:
+                            default:
+                                provider = Provider.other
+                            }
+                            return <AcmInlineProvider provider={provider} />
                         },
                         search: (item: ProviderConnection) => {
                             return getProvider(item.metadata?.labels)
