eeeb32fc83ff576d2217c3e1286cbd8c06faffd3
James Talton <jtalton@redhat.com>
2020-12-11 10:05:10 -0500

    logging

--- backend/src/logger.ts
+++ backend/src/logger.ts
@@ -2,40 +2,33 @@ export type LogEntry = string | number | Error
 export type Log = LogEntry[]
 export type Logs = Log[]
 
-const Reset = '\x1b[0m'
-
-const Dim = '\x1b[2m'
-const FgGreen = '\x1b[32m'
-
 function writeLogEntry(logEntry: unknown) {
     if (typeof logEntry === 'string') {
-        process.stdout.write(logEntry)
+        return logEntry
     } else if (typeof logEntry === 'number') {
-        process.stdout.write(logEntry.toString())
+        return logEntry.toString()
     } else {
-        process.stdout.write('UNKNOWN')
+        return 'UNKNOWN'
     }
 }
 
 export function info(logs: Logs): void {
     let index = 0
     for (const log of logs) {
+        let line = ''
         if (index++ === 0) {
-            process.stdout.write(FgGreen)
-            process.stdout.write(' INFO ')
-            process.stdout.write(Reset)
+            line += ' INFO '
         } else {
-            process.stdout.write(Dim)
-            process.stdout.write('      ')
+            line += '      '
         }
         let index2 = 0
         for (const logEntry of log) {
             if (index2++ !== 0) {
-                process.stdout.write(' ')
+                line += ' '
             }
-            writeLogEntry(logEntry)
+            line += writeLogEntry(logEntry)
         }
-        process.stdout.write(Reset)
-        process.stdout.write('\n')
+        line += '\n'
+        process.stdout.write(line)
     }
 }
