c71bd3c7949b2d5be09643d4124f0964e7c7cd1e
James Talton <jtalton@redhat.com>
2020-12-12 10:47:44 -0500

    backend dev env variable loading

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,18 @@
 /* istanbul ignore file */
+
+import { readFileSync } from 'fs'
+try {
+    const lines = readFileSync('.env').toString().split('\n')
+    for (const line of lines) {
+        const parts = line.split('=')
+        if (parts.length === 2) {
+            process.env[parts[0]] = parts[1]
+        }
+    }
+} catch (err) {
+    // Do Nothing
+}
+
 import { requestHandler } from './app'
 import { startServer, stopServer } from './server'
 
