5af2ff22b95ef6fe4d2de1424fb60858596a2524
James Talton <jtalton@redhat.com>
2020-12-14 12:09:09 -0500

    Remove colors in logs for production

--- backend/src/logger.ts
+++ backend/src/logger.ts
@@ -2,31 +2,18 @@ export type LogEntry = string | number | Error
 export type Log = LogEntry[]
 export type Logs = Log[]
 
-const Reset = '\x1b[0m'
-const Bright = '\x1b[1m'
-const Dim = '\x1b[2m'
-const Underscore = '\x1b[4m'
-const Blink = '\x1b[5m'
-const Reverse = '\x1b[7m'
-const Hidden = '\x1b[8m'
+const Reset = process.env.NODE_ENV !== 'production' ? '\x1b[0m' : ''
+// const Bright = process.env.NODE_ENV !== 'production' ? '\x1b[1m' : ''
+const Dim = process.env.NODE_ENV !== 'production' ? '\x1b[2m' : ''
 
-const FgBlack = '\x1b[30m'
-const FgRed = '\x1b[31m'
-const FgGreen = '\x1b[32m'
-const FgYellow = '\x1b[33m'
-const FgBlue = '\x1b[34m'
-const FgMagenta = '\x1b[35m'
-const FgCyan = '\x1b[36m'
-const FgWhite = '\x1b[37m'
-
-const BgBlack = '\x1b[40m'
-const BgRed = '\x1b[41m'
-const BgGreen = '\x1b[42m'
-const BgYellow = '\x1b[43m'
-const BgBlue = '\x1b[44m'
-const BgMagenta = '\x1b[45m'
-const BgCyan = '\x1b[46m'
-const BgWhite = '\x1b[47m'
+// const FgBlack = process.env.NODE_ENV !== 'production' ? '\x1b[30m' : ''
+const FgRed = process.env.NODE_ENV !== 'production' ? '\x1b[31m' : ''
+const FgGreen = process.env.NODE_ENV !== 'production' ? '\x1b[32m' : ''
+// const FgYellow = process.env.NODE_ENV !== 'production' ? '\x1b[33m' : ''
+const FgBlue = process.env.NODE_ENV !== 'production' ? '\x1b[34m' : ''
+// const FgMagenta = process.env.NODE_ENV !== 'production' ? '\x1b[35m' : ''
+// const FgCyan = process.env.NODE_ENV !== 'production' ? '\x1b[36m' : ''
+// const FgWhite = process.env.NODE_ENV !== 'production' ? '\x1b[37m' : ''
 
 function writeLogEntry(logEntry: unknown) {
     if (typeof logEntry === 'string') {
@@ -50,9 +37,7 @@ function writeLogs(logs: Logs, type: string): void {
         }
         let index2 = 0
         for (const logEntry of log) {
-            if (index2++ !== 0) {
-                lines += ' '
-            }
+            if (index2++ !== 0) lines += ' '
             lines += writeLogEntry(logEntry)
         }
         lines += `${Reset}\n`
