98525bc92f44795fbd00975cca3b51ca47d9c0da
John Swanke <jswanke@redhat.com>
2020-12-16 12:12:52 -0500

    test

--- frontend/src/routes/ClusterManagement/Clusters/CreateCluster/CreateCluster.test.tsx
+++ frontend/src/routes/ClusterManagement/Clusters/CreateCluster/CreateCluster.test.tsx
@@ -1,8 +1,6 @@
-import { render, waitFor } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render } from '@testing-library/react'
 import React from 'react'
 import { MemoryRouter, Route } from 'react-router-dom'
-import { mockBadRequestStatus, nockCreate, nockDelete, nockGet, nockList } from '../../../../lib/nock-util'
 import CreateClusterPage from './CreateCluster'
 
 jest.mock('react-i18next', () => ({
@@ -27,12 +25,11 @@ describe('CreateCluster', () => {
     }
 
     test('renders', () => {
-        const { getByTestId } = render(<Component />)
-//        expect(getByTestId('create-cluster-form')).toBeInTheDocument()
-//        expect(getByTestId('clusterName-label')).toBeInTheDocument()
-//        expect(getByTestId('cloudLabel-label')).toBeInTheDocument()
-//        expect(getByTestId('environmentLabel-label')).toBeInTheDocument()
-//        expect(getByTestId('additionalLabels-label')).toBeInTheDocument()
-//        expect(getByTestId('submit')).toBeInTheDocument()
+        const { getByTestId, getByText } = render(<Component />)
+        expect(getByTestId('notifications')).toBeInTheDocument()
+        expect(getByTestId('create-button-portal-id')).toBeInTheDocument()
+        expect(getByTestId('name')).toBeInTheDocument()
+        expect(getByText('creation.ocp.cluster.details')).toBeInTheDocument()
+        expect(getByTestId('main-creation.ocp.cluster.details')).toBeInTheDocument()
     })
 })
