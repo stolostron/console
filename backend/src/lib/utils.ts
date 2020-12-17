fb3da5056682cb8d603138ea7a125fb1aaa9254e
Hanqiu Zhang <hanzhang@redhat.com>
2020-12-17 16:39:01 -0500

    added upgrade

---
+++ backend/src/lib/utils.ts
@@ -0,0 +1,425 @@
+import { parse as parseUrl } from 'url'
+import { IncomingMessage, request as httpRequest, RequestOptions } from 'http'
+
+import { Agent } from 'https'
+import { createHash } from 'crypto'
+
+import {
+    ManagedClusterView,
+    ManagedClusterViewVersion,
+    ManagedClusterViewResources,
+    ManagedClusterViewApiGroup,
+    ManagedClusterViewApiVersion,
+    ManagedClusterViewKind,
+} from './managedclusterview'
+import {
+    ManagedClusterAction,
+    ManagedClusterActionVersion,
+    ManagedClusterActionResources,
+    ManagedClusterActionApiGroup,
+    ManagedClusterActionApiVersion,
+    ManagedClusterActionKind,
+} from './managedclusteraction'
+import { promises } from 'fs'
+import { get as _get, set as _set } from 'lodash'
+
+interface KubernetesGVR {
+    apiGroup: string
+    version: string
+    resources: string
+}
+interface namespacedName {
+    name: string
+    namespace: string
+}
+interface requestOptions {
+    host: string
+    token: string
+    agent: Agent
+}
+type verifyStatusFn<T> = (
+    response: IncomingMessage
+) => Promise<{
+    isValid: boolean
+    isRetryRequired: boolean
+    retData?: T
+    code?: number
+    msg?: string
+}>
+export interface requestException {
+    code: number
+    msg: string
+}
+
+const pollInterval = 2000
+
+const gvrManagedClusterView: KubernetesGVR = {
+    apiGroup: ManagedClusterViewApiGroup,
+    version: ManagedClusterViewVersion,
+    resources: ManagedClusterViewResources,
+}
+
+const gvrManagedClusterAction: KubernetesGVR = {
+    apiGroup: ManagedClusterActionApiGroup,
+    version: ManagedClusterActionVersion,
+    resources: ManagedClusterActionResources,
+}
+
+export function parseBody(req: IncomingMessage): Promise<string> {
+    return new Promise((resolve, reject) => {
+        let data = ''
+        req.on('error', reject)
+        req.on('data', (chunk) => (data += chunk))
+        req.on('end', () => {
+            resolve(data)
+        })
+    })
+}
+
+export async function parseJsonBody<T>(req: IncomingMessage): Promise<T> {
+    const body = await parseBody(req)
+    return JSON.parse(body) as T
+}
+
+// get resources on local cluster
+export function getResource(opt: requestOptions, gvr: KubernetesGVR, nsn: namespacedName): Promise<IncomingMessage> {
+    const headers = { authorization: `Bearer ${opt.token}`, host: opt.host }
+    const options: RequestOptions = {
+        ...parseUrl(
+            `${process.env.CLUSTER_API_URL}/apis/${gvr.apiGroup}/${gvr.version}/namespaces/${nsn.namespace}/${gvr.resources}/${nsn.name}`
+        ),
+        ...{ method: 'GET', headers, agent: opt.agent },
+    }
+
+    return new Promise<IncomingMessage>((resolve) => {
+        const getReq = httpRequest(options, (response) => {
+            resolve(response)
+        })
+        getReq.end()
+    })
+}
+
+export function deleteResource(opt: requestOptions, gvr: KubernetesGVR, nsn: namespacedName): Promise<IncomingMessage> {
+    const headers = { authorization: `Bearer ${opt.token}`, host: opt.host }
+    const options: RequestOptions = {
+        ...parseUrl(
+            `${process.env.CLUSTER_API_URL}/apis/${gvr.apiGroup}/${gvr.version}/namespaces/${nsn.namespace}/${gvr.resources}/${nsn.name}`
+        ),
+        ...{ method: 'DELETE', headers, agent: opt.agent },
+    }
+
+    return new Promise<IncomingMessage>((resolve) => {
+        const getReq = httpRequest(options, (response) => {
+            resolve(response)
+        })
+        getReq.end()
+    })
+}
+function createResource<T>(
+    opt: requestOptions,
+    gvr: KubernetesGVR,
+    nsn: namespacedName,
+    obj: T
+): Promise<IncomingMessage> {
+    const postData = JSON.stringify(obj)
+    const options: RequestOptions = {
+        ...parseUrl(
+            `${process.env.CLUSTER_API_URL}/apis/${gvr.apiGroup}/${gvr.version}/namespaces/${nsn.namespace}/${gvr.resources}/${nsn.name}`
+        ),
+        ...{ method: 'POST', agent: opt.agent },
+        headers: {
+            authorization: `Bearer ${opt.token}`,
+            host: opt.host,
+            'Content-Type': 'application/json; charset=utf-8',
+            'Content-Length': Buffer.byteLength(postData),
+        },
+    }
+
+    return new Promise<IncomingMessage>((resolve) => {
+        const postReq = httpRequest(options, (response) => {
+            resolve(response)
+        })
+        postReq.write(postData)
+        postReq.end()
+    })
+}
+// pollResource poll the given resource every pollInterval ms
+// polling will stop if verifyStatus function returns valid or no need of retry
+// will reject if not valid or hit some error
+// it will use code & msg from verifyStatus function when reject
+function pollResource<T>(
+    opt: requestOptions,
+    gvr: KubernetesGVR,
+    nsn: namespacedName,
+    pollInterval: number,
+    verifyStatus: verifyStatusFn<T>,
+    maxPollTimes: number
+): { poll: Promise<T>; cancel: () => void } {
+    if (maxPollTimes < 1) {
+        maxPollTimes = 1
+    }
+    let currPoll = 0
+    let pollVersions: NodeJS.Timeout
+    const cancel = () => {
+        if (pollVersions) {
+            clearInterval(pollVersions)
+            pollVersions = undefined
+        }
+    }
+    const poll = new Promise<T>((resolve, reject) => {
+        pollVersions = setInterval(async () => {
+            console.log('waiting for interval setting')
+            try {
+                console.log('before get')
+                const resGet = await getResource(opt, gvr, nsn)
+
+                const { isValid, isRetryRequired, retData, code, msg } = await verifyStatus(resGet)
+                if (isValid) {
+                    resolve(retData)
+                    cancel()
+                } else if (!isRetryRequired) {
+                    reject({ code, msg })
+                    cancel()
+                }
+            } catch (err) {
+                reject(err)
+                cancel()
+            }
+            currPoll++
+            if (currPoll > maxPollTimes) {
+                reject({ code: 500, msg: '{"message":"request timeout"}' })
+            }
+        }, pollInterval)
+    })
+
+    return { poll, cancel }
+}
+
+// createPollHelper does get->create->poll->verify->delete operation on a resource
+// if object exist, will not create but poll directly
+// if something goes wrong when doing requests, will thow an error in requestException format
+async function createPollHelper<TRet, TPoll>(
+    opt: requestOptions,
+    gvr: KubernetesGVR,
+    nsn: namespacedName,
+    obj: TPoll,
+    pollInterval: number,
+    verifyStatus: verifyStatusFn<TRet>,
+    maxPollTimes: number
+): Promise<TRet> {
+    // check if a mcv is already exist
+    const getRes = await getResource(opt, gvr, nsn)
+    if (getRes.statusCode === 404) {
+        // create if not found
+        const createRes = await createResource<TPoll>(opt, gvr, nsn, obj)
+        if (createRes.statusCode == 409) {
+            const createResponse = await parseJsonBody<Record<string, unknown>>(createRes)
+            // if existed, will keep progress
+            if ('AlreadyExists' !== _get(createResponse, ['reason'], '')) {
+                console.log('unexpected error')
+                throw { code: 409, msg: JSON.stringify(createResponse) }
+            }
+        } else if (!(createRes.statusCode >= 200 && createRes.statusCode < 300)) {
+            const createResponse = await parseBody(createRes)
+            throw { code: createRes.statusCode, msg: createResponse }
+        }
+    }
+    //polling & return
+    const { poll, cancel } = pollResource(opt, gvr, nsn, pollInterval, verifyStatus, maxPollTimes)
+
+    let retData = undefined
+    // when failed, poll will throw error in requestException format
+    try {
+        retData = await poll
+    } catch (err) {
+        cancel()
+        throw err
+    }
+    cancel()
+    //delete, the result doesn't matter for users
+    deleteResource(opt, gvr, nsn)
+        .then(() => {
+            console.log('deleted')
+        })
+        .catch((err) => console.log(err))
+    return retData
+}
+
+// getRemoteResource uses managedclusterview to get resources on remote cluster
+// a managedclusterview will be created
+// the managedclusterview is ephemeraland, and it will be destroyed once we get resource from status
+export async function getRemoteResource<T>(
+    host: string,
+    token: string,
+    agent: Agent,
+    clusterName: string,
+    apiGroup: string,
+    version: string,
+    resources: string,
+    kind: string,
+    name: string,
+    namespace: string,
+    pollInterval: number,
+    maxPollTimes: number
+): Promise<T> {
+    const opt: requestOptions = { host, token, agent }
+    const gvr: KubernetesGVR = { apiGroup, version, resources }
+    const nsn: namespacedName = { name, namespace }
+    const viewName = createHash('sha1')
+        .update(`${clusterName}-${resources}-${namespace}-${name}`)
+        .digest('hex')
+        .substr(0, 63)
+
+    const view: ManagedClusterView = {
+        apiVersion: ManagedClusterViewApiVersion,
+        kind: ManagedClusterViewKind,
+        metadata: {
+            labels: {
+                name: viewName,
+                'console.open-cluster-management.io/view': 'true',
+            },
+            name: viewName,
+            namespace: clusterName,
+        },
+        spec: {
+            scope: {
+                name,
+                apiGroup,
+                kind,
+                version,
+                namespace: namespace,
+            },
+        },
+    }
+    // only approve when we detected conditions has a type=processing, and will return status.result
+    const verifyStatus: verifyStatusFn<T> = async (response: IncomingMessage) => {
+        try {
+            const viewObj = await parseJsonBody<ManagedClusterView>(response)
+            if (response.statusCode >= 200 && response.statusCode < 300) {
+                if (
+                    (_get(viewObj, ['status', 'conditions', '0', 'type'], '') as string) === 'Processing' &&
+                    viewObj.status
+                ) {
+                    const retData = _get(viewObj, ['status', 'result'], undefined) as T
+                    return {
+                        isValid: true,
+                        isRetryRequired: false,
+                        retData,
+                        code: response.statusCode,
+                    }
+                } else {
+                    return { isValid: false, isRetryRequired: true }
+                }
+            } else {
+                return {
+                    isValid: false,
+                    isRetryRequired: false,
+                    code: response.statusCode,
+                    msg: JSON.stringify(viewObj),
+                }
+            }
+        } catch (err) {
+            console.log(err)
+            throw { code: 500, msg: '' } as requestException
+        }
+    }
+
+    return await createPollHelper<T, ManagedClusterView>(
+        opt,
+        gvrManagedClusterView,
+        { name: viewName, namespace: clusterName },
+        view,
+        pollInterval,
+        verifyStatus,
+        maxPollTimes
+    )
+}
+
+// updateRemoteResource uses managedclusteraction to update remote resources
+// managedclusteractions are ephemeral, and will be deleted after the work is done
+// if action failed, will return msg of the action
+export async function updateRemoteResource(
+    host: string,
+    token: string,
+    agent: Agent,
+    clusterName: string,
+    resources: string,
+    name: string,
+    namespace: string,
+    body: Record<string, unknown>,
+    pollInterval: number,
+    maxPollTimes: number
+): Promise<unknown> {
+    const opt: requestOptions = { host, token, agent }
+    const actionName = createHash('sha1')
+        .update(`${clusterName}-${resources}-${namespace}-${name}`)
+        .digest('hex')
+        .substr(0, 63)
+    const action: ManagedClusterAction = {
+        apiVersion: ManagedClusterActionApiVersion,
+        kind: ManagedClusterActionKind,
+        metadata: {
+            name: actionName,
+            namespace: clusterName,
+        },
+        spec: {
+            actionType: 'Update',
+            kube: {
+                resource: resources,
+                name: name,
+                namespace: namespace,
+                template: body,
+            },
+        },
+    }
+
+    const verifyStatus: verifyStatusFn<unknown> = async (response: IncomingMessage) => {
+        try {
+            if (response.statusCode >= 200 && response.statusCode < 300) {
+                const actionObj = await parseJsonBody<ManagedClusterAction>(response)
+                if (_get(actionObj, ['status', 'conditions', '0', 'type']) === 'Completed') {
+                    const status = _get(actionObj, ['status', 'conditions', '0', 'status'], '') as string
+                    // only accept type=completed & status=true
+                    if (status.toLocaleLowerCase() === 'true') {
+                        return { isValid: true, isRetryRequired: false }
+                    } else {
+                        // failed, return msg
+                        return {
+                            isValid: false,
+                            isRetryRequired: false,
+                            code: 500,
+                            msg: _get(
+                                actionObj,
+                                ['status', 'conditions', '0', 'message'],
+                                'failed to apply resource update'
+                            ) as string,
+                        }
+                    }
+                } else {
+                    // not ready yet, retry
+                    return { isValid: false, isRetryRequired: true }
+                }
+            }
+            // wrong status code, return error
+            const msg = await parseBody(response)
+            return {
+                isValid: false,
+                isRetryRequired: false,
+                code: response.statusCode,
+                msg: msg,
+            }
+        } catch (err) {
+            console.log(err)
+            throw { code: 500, msg: '' } as requestException
+        }
+    }
+    return await createPollHelper<unknown, ManagedClusterAction>(
+        opt,
+        gvrManagedClusterAction,
+        { name: actionName, namespace: clusterName },
+        action,
+        pollInterval,
+        verifyStatus,
+        maxPollTimes
+    )
+}
--- frontend/src/components/ClusterCommon.tsx
+++ frontend/src/components/ClusterCommon.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Axios, { AxiosError } from 'axios'
 import { useTranslation } from 'react-i18next'
-import { AcmInlineStatus, StatusType, AcmButton } from '@open-cluster-management/ui-components'
-import { ButtonVariant } from '@patternfly/react-core'
+import {
+    AcmInlineStatus,
+    StatusType,
+    AcmButton,
+    AcmModal,
+    AcmForm,
+    AcmSubmit,
+    AcmSelect,
+    AcmAlert,
+} from '@open-cluster-management/ui-components'
+import {
+    ButtonVariant,
+    ModalVariant,
+    ActionGroup,
+    SelectOption,
+    Title,
+    Text,
+    AlertVariant,
+} from '@patternfly/react-core'
 import { ClusterStatus, DistributionInfo } from '../lib/get-cluster'
 
 export function StatusField(props: { status: ClusterStatus }) {
@@ -34,18 +52,122 @@ export function StatusField(props: { status: ClusterStatus }) {
     return <AcmInlineStatus type={type} status={t(`status.${props.status}`)} />
 }
 
-export function DistributionField(props: { data: DistributionInfo | undefined }) {
+export function DistributionField(props: { clusterName: string; data: DistributionInfo | undefined }) {
     const { t } = useTranslation(['cluster'])
+    const [open, toggleOpen] = useState<boolean>(false)
+    const [selectVersion, setSelectVersion] = useState<string>()
+    const [upgradeError, setUpgradeError] = useState<string>()
+    const toggle = () => toggleOpen(!open)
+
     if (!props.data) return <>-</>
-    if (props.data.ocp?.availableUpdates && props.data.ocp.availableUpdates.length > 0) {
+    if (props.data.ocp?.upgradeFailed) {
+        return <AcmInlineStatus type={StatusType.danger} status={t(`upgrade.upgradefailed`)} />
+    } else if (
+        props.data.ocp?.desiredVersion &&
+        props.data.ocp?.version &&
+        props.data.ocp?.desiredVersion !== props.data.ocp?.version
+    ) {
+        return (
+            <AcmInlineStatus
+                type={StatusType.progress}
+                status={t(`upgrade.upgrading`) + ' ' + props.data.ocp?.desiredVersion}
+            />
+        )
+    } else if (props.data.ocp?.availableUpdates && props.data.ocp.availableUpdates.length > 0) {
         return (
             <span>
                 {props.data.displayVersion}{' '}
                 <span style={{ whiteSpace: 'nowrap' }}>
                     (
-                    <AcmButton variant={ButtonVariant.link} style={{ padding: 0, margin: 0, fontSize: '14px' }}>
+                    <AcmButton
+                        onClick={toggle}
+                        variant={ButtonVariant.link}
+                        style={{ padding: 0, margin: 0, fontSize: '14px' }}
+                    >
                         {t('upgrade.available')}
                     </AcmButton>
+                    <AcmModal
+                        variant={ModalVariant.small}
+                        isOpen={open}
+                        onClose={toggle}
+                        title={t('upgrade.title') + ' ' + props.clusterName}
+                    >
+                        {upgradeError && (
+                            <AcmAlert
+                                title={t('upgrade.upgradefailed')}
+                                subtitle={upgradeError}
+                                variant={AlertVariant.danger}
+                                isInline
+                                style={{ marginTop: '24px' }}
+                            />
+                        )}
+                        <AcmForm>
+                            <Title headingLevel="h5" size="md">
+                                {t('upgrade.current.version')}
+                            </Title>
+                            <Text>{props.data.ocp?.version || props.data.displayVersion}</Text>
+                            <AcmSelect
+                                id="upgradeVersionSelect"
+                                label={t('upgrade.select.label')}
+                                maxHeight={'6em'}
+                                placeholder={t('upgrade.select.placeholder')}
+                                value={selectVersion}
+                                onChange={(value) => {
+                                    setSelectVersion(value)
+                                    setUpgradeError('')
+                                }}
+                                isRequired
+                            >
+                                {props.data.ocp.availableUpdates
+                                    .sort((a: string, b: string) => {
+                                        // basic sort semvers without preversion
+                                        const aVersion = a.split('.')
+                                        const bVersion = b.split('.')
+                                        for (let i = 0; i < Math.min(aVersion.length, bVersion.length); i++) {
+                                            if (aVersion[i] !== bVersion[i]) {
+                                                return Number(bVersion[i]) - Number(aVersion[i])
+                                            }
+                                        }
+                                        return bVersion.length - aVersion.length
+                                    })
+                                    .map((version) => (
+                                        <SelectOption key={version} value={version}>
+                                            {version}
+                                        </SelectOption>
+                                    ))}
+                            </AcmSelect>
+
+                            <ActionGroup>
+                                <AcmSubmit
+                                    onClick={() => {
+                                        setUpgradeError('')
+                                        const url = '/cluster-management/upgrade-1-2'
+                                        Axios.post(url, {
+                                            clusterName: props.clusterName,
+                                            version: selectVersion,
+                                        })
+                                            .then(() => {
+                                                toggle()
+                                            })
+                                            .catch((reason: AxiosError) => {
+                                                setUpgradeError(reason.message)
+                                            })
+                                    }}
+                                >
+                                    submit
+                                </AcmSubmit>
+                                <AcmButton
+                                    onClick={() => {
+                                        toggle()
+                                        setUpgradeError('')
+                                    }}
+                                    variant={ButtonVariant.link}
+                                >
+                                    cancel
+                                </AcmButton>
+                            </ActionGroup>
+                        </AcmForm>
+                    </AcmModal>
                     )
                 </span>
             </span>
