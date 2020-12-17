cd8df4eca682c0b33425d0c4de152fb479097a8d
Sho Weimer <sweimer@redhat.com>
2020-12-17 14:26:20 -0500

    update package and update tests

--- frontend/src/components/AppContext.tsx
+++ frontend/src/components/AppContext.tsx
@@ -15,25 +15,25 @@ export function AppContextContainer(props: { children: React.ReactNode[] }) {
     const [clusterManagementAddons, setClusterManagementAddons] = useState<ClusterManagementAddOn[]>([])
 
     useEffect(() => {
-        try {
-            listClusterManagementAddOns().promise.then((items) => {
-                setClusterManagementAddons(items)
-            })
-        } catch (err) {
-            console.error(err)
-        }
         // TODO change discovery FG to use a label
         // i.e. console.open-cluster-management.io/feature-gate
-        try {
-            listFeatureGates().promise.then((items) => {
-                const discoveryFeature = items.find(
-                    (item: FeatureGate) => item.metadata.name === 'open-cluster-management-discovery'
-                )
+        (async () => {
+            const calls = [listClusterManagementAddOns(), listFeatureGates()]
+            const results = await Promise.allSettled(calls.map((call) => call.promise))
+            if (results[0].status === 'fulfilled') {
+                setClusterManagementAddons(results[0].value as ClusterManagementAddOn[])
+            } else {
+                console.error(results[0])
+            }
+
+            if (results[1].status === 'fulfilled') {
+                const featureGates = results[1].value as FeatureGate[]
+                const discoveryFeature = featureGates.find((fg) => fg.metadata.name === 'open-cluster-management-discovery')
                 discoveryFeature && setFeatureGates({ 'open-cluster-management-discovery': discoveryFeature })
-            })
-        } catch (err) {
-            console.error(err)
-        }
+            } else {
+                console.error(results[1])
+            }
+        })()
     }, [])
 
     return <AppContext.Provider value={{ featureGates, clusterManagementAddons }}>{props.children}</AppContext.Provider>
