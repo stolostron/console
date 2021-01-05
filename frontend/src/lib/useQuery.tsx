1b6058b6164dcce337c9950fbf34c3bf187d9b1c
James Talton <jtalton@redhat.com>
2021-01-05 13:31:10 -0500

    only clear data on valid error

--- frontend/src/lib/useQuery.tsx
+++ frontend/src/lib/useQuery.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
-import { IRequestResult } from './resource-request'
+import { IRequestResult, ResourceError, ResourceErrorCode } from './resource-request'
 
 export function useQuery<T>(restFunc: () => IRequestResult<T | T[]>, initialData?: T[]) {
     const [data, setData] = useState<T[] | undefined>(initialData)
@@ -29,19 +29,35 @@ export function useQuery<T>(restFunc: () => IRequestResult<T | T[]>, initialData
             result.promise
                 .then((data) => {
                     setData(Array.isArray(data) ? data : [data])
-                    setLoading(false)
                     setError(undefined)
                 })
                 .catch((err: Error) => {
-                    if (err.name === 'AbortError') {
+                    if (err instanceof ResourceError) {
+                        switch (err.code) {
+                            case ResourceErrorCode.RequestCancelled:
+                                dataRef.current.aborted = true
+                                break
+                            case ResourceErrorCode.TooManyRequests:
+                            case ResourceErrorCode.Timeout:
+                            case ResourceErrorCode.ServiceUnavailable:
+                            case ResourceErrorCode.NetworkError:
+                            case ResourceErrorCode.InternalServerError:
+                            case ResourceErrorCode.GatewayTimeout:
+                            case ResourceErrorCode.ConnectionReset:
+                            case ResourceErrorCode.BadGateway:
+                                break
+                            default:
+                                setData(undefined)
+                                break
+                        }
+                    } else if (err.name === 'AbortError') {
                         dataRef.current.aborted = true
                     } else {
                         setError(err)
-                        setLoading(false)
-                        setData(undefined)
                     }
                 })
                 .finally(() => {
+                    setLoading(false)
                     if (dataRef.current.aborted) return
                     dataRef.current.promise = undefined
                     if (dataRef.current.timeout) {
