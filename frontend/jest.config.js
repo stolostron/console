ad1631fcdcdac544c388256982af7093120943b6
Sho Weimer <sweimer@redhat.com>
2021-01-05 12:27:11 -0500

    first pass

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     "preset": "ts-jest",
     "testEnvironment": "jsdom",
     "rootDir": "./src",
+    "testResultsProcessor": "jest-sonar-reporter",
     "setupFilesAfterEnv": [
         "<rootDir>/setupTests.ts"
     ],
