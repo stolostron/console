c6bf5c60982593fca0cf3c2d77bb92b16be64f43
randybrunopiverger <rbrunopi@redhat.com>
2021-01-07 13:40:18 -0500

    conditional for tooltip

--- frontend/src/routes/ClusterManagement/Clusters/Clusters.tsx
+++ frontend/src/routes/ClusterManagement/Clusters/Clusters.tsx
@@ -62,8 +62,8 @@ const PageActions = () => {
             })
     }, [])
     const dropdownItems:AcmDropdownItems[] = [
-        { id: 'create-cluster', text: t('managed.createCluster'), isDisabled: accessRestriction, tooltip:t('common:rbac.unauthorized')},
-        { id: 'import-cluster', text: t('managed.importCluster'), isDisabled: accessRestriction, tooltip:t('common:rbac.unauthorized')},
+        { id: 'create-cluster', text: t('managed.createCluster'), isDisabled: accessRestriction, tooltip: accessRestriction ? t('common:rbac.unauthorized'): ''},
+        { id: 'import-cluster', text: t('managed.importCluster'), isDisabled: accessRestriction, tooltip: accessRestriction ? t('common:rbac.unauthorized'): ''},
     ]
     const onSelect = (id: string) => {
         switch (id) {
