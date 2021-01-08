6deb3ecf759d39fef21001840beedcdcd78450d4
Sho Weimer <sweimer@redhat.com>
2021-01-08 15:39:37 -0500

    add search cluster

--- frontend/src/routes/ClusterManagement/Clusters/Clusters.tsx
+++ frontend/src/routes/ClusterManagement/Clusters/Clusters.tsx
@@ -278,7 +278,7 @@ export function ClustersTable(props: {
                                 {
                                     id: 'search-cluster',
                                     text: t('managed.search'),
-                                    click: (cluster: Cluster) => {},
+                                    click: (cluster: Cluster) => window.location.assign(`/search?filters={"textsearch":"cluster%3A${cluster?.name}"}`),
                                 },
                                 {
                                     id: 'detach-cluster',
