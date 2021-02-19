3b5be758894de39f780dca0a1344a6ce3d3bbdca
Sho Weimer <sweimer@redhat.com>
2021-02-18 22:07:32 -0500

    refactor namespace rbac, clean up files (#310)
    * refactor namespace rbac, clean up files
    
    * clean up

---
+++ frontend/src/lib/rbac-helpers.ts
@@ -0,0 +1,60 @@
+import {
+    ResourceAttributes,
+    createSubjectAccessReview,
+    createSubjectAccessReviews,
+} from '../resources/self-subject-access-review'
+import { listProjects } from '../resources/project'
+
+export function getAuthorizedNamespaces(resourceAttributes: ResourceAttributes[]) {
+    return new Promise<string[]>(async (resolve, reject) => {
+        try {
+            const projects = await listProjects().promise
+            const namespaces: string[] = projects.map((project) => project.metadata.name!)
+
+            if (namespaces.length === 0) {
+                return resolve([])
+            }
+
+            if (await checkAdminAccess()) {
+                return resolve(namespaces)
+            }
+
+            const resourceList: Array<ResourceAttributes> = []
+
+            namespaces.forEach((namespace) => {
+                resourceList.push(...resourceAttributes.map((attribute) => ({ ...attribute, namespace })))
+            })
+
+            let authorizedNamespaces: string[] = []
+            const promiseResult = createSubjectAccessReviews(resourceList)
+            await promiseResult.promise.then((results) => {
+                results.forEach((result) => {
+                    if (result.status === 'fulfilled') {
+                        if (result.value.status?.allowed) {
+                            authorizedNamespaces.push(result.value.spec.resourceAttributes.namespace!)
+                        }
+                    }
+                })
+                // remove duplicates from filtered list
+                authorizedNamespaces = Array.from(new Set(authorizedNamespaces))
+            })
+            return resolve(authorizedNamespaces)
+        } catch (err) {
+            return reject(err)
+        }
+    })
+}
+
+export async function checkAdminAccess() {
+    try {
+        const result = await createSubjectAccessReview({
+            name: '*',
+            namespace: '*',
+            resource: '*',
+            verb: '*',
+        }).promise
+        return result.status!.allowed
+    } catch (err) {
+        return false
+    }
+}
