c2c677a75a63f486b678231ba68a60d25d0a3303
Chris Ahl <cahl@redhat.com>
2021-02-22 14:00:12 -0500

    Add CONTRIBUTING file (#319)
    * Add CONTRIBUTING file
    
    * Update copyrights
    
    * add some CICD members to OWNERS

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 ###############################################################################
 # Copyright (c) 2020 Red Hat, Inc.
+# Copyright IBM, Red Hat, and collected contributors to the OCM project
 ###############################################################################
