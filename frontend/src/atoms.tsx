ec95a85ed82db4a7b991aaffa2ac6b1118c53175
James Talton <jtalton@redhat.com>
2021-03-04 16:18:09 -0500

    fix

--- frontend/src/atoms.tsx
+++ frontend/src/atoms.tsx
@@ -121,7 +121,7 @@ export function Startup(props: { children?: ReactNode }) {
             }
         }
 
-        const evtSource = new EventSource('/watch', { withCredentials: true })
+        const evtSource = new EventSource(`${process.env.REACT_APP_BACKEND_PATH}/watch`, { withCredentials: true })
 
         evtSource.onmessage = function (event) {
             const data = JSON.parse(event.data) as IEventData
