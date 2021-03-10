2cc75c2b447404f306d50b2c2f5ec47eaaea6081
John Swanke <jswanke@redhat.com>
2021-03-10 08:39:59 -0500

    fix any

--- frontend/src/lib/bare-metal-assets.ts
+++ frontend/src/lib/bare-metal-assets.ts
@@ -1,12 +1,14 @@
 /* Copyright Contributors to the Open Cluster Management project */
 
 import {
+    BareMetalAsset,
     ImportedBareMetalAsset,
     createBareMetalAssetResource,
     createBareMetalAssetSecret,
     listBareMetalAssets,
 } from '../resources/bare-metal-asset'
-import { getSecret } from '../resources/secret'
+import { IResource } from '../resources/resource'
+import { Secret, getSecret } from '../resources/secret'
 import { createProject } from '../resources/project'
 import { patchResource } from '../lib/resource-request'
 import { set, get, keyBy } from 'lodash'
@@ -18,19 +20,19 @@ const BMC_PASSWORD = 'bmc.password'
 const CREDENTIAL_NAME = 'spec.bmc.credentialsName'
 const CREDENTIAL_NAMESPACE = 'metadata.namespace'
 
-export async function syncBMAs(hosts: any[], resources: any[]) {
+export async function syncBMAs(hosts: ImportedBareMetalAsset[], resources: IResource[] ) {
     // make sure all hosts have a bare metal asset
     // (bma's may have been imported from a csv)
     let results
-    const assets: any[] = []
-    const errors: any[] = []
+    const assets: BareMetalAsset[] = []
+    const errors: Error[] = []
     let response = await listBareMetalAssets().promise
     const assetsMap = keyBy(response, (item) => {
         const name = get(item, 'metadata.name')
         const namespace = get(item, 'metadata.namespace')
         return `${name}-${namespace}`
     })
-    const newAssets: any[] = []
+    const newAssets: ImportedBareMetalAsset[] = []
     hosts.forEach((host) => {
         const { name, namespace } = host
         const asset = assetsMap[`${name}-${namespace}`]
@@ -46,46 +48,48 @@ export async function syncBMAs(hosts: any[], resources: any[]) {
 
     // make sure all hosts have a user/password in both ClusterDeployment and install-config.yaml
     const hostsWithoutCred = hosts.filter((host) => !get(host, BMC_USERNAME))
-    const installConfig = resources.find(({ data }) => data && data[INSTALL_CONFIG])
-    const installConfigData = yaml.safeLoad(Buffer.from(installConfig.data[INSTALL_CONFIG], 'base64').toString('ascii'))
-    const installConfigHosts = get(installConfigData, 'platform.baremetal.hosts', [])
-    const installConfigHostsWithoutCred = installConfigHosts.filter((host: any) => !get(host, BMC_USERNAME))
-    if (hostsWithoutCred.length > 0 || installConfigHostsWithoutCred.length > 0) {
-        results = hostsWithoutCred.map(({ namespace, name }) => {
-            const asset = assetsMap[`${name}-${namespace}`]
-            return !asset
-                ? { promise: Promise.resolve() }
-                : getSecret({ namespace: get(asset, CREDENTIAL_NAMESPACE), name: get(asset, CREDENTIAL_NAME) })
-        })
-        const secrets = await Promise.allSettled(results.map((result) => result.promise))
-        const secretMap = keyBy(secrets, (secret) => {
-            const name = get(secret, 'value.metadata.name')
-            const namespace = get(secret, 'value.metadata.namespace')
-            return `${name}-${namespace}`
-        })
-        const setSecrets = (fhosts: any[]) => {
-            fhosts.forEach((host) => {
-                const { name, namespace } = host
+    const installConfig = (resources as Secret[]).find(({ data }) => data && data[INSTALL_CONFIG])
+    if (installConfig?.data) {
+        const installConfigData = yaml.safeLoad(Buffer.from(installConfig.data[INSTALL_CONFIG], 'base64').toString('ascii'))
+        const installConfigHosts = get(installConfigData, 'platform.baremetal.hosts', [])
+        const installConfigHostsWithoutCred = installConfigHosts.filter((host: any) => !get(host, BMC_USERNAME))
+        if (hostsWithoutCred.length > 0 || installConfigHostsWithoutCred.length > 0) {
+            results = hostsWithoutCred.map(({ namespace, name }) => {
                 const asset = assetsMap[`${name}-${namespace}`]
-                const credName = get(asset, 'spec.bmc.credentialsName', name)
-                const secret = secretMap[`${credName}-${namespace}`]
-                if (secret) {
-                    const { username, password } = get(secret, 'value.data', {})
-                    set(host, BMC_USERNAME, username ? Buffer.from(username, 'base64').toString('ascii') : undefined)
-                    set(host, BMC_PASSWORD, password ? Buffer.from(password, 'base64').toString('ascii') : undefined)
-                }
+                return !asset
+                    ? { promise: Promise.resolve() }
+                    : getSecret({ namespace: get(asset, CREDENTIAL_NAMESPACE), name: get(asset, CREDENTIAL_NAME) })
             })
-        }
-        setSecrets(hostsWithoutCred)
-        setSecrets(installConfigHostsWithoutCred)
-        if (installConfigHostsWithoutCred.length > 0) {
-            installConfig.data[INSTALL_CONFIG] = Buffer.from(yaml.safeDump(installConfigData)).toString('base64')
+            const secrets = await Promise.allSettled(results.map((result) => result.promise))
+            const secretMap = keyBy(secrets, (secret) => {
+                const name = get(secret, 'value.metadata.name')
+                const namespace = get(secret, 'value.metadata.namespace')
+                return `${name}-${namespace}`
+            })
+            const setSecrets = (fhosts: any[]) => {
+                fhosts.forEach((host) => {
+                    const { name, namespace } = host
+                    const asset = assetsMap[`${name}-${namespace}`]
+                    const credName = get(asset, 'spec.bmc.credentialsName', name)
+                    const secret = secretMap[`${credName}-${namespace}`]
+                    if (secret) {
+                        const { username, password } = get(secret, 'value.data', {})
+                        set(host, BMC_USERNAME, username ? Buffer.from(username, 'base64').toString('ascii') : undefined)
+                        set(host, BMC_PASSWORD, password ? Buffer.from(password, 'base64').toString('ascii') : undefined)
+                    }
+                })
+            }
+            setSecrets(hostsWithoutCred)
+            setSecrets(installConfigHostsWithoutCred)
+            if (installConfigHostsWithoutCred.length > 0) {
+                installConfig.data[INSTALL_CONFIG] = Buffer.from(yaml.safeDump(installConfigData)).toString('base64')
+            }
         }
     }
     return { assets, errors }
 }
 
-export async function attachBMAs(assets: any[], hosts: any[], clusterName: string, errors: any[]) {
+export async function attachBMAs(assets: BareMetalAsset[], hosts: ImportedBareMetalAsset[], clusterName: string, errors: Error[]) {
     // mark asset as being used by this cluster
     let results = assets.map((asset, inx) => {
         const patch = {
@@ -102,7 +106,7 @@ export async function attachBMAs(assets: any[], hosts: any[], clusterName: strin
     const response = await Promise.allSettled(results.map((result) => result.promise))
     response.forEach((result, inx) => {
         if (result.status === 'rejected') {
-            errors.push({ message: result.reason.message })
+            errors.push({ name: 'attachBMA', message: result.reason.message })
         }
     })
 }
@@ -127,48 +131,47 @@ export async function importBMAs() {
 
                 // parse csv
                 if (typeof content === 'string') {
-                    setTimeout(() => {
-                        const allTextLines = content.split(/\r\n|\n/)
-                        const headers = allTextLines.shift()
-                        if (headers) {
-                            const header = headers.split(',')
-                            if (header.length > 3) {
-                                const lines: string[] = []
-                                allTextLines.forEach((line) => {
-                                    const data = line.split(',')
-                                    if (data.length === header.length) {
-                                        const arr: string[] = []
-                                        header.forEach((header, inx) => {
-                                            arr.push(`"${header.trim()}": "${data[inx].trim()}"`)
-                                        })
-                                        lines.push(`{${arr.join(',')}}`)
-                                    }
+                    await new Promise((r) => setTimeout(r, 100))
+                    const allTextLines = content.split(/\r\n|\n/)
+                    const headers = allTextLines.shift()
+                    if (headers) {
+                        const header = headers.split(',')
+                        if (header.length > 3) {
+                            const lines: string[] = []
+                            allTextLines.forEach((line) => {
+                                const data = line.split(',')
+                                if (data.length === header.length) {
+                                    const arr: string[] = []
+                                    header.forEach((header, inx) => {
+                                        arr.push(`"${header.trim()}": "${data[inx].trim()}"`)
+                                    })
+                                    lines.push(`{${arr.join(',')}}`)
+                                }
+                            })
+
+                            try {
+                                let bmas = JSON.parse(`[${lines.join(',')}]`)
+                                bmas = bmas.map((bma: any) => {
+                                    return {
+                                        name: bma.hostName,
+                                        namespace: bma.hostNamespace,
+                                        bootMACAddress: bma.macAddress,
+                                        role: bma.role,
+                                        uid: `${bma.hostNamespace}/${bma.hostName}`,
+                                        bmc: {
+                                            address: bma.bmcAddress,
+                                            username: bma.username,
+                                            password: bma.password,
+                                        },
+                                    } as ImportedBareMetalAsset
                                 })
 
-                                try {
-                                    let bmas = JSON.parse(`[${lines.join(',')}]`)
-                                    bmas = bmas.map((bma: any) => {
-                                        return {
-                                            name: bma.hostName,
-                                            namespace: bma.hostNamespace,
-                                            bootMACAddress: bma.macAddress,
-                                            role: bma.role,
-                                            uid: `${bma.hostNamespace}/${bma.hostName}`,
-                                            bmc: {
-                                                address: bma.bmcAddress,
-                                                username: bma.username,
-                                                password: bma.password,
-                                            },
-                                        } as ImportedBareMetalAsset
-                                    })
-
-                                    resolve(bmas)
-                                } catch (err) {
-                                    reject(err)
-                                }
+                                resolve(bmas)
+                            } catch (err) {
+                                reject(err)
                             }
                         }
-                    }, 100)
+                    }
                 }
             }
         }
@@ -176,7 +179,7 @@ export async function importBMAs() {
     })
 }
 
-export async function createBMAs(bmas: any[], assets: any[] = [], errors: any[] = []) {
+export async function createBMAs(bmas: any[], assets: BareMetalAsset[] = [], errors: Error[] = []) {
     let results
     let response
     // determine the namespaces required and create them
@@ -186,7 +189,7 @@ export async function createBMAs(bmas: any[], assets: any[] = [], errors: any[] 
     response.forEach((result, inx) => {
         if (result.status === 'rejected') {
             if (result.reason.code !== 409) {
-                errors.push({ message: result.reason.message })
+                errors.push({ name: 'create BMA namespaces', message: result.reason.message })
             }
         }
     })
@@ -196,14 +199,14 @@ export async function createBMAs(bmas: any[], assets: any[] = [], errors: any[] 
     response = await Promise.allSettled(results.map((result) => result.promise))
     response.forEach((result, inx) => {
         if (result.status === 'rejected') {
-            errors.push({ message: result.reason.message, item: bmas[inx] })
+            errors.push({ name: 'create BMA secret', message: result.reason.message })
         }
     })
     results = bmas.map((asset) => createBareMetalAssetResource(asset))
     response = await Promise.allSettled(results.map((result) => result.promise))
     response.forEach((result, inx) => {
         if (result.status === 'rejected') {
-            errors.push({ message: result.reason.message, item: bmas[inx] })
+            errors.push({ name: 'create BMA resource', message: result.reason.message })
         } else {
             assets.push(result.value)
         }
