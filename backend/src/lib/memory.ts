12f2b92d32ff8d05f7625d50037774ae469be81f
James Talton <jtalton@redhat.com>
2021-03-12 14:31:27 -0500

    fixes for backend events and shutdown (#370)

--- backend/src/lib/memory.ts
+++ backend/src/lib/memory.ts
@@ -5,23 +5,11 @@ import { logger } from './logger'
 
 function logMemory() {
     const used = process.memoryUsage()
-    logger.debug(
-        Object.keys(used).reduce(
-            (result, key) => {
-                result[key] = `${Math.round(((used as unknown) as Record<string, number>)[key] / 1024 / 1024)} MB`
-                return result
-            },
-            { msg: 'memory' } as Record<string, string>
-        )
-    )
+    logger.debug({
+        msg: 'memory',
+        used: `${Math.round(used.rss / 1024 / 1024)} MB`,
+    })
     setTimeout(logMemory, 60 * 1000).unref()
-
-    // try {
-    //     global.gc()
-    // } catch (e) {
-    //     console.log('`node --expose-gc index.js`')
-    //     process.exit()
-    // }
 }
 
 export function startLoggingMemory(): void {
