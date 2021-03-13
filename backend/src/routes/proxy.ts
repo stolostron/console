58d15a4d9d633ca36bc5f813fb33011f3abac477
James Talton <jtalton@redhat.com>
2021-03-13 09:46:02 -0500

    tweak

--- backend/src/routes/proxy.ts
+++ backend/src/routes/proxy.ts
@@ -10,8 +10,9 @@ import { notFound, unauthorized } from '../lib/respond'
 const proxyHeaders = [
     constants.HTTP2_HEADER_ACCEPT,
     constants.HTTP2_HEADER_ACCEPT_ENCODING,
-    constants.HTTP2_HEADER_CONTENT_TYPE,
     constants.HTTP2_HEADER_CONTENT_ENCODING,
+    constants.HTTP2_HEADER_CONTENT_LENGTH,
+    constants.HTTP2_HEADER_CONTENT_TYPE,
 ]
 const proxyResponseHeaders = [
     constants.HTTP2_HEADER_CACHE_CONTROL,
