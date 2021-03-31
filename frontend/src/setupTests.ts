4b7c65d797043df3d1bfc5c9ee1f904092a74dd4
John Swanke <jswanke@redhat.com>
2021-03-31 10:48:35 -0400

    add missing mock messages

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -22,9 +22,66 @@ async function setupBeforeAll(): Promise<void> {
     nock.enableNetConnect('localhost')
 }
 
-let noMatches: string[]
+let missingNocks: string[]
 let consoleWarnings: any[]
 let consoleErrors: any[]
+
+// define some custom expects
+declare global {
+    namespace jest {
+        interface Matchers<R> {
+            hasMissingMocks(): CustomMatcherResult
+            hasUnusedMocks(): CustomMatcherResult
+        }
+    }
+}
+
+expect.extend({
+    hasMissingMocks(missing) {
+        const msgs: string[] = []
+        const pass: boolean = missing.length === 0
+        if (!pass) {
+            msgs.push('This test cannot find the mock of an API call:')
+            msgs.push('  1. The tested code may be making a new API call which hasn\'t been mocked yet in the test or')
+            msgs.push('  2. The tested code may have changed requiring the current mock to be updated')
+            msgs.push('Ensure there is a mock for each of these:\n')
+            missing.forEach((req: { method: any; path: any, requestBodyBuffers: any[] }) => {
+                const missingNock = []
+                missingNock.push(req.method)
+                missingNock.push(req.path)
+                req.requestBodyBuffers.forEach(buffer => {
+                    missingNock.push(`\n${buffer.toString('utf8')}`)
+                });
+                msgs.push(missingNock.join(' '))
+            })
+        }
+        const message: () => string = () =>  msgs.join('\n')
+        return {
+            message,
+            pass,
+        };
+    },
+    hasUnusedMocks(unused) {
+        const msgs: string[] = []
+        const pass: boolean = unused.length === 0
+        if (!pass) {
+            msgs.push('This test finished without using all mocks defined in the test:')
+            msgs.push('  1. If there\'s a timeout message above, rerun the test or increase the timeout or')
+            msgs.push('  2. The tested code may no longer need a mock, which should be removed')
+            msgs.push('These are the unused mocks:\n')
+            unused.forEach((pending: string) => {
+                msgs.push(pending)
+            })
+        }
+        const message: () => string = () =>  msgs.join('\n')
+        return {
+            message,
+            pass,
+        };
+    },
+})
+
+
 
 console.warn = (message?: any, ..._optionalParams: any[]) => {
     if (typeof message === 'string') {
@@ -39,14 +96,11 @@ console.error = (message?: any, ...optionalParams: any[]) => {
 }
 
 function logNoMatch(req: any) {
-    if (noMatches.length === 0) {
-        noMatches.push('No match for requests')
-    }
-    noMatches.push(`${req.method} ${req.path}`)
+    missingNocks.push(req)
 }
 
 function setupBeforeEach(): void {
-    noMatches = []
+    missingNocks = []
     consoleErrors = []
     consoleWarnings = []
     nock.emitter.on('no match', logNoMatch)
@@ -54,11 +108,12 @@ function setupBeforeEach(): void {
 
 async function setupAfterEach(): Promise<void> {
     await new Promise((resolve) => setTimeout(resolve, 100))
-    expect(noMatches).toEqual([])
+
+    expect(missingNocks).hasMissingMocks()
     // expect(consoleErrors).toEqual([])
     // expect(consoleWarnings).toEqual([])
-    const error = nock.isDone() ? undefined : `Pending Nocks: ${nock.pendingMocks().join(',')}`
-    expect(error).toBeUndefined()
+    expect(nock.pendingMocks()).hasUnusedMocks()
+
     nock.emitter.off('no match', logNoMatch)
     nock.cleanAll()
 }
