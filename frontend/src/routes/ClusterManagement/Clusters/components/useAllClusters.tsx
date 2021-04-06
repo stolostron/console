b3544ed59a4e7552118f1ebc14911d87fd4fb172
Sho Weimer <sweimer@redhat.com>
2021-04-06 19:02:56 -0400

    clean up

--- frontend/src/routes/ClusterManagement/Clusters/components/useAllClusters.tsx
+++ frontend/src/routes/ClusterManagement/Clusters/components/useAllClusters.tsx
@@ -27,7 +27,6 @@ export function useAllClusters() {
             managedClusterAddonsState,
         ])
     )
-    console.log('clusterDeployments', clusterDeployments)
     const clusters = useMemo(
         () =>
             mapClusters(
@@ -39,6 +38,5 @@ export function useAllClusters() {
             ),
         [clusterDeployments, managedClusterInfos, certificateSigningRequests, managedClusters, managedClusterAddons]
     )
-    console.log('clusters', clusters)
     return clusters as Cluster[]
 }
