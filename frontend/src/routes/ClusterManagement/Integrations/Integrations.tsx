66a862fb64ffdd88988ac570ca449bc119e2a178
randybrunopiverger <rbrunopi@redhat.com>
2021-04-12 11:24:30 -0400

    integration page additions

--- frontend/src/routes/ClusterManagement/Integrations/Integrations.tsx
+++ frontend/src/routes/ClusterManagement/Integrations/Integrations.tsx
@@ -1,12 +1,23 @@
 /* Copyright Contributors to the Open Cluster Management project */
 
 import React, { Fragment, useContext, useEffect, useState } from 'react'
-import { AcmAlertContext, AcmForm, AcmPageContent, AcmTable } from '@open-cluster-management/ui-components'
+import {
+    AcmAlertContext,
+    AcmEmptyState,
+    AcmForm,
+    AcmPageContent,
+    AcmTable,
+} from '@open-cluster-management/ui-components'
 import { PageSection } from '@patternfly/react-core'
 import { BulkActionModel, errorIsNot, IBulkActionModelProps } from '../../../components/BulkActionModel'
-
-import { AnsibleTowerSecret } from '../../../resources/ansible-tower-secret'
+import { useTranslation, Trans } from 'react-i18next'
+import {
+    AnsibleTowerSecret,
+    AnsibleTowerSecretApiVersion,
+    AnsibleTowerSecretKind,
+} from '../../../resources/ansible-tower-secret'
 import { TableGridBreakpoint } from '@patternfly/react-table'
+import { V1ObjectMeta } from '@kubernetes/client-node'
 
 export default function IntegrationsPage() {
     const alertContext = useContext(AcmAlertContext)
@@ -15,7 +26,9 @@ export default function IntegrationsPage() {
 
     return (
         <AcmPageContent id="clusters">
-            <PageSection variant="light" isFilled={true}></PageSection>
+            <PageSection variant="light" isFilled={true}>
+                <IntegrationTable></IntegrationTable>
+            </PageSection>
         </AcmPageContent>
     )
 }
@@ -23,6 +36,108 @@ export default function IntegrationsPage() {
 function IntegrationTable() {
     // Load Data
 
+    const exampleIntegrations: AnsibleTowerSecret[] = [
+        {
+            apiVersion: AnsibleTowerSecretApiVersion,
+            kind: AnsibleTowerSecretKind,
+            metadata: {
+                name: 'Test-connection',
+                namespace: 'default',
+            },
+        },
+    ]
+    function mckeyFn(ansibleTowerSecret: AnsibleTowerSecret) {
+        return ansibleTowerSecret.metadata.uid!
+    }
+    const { t } = useTranslation(['cluster'])
+
     // Set table
-    return <Fragment></Fragment>
+    return (
+        <Fragment>
+            <AcmTable<AnsibleTowerSecret>
+                gridBreakPoint={TableGridBreakpoint.none}
+                plural="integrations"
+                items={exampleIntegrations}
+                columns={[
+                    {
+                        header: t('table.name'),
+                        sort: 'metadata.name',
+                        search: 'metadata.name',
+                        cell: (ansibleSecret) => {
+                            return ansibleSecret.metadata.name
+                        },
+                    },
+                    {
+                        header: t('table.linkedCred'),
+                        cell: (ansibleSecret) => {
+                            return ''
+                        },
+                    },
+                    {
+                        header: t('table.clustersInUse'),
+                        cell: (ansibleSecret) => {
+                            return ''
+                        },
+                    },
+                    {
+                        header: t('table.defaultTemplate'),
+                        cell: (ansibleSecret) => {
+                            return ''
+                        },
+                    },
+                ]}
+                keyFn={mckeyFn}
+                key="integrationTable"
+                bulkActions={[
+                    {
+                        id: 'deleteIntegrations',
+                        title: t('bulk.delete.integrations'),
+                        click: (integrations) => {
+                            // setModalProps({
+                            //     open: true,
+                            //     title: t('bulk.delete.clusterPools'),
+                            //     action: t('common:delete'),
+                            //     processing: t('common:deleting'),
+                            //     resources: clusterPools,
+                            //     description: t('bulk.message.deleteClusterPool'),
+                            //     columns: modalColumns,
+                            //     keyFn: mckeyFn,
+                            //     actionFn: deleteResource,
+                            //     close: () => setModalProps({ open: false }),
+                            //     isDanger: true,
+                            //     confirmText: t('confirm').toLowerCase(),
+                            //     isValidError: errorIsNot([ResourceErrorCode.NotFound]),
+                            // })
+                        },
+                    },
+                ]}
+                tableActions={[
+                    
+                ]}
+                rowActions={[]}
+                emptyState={
+                    <AcmEmptyState
+                        key="mcEmptyState"
+                        title={t('cluster:integration.emptyStateHeader')}
+                        message={
+                            <Trans
+                                i18nKey={'integration.integrations.emptyStateMsg'}
+                                components={{ bold: <strong />, p: <p /> }}
+                            />
+                        }
+                        // action={
+                        //     <AcmButton
+                        //         role="link"
+                        //         onClick={() => history.push(NavigationPath.clusterPools)}
+                        //         disabled={!canCreateClusterPool}
+                        //         tooltip={t('common:rbac.unauthorized')}
+                        //     >
+                        //         {t('managed.createClusterPool')}
+                        //     </AcmButton>
+                        // }
+                    />
+                }
+            ></AcmTable>
+        </Fragment>
+    )
 }
