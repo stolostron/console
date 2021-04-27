6ff298163cf3537961d224c1bcc2563b4fd534f2
James Talton <jtalton@redhat.com>
2021-04-27 12:02:30 -0700

    update patch deployment to force pod reload (#511)
    * update patch deployment to force pod reload
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * cleanup users script
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * fix
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * tweak
    
    Signed-off-by: James Talton <jtalton@redhat.com>

--- scripts/setup-users.sh
+++ scripts/setup-users.sh
@@ -1,16 +1,34 @@
 #!/usr/bin/env bash
+# Copyright Contributors to the Open Cluster Management project
 
-# --------------------------------------------------------------------------------
-# USERS HTPASSWORD SECRET
-htpasswd -c -B -b users.htpasswd admin $1
-htpasswd -b users.htpasswd manager $1
-htpasswd -b users.htpasswd viewer $1
-htpasswd -b users.htpasswd none $1
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied";
+    echo "USAGE: ./setup-users.sh <PASSWORD>";
+    exit 1
+fi
+
+PASSWORD=$1
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $DIR
+
+echo
+echo CREATING PASSWORDS SECRET
+echo
+htpasswd -c -B -b users.htpasswd cluster-manager-admin $PASSWORD
+htpasswd -b users.htpasswd cluster-admin $PASSWORD
+htpasswd -b users.htpasswd admin $PASSWORD
+htpasswd -b users.htpasswd edit $PASSWORD
+htpasswd -b users.htpasswd view $PASSWORD
+htpasswd -b users.htpasswd none $PASSWORD
+oc delete secret htpass-secret -n openshift-config &> /dev/null
 oc create secret generic htpass-secret --from-file=htpasswd=users.htpasswd -n openshift-config
 rm -f users.htpasswd
+echo
 
-# --------------------------------------------------------------------------------
-# OPENSHIFT OAUTH IDENTITYPROVIDER
+echo
+echo UPDATING OPENSHIFT OAUTH IDENTITYPROVIDER
+echo
 read -r -d '' OAUTH << EOM
 apiVersion: config.openshift.io/v1
 kind: OAuth
@@ -26,21 +44,27 @@ spec:
                   name: htpass-secret
 EOM
 echo "$OAUTH"  | envsubst | oc apply -f -
+echo
 
-# Create user group "managers" with user "manager"
-oc adm groups new managers manager
+echo
+echo CREATING ROLE BINDINGS
+echo
+oc create clusterrolebinding cluster-manager-admin --clusterrole=open-cluster-management:cluster-manager-admin --user=cluster-manager-admin
+oc create clusterrolebinding cluster-admin --clusterrole=cluster-admin --user=cluster-admin
+oc create clusterrolebinding admin --clusterrole=admin --user=admin
+oc create clusterrolebinding edit --clusterrole=edit --user=edit
+oc create clusterrolebinding view --clusterrole=view --user=view
+echo
 
-# Create user group "viewers" with user "viewer"
-oc adm groups new viewers viewer
-
-# Create a ClusterRolebinding named "admin" using the "cluster-manager-admin" ClusterRole for user "admin"
-oc create clusterrolebinding admin --clusterrole=open-cluster-management:cluster-manager-admin --user=admin
-
-# Create namespace "connections"
-oc create namespace connections
-
-# Create a RoleBinding named "connections:admin" for namespace "connections" using the "admin" ClusterRole for group "managers" 
-oc create rolebinding connections:admin --namespace=connections --clusterrole=admin --group=managers
-
-# Create a RoleBinding named "connections:view" for namespace "connections" using the "view" ClusterRole for group "viewers" 
-oc create rolebinding connections:view --namespace=connections --clusterrole=view --group=viewers
+echo
+echo USER SUMMARY
+echo
+echo "                  USER | RESOURCES  | NAMESPACE  | SECRETS    | ROLES      "
+echo "-----------------------|------------|------------|------------|------------"
+echo " cluster-manager-admin | read/write | read/write | read/write | read/write "
+echo "         cluster-admin | read/write | read/write | read/write | read/write "
+echo "                 admin | read/write | read       | read/write | read/write "
+echo "                  edit | read/write | read       | read/write |            "
+echo "                  view | read       | read       |            |            "
+echo "                  none |            |            |            |            "
+echo
