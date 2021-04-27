6002f1b4efbefec97733b0624d3eae1770d1a26d
James Talton <jtalton@redhat.com>
2021-04-27 09:24:07 -0400

    cleanup users script
    Signed-off-by: James Talton <jtalton@redhat.com>

--- scripts/patch-deployment.sh
+++ scripts/patch-deployment.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 # Copyright Contributors to the Open Cluster Management project
+set -e # Exit immediately if a command returns a non-zero status.
 
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied";
+    exit 1
+fi
+
+VERSION=$1
 IMAGE=${2:-quay.io/open-cluster-management/console}
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $DIR
 
 DEPLOYMENT=`oc get deployment -n open-cluster-management --selector=app=console-chart-v2,component=console -o="jsonpath={.items[0].metadata.name}"`
 
--- scripts/setup-users.sh
+++ scripts/setup-users.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
+# Copyright Contributors to the Open Cluster Management project
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied";
+    echo "USAGE: ./setup-users.sh <PASSWORD>";
+    exit 1
+fi
+
+PASSWORD=$1
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $DIR
 
 # --------------------------------------------------------------------------------
 # USERS HTPASSWORD SECRET
-htpasswd -c -B -b users.htpasswd admin $1
-htpasswd -b users.htpasswd manager $1
-htpasswd -b users.htpasswd viewer $1
-htpasswd -b users.htpasswd none $1
+htpasswd -c -B -b users.htpasswd cluster-manager-admin $PASSWORD
+htpasswd -b users.htpasswd cluster-admin $PASSWORD
+htpasswd -b users.htpasswd admin $PASSWORD
+htpasswd -b users.htpasswd manager $PASSWORD
+htpasswd -b users.htpasswd viewer $PASSWORD
+htpasswd -b users.htpasswd none $PASSWORD
+oc delete secret htpass-secret -n openshift-config &> /dev/null
 oc create secret generic htpass-secret --from-file=htpasswd=users.htpasswd -n openshift-config
 rm -f users.htpasswd
 
@@ -27,20 +42,19 @@ spec:
 EOM
 echo "$OAUTH"  | envsubst | oc apply -f -
 
-# Create user group "managers" with user "manager"
-oc adm groups new managers manager
+oc create clusterrolebinding cluster-manager-admin --clusterrole=open-cluster-management:cluster-manager-admin --user=cluster-manager-admin
+oc create clusterrolebinding cluster-admin --clusterrole=cluster-admin --user=cluster-admin
+oc create clusterrolebinding admin --clusterrole=admin --user=admin
+oc create clusterrolebinding edit --clusterrole=edit --user=edit
+oc create clusterrolebinding view --clusterrole=view --user=view
 
-# Create user group "viewers" with user "viewer"
-oc adm groups new viewers viewer
-
-# Create a ClusterRolebinding named "admin" using the "cluster-manager-admin" ClusterRole for user "admin"
-oc create clusterrolebinding admin --clusterrole=open-cluster-management:cluster-manager-admin --user=admin
-
-# Create namespace "connections"
-oc create namespace connections
-
-# Create a RoleBinding named "connections:admin" for namespace "connections" using the "admin" ClusterRole for group "managers" 
-oc create rolebinding connections:admin --namespace=connections --clusterrole=admin --group=managers
-
-# Create a RoleBinding named "connections:view" for namespace "connections" using the "view" ClusterRole for group "viewers" 
-oc create rolebinding connections:view --namespace=connections --clusterrole=view --group=viewers
+echo
+echo "                  USER | RESOURCES  | NAMESPACE  | SECRETS    | ROLES      "
+echo "-----------------------|------------|------------|------------|------------"
+echo " cluster-manager-admin | read/write | read/write | read/write | read/write "
+echo "         cluster-admin | read/write | read/write | read/write | read/write "
+echo "                 admin | read/write | read       | read/write | read/write "
+echo "                  edit | read/write | read       | read/write |            "
+echo "                  view | read       | read       |            |            "
+echo "                  none |            |            |            |            "
+echo
