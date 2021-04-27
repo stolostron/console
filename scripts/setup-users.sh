f56a221c2447e4e75e29808a21c027c5fc764f7b
James Talton <jtalton@redhat.com>
2021-04-27 09:36:21 -0400

    tweak
    Signed-off-by: James Talton <jtalton@redhat.com>

--- scripts/setup-users.sh
+++ scripts/setup-users.sh
@@ -12,8 +12,9 @@ PASSWORD=$1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $DIR
 
-# --------------------------------------------------------------------------------
-# USERS HTPASSWORD SECRET
+echo
+echo CREATING PASSWORDS SECRET
+echo
 htpasswd -c -B -b users.htpasswd cluster-manager-admin $PASSWORD
 htpasswd -b users.htpasswd cluster-admin $PASSWORD
 htpasswd -b users.htpasswd admin $PASSWORD
@@ -23,9 +24,11 @@ htpasswd -b users.htpasswd none $PASSWORD
 oc delete secret htpass-secret -n openshift-config &> /dev/null
 oc create secret generic htpass-secret --from-file=htpasswd=users.htpasswd -n openshift-config
 rm -f users.htpasswd
+echo
 
-# --------------------------------------------------------------------------------
-# OPENSHIFT OAUTH IDENTITYPROVIDER
+echo
+echo UPDATING OPENSHIFT OAUTH IDENTITYPROVIDER
+echo
 read -r -d '' OAUTH << EOM
 apiVersion: config.openshift.io/v1
 kind: OAuth
@@ -41,13 +44,20 @@ spec:
                   name: htpass-secret
 EOM
 echo "$OAUTH"  | envsubst | oc apply -f -
+echo
 
+echo
+echo CREATING ROLE BINDINGS
+echo
 oc create clusterrolebinding cluster-manager-admin --clusterrole=open-cluster-management:cluster-manager-admin --user=cluster-manager-admin
 oc create clusterrolebinding cluster-admin --clusterrole=cluster-admin --user=cluster-admin
 oc create clusterrolebinding admin --clusterrole=admin --user=admin
 oc create clusterrolebinding edit --clusterrole=edit --user=edit
 oc create clusterrolebinding view --clusterrole=view --user=view
+echo
 
+echo
+echo USER SUMMARY
 echo
 echo "                  USER | RESOURCES  | NAMESPACE  | SECRETS    | ROLES      "
 echo "-----------------------|------------|------------|------------|------------"
