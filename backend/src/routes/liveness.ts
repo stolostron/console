67c18be30304bbd5d26a529d51ca2edc8e68d63f
James Talton <jtalton@redhat.com>
2021-04-30 16:41:33 -0400

    change to cause a rebuild
    Signed-off-by: James Talton <jtalton@redhat.com>

--- backend/src/routes/liveness.ts
+++ backend/src/routes/liveness.ts
@@ -3,8 +3,8 @@ import { Http2ServerRequest, Http2ServerResponse } from 'http2'
 import { respondInternalServerError, respondOK } from '../lib/respond'
 import { oauthInfoPromise } from './oauth'
 
-// The kubelet uses readiness probes to know when a container is ready to start accepting traffic
-export async function readiness(req: Http2ServerRequest, res: Http2ServerResponse): Promise<void> {
+// The kubelet uses liveness probes to know when to restart a container.
+export async function liveness(req: Http2ServerRequest, res: Http2ServerResponse): Promise<void> {
     const oauthInfo = await oauthInfoPromise
     if (!oauthInfo.authorization_endpoint) {
         return respondInternalServerError(req, res)
