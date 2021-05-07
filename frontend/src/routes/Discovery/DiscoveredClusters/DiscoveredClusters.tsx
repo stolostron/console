26b5ca9d141cd81b7ea4b11f7827bef4c32e49b4
Zack Kayyali <zkayyali@redhat.com>
2021-05-07 11:12:27 -0400

    Discovery Table/ Discovery Config pages updates (#561)
    * Initial fixes
    
    Signed-off-by: Zachary Kayyali <zkayyali@redhat.com>
    
    * Move last active column to right of status
    
    Signed-off-by: Zachary Kayyali <zkayyali@redhat.com>
    
    * Update dropdown button action
    
    Signed-off-by: Zachary Kayyali <zkayyali@redhat.com>
    
    * Use SSE for Credentials
    
    Signed-off-by: Zachary Kayyali <zkayyali@redhat.com>

--- frontend/src/routes/Discovery/DiscoveredClusters/DiscoveredClusters.tsx
+++ frontend/src/routes/Discovery/DiscoveredClusters/DiscoveredClusters.tsx
@@ -3,6 +3,7 @@
 import {
     AcmAlertContext,
     AcmButton,
+    AcmDropdown,
     AcmEmptyState,
     AcmEmptyStateImage,
     AcmInlineProvider,
@@ -14,8 +15,7 @@ import {
     Provider,
     StatusType,
 } from '@open-cluster-management/ui-components'
-import { Card, CardHeader, PageSection, Stack, Title } from '@patternfly/react-core'
-import AddIcon from '@patternfly/react-icons/dist/js/icons/add-circle-o-icon'
+import { PageSection } from '@patternfly/react-core'
 import ExternalLink from '@patternfly/react-icons/dist/js/icons/external-link-alt-icon'
 import { TableGridBreakpoint } from '@patternfly/react-table'
 import * as moment from 'moment'
@@ -59,13 +59,37 @@ function EmptyStateNoCRHCredentials() {
 
 function EmptyStateCRHCredentials(props: { credentials?: ProviderConnection[] }) {
     const { t } = useTranslation(['discovery'])
+    const history = useHistory()
+
+    const onSelect = (credential: string) => {
+        // TODO: Filter by namespace
+        sessionStorage.setItem('DiscoveryCredential', credential)
+        history.push(NavigationPath.createDiscovery)
+    }
+
+    const action =
+        props.credentials!.length > 1 ? (
+            <AcmDropdown
+                text="Configure discovery settings"
+                onSelect={onSelect}
+                id="configureDiscoveryDropdown"
+                isKebab={false}
+                isPrimary={true}
+                dropdownItems={props.credentials!.map((credential) => {
+                    return {
+                        id: credential.metadata.namespace! + '/' + credential.metadata.name!,
+                        text: credential.metadata.namespace! + '/' + credential.metadata.name!,
+                    }
+                })}
+            />
+        ) : (
+            <AcmButton component={Link} to={NavigationPath.createDiscovery}>
+                {t('emptystate.enableClusterDiscovery')}
+            </AcmButton>
+        )
     return (
         <AcmEmptyState
-            action={
-                <AcmButton component={Link} to={NavigationPath.addDiscoveryConfig}>
-                    {t('emptystate.enableClusterDiscovery')}
-                </AcmButton>
-            }
+            action={action}
             title={t('emptystate.credentials.title')}
             message={
                 <Trans
@@ -102,7 +126,6 @@ function EmptyStateAwaitingDiscoveredClusters() {
 }
 
 export function DiscoveredClustersPageContent() {
-    const { t } = useTranslation(['discovery'])
     const alertContext = useContext(AcmAlertContext)
     // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(() => alertContext.clearAlerts, [])
@@ -130,26 +153,14 @@ export function DiscoveredClustersPageContent() {
 
     sessionStorage.removeItem('DiscoveredClusterName')
     sessionStorage.removeItem('DiscoveredClusterConsoleURL')
+    sessionStorage.removeItem('DiscoveryCredential')
 
     return (
-        <Stack hasGutter style={{ height: 'unset' }}>
-            <Card>
-                <CardHeader>
-                    <Title headingLevel="h4">{t('quickActions')}</Title>
-                    <span>
-                        &nbsp;
-                        <Link to={NavigationPath.addCredentials}>
-                            {t('quickActions.AddRHOCMCredential')} <AddIcon />
-                        </Link>
-                    </span>
-                </CardHeader>
-            </Card>
-            <DiscoveredClustersTable
-                discoveredClusters={unmanagedClusters}
-                credentials={credentials}
-                discoveryConfigs={discoveryConfigs}
-            />
-        </Stack>
+        <DiscoveredClustersTable
+            discoveredClusters={unmanagedClusters}
+            credentials={credentials}
+            discoveryConfigs={discoveryConfigs}
+        />
     )
 }
 
@@ -213,6 +224,24 @@ export function DiscoveredClustersTable(props: {
             },
         },
         {
+            header: t('dcTbl.lastActive'),
+            sort: 'spec.activity_timestamp',
+            cell: (discoveredCluster) => (
+                <span style={{ whiteSpace: 'nowrap' }} key="dcLastActive">
+                    {discoveredCluster.spec.activity_timestamp === undefined
+                        ? ['N/A']
+                        : moment
+                              .duration(
+                                  Math.abs(
+                                      new Date().getTime() -
+                                          new Date(discoveredCluster.spec.activity_timestamp).getTime()
+                                  )
+                              )
+                              .humanize()}
+                </span>
+            ),
+        },
+        {
             header: t('dcTbl.namespace'),
             sort: (a: DiscoveredCluster, b: DiscoveredCluster) =>
                 compareStrings(a?.metadata.namespace, b?.metadata.namespace),
@@ -255,24 +284,6 @@ export function DiscoveredClustersTable(props: {
                 ) : (
                     '-'
                 ),
-        },
-        {
-            header: t('dcTbl.lastActive'),
-            sort: 'spec.activity_timestamp',
-            cell: (discoveredCluster) => (
-                <span style={{ whiteSpace: 'nowrap' }} key="dcLastActive">
-                    {discoveredCluster.spec.activity_timestamp === undefined
-                        ? ['N/A']
-                        : moment
-                              .duration(
-                                  Math.abs(
-                                      new Date().getTime() -
-                                          new Date(discoveredCluster.spec.activity_timestamp).getTime()
-                                  )
-                              )
-                              .humanize()}
-                </span>
-            ),
         },
         {
             header: t('dcTbl.created'),
@@ -320,7 +331,18 @@ export function DiscoveredClustersTable(props: {
             columns={discoveredClusterCols}
             keyFn={dckeyFn}
             key="tbl-discoveredclusters"
-            tableActions={[]}
+            tableActions={[
+                {
+                    id: 'configureDiscovery',
+                    title: t('discovery.configureDiscovery'),
+                    click: () => history.push(NavigationPath.configureDiscovery),
+                },
+                {
+                    id: 'addDiscovery',
+                    title: t('discovery.addDiscovery'),
+                    click: () => history.push(NavigationPath.createDiscovery),
+                },
+            ]}
             bulkActions={[]}
             rowActions={[
                 {
