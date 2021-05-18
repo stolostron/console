9a4f38c646757477d94b9335c91bf2e08f4694c3
Zachary Kayyali <zkayyali@redhat.com>
2021-05-18 15:56:35 -0400

    use sessionStorage instead of localStorage
    Signed-off-by: Zachary Kayyali <zkayyali@redhat.com>

--- frontend/src/routes/Discovery/DiscoveryConfig/DiscoveryConfig.tsx
+++ frontend/src/routes/Discovery/DiscoveryConfig/DiscoveryConfig.tsx
@@ -208,7 +208,7 @@ export function DiscoveryConfigPageContent(props: {
                                     message: '',
                                 })
                                 resolve(undefined)
-                                localStorage.setItem(
+                                sessionStorage.setItem(
                                     'DISCOVERY_OP',
                                     JSON.stringify({ Operation: 'Delete', Name: discoveryConfig.metadata.namespace })
                                 )
@@ -258,13 +258,13 @@ export function DiscoveryConfigPageContent(props: {
             try {
                 if (!editing) {
                     discoveryConfig.metadata!.name = 'discovery'
-                    localStorage.setItem(
+                    sessionStorage.setItem(
                         'DISCOVERY_OP',
                         JSON.stringify({ Operation: 'Create', Name: discoveryConfig.metadata.namespace })
                     )
                     await createDiscoveryConfig(discoveryConfig as DiscoveryConfig).promise
                 } else {
-                    localStorage.setItem(
+                    sessionStorage.setItem(
                         'DISCOVERY_OP',
                         JSON.stringify({ Operation: 'Update', Name: discoveryConfig.metadata.namespace })
                     )
