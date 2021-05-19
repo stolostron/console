6b14b6ab4208d89fd0b052af39fa52e93bb9ab21
Cameron Wall <75807506+cameronmwall@users.noreply.github.com>
2021-05-19 11:45:55 -0400

    Auto fill server (#595)
    * auto fill
    
    Signed-off-by: Cameron Wall <cwall@redhat.com>
    
    * prettify
    
    Signed-off-by: Cameron Wall <cwall@redhat.com>
    
    * unit test fix
    
    Signed-off-by: Cameron Wall <cwall@redhat.com>
    
    * disable
    
    Signed-off-by: Cameron Wall <cwall@redhat.com>

--- frontend/src/routes/Discovery/DiscoveredClusters/DiscoveredClusters.tsx
+++ frontend/src/routes/Discovery/DiscoveredClusters/DiscoveredClusters.tsx
@@ -150,7 +150,9 @@ export function DiscoveredClustersPageContent() {
     })
 
     sessionStorage.removeItem('DiscoveredClusterName')
+    sessionStorage.removeItem('DiscoveredClusterDisplayName')
     sessionStorage.removeItem('DiscoveredClusterConsoleURL')
+    sessionStorage.removeItem('DiscoveredClusterApiURL')
     sessionStorage.removeItem('DiscoveryCredential')
     return (
         <Fragment>
@@ -334,7 +336,9 @@ export function DiscoveredClustersTable(props: {
                     title: t('discovery.import'),
                     click: (item) => {
                         sessionStorage.setItem('DiscoveredClusterName', item.spec.name)
+                        sessionStorage.setItem('DiscoveredClusterDisplayName', item.spec.displayName)
                         sessionStorage.setItem('DiscoveredClusterConsoleURL', item.spec.console)
+                        sessionStorage.setItem('DiscoveredClusterApiURL', item.spec.apiUrl)
                         history.push(NavigationPath.importCluster)
                     },
                 },
