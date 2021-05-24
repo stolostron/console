21c5a6ad6b44f1babff276ad9553b3a3bd7b6da8
Nathan Weatherly <nweather@redhat.com>
2021-05-24 12:58:35 -0400

    adding type to discovered clusters table
    Signed-off-by: Nathan Weatherly <nweather@redhat.com>

--- frontend/src/resources/discovered-cluster.ts
+++ frontend/src/resources/discovered-cluster.ts
@@ -29,6 +29,7 @@ export interface DiscoveredCluster extends IResource {
         openshiftVersion: string
         cloudProvider: string
         status: string
+        type: string
         isManagedCluster: boolean
         credential?: {
             apiVersion: string
