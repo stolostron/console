9ee7ce57934e631030f1a75da68c4db396da9b93
James Talton <jtalton@redhat.com>
2021-06-10 12:46:43 -0400

    fix test
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/src/lib/validation.ts
+++ frontend/src/lib/validation.ts
@@ -153,41 +153,42 @@ export function validateCloudsYaml(yamlValue: string, cloudValue: string, t: TFu
         try {
             //ensure we have valid YAML
             const yamlData = YAML.parse(yamlValue) as {
-                clouds: Record<
-                    string,
-                    {
+                clouds: {
+                    [cloud: string]: {
                         auth?: {
                             auth_url?: string
                             password?: string
                             username?: string
                         }
                     }
-                >
+                }
             }
 
             //check for the clouds key
             const clouds = yamlData.clouds
-            if (clouds !== undefined) {
-                let found = false
-                for (const key in clouds) {
-                    //look for matching cloud name
-                    if (cloudValue !== undefined && key === cloudValue) {
-                        found = true
-                    }
-                    //check a few of the required fields, especially password, since the user
-                    //would have had to add this manually
-                    if (
-                        clouds[key]?.auth?.auth_url === undefined ||
-                        clouds[key]?.auth?.password === undefined ||
-                        clouds[key]?.auth?.username === undefined
-                    ) {
-                        return t('validate.yaml.not.valid')
-                    }
+            if (clouds === undefined) {
+                return t('validate.yaml.not.valid')
+            }
+
+            let found = false
+            for (const key in clouds) {
+                //look for matching cloud name
+                if (cloudValue !== undefined && key === cloudValue) {
+                    found = true
                 }
-                //Uh-oh, cloud name not found in clouds.yaml
-                if (cloudValue !== undefined && !found) {
-                    return t('validate.yaml.cloud.not.found')
+                //check a few of the required fields, especially password, since the user
+                //would have had to add this manually
+                if (
+                    clouds[key]?.auth?.auth_url === undefined ||
+                    clouds[key]?.auth?.password === undefined ||
+                    clouds[key]?.auth?.username === undefined
+                ) {
+                    return t('validate.yaml.not.valid')
                 }
+            }
+            //Uh-oh, cloud name not found in clouds.yaml
+            if (cloudValue !== undefined && !found) {
+                return t('validate.yaml.cloud.not.found')
             }
         } catch (e) {
             return t('validate.yaml.not.valid')
