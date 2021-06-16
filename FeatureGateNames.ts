3e13484ab93986caeaae766953877392e3945c56
randybrunopiverger <rbrunopi@redhat.com>
2021-06-16 10:31:16 -0400

    PR change
    Signed-off-by: randybrunopiverger <rbrunopi@redhat.com>

---
+++ FeatureGateNames.ts
@@ -0,0 +1,7 @@
+/* Copyright Contributors to the Open Cluster Management project */
+/* istanbul ignore file */
+
+export enum FeatureGatePath {
+  clusterSet = "open-cluster-management-cluster-sets",
+  ansibleAutomationTemplate = "ansible-automation-template",
+}
