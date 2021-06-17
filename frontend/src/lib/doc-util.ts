65e2424ec2189536336ff19f14a2dbd21ea2c0b7
Sho Weimer <sweimer@redhat.com>
2021-06-17 16:04:43 -0400

    Doc integration updates (#728)
    * Doc link updates
    
    Signed-off-by: Sho Weimer <sweimer@redhat.com>
    
    * More updates
    
    Signed-off-by: Sho Weimer <sweimer@redhat.com>

--- frontend/src/lib/doc-util.ts
+++ frontend/src/lib/doc-util.ts
@@ -1,6 +1,6 @@
 /* Copyright Contributors to the Open Cluster Management project */
 
-export const DOC_VERSION = '2.2'
+export const DOC_VERSION = '2.3'
 
 export const DOC_BASE_PATH = `https://access.redhat.com/documentation/en-us/red_hat_advanced_cluster_management_for_kubernetes/${DOC_VERSION}/html`
 export const OCP_DOC_BASE_PATH = 'https://docs.openshift.com/container-platform'
@@ -12,8 +12,8 @@ export const DOC_LINKS = {
     CREATE_CLUSTER: `${DOC_BASE_PATH}/manage_cluster/creating-a-cluster`,
     IMPORT_CLUSTER: `${DOC_BASE_PATH}/manage_cluster/importing-a-target-managed-cluster-to-the-hub-cluster`,
     CLUSTER_SETS: `${DOC_BASE_PATH}/manage_cluster/managedclustersets`,
-    CLUSTER_POOLS: `${DOC_BASE_PATH}`,
+    CLUSTER_POOLS: `${DOC_BASE_PATH}/clusters/managing-cluster-pools`,
     SUBMARINER: `${DOC_BASE_PATH}/manage_cluster/submariner`,
     DISCOVERED_CLUSTERS: `${DOC_BASE_PATH}/clusters/#discovery-intro`,
-    MACHINE_POOLS: `${DOC_BASE_PATH}`,
+    MACHINE_POOLS: `${DOC_BASE_PATH}/manage_cluster/index#resizing-a-cluster`,
 }
