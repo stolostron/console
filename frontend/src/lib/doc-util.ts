dccb8b044a124c99e94796cb47f0b46d9c2b1f54
Chris Ahl <cahl@redhat.com>
2021-06-24 11:41:12 -0400

    change help URL links for 2.3 (#743)
    Signed-off-by: Chris Ahl <cahl@redhat.com>

--- frontend/src/lib/doc-util.ts
+++ frontend/src/lib/doc-util.ts
@@ -6,14 +6,14 @@ export const DOC_BASE_PATH = `https://access.redhat.com/documentation/en-us/red_
 export const OCP_DOC_BASE_PATH = 'https://docs.openshift.com/container-platform'
 
 export const DOC_LINKS = {
-    CLUSTERS: `${DOC_BASE_PATH}/manage_cluster/managing-your-clusters`,
-    BARE_METAL_ASSETS: `${DOC_BASE_PATH}/manage_cluster/creating-and-modifying-bare-metal-assets`,
-    CREATE_CONNECTION: `${DOC_BASE_PATH}/manage_cluster/creating-a-provider-connection`,
-    CREATE_CLUSTER: `${DOC_BASE_PATH}/manage_cluster/creating-a-cluster`,
-    IMPORT_CLUSTER: `${DOC_BASE_PATH}/manage_cluster/importing-a-target-managed-cluster-to-the-hub-cluster`,
-    CLUSTER_SETS: `${DOC_BASE_PATH}/manage_cluster/managedclustersets`,
+    CLUSTERS: `${DOC_BASE_PATH}/clusters/managing-your-clusters`,
+    BARE_METAL_ASSETS: `${DOC_BASE_PATH}/clusters/creating-and-modifying-bare-metal-assets`,
+    CREATE_CONNECTION: `${DOC_BASE_PATH}/credentials/credentials`,
+    CREATE_CLUSTER: `${DOC_BASE_PATH}/clusters/creating-a-cluster`,
+    IMPORT_CLUSTER: `${DOC_BASE_PATH}/clusters/importing-a-target-managed-cluster-to-the-hub-cluster`,
+    CLUSTER_SETS: `${DOC_BASE_PATH}/clusters/managedclustersets`,
     CLUSTER_POOLS: `${DOC_BASE_PATH}/clusters/managing-cluster-pools`,
-    SUBMARINER: `${DOC_BASE_PATH}/manage_cluster/submariner`,
+    SUBMARINER: `${DOC_BASE_PATH}/services/submariner`,
     DISCOVERED_CLUSTERS: `${DOC_BASE_PATH}/clusters/#discovery-intro`,
-    MACHINE_POOLS: `${DOC_BASE_PATH}/manage_cluster/index#resizing-a-cluster`,
+    MACHINE_POOLS: `${DOC_BASE_PATH}/clusters/index#resizing-a-cluster`,
 }
