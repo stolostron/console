238adfc61b3d6e740cb45ca60c0f7a0f604a2c66
John Swanke <jswanke@redhat.com>
2021-07-26 15:04:19 -0400

    use latest monaco (#757)
    * use latest monaco
    
    Signed-off-by: John Swanke <jswanke@redhat.com>
    
    * use jest-raw-loader
    
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -8,13 +8,8 @@ module.exports = {
             if (_rule.oneOf) {
                 _rule.oneOf.unshift({
                     test: [/\.hbs$/],
-                    loader: 'handlebars-loader',
-                    query: {
-                        precompileOptions: {
-                            knownHelpersOnly: false,
-                        },
-                    },
-                })
+                    loader: 'raw-loader',
+               })
                 break
             }
         }
@@ -28,11 +23,22 @@ module.exports = {
         // Turn off mergeLonghand css minification optimizations
         // This fixes patternfly select input not having borders in production
         for (let plugin of config.optimization.minimizer) {
-            if (plugin.pluginDescriptor?.name === 'OptimizeCssAssetsWebpackPlugin') {
+            if (plugin.pluginDescriptor && plugin.pluginDescriptor.name === 'OptimizeCssAssetsWebpackPlugin') {
                 plugin.options.cssProcessorPluginOptions.preset[1].mergeLonghand = false
             }
         }
-
         return config
     },
+    // to add a transform you have to add it to the map at the top
+    // because cra has a catchall fileTransform at the bottom to 
+    // catch any file BEFORE it would get to yours--and because transform is
+    // a map and cra iterates thru the map like an array, you need to unshift
+    // using Object assign
+    jest: function (config, env) {
+        config.transform = Object.assign({
+        "^.+\\.hbs$": "jest-raw-loader"
+        }, config.transform)
+        return config
+    }
+
 }
