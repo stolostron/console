c304ded4671c8dfbbe34856026e355711d8ce1c7
Aswin Surayanarayanan <asuryana@redhat.com>
2021-08-12 15:56:43 +0530

    Add vxlan driver support for submariner-addon
    Signed-off-by: Aswin Surayanarayanan <asuryana@redhat.com>

--- @open-cluster-management/resources/src/submariner-config.ts
+++ @open-cluster-management/resources/src/submariner-config.ts
@@ -19,6 +19,7 @@ export enum CableDriver {
     libreswan = 'libreswan',
     strongswan = 'strongswan',
     wireguard = 'wireguard',
+    vxlan = 'vxlan',
 }
 
 export interface SubmarinerConfig extends IResource {
