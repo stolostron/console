f8651726ee49bfbad3147d331a2774c8cdebb87d
James Talton <jtalton@redhat.com>
2021-08-12 11:49:20 -0400

    work
    Signed-off-by: James Talton <jtalton@redhat.com>

--- util/copyright-check.ts
+++ util/copyright-check.ts
@@ -1,34 +1,25 @@
 /* Copyright Contributors to the Open Cluster Management project */
-import { lstat, readdir, readFile } from "fs/promises";
-import { join } from "path";
+import { lstat, readdir, readFile } from 'fs/promises'
+import { join } from 'path'
 
-const ignoreDirectories = [".git", "node_modules", "coverage", "build", "lib"];
+const ignoreDirectories = ['.git', 'node_modules', 'coverage', 'build', 'lib']
 
-export async function checkCopyright(
-    directory: string,
-    extensions = [".ts"]
-): Promise<void> {
-    const names = await readdir(directory);
+export async function checkCopyright(directory: string, extensions = ['.ts']): Promise<void> {
+    const names = await readdir(directory)
     for (const name of names) {
-        if (ignoreDirectories.find((ignore) => name.includes(ignore))) continue;
-        const path = join(directory, name);
-        const stats = await lstat(path);
+        if (ignoreDirectories.find((ignore) => name.includes(ignore))) continue
+        const path = join(directory, name)
+        const stats = await lstat(path)
         if (stats.isDirectory()) {
-            void checkCopyright(path);
+            void checkCopyright(path)
         }
-        if (!extensions.find((ext) => name.endsWith(ext))) continue;
-        const file = await readFile(path);
-        if (
-            !file
-                .toString()
-                .startsWith(
-                    "/* Copyright Contributors to the Open Cluster Management project */\n"
-                )
-        ) {
-            console.log("error:", path, "needs Copyright");
-            process.exitCode = 1;
+        if (!extensions.find((ext) => name.endsWith(ext))) continue
+        const file = await readFile(path)
+        if (!file.toString().startsWith('/* Copyright Contributors to the Open Cluster Management project */\n')) {
+            console.log('error:', path, 'needs Copyright')
+            process.exitCode = 1
         }
     }
 }
 
-void checkCopyright(".");
+void checkCopyright('.')
