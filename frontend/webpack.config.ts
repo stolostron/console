3cda9cba337f13a9b4246a4cdd599eda56ee26f5
James Talton <jtalton@redhat.com>
2021-08-25 13:01:54 -0400

    work
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -5,14 +5,44 @@ import CompressionPlugin from 'compression-webpack-plugin'
 import CopyPlugin from 'copy-webpack-plugin'
 import * as webpack from 'webpack'
 import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin'
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
-import 'webpack-dev-server'
+import { Configuration, WebpackPluginInstance } from 'webpack'
+import { Configuration as DevServerConfiguration } from 'webpack-dev-server'
+import * as path from 'path'
 
-module.exports = function (_env: any, argv: { mode: string | undefined }) {
+module.exports = function (_env: any, argv: { hot?: boolean; mode: string | undefined }) {
     const isProduction = argv.mode === 'production' || argv.mode === undefined
     const isDevelopment = !isProduction
 
-    return {
+    const plugins = [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': isProduction ? JSON.stringify('production') : JSON.stringify('development'),
+            'process.env.REACT_APP_BACKEND_HOST': JSON.stringify('https://localhost:4000'),
+            'process.env.REACT_APP_BACKEND_PATH': JSON.stringify('/multicloud'),
+        }) as unknown as WebpackPluginInstance,
+        new webpack.ProvidePlugin({ Buffer: ['buffer', 'Buffer'], process: 'process' }),
+        new MonacoWebpackPlugin({ languages: ['yaml'] }),
+        new HtmlWebpackPlugin({ template: './public/index.html' }),
+    ].filter(Boolean) as WebpackPluginInstance[]
+
+    if (isProduction) {
+        plugins.push(
+            new CopyPlugin({
+                patterns: [{ from: 'public', globOptions: { ignore: ['**/*.html'] } }],
+            }) as unknown as WebpackPluginInstance
+        )
+        plugins.push(new CompressionPlugin({ algorithm: 'gzip' }) as unknown as WebpackPluginInstance)
+        plugins.push(
+            new CompressionPlugin({
+                algorithm: 'brotliCompress',
+                filename: '[path][base].br',
+            }) as unknown as WebpackPluginInstance
+        )
+    } else {
+        plugins.push(new webpack.HotModuleReplacementPlugin())
+    }
+
+    const config: Configuration & { devServer: DevServerConfiguration } = {
+        entry: ['react-hot-loader/patch', './src/index.tsx'],
         resolve: {
             extensions: ['.ts', '.tsx', '.js', '.jsx'],
             fallback: {
@@ -26,6 +56,7 @@ module.exports = function (_env: any, argv: { mode: string | undefined }) {
             },
             alias: {
                 handlebars: 'handlebars/dist/handlebars.js',
+                'react-dom': '@hot-loader/react-dom',
             },
         },
         module: {
@@ -36,32 +67,8 @@ module.exports = function (_env: any, argv: { mode: string | undefined }) {
                     loader: 'raw-loader',
                 },
                 {
-                    test: /\.jsx?$/,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/env', ['@babel/preset-react', { runtime: 'automatic' }]],
-                        plugins: [
-                            // ... other plugins
-                            isDevelopment && 'react-refresh/babel',
-                        ].filter(Boolean),
-                    },
-                },
-                {
-                    test: /\.tsx?$/,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/env',
-                            ['@babel/preset-react', { runtime: 'automatic' }],
-                            '@babel/preset-typescript',
-                        ],
-                        plugins: [
-                            // ... other plugins
-                            isDevelopment && 'react-refresh/babel',
-                        ].filter(Boolean),
-                    },
+                    test: /\.(css)$/,
+                    use: ['style-loader', 'css-loader'],
                 },
                 {
                     test: /\.(svg|ttf|eot|woff|woff2|jpg|jpeg|png|gif)$/,
@@ -74,35 +81,27 @@ module.exports = function (_env: any, argv: { mode: string | undefined }) {
                     },
                 },
                 {
-                    test: /\.(css)$/,
-                    use: ['style-loader', 'css-loader'],
+                    test: /\.(ts|tsx|js|jsx)$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/,
+                    options: { transpileOnly: isDevelopment },
                 },
             ],
         },
-        plugins: [
-            new webpack.DefinePlugin({
-                'process.env.NODE_ENV': isProduction ? JSON.stringify('production') : JSON.stringify('development'),
-                'process.env.REACT_APP_BACKEND_HOST': JSON.stringify('https://localhost:4000'),
-                'process.env.REACT_APP_BACKEND_PATH': JSON.stringify('/multicloud'),
-            }),
-            new webpack.ProvidePlugin({ Buffer: ['buffer', 'Buffer'], process: 'process' }),
-            new MonacoWebpackPlugin({ languages: ['yaml'] }),
-            new HtmlWebpackPlugin({ template: './public/index.html' }),
-            isProduction && new CopyPlugin({ patterns: [{ from: 'public', globOptions: { ignore: ['**/*.html'] } }] }),
-            isProduction && new CompressionPlugin({ algorithm: 'gzip' }),
-            isProduction && new CompressionPlugin({ algorithm: 'brotliCompress', filename: '[path][base].br' }),
-            isDevelopment && new webpack.HotModuleReplacementPlugin(),
-            isDevelopment && new ReactRefreshWebpackPlugin(),
-        ].filter(Boolean),
-        output: isProduction
-            ? {
-                  chunkFilename: 'js/[contenthash:12].js',
-                  filename: `js/[contenthash].js`,
-                  publicPath: '/multicloud',
-                  clean: true,
-              }
-            : { publicPath: '/' },
-        devtool: isDevelopment && 'inline-source-map',
+        plugins,
+        output: {
+            publicPath: isProduction ? '/multicloud' : '/',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+        },
+        optimization: {
+            runtimeChunk: 'single',
+            splitChunks: {
+                cacheGroups: {
+                    vendor: { test: /[\\/]node_modules[\\/]/, name: 'vendors', chunks: 'all' },
+                },
+            },
+        },
         devServer: {
             port: 3000,
             proxy: {
@@ -114,15 +113,21 @@ module.exports = function (_env: any, argv: { mode: string | undefined }) {
                 '/multicloud/common': { target: 'https://localhost:4000', secure: false },
                 '/multicloud/version': { target: 'https://localhost:4000', secure: false },
             },
-            contentBase: './public/',
-            contentBasePublicPath: '/multicloud',
             open: true,
-            inline: true,
             historyApiFallback: true,
             compress: true,
             https: true,
-            overlay: true,
-            hot: true,
+            hot: 'only',
+            static: {
+                publicPath: '/multicloud',
+            },
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+            },
         },
+        devtool: isDevelopment && 'inline-source-map',
     }
+
+    return config
 }
