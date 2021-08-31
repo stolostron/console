63da96f2f043eb9f94c4ce08e8583490e63655ef
James Talton <jtalton@redhat.com>
2021-08-31 15:43:39 -0400

    work
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -1,5 +1,6 @@
 /* Copyright Contributors to the Open Cluster Management project */
 /* eslint-disable @typescript-eslint/no-var-requires */
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import CompressionPlugin from 'compression-webpack-plugin'
 import CopyPlugin from 'copy-webpack-plugin'
@@ -7,41 +8,13 @@ import webpack from 'webpack'
 import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin'
 import { Configuration as DevServerConfiguration } from 'webpack-dev-server'
 import * as path from 'path'
+import ReactRefreshTypeScript from 'react-refresh-typescript'
 
 module.exports = function (_env: any, argv: { hot?: boolean; mode: string | undefined }) {
     const isProduction = argv.mode === 'production' || argv.mode === undefined
     const isDevelopment = !isProduction
 
-    const plugins = [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': isProduction ? JSON.stringify('production') : JSON.stringify('development'),
-            'process.env.REACT_APP_BACKEND_HOST': JSON.stringify('https://localhost:4000'),
-            'process.env.REACT_APP_BACKEND_PATH': JSON.stringify('/multicloud'),
-        }) as unknown as webpack.WebpackPluginInstance,
-        new webpack.ProvidePlugin({ Buffer: ['buffer', 'Buffer'], process: 'process' }),
-        new MonacoWebpackPlugin({ languages: ['yaml'] }),
-        new HtmlWebpackPlugin({ template: './public/index.html' }),
-    ].filter(Boolean) as webpack.WebpackPluginInstance[]
-
-    if (isProduction) {
-        plugins.push(
-            new CopyPlugin({
-                patterns: [{ from: 'public', globOptions: { ignore: ['**/*.html'] } }],
-            }) as unknown as webpack.WebpackPluginInstance
-        )
-        plugins.push(new CompressionPlugin({ algorithm: 'gzip' }) as unknown as webpack.WebpackPluginInstance)
-        plugins.push(
-            new CompressionPlugin({
-                algorithm: 'brotliCompress',
-                filename: '[path][base].br',
-            }) as unknown as webpack.WebpackPluginInstance
-        )
-    } else {
-        plugins.push(new webpack.HotModuleReplacementPlugin())
-    }
-
     const config: webpack.Configuration & { devServer: DevServerConfiguration } = {
-        entry: ['react-hot-loader/patch', './src/index.tsx'],
         resolve: {
             extensions: ['.ts', '.tsx', '.js', '.jsx'],
             fallback: {
@@ -55,33 +28,47 @@ module.exports = function (_env: any, argv: { hot?: boolean; mode: string | unde
             },
             alias: {
                 handlebars: 'handlebars/dist/handlebars.js',
-                'react-dom': '@hot-loader/react-dom',
             },
         },
         module: {
             rules: [
-                {
-                    test: /\.hbs$/,
-                    loader: 'raw-loader',
-                    exclude: /node_modules/,
-                },
-                {
-                    test: /\.(css)$/,
-                    use: ['style-loader', 'css-loader'],
-                },
-                {
-                    test: /\.(svg|ttf|eot|woff|woff2|jpg|jpeg|png|gif)$/,
-                    use: 'file-loader',
-                },
+                { test: /\.hbs$/, loader: 'raw-loader', exclude: /node_modules/ },
+                { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+                { test: /\.(svg|ttf|eot|woff|woff2|jpg|jpeg|png|gif)$/, use: 'file-loader' },
                 {
                     test: /\.(ts|tsx|js|jsx)$/,
+                    include: path.join(__dirname, 'src'),
                     loader: 'ts-loader',
-                    exclude: /node_modules/,
-                    options: { transpileOnly: isDevelopment },
+                    options: {
+                        configFile: isDevelopment ? 'tsconfig.dev.json' : 'tsconfig.json',
+                        transpileOnly: true,
+                        ...(isDevelopment && {
+                            getCustomTransformers: () => ({
+                                before: [ReactRefreshTypeScript()],
+                            }),
+                        }),
+                    },
                 },
             ],
         },
-        plugins,
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': isProduction ? JSON.stringify('production') : JSON.stringify('development'),
+                'process.env.REACT_APP_BACKEND_HOST': JSON.stringify('https://localhost:4000'),
+                'process.env.REACT_APP_BACKEND_PATH': JSON.stringify('/multicloud'),
+            }) as unknown as webpack.WebpackPluginInstance,
+            new webpack.ProvidePlugin({ Buffer: ['buffer', 'Buffer'], process: 'process' }),
+            new MonacoWebpackPlugin({ languages: ['yaml'] }),
+            isProduction &&
+                new CopyPlugin({
+                    patterns: [{ from: 'public', globOptions: { ignore: ['**/*.html'] } }],
+                }),
+            isProduction && new CompressionPlugin({ algorithm: 'gzip' }),
+            isProduction && new CompressionPlugin({ algorithm: 'brotliCompress', filename: '[path][base].br' }),
+            isDevelopment && new ReactRefreshWebpackPlugin(),
+            // new HtmlWebpackPlugin({ title: 'test', favicon: 'public/favicon.svg' }),
+            new HtmlWebpackPlugin({ template: './public/index.html' }),
+        ].filter(Boolean) as webpack.WebpackPluginInstance[],
         output: {
             publicPath: isProduction ? '/multicloud' : '/',
             path: path.resolve(__dirname, 'dist'),
@@ -119,7 +106,8 @@ module.exports = function (_env: any, argv: { hot?: boolean; mode: string | unde
                 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
             },
         },
-        devtool: isDevelopment && 'inline-source-map',
+        // devtool: isDevelopment && 'inline-source-map',
+        devtool: isDevelopment && 'eval-cheap-module-source-map',
     }
 
     return config
