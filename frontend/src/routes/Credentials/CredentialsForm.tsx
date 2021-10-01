9cbf22d35ea59337a4f3430d14f02f2cc40e57e0
randybrunopiverger <rbrunopi@redhat.com>
2021-10-01 11:59:58 -0400

    prettier
    Signed-off-by: randybrunopiverger <rbrunopi@redhat.com>

--- frontend/src/routes/Credentials/CredentialsForm.tsx
+++ frontend/src/routes/Credentials/CredentialsForm.tsx
@@ -201,7 +201,9 @@ export function CredentialsForm(props: {
         AzureUSGovernmentCloud = 'AzureUSGovernmentCloud',
     }
 
-    const [cloudName, setCloudName] = useState<CloudNames | string>(providerConnection?.stringData?.cloudName ?? CloudNames.AzurePublicCloud)
+    const [cloudName, setCloudName] = useState<CloudNames | string>(
+        providerConnection?.stringData?.cloudName ?? CloudNames.AzurePublicCloud
+    )
 
     let osServicePrincipalJson:
         | {
