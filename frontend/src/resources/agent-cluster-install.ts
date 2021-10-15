15e54409ae40eb126bef4247f083a00ad7e40e59
Rastislav Wagner <rawagner@redhat.com>
2021-10-15 11:02:43 +0200

    Fix ACI apiVersion/group
    Signed-off-by: Rastislav Wagner <rawagner@redhat.com>

--- frontend/src/resources/agent-cluster-install.ts
+++ frontend/src/resources/agent-cluster-install.ts
@@ -1,5 +1,5 @@
 /* Copyright Contributors to the Open Cluster Management project */
 export const AgentClusterInstallKind = 'AgentClusterInstall'
-export const AgentClusterInstallGroup = 'extensions.hive.openshift.io/v1beta1'
-export const AgentClusterInstallVersionOnly = `v1beta1`
-export const AgentClusterInstallVersion = `${AgentClusterInstallGroup}/${AgentClusterInstallVersionOnly}`
+export const AgentClusterInstallGroup = 'extensions.hive.openshift.io'
+export const AgentClusterInstallVersion = `v1beta1`
+export const AgentClusterInstallApiVersion = `${AgentClusterInstallGroup}/${AgentClusterInstallVersion}`
