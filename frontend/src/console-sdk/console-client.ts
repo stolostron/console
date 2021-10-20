2d46ed2a913da27ca98120535cd3533ee297185a
Zack Layne <zlayne@redhat.com>
2021-10-20 13:32:17 -0400

    Migrate search-ui and overview pages into console
    Signed-off-by: Zack Layne <zlayne@redhat.com>

---
+++ frontend/src/console-sdk/console-client.ts
@@ -0,0 +1,25 @@
+/* Copyright Contributors to the Open Cluster Management project */
+// Copyright (c) 2021 Red Hat, Inc.
+// Copyright Contributors to the Open Cluster Management project
+
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+
+export const consoleClient = new ApolloClient({
+    connectToDevTools: process.env.NODE_ENV === 'development',
+    uri: 'proxy/consoleapi',
+    cache: new InMemoryCache(),
+    credentials: 'same-origin',
+    defaultOptions: {
+        watchQuery: {
+            fetchPolicy: 'network-only',
+            errorPolicy: 'all',
+        },
+        query: {
+            fetchPolicy: 'network-only',
+            errorPolicy: 'all',
+        },
+        mutate: {
+            errorPolicy: 'all',
+        },
+    },
+})
---
+++ frontend/src/search-sdk/search-client.ts
@@ -0,0 +1,25 @@
+/* Copyright Contributors to the Open Cluster Management project */
+// Copyright (c) 2021 Red Hat, Inc.
+// Copyright Contributors to the Open Cluster Management project
+
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+
+export const searchClient = new ApolloClient({
+    connectToDevTools: process.env.NODE_ENV === 'development',
+    uri: 'proxy/search',
+    cache: new InMemoryCache(),
+    credentials: 'same-origin',
+    defaultOptions: {
+        watchQuery: {
+            fetchPolicy: 'network-only',
+            errorPolicy: 'all',
+        },
+        query: {
+            fetchPolicy: 'network-only',
+            errorPolicy: 'all',
+        },
+        mutate: {
+            errorPolicy: 'all',
+        },
+    },
+})
