7fc9244cc2bfc60715773fa7e7ac8e8b660a3fba
Maggie Chen <magchen@redhat.com>
2021-10-20 10:36:02 -0400

    Add license
    Signed-off-by: Maggie Chen <magchen@redhat.com>

--- frontend/src/routes/Applications/CreateApplication/ControlData/ControlDataHelm.js
+++ frontend/src/routes/Applications/CreateApplication/ControlData/ControlDataHelm.js
@@ -1,13 +1,4 @@
-/** *****************************************************************************
- * Licensed Materials - Property of IBM
- * (c) Copyright IBM Corporation 2019. All Rights Reserved.
- *
- * Note to U.S. Government Users Restricted Rights:
- * Use, duplication or disclosure restricted by GSA ADP Schedule
- * Contract with IBM Corp.
- ****************************************************************************** */
-// Copyright (c) 2020 Red Hat, Inc.
-// Copyright Contributors to the Open Cluster Management project
+/* Copyright Contributors to the Open Cluster Management project */
 'use strict'
 
 import { VALIDATE_URL } from 'temptifly'
