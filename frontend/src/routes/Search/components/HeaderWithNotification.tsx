62d0a77217af25a26532811c88a2120f96bcd2b8
Zack Layne <zlayne@redhat.com>
2021-10-27 12:20:56 -0400

    Update search to use new routes
    Signed-off-by: Zack Layne <zlayne@redhat.com>

--- frontend/src/routes/Search/components/HeaderWithNotification.tsx
+++ frontend/src/routes/Search/components/HeaderWithNotification.tsx
@@ -1,11 +1,11 @@
 /* Copyright Contributors to the Open Cluster Management project */
 // Copyright (c) 2021 Red Hat, Inc.
 // Copyright Contributors to the Open Cluster Management project
+import { AcmInlineStatus, AcmPageHeader, StatusType } from '@open-cluster-management/ui-components'
+import { Card, CardBody } from '@patternfly/react-core'
 import '@patternfly/react-core/dist/styles/base.css'
 import { useTranslation } from 'react-i18next'
-import { AcmPageHeader, AcmInlineStatus, StatusType } from '@open-cluster-management/ui-components'
 import { Message } from '../../../search-sdk/search-sdk'
-import { Card, CardBody } from '@patternfly/react-core'
 
 export default function HeaderWithNotification(props: { messages: Message[] }) {
     const { t } = useTranslation(['search'])
@@ -32,7 +32,7 @@ export default function HeaderWithNotification(props: { messages: Message[] }) {
                                     headerContent: displayShortText,
                                     bodyContent: displayLongText,
                                     footerContent: msg.id === 'S20' && (
-                                        <a href='/search?filters={"textsearch":"kind%3Acluster%20addon%3Asearch-collector%3Dfalse%20name%3A!local-cluster"}'>
+                                        <a href='/multicloud/search?filters={"textsearch":"kind%3Acluster%20addon%3Asearch-collector%3Dfalse%20name%3A!local-cluster"}'>
                                             {footerText}
                                         </a>
                                     ),
