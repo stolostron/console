8a823bd36c0fafd89b85b82739f1b854e6e10938
randybrunopiverger <rbrunopi@redhat.com>
2021-11-16 13:59:49 -0500

    lbFloatingIP addition, Kuryr addition
    Signed-off-by: randybrunopiverger <rbrunopi@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/controlData/ControlDataHelpers.js
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/controlData/ControlDataHelpers.js
@@ -284,6 +284,13 @@ export const onChangeDisconnect = (control, controlData) => {
         }
     })
 }
+export function getOSTNetworkingControlData() {
+    // Kuryr should only be available for Openstack
+    const networkData = JSON.parse(JSON.stringify(networkingControlData))
+    const modifiedData = networkData.find((object) => object.id == 'networkType')
+    modifiedData.available.push('Kuryr')
+    return networkData
+}
 
 export const clusterDetailsControlData = [
     {
