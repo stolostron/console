7e8564424f206d1f92bb9b3b9f70a5b0ff25a5b7
James Talton <jtalton@redhat.com>
2021-11-18 15:09:54 -0500

    fix
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/src/routes/Governance/Governance.tsx
+++ frontend/src/routes/Governance/Governance.tsx
@@ -2,10 +2,14 @@
 import { Redirect, Route, Switch } from 'react-router-dom'
 import { NavigationPath } from '../../NavigationPath'
 import GovernancePage from './GovernancePage'
+import PoliciesPage from './policies/Policies'
+import PolicySetsPage from './policy-sets/PolicySets'
 
 export default function Governance() {
     return (
         <Switch>
+            <Route exact path={NavigationPath.policies} component={PoliciesPage} />
+            <Route exact path={NavigationPath.policySets} component={PolicySetsPage} />
             <Route exact path={NavigationPath.governance} component={GovernancePage} />
             <Route path="*">
                 <Redirect to={NavigationPath.governance} />
