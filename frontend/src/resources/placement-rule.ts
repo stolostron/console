358b06cf620a9f60060b959081dc20f21659873d
rhowingt@redhat.com <rhowingt@redhat.com>
2021-11-17 21:41:39 -0500

    handle conflict
    Signed-off-by: rhowingt@redhat.com <rhowingt@redhat.com>

--- frontend/src/resources/placement-rule.ts
+++ frontend/src/resources/placement-rule.ts
@@ -1,6 +1,7 @@
 /* Copyright Contributors to the Open Cluster Management project */
 import { Metadata } from './metadata'
 import { Selector } from './selector'
+import { IResource, IResourceDefinition } from './resource'
 
 export const PlacementRuleApiVersion = 'apps.open-cluster-management.io/v1'
 export type PlacementRuleApiVersionType = 'apps.open-cluster-management.io/v1'
@@ -8,11 +9,17 @@ export type PlacementRuleApiVersionType = 'apps.open-cluster-management.io/v1'
 export const PlacementRuleKind = 'PlacementRule'
 export type PlacementRuleKindType = 'PlacementRule'
 
-export interface PlacementRule {
+export const PlacementRuleDefinition: IResourceDefinition = {
+    apiVersion: PlacementRuleApiVersion,
+    kind: PlacementRuleKind,
+}
+
+export interface PlacementRule extends IResource {
     apiVersion: PlacementRuleApiVersionType
-    kind: PlacementRuleApiVersionType
+    kind: PlacementRuleKindType
     metadata: Metadata
     spec: {
+        clusterReplicas?: number
         clusterConditions?: {
             status: string
             type: string
