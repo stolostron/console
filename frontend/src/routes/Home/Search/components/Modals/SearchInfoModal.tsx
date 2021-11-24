b72c6241d8405bd2c0718fb0b7cc4dcf171451a1
rhowingt@redhat.com <rhowingt@redhat.com>
2021-11-23 23:15:19 -0500

    search-components i18n-refactor
    Signed-off-by: rhowingt@redhat.com <rhowingt@redhat.com>

--- frontend/src/routes/Home/Search/components/Modals/SearchInfoModal.tsx
+++ frontend/src/routes/Home/Search/components/Modals/SearchInfoModal.tsx
@@ -44,50 +44,62 @@ const useStyles = makeStyles({
     },
 })
 export const SearchInfoModal = (props: any) => {
-    const { t } = useTranslation(['search'])
+    const { t } = useTranslation()
     const classes = useStyles()
     return (
         <Fragment>
             <AcmModal
                 className={classes.root}
-                title={t('search.modal.info.title')}
+                title={t('Search guide')}
                 width={'50%'}
                 onClose={props.onClose}
                 isOpen={props.isOpen}
             >
                 <div>
-                    <h2>{t('search.modal.info.subtitle')}</h2>
+                    <h2>{t('Use keywords or property filters to search for resources.')}</h2>
                 </div>
                 <hr />
                 <div>
-                    <p>{t('search.modal.info.keyword.section.title')}</p>
+                    <p>{t('To search for a keyword, type the word in the search box.')}</p>
                     <div className={classes.exampleRow}>
                         <AcmLabels labels={['Type']} />
                         <p>OpenShift</p>
                     </div>
                     <div className={classes.exampleRow}>
                         <AcmLabels labels={['Show']} />
-                        <p>{t('search.modal.info.keyword.section.desc')}</p>
+                        <p>{t('A list of resources that contain the keyword "OpenShift" in any field.')}</p>
                     </div>
                 </div>
                 <hr />
                 <div>
-                    <p>{t('search.modal.info.property.section.title')}</p>
+                    <p>
+                        {t(
+                            'To search for resources with a given property value, type or select the property name from the autocomplete list. Then type or select the value for the selected property filter.'
+                        )}
+                    </p>
                     <div className={classes.exampleRow}>
                         <AcmLabels labels={['Type']} />
                         <p>status: failed,pending</p>
                     </div>
                     <div className={classes.exampleRow}>
                         <AcmLabels labels={['Show']} />
-                        <p>{t('search.modal.info.property.section.desc')}</p>
+                        <p>{t('Resources with "failed" or "pending" status.')}</p>
                     </div>
                 </div>
                 <hr />
                 <div>
-                    <p>{t('search.modal.info.additional.info')}</p>
+                    <p>{t('Additional information:')}</p>
                     <ul>
-                        <li>{t('search.modal.info.additional.info.1')}</li>
-                        <li>{t('search.modal.info.additional.info.2')}</li>
+                        <li>
+                            {t(
+                                'You can include any combination of filters and keywords to make your search more specific.'
+                            )}
+                        </li>
+                        <li>
+                            {t(
+                                'You can apply multiple values to the same property filter. First, type or select the filter name, then select the value. Both values combine into a single filter.'
+                            )}
+                        </li>
                     </ul>
                 </div>
             </AcmModal>
--- frontend/src/routes/Home/Search/components/Modals/ShareSearchModal.test.tsx
+++ frontend/src/routes/Home/Search/components/Modals/ShareSearchModal.test.tsx
@@ -6,11 +6,11 @@ import { ShareSearchModal } from './ShareSearchModal'
 
 test('renders share modal with searchText', () => {
     const { getByText } = render(<ShareSearchModal shareSearch={{ searchText: 'kind:pod name:testPod' }} />)
-    expect(getByText('search.modal.share.title')).toBeInTheDocument()
+    expect(getByText('Share search')).toBeInTheDocument()
     expect(getByText('http://localhost/?filters={"textsearch":"kind%3Apod%20name%3AtestPod"}')).toBeInTheDocument()
 })
 
 test('renders share modal without searchText', () => {
     const { getByText } = render(<ShareSearchModal shareSearch={null} />)
-    expect(getByText('search.modal.share.title')).toBeInTheDocument()
+    expect(getByText('Share search')).toBeInTheDocument()
 })
