2b7c907053c6d623ca717b3e9f2e1a9837343928
Kevin Cormier <kcormier@redhat.com>
2021-11-29 11:46:18 -0500

    Delay search url resolution (#1004)
    * Delay search url resolution
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Add X-CSRFToken to requests if available
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/routes/Home/Search/search-sdk/search-client.ts
+++ frontend/src/routes/Home/Search/search-sdk/search-client.ts
@@ -2,14 +2,33 @@
 // Copyright (c) 2021 Red Hat, Inc.
 // Copyright Contributors to the Open Cluster Management project
 
-import { ApolloClient, InMemoryCache } from '@apollo/client'
-import { getBackendUrl } from '../../../../resources'
+import { ApolloClient, ApolloLink, from, HttpLink, InMemoryCache } from '@apollo/client'
+import { getBackendUrl, getCookie } from '../../../../resources'
+
+const httpLink = new HttpLink({
+    uri: () => `${getBackendUrl()}/proxy/search`,
+})
+
+const csrfHeaderLink = new ApolloLink((operation, forward) => {
+    const csrfToken = getCookie('csrf-token')
+    if (csrfToken) {
+        operation.setContext(({ headers = {} }) => ({
+            headers: {
+                ...headers,
+                'X-CSRFToken': csrfToken,
+            },
+        }))
+    }
+
+    return forward(operation)
+})
 
 export const searchClient = new ApolloClient({
     connectToDevTools: process.env.NODE_ENV === 'development',
-    uri: `${getBackendUrl()}/proxy/search`,
+    link: from([csrfHeaderLink, httpLink]),
     cache: new InMemoryCache(),
     credentials: 'same-origin',
+
     defaultOptions: {
         watchQuery: {
             fetchPolicy: 'network-only',
