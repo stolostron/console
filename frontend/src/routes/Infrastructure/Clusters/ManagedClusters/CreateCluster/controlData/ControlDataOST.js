c524ee71b24f92e15af2bfa21bc12fecb82f9731
randybrunopiverger <rbrunopi@redhat.com>
2021-12-13 16:38:31 -0500

    PR changes
    Signed-off-by: randybrunopiverger <rbrunopi@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/controlData/ControlDataOST.js
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/controlData/ControlDataOST.js
@@ -211,7 +211,12 @@ const controlDataOST = [
         name: 'creation.ocp.cluster.ost.lb.floating.ip',
         placeholder: 'placeholder.creation.ocp.cluster.ost.lb.floating.ip',
         tooltip: 'tooltip.creation.ocp.cluster.ost.lb.floating.ip',
-        hidden: isHidden_gt_OCP46,
+        hidden: (control, controlData) => {
+            if (isHidden_gt_OCP46(control, controlData)) {
+                control.active = undefined
+                return true
+            }
+        },
         active: '',
         validation: VALIDATE_IP,
     },
@@ -221,7 +226,12 @@ const controlDataOST = [
         name: 'creation.ocp.cluster.ost.api.floating.ip',
         placeholder: 'placeholder.creation.ocp.cluster.ost.api.floating.ip',
         tooltip: 'tooltip.creation.ocp.cluster.ost.api.floating.ip',
-        hidden: (control, controlData) => !isHidden_gt_OCP46(control, controlData),
+        hidden: (control, controlData) => {
+            if (!isHidden_gt_OCP46(control, controlData)) {
+                control.active = undefined
+                return true
+            }
+        },
         active: '',
         validation: VALIDATE_IP,
     },
