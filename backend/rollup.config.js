57aff4825745d5380b23e06e216ed1761dacfbd2
James Talton <jtalton@redhat.com>
2022-01-11 11:39:51 -0500

    es module support
    Signed-off-by: James Talton <jtalton@redhat.com>

---
+++ backend/rollup.config.js
@@ -0,0 +1,8 @@
+/* Copyright Contributors to the Open Cluster Management project */
+export default {
+    input: 'build/lib/main.js',
+    output: {
+        file: 'backend.js',
+        format: 'es',
+    },
+}
