3d0cd752f809d9756084b8335ba9866500c90675
James Talton <jtalton@redhat.com>
2022-01-11 14:44:17 -0500

    remove rollup config
    Signed-off-by: James Talton <jtalton@redhat.com>

--- backend/rollup.config.js
+++
@@ -1,8 +0,0 @@
-/* Copyright Contributors to the Open Cluster Management project */
-export default {
-    input: 'build/lib/main.js',
-    output: {
-        file: 'backend.js',
-        format: 'es',
-    },
-}
