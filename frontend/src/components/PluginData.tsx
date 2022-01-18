a04221b9a62bb970c77cbefad35ac4aadcadee75
Kevin Cormier <kcormier@redhat.com>
2022-01-18 09:22:08 -0500

    Use proxy alias (#1101)
    * Update proxy lookup for new CRD schema
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Update to new proxy alias
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Fix proxy path
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/components/PluginData.tsx
+++ frontend/src/components/PluginData.tsx
@@ -1,12 +1,7 @@
 /* Copyright Contributors to the Open Cluster Management project */
 import { ReactNode } from 'react'
-import { usePluginProxy } from '../lib/usePluginProxy'
 import { LoadData } from '../atoms'
-import { LoadingPage } from './LoadingPage'
 
 export function PluginData(props: { children?: ReactNode }) {
-    const loaded = usePluginProxy()
-
-    if (!loaded) return <LoadingPage />
     return <LoadData>{props.children}</LoadData>
 }
