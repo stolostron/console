5989de6992bf637791ef150e239dfd6b12f962b0
John Swanke <jswanke@redhat.com>
2022-02-10 10:24:51 -0500

    work w/o deployables
    Signed-off-by: John Swanke <jswanke@redhat.com>

---
+++ frontend/src/resources/subscription-report.ts
@@ -0,0 +1,21 @@
+/* Copyright Contributors to the Open Cluster Management project */
+import { Metadata } from './metadata'
+import { IResource, IResourceDefinition } from './resource'
+
+export const SubscriptionReportApiVersion = 'apps.open-cluster-management.io/v1alpha1'
+export type SubscriptionReportApiVersionType = 'apps.open-cluster-management.io/v1alpha1'
+
+export const SubscriptionReportKind = 'SubscriptionReport'
+export type SubscriptionReportKindType = 'SubscriptionReport'
+
+export const SubscriptionReportDefinition: IResourceDefinition = {
+    apiVersion: SubscriptionReportApiVersion,
+    kind: SubscriptionReportKind,
+}
+
+export interface SubscriptionReport extends IResource {
+    apiVersion: SubscriptionReportApiVersionType
+    kind: SubscriptionReportKindType
+    metadata: Metadata
+    spec: {}
+}
