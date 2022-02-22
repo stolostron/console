da3be49fcbcc9cc8ef8d14287a156263c8fc4f3d
James Talton <jtalton@redhat.com>
2022-02-22 17:06:55 -0500

    set explicit package versions
    Signed-off-by: James Talton <jtalton@redhat.com>

--- backend/src/lib/logger.ts
+++ backend/src/lib/logger.ts
@@ -1,17 +1,8 @@
 /* Copyright Contributors to the Open Cluster Management project */
-import pino, { TransportSingleOptions } from 'pino'
+import pino from 'pino'
 export const logLevel = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'debug'
 
-const transport: TransportSingleOptions | undefined =
-    process.env.NODE_ENV === 'development'
-        ? { target: 'pino-zen', options: { formatter: { msg: { padStart: 6 } } } }
-        : undefined
-
-const options: pino.LoggerOptions = {
-    level: logLevel,
-    base: {},
-    transport,
-}
+const options: pino.LoggerOptions = { level: logLevel, base: {} }
 export const logger = pino(options)
 export function stopLogger(): void {
     // do nothing
