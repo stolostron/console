27ea0561908dc54d6c1bcc2aadf651e3acac00f6
John Swanke <jswanke@redhat.com>
2022-02-23 09:15:44 -0500

    fix test
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- frontend/src/resources/utils/utils.ts
+++ frontend/src/resources/utils/utils.ts
@@ -32,7 +32,7 @@ export function getCookie(name: string) {
     const value = `; ${document.cookie}`
     const parts = value.split(`; ${name}=`)
     if (parts.length === 2) {
-        const cookie = parts.pop()
+        const cookie = parts[parts.length - 1]
         if (cookie) return cookie.split(';').shift()
     }
 }
