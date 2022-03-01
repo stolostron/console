7e08b19128ff2d9e6ff1e321897ac616859c3ee6
Kevin Cormier <kcormier@redhat.com>
2022-03-01 15:48:38 -0500

    Fix timezone to UTC for moment in tests (#1266)
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -150,12 +150,18 @@ i18n
         nsSeparator: '~',
         supportedLngs: ['en'], // only languages from this array will attempt to be loaded
         simplifyPluralSuffix: true,
-        lng: "en",
-        fallbackLng: "en",
-        ns: "translation",
+        lng: 'en',
+        fallbackLng: 'en',
+        ns: 'translation',
         resources: {
             en: {
-              translation: require('../public/locales/en/translation.json')
+                translation: require('../public/locales/en/translation.json'),
             },
-          },
+        },
     })
+
+const moment = jest.requireActual('moment-timezone')
+jest.doMock('moment', () => {
+    moment.tz.setDefault('UTC')
+    return moment
+})
