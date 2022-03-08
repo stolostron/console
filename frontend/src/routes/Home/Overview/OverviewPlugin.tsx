9f685d10b41987a108edf595eb8280e5f9fb1ad2
Kevin Cormier <kcormier@redhat.com>
2022-03-08 18:25:55 -0500

    Make portions of MCE UI conditional on ACM (#1267)
    * Implement PluginContext
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Disable Submariner in MCE
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Disable Governance in MCE
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Disable Applications in MCE
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Disable Search in MCE
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Unit tests
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Update default page for perspective
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Update official MCE name
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * prettier
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/routes/Home/Overview/OverviewPlugin.tsx
+++ frontend/src/routes/Home/Overview/OverviewPlugin.tsx
@@ -1,14 +1,17 @@
 /* Copyright Contributors to the Open Cluster Management project */
 import { RecoilRoot } from 'recoil'
 import { PluginData } from '../../../components/PluginData'
+import { PluginContextProvider } from '../../../components/PluginContextProvider'
 import OverviewPage from './OverviewPage'
 
 export default function OverviewPlugin() {
     return (
-        <RecoilRoot>
-            <PluginData>
-                <OverviewPage />
-            </PluginData>
-        </RecoilRoot>
+        <PluginContextProvider>
+            <RecoilRoot>
+                <PluginData>
+                    <OverviewPage />
+                </PluginData>
+            </RecoilRoot>
+        </PluginContextProvider>
     )
 }
--- frontend/src/routes/Infrastructure/BareMetalAssets/BareMetalAssetsPlugin.tsx
+++ frontend/src/routes/Infrastructure/BareMetalAssets/BareMetalAssetsPlugin.tsx
@@ -1,14 +1,17 @@
 /* Copyright Contributors to the Open Cluster Management project */
 import { RecoilRoot } from 'recoil'
 import { PluginData } from '../../../components/PluginData'
+import { PluginContextProvider } from '../../../components/PluginContextProvider'
 import BareMetalAssets from './BareMetalAssets'
 
 export default function BareMetalAssetsPlugin() {
     return (
-        <RecoilRoot>
-            <PluginData>
-                <BareMetalAssets />
-            </PluginData>
-        </RecoilRoot>
+        <PluginContextProvider>
+            <RecoilRoot>
+                <PluginData>
+                    <BareMetalAssets />
+                </PluginData>
+            </RecoilRoot>
+        </PluginContextProvider>
     )
 }
