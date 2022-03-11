7796e5773f752b4def42fefcc713783c6848a30f
Zack Layne <zlayne@redhat.com>
2022-03-11 11:10:29 -0500

    Open a drawer to display policies on a specific cluster in policy clusters tab
    Signed-off-by: Zack Layne <zlayne@redhat.com>

--- frontend/src/routes/Governance/clusters/GovernanceClustersPage.tsx
+++ frontend/src/routes/Governance/clusters/GovernanceClustersPage.tsx
@@ -1,9 +1,10 @@
 /* Copyright Contributors to the Open Cluster Management project */
 import { PageSection } from '@patternfly/react-core'
-import { AcmTable, IAcmTableColumn } from '@stolostron/ui-components'
-import { useCallback, useMemo } from 'react'
+import { AcmDrawerContext, AcmTable, IAcmTableColumn } from '@stolostron/ui-components'
+import { Fragment, useCallback, useContext, useMemo } from 'react'
 import { useRecoilState } from 'recoil'
 import { clusterCuratorsState, usePolicies } from '../../../atoms'
+import { useTranslation } from '../../../lib/acm-i18next'
 import { Cluster } from '../../../resources'
 import { useAllClusters } from '../../Infrastructure/Clusters/ManagedClusters/components/useAllClusters'
 import {
@@ -17,14 +18,16 @@ import {
     GovernanceCreatePolicyEmptyState,
     GovernanceManagePoliciesEmptyState,
 } from '../components/GovernanceEmptyState'
+import { PolicyViolationIcons2 } from '../components/PolicyViolations'
 import { ClusterViolationSummaryMap, useClusterViolationSummaryMap } from '../overview/ClusterViolationSummary'
 import { usePolicyViolationSummary } from '../overview/PolicyViolationSummary'
-import { useClusterPolicyViolationsColumn } from './useClusterPolicyViolationsColumn'
+import { PolicySummarySidebar } from './PolicySummarySidebar'
 
 export default function GovernanceClustersPage() {
     const policies = usePolicies()
     const policyViolationSummary = usePolicyViolationSummary(policies)
     const clusterViolationSummaryMap = useClusterViolationSummaryMap(policies)
+
     if (policies.length === 0) {
         return <GovernanceCreatePolicyEmptyState />
     }
@@ -40,6 +43,8 @@ export default function GovernanceClustersPage() {
 }
 
 export function GovernanceClustersTable(props: { clusterViolationSummaryMap: ClusterViolationSummaryMap }) {
+    const { t } = useTranslation()
+    const { setDrawerContext } = useContext(AcmDrawerContext)
     const [clusterCurators] = useRecoilState(clusterCuratorsState)
     let clusters = useAllClusters()
     clusters = clusters.filter((cluster) => {
@@ -50,9 +55,25 @@ export function GovernanceClustersTable(props: { clusterViolationSummaryMap: Clu
             return true
         }
     })
+
+    const onClick = useCallback(
+        (cluster: Cluster, compliance: string) => {
+            setDrawerContext({
+                isExpanded: true,
+                onCloseClick: () => {
+                    setDrawerContext(undefined)
+                },
+                panelContent: <PolicySummarySidebar cluster={cluster} compliance={compliance} />,
+                panelContentProps: { defaultSize: '40%' },
+                isInline: true,
+                isResizable: true,
+            })
+        },
+        [setDrawerContext]
+    )
+
     const mckeyFn = useCallback((cluster: Cluster) => cluster.name!, [])
     const clusterNameColumn = useClusterNameColumn()
-    const clusterPolicyViolationsColumn = useClusterPolicyViolationsColumn(props.clusterViolationSummaryMap)
     const clusterStatusColumn = useClusterStatusColumn()
     const clusterProviderColumn = useClusterProviderColumn()
     const clusterDistributionColumn = useClusterDistributionColumn(clusterCurators)
@@ -60,7 +81,33 @@ export function GovernanceClustersTable(props: { clusterViolationSummaryMap: Clu
     const columns = useMemo<IAcmTableColumn<Cluster>[]>(
         () => [
             clusterNameColumn,
-            clusterPolicyViolationsColumn,
+            {
+                header: t('Policy violations'),
+                cell: (cluster: Cluster) => {
+                    const clusterViolationSummary = props.clusterViolationSummaryMap[cluster.name ?? '']
+                    if (!clusterViolationSummary) return <Fragment />
+                    return (
+                        <PolicyViolationIcons2
+                            compliant={clusterViolationSummary.compliant}
+                            compliantOnClick={() => onClick(cluster, 'compliant')}
+                            noncompliant={clusterViolationSummary.noncompliant}
+                            violationOnClick={() => onClick(cluster, 'noncompliant')}
+                        />
+                    )
+                },
+                sort: (lhs, rhs) => {
+                    const lhsViolations = props.clusterViolationSummaryMap[lhs.name ?? '']
+                    const rhsViolations = props.clusterViolationSummaryMap[rhs.name ?? '']
+                    if (lhsViolations === rhsViolations) return 0
+                    if (!lhsViolations) return -1
+                    if (!rhsViolations) return 1
+                    if (lhsViolations.noncompliant > rhsViolations.noncompliant) return -1
+                    if (lhsViolations.noncompliant < rhsViolations.noncompliant) return 1
+                    if (lhsViolations.compliant > rhsViolations.compliant) return -1
+                    if (lhsViolations.compliant < rhsViolations.compliant) return 1
+                    return 0
+                },
+            },
             clusterStatusColumn,
             clusterProviderColumn,
             clusterDistributionColumn,
@@ -68,11 +115,13 @@ export function GovernanceClustersTable(props: { clusterViolationSummaryMap: Clu
         ],
         [
             clusterNameColumn,
-            clusterPolicyViolationsColumn,
             clusterStatusColumn,
             clusterProviderColumn,
             clusterDistributionColumn,
             clusterLabelsColumn,
+            props.clusterViolationSummaryMap,
+            onClick,
+            t,
         ]
     )
 
--- frontend/src/routes/Governance/components/PolicyViolations.tsx
+++ frontend/src/routes/Governance/components/PolicyViolations.tsx
@@ -9,30 +9,39 @@ export function PolicyViolationIcons(props: {
     compliantHref?: string
     violationHref?: string
     unknownHref?: string
+    compliantOnClick?: () => void
+    violationOnClick?: () => void
+    unknownOnClick?: () => void
 }) {
     const { t } = useTranslation()
-    const { risks, compliantHref, violationHref, unknownHref } = props
+    const { risks, compliantHref, violationHref, unknownHref, compliantOnClick, violationOnClick, unknownOnClick } =
+        props
     const violations = risks.high + risks.medium + risks.low
     return (
         <StatusIcons
             compliant={risks.synced}
             compliantTooltip={t('policies.noviolations', { count: risks.synced })}
             compliantHref={compliantHref}
+            compliantOnClick={compliantOnClick}
             violations={violations}
             violationsTooltip={t('policies.violations', { count: violations })}
             violationHref={violationHref}
+            violationOnClick={violationOnClick}
             unknown={risks.unknown}
             unknownTooltip={t('policies.unknown', { count: risks.unknown })}
             unknownHref={unknownHref}
+            unknownOnClick={unknownOnClick}
         />
     )
 }
 
 export function PolicyViolationIcons2(props: {
     compliant: number
-    noncompliant: number
     compliantHref?: string
+    compliantOnClick?: () => void
+    noncompliant: number
     violationHref?: string
+    violationOnClick?: () => void
 }) {
     const { t } = useTranslation()
     return (
@@ -40,9 +49,11 @@ export function PolicyViolationIcons2(props: {
             compliant={props.compliant}
             compliantTooltip={t('policies.noviolations', { count: props.compliant })}
             compliantHref={props.compliantHref}
+            compliantOnClick={props.compliantOnClick}
             violations={props.noncompliant}
             violationsTooltip={t('policy.violations', { count: props.noncompliant })}
             violationHref={props.violationHref}
+            violationOnClick={props.violationOnClick}
         />
     )
 }
