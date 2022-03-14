18e82bec4b625c90838f1a3c5dbb2752ce702f08
Kevin Cormier <kcormier@redhat.com>
2022-03-14 09:26:33 -0400

    Remove Tech Preview banner for cluster sets
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ClusterSets/ClusterSets.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ClusterSets/ClusterSets.tsx
@@ -105,7 +105,6 @@ export default function ClusterSetsPage() {
     return (
         <AcmPageContent id="clusters">
             <PageSection>
-                <TechPreviewAlert i18nKey="preview.clusterSets" docHref={DOC_LINKS.CLUSTER_SETS} />
                 <Stack hasGutter style={{ height: 'unset' }}>
                     <AcmExpandableCard title={t('learn.terminology')} id="cluster-sets-learn">
                         <Flex style={{ flexWrap: 'inherit' }}>
