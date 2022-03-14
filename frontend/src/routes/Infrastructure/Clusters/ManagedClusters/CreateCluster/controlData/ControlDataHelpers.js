a77e46469fcaa727cfc2faa762f6e3a946a177d4
randybrunopiverger <rbrunopi@redhat.com>
2022-03-14 00:08:41 -0700

    fix
    Signed-off-by: randybrunopiverger <rbrunopi@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/controlData/ControlDataHelpers.js
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/controlData/ControlDataHelpers.js
@@ -459,13 +459,7 @@ export const isHidden_lt_OCP48 = (control, controlData) => {
 export const isHidden_gt_OCP46 = (control, controlData) => {
     const singleNodeFeatureFlag = controlData.find(({ id }) => id === 'singleNodeFeatureFlag')
     const imageSet = controlData.find(({ id }) => id === 'imageSet')
-    if (
-        singleNodeFeatureFlag &&
-        singleNodeFeatureFlag.active &&
-        imageSet &&
-        imageSet.active &&
-        imageSet.active.includes('release:4.6')
-    ) {
+    if (imageSet && imageSet.active && imageSet.active.includes('release:4.6')) {
         return false
     }
     return true
