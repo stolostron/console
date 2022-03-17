bc12980a25bbc95ae8b22f2e682e2d592865fac9
rhowingt@redhat.com <rhowingt@redhat.com>
2022-03-17 13:01:32 -0400

    prettier
    Signed-off-by: rhowingt@redhat.com <rhowingt@redhat.com>

--- frontend/src/routes/Governance/components/AutomationDetailsSidebar.tsx
+++ frontend/src/routes/Governance/components/AutomationDetailsSidebar.tsx
@@ -284,7 +284,7 @@ export function AutomationDetailsSidebar(props: {
                             },
                             onCancel: () => {
                                 setModalProps({ open: false })
-                            }
+                            },
                         })
                     }
                 >
