e6750be2f8bebcecae4d6d456db0eacafb7fac91
Kevin Cormier <kcormier@redhat.com>
2022-03-25 10:54:47 -0400

    prettier
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/routes/Governance/overview/Overview.tsx
+++ frontend/src/routes/Governance/overview/Overview.tsx
@@ -218,7 +218,11 @@ function ClustersCard() {
                                 <Fragment>
                                     <span>{cluster.metadata.name}</span>
                                     {clusterViolationSummary.compliant ? (
-                                        <Tooltip content={t('policies.noviolations', { count: clusterViolationSummary.compliant})}>
+                                        <Tooltip
+                                            content={t('policies.noviolations', {
+                                                count: clusterViolationSummary.compliant,
+                                            })}
+                                        >
                                             <span style={{ whiteSpace: 'nowrap', textAlign: 'right' }}>
                                                 <Fragment>
                                                     <Button
@@ -237,7 +241,11 @@ function ClustersCard() {
                                         <span />
                                     )}
                                     {clusterViolationSummary.noncompliant ? (
-                                        <Tooltip content={t('policy.violations', { count: clusterViolationSummary.noncompliant })}>
+                                        <Tooltip
+                                            content={t('policy.violations', {
+                                                count: clusterViolationSummary.noncompliant,
+                                            })}
+                                        >
                                             <span style={{ whiteSpace: 'nowrap', textAlign: 'right' }}>
                                                 <Button
                                                     isInline
