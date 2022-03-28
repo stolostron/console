e353d7273c4421a3160002b9d9e86a2ee176bb68
Zack Layne <zlayne@redhat.com>
2022-03-28 16:58:11 -0400

    Fix string for associated placement resources (#1429)
    Signed-off-by: Zack Layne <zlayne@redhat.com>

--- frontend/src/routes/Governance/policies/Policies.tsx
+++ frontend/src/routes/Governance/policies/Policies.tsx
@@ -1010,7 +1010,7 @@ export function DeletePolicyModal(props: { item: PolicyTableItem; onClose: () =>
                         id="delete-placements"
                         isChecked={deletePlacements}
                         onChange={setDeletePlacements}
-                        label={t('policy.modal.delete.associatedResources.placementRule')}
+                        label={t('policy.modal.delete.associatedResources.placement')}
                     />
                 </StackItem>
                 {props.item.source !== 'Local' ? (
