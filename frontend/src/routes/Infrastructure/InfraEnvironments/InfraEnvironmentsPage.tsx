9c52f6af7af9ed593660e67b07a760cc1e3ec82b
Montse Ortega <ammont82@users.noreply.github.com>
2022-03-29 08:49:43 +0200

    Bug  2064387 - [UI] when trying to delete an infraenv spoke during installation, there is no alert saying we can't (#1391)
    Signed-off-by: Montse Ortega <mortegag@redhat.com>

--- frontend/src/routes/Infrastructure/InfraEnvironments/InfraEnvironmentsPage.tsx
+++ frontend/src/routes/Infrastructure/InfraEnvironments/InfraEnvironmentsPage.tsx
@@ -41,7 +41,27 @@ const deleteInfraEnv = (infraEnv: InfraEnvK8sResource) => {
             },
         })
     }
-    return deleteResources(resources)
+    const deleteResourcesResult = deleteResources(resources)
+
+    return {
+        promise: new Promise((resolve, reject) => {
+            deleteResourcesResult.promise.then((promisesSettledResult) => {
+                if (promisesSettledResult[0]?.status === 'rejected') {
+                    const error = promisesSettledResult[0].reason
+                    if (error) {
+                        reject(promisesSettledResult[0].reason)
+                        return
+                    }
+                }
+                if (promisesSettledResult[1]?.status === 'rejected') {
+                    reject(promisesSettledResult[1].reason)
+                    return
+                }
+                resolve(promisesSettledResult)
+            })
+        }),
+        abort: deleteResourcesResult.abort,
+    }
 }
 
 const InfraEnvironmentsPage: React.FC = () => {
