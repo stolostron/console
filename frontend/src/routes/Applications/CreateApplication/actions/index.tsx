2404be1a863aefff8385da50505648add09578b5
Maggie Chen <magchen@redhat.com>
2022-03-30 11:12:36 -0400

    try again without replace
    Signed-off-by: Maggie Chen <magchen@redhat.com>

--- frontend/src/routes/Applications/CreateApplication/actions/index.tsx
+++ frontend/src/routes/Applications/CreateApplication/actions/index.tsx
@@ -1,4 +1,4 @@
 /* Copyright Contributors to the Open Cluster Management project */
-export const argoAppSetQueryString = '?' + 'apiVersion=applicationset.argoproj.io'.replace(/\./g, '%2E')
+export const argoAppSetQueryString = '?' + 'apiVersion=applicationset.argoproj.io'
 
-export const subscriptionAppQueryString = '?' + 'apiVersion=application.app.k8s.io'.replace(/\./g, '%2E')
+export const subscriptionAppQueryString = '?' + 'apiVersion=application.app.k8s.io'
