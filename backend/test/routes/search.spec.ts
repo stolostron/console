dcc3922c515cda4808abfcb62117859aa9f6416c
Kevin Cormier <kcormier@redhat.com>
2022-03-31 09:03:46 -0400

    Add 2nd test for search route
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- backend/test/routes/search.spec.ts
+++ backend/test/routes/search.spec.ts
@@ -4,7 +4,7 @@ import { parseResponseJsonBody } from '../../src/lib/body-parser'
 import nock from 'nock'
 
 describe(`search Route`, function () {
-    it(`use search-api in the namespace of the MultiClusterHub`, async function () {
+    it(`uses search-api in the namespace of the MultiClusterHub`, async function () {
         nock(process.env.CLUSTER_API_URL).get('/apis').reply(200, {
             status: 200,
         })
@@ -23,6 +23,19 @@ describe(`search Route`, function () {
                 ],
             })
         nock('https://search-search-api.ocm.svc.cluster.local:4010').post('/searchapi/graphql').reply(200)
+        const res = await request('POST', '/proxy/search')
+        expect(res.statusCode).toEqual(200)
+    })
+    it(`uses search-api default route if no MultiClusterHub`, async function () {
+        nock(process.env.CLUSTER_API_URL).get('/apis').reply(200, {
+            status: 200,
+        })
+        nock(process.env.CLUSTER_API_URL)
+            .get('/apis/operator.open-cluster-management.io/v1/multiclusterhubs')
+            .reply(200, {
+                items: [],
+            })
+        nock('https://search-search-api:4010').post('/searchapi/graphql').reply(200)
         const res = await request('POST', '/proxy/search')
         expect(res.statusCode).toEqual(200)
     })
