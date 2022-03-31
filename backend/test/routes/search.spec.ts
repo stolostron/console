f39c7051296bd5ef8f5642bdbee87a7358ddd9d0
Kevin Cormier <kcormier@redhat.com>
2022-03-31 11:00:18 -0400

    Search API access for MCE backend (#1462)
    * Find search service from MCH namespace
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Add 2nd test for search route
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

---
+++ backend/test/routes/search.spec.ts
@@ -0,0 +1,42 @@
+/* Copyright Contributors to the Open Cluster Management project */
+import { request } from '../mock-request'
+import { parseResponseJsonBody } from '../../src/lib/body-parser'
+import nock from 'nock'
+
+describe(`search Route`, function () {
+    it(`uses search-api in the namespace of the MultiClusterHub`, async function () {
+        nock(process.env.CLUSTER_API_URL).get('/apis').reply(200, {
+            status: 200,
+        })
+        nock(process.env.CLUSTER_API_URL)
+            .get('/apis/operator.open-cluster-management.io/v1/multiclusterhubs')
+            .reply(200, {
+                items: [
+                    {
+                        metadata: {
+                            namespace: 'ocm',
+                        },
+                        status: {
+                            currentVersion: '2.5.1',
+                        },
+                    },
+                ],
+            })
+        nock('https://search-search-api.ocm.svc.cluster.local:4010').post('/searchapi/graphql').reply(200)
+        const res = await request('POST', '/proxy/search')
+        expect(res.statusCode).toEqual(200)
+    })
+    it(`uses search-api default route if no MultiClusterHub`, async function () {
+        nock(process.env.CLUSTER_API_URL).get('/apis').reply(200, {
+            status: 200,
+        })
+        nock(process.env.CLUSTER_API_URL)
+            .get('/apis/operator.open-cluster-management.io/v1/multiclusterhubs')
+            .reply(200, {
+                items: [],
+            })
+        nock('https://search-search-api:4010').post('/searchapi/graphql').reply(200)
+        const res = await request('POST', '/proxy/search')
+        expect(res.statusCode).toEqual(200)
+    })
+})
