0907565f9970f516e4608f9d5abe20e11b8c30de
Kevin Cormier <kcormier@redhat.com>
2022-04-04 16:54:42 -0400

    Fix path for plugin metadata (#1486)
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- backend/src/routes/serve.ts
+++ backend/src/routes/serve.ts
@@ -30,7 +30,7 @@ export async function serve(req: Http2ServerRequest, res: Http2ServerResponse): 
             res.setHeader('X-DNS-Prefetch-Control', 'off')
             res.setHeader('Expect-CT', 'enforce, max-age=30')
             // res.setHeader('Content-Security-Policy', ["default-src 'self'"].join(';'))
-        } else if (url === '/plugin-entry.js' || url === '/plugin-manifest.json') {
+        } else if (url === '/plugin/plugin-entry.js' || url === '/plugin/plugin-manifest.json') {
             res.setHeader('Cache-Control', 'no-cache')
         } else {
             res.setHeader('Cache-Control', cacheControl)
