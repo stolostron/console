5478bf1267e481ef43b53d1164158ac7d2511de4
James Talton <jtalton@redhat.com>
2022-04-27 18:30:04 -0400

    application performance (#1601)
    * application performance
    
    Signed-off-by: James Talton <jtalton@redhat.com>
    
    * linting
    
    Signed-off-by: James Talton <jtalton@redhat.com>

--- frontend/src/routes/Applications/ApplicationsPage.tsx
+++ frontend/src/routes/Applications/ApplicationsPage.tsx
@@ -1,11 +1,20 @@
 /* Copyright Contributors to the Open Cluster Management project */
 
-import { AcmPage, AcmPageHeader, AcmRoute, AcmSecondaryNav, AcmSecondaryNavItem } from '@stolostron/ui-components'
+import {
+    AcmLoadingPage,
+    AcmPage,
+    AcmPageHeader,
+    AcmRoute,
+    AcmSecondaryNav,
+    AcmSecondaryNavItem,
+} from '@stolostron/ui-components'
 import { Fragment, lazy, Suspense, useEffect } from 'react'
-import { useTranslation } from '../../lib/acm-i18next'
-import { Link, Route, Switch, useLocation, Redirect } from 'react-router-dom'
+import { Link, Redirect, Route, Switch, useLocation } from 'react-router-dom'
 import { useRecoilState } from 'recoil'
-import { acmRouteState } from '../../atoms'
+import { acmRouteState, discoveredApplicationsState } from '../../atoms'
+import { useTranslation } from '../../lib/acm-i18next'
+import { queryRemoteArgoApps } from '../../lib/search'
+import { useQuery } from '../../lib/useQuery'
 import { NavigationPath } from '../../NavigationPath'
 
 const ApplicationsOverviewPage = lazy(() => import('./Overview'))
@@ -17,6 +26,21 @@ export default function ApplicationsPage() {
 
     const [, setRoute] = useRecoilState(acmRouteState)
     useEffect(() => setRoute(AcmRoute.Applications), [setRoute])
+
+    const { data, loading, startPolling } = useQuery(queryRemoteArgoApps)
+    useEffect(startPolling, [startPolling])
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_, setDiscoveredAppilcations] = useRecoilState(discoveredApplicationsState)
+    useEffect(() => {
+        const remoteArgoApps = data?.[0]?.data?.searchResult?.[0]?.items || []
+        setDiscoveredAppilcations(remoteArgoApps)
+    }, [data, setDiscoveredAppilcations])
+
+    if (loading) {
+        return <AcmLoadingPage />
+    }
+
     return (
         <AcmPage
             hasDrawer
