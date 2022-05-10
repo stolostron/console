452a894c3c984c8bb08ef9ac7fd81cefdc862832
James Talton <jtalton@redhat.com>
2022-05-10 16:15:04 -0400

    test
    Signed-off-by: James Talton <jtalton@redhat.com>

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -4,3 +4,4 @@
 # Copyright IBM, Red Hat, and collected contributors to the OCM project
 # Copyright Contributors to the Open Cluster Management project
 ###############################################################################
+export CYPRESS_CACHE_FOLDER=/go/src/github.com/stolostron/console/cypress_cache
