9638a4b347da5ffac07efe958190cd6c9676740a
John Swanke <jswanke@redhat.com>
2022-05-26 08:59:33 -0400

    limit 1 fix
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/controlData/ControlDataHelpers.js
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/controlData/ControlDataHelpers.js
@@ -414,6 +414,16 @@ export const proxyControlData = [
     },
 ]
 
+export const onChangeAutomationTemplate = (control, controlData) => {
+    var installAttemptsLimit = controlData.find(({ id }) => id === 'installAttemptsLimit')
+    if (control.active) {
+        installAttemptsLimit.active = 1
+        installAttemptsLimit.immutable = 'ClusterDeployment[0].spec.installAttemptsLimit'
+    } else {
+        delete installAttemptsLimit.immutable
+    }
+}
+
 export const automationControlData = [
     ///////////////////////  automation  /////////////////////////////////////
     {
@@ -431,6 +441,7 @@ export const automationControlData = [
         id: 'templateName',
         type: 'combobox',
         placeholder: 'template.clusterCreate.select.placeholder',
+        onSelect: onChangeAutomationTemplate,
         validation: {
             required: false,
         },
