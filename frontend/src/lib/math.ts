2d1a23391cf9124e2988a4268babcdb6d7691869
Kevin Cormier <kcormier@redhat.com>
2022-05-31 10:55:54 -0400

    Coverage for same file in 2 different reports
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

---
+++ frontend/src/lib/math.ts
@@ -0,0 +1,8 @@
+/* Copyright Contributors to the Open Cluster Management project */
+export function add(a: number, b: number) {
+    return a + b
+}
+
+export function substract(a: number, b: number) {
+    return a - b
+}
