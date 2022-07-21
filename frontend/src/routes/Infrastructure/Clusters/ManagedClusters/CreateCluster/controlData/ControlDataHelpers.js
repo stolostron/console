04106f239e6c6970f7de279a57a1e2722eb7a21d
Kevin Cormier <kcormier@redhat.com>
2022-07-21 17:54:55 -0400

    Improve regex
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/controlData/ControlDataHelpers.js
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/controlData/ControlDataHelpers.js
@@ -580,10 +580,10 @@ export const architectureData = [
     },
 ]
 
-const versionRegex = /release:([\d]+)\.([\d]+)\.([\d]+)/
+const versionRegex = /release:([\d]{1,5})\.([\d]{1,5})\.([\d]{1,5})/
 function versionGreater(version, x, y) {
     const matches = version.match(versionRegex)
-    return matches && parseInt(matches[1], 10) >= x && parseInt(matches[2]) > y
+    return matches && parseInt(matches[1]) >= x && parseInt(matches[2]) > y
 }
 
 export const isHidden_lt_OCP48 = (control, controlData) => {
