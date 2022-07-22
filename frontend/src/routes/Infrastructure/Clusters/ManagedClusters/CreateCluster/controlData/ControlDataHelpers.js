fd427fcf66b9d5a72df140e8644845aeaffcd4d0
Maggie Chen <magchen@redhat.com>
2022-07-22 09:49:18 -0400

    Set default active for credential
    Signed-off-by: Maggie Chen <magchen@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/controlData/ControlDataHelpers.js
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/controlData/ControlDataHelpers.js
@@ -193,6 +193,7 @@ export const setAvailableConnections = (control, secrets) => {
         control.isLoaded = true
     })
     control.available = connections.map((secret) => secret.metadata.name)
+    control.active = control.available[0]
 }
 
 export const setAvailableTemplates = (control, templates) => {
