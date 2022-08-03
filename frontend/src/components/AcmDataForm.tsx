390322679067bd645657592185e42e36e5305ccf
Randy Bruno Piverger <rbrunopi@redhat.com>
2022-08-03 14:02:47 -0700

    Updating implemented linkout icons (#1949)
    * initial changes
    
    Signed-off-by: randybrunopiverger <rbrunopi@redhat.com>
    
    * linter changes
    
    Signed-off-by: randybrunopiverger <rbrunopi@redhat.com>

--- frontend/src/components/AcmDataForm.tsx
+++ frontend/src/components/AcmDataForm.tsx
@@ -1,5 +1,5 @@
 /* Copyright Contributors to the Open Cluster Management project */
-import { AcmButton, AcmIcon, AcmIconVariant, AcmPageHeader } from '../ui-components'
+import { AcmButton, AcmPageHeader } from '../ui-components'
 import {
     ActionGroup,
     ActionList,
@@ -914,13 +914,11 @@ export function AcmDataFormInputs(props: {
                                                     onClick={input.prompt.callback}
                                                 >
                                                     {input.prompt.text}
-                                                    {input.prompt.linkType === LinkType.external ? (
-                                                        <ExternalLinkAltIcon />
-                                                    ) : input.prompt.linkType === LinkType.internalNewTab ? (
-                                                        <AcmIcon
+                                                    {input.prompt.linkType === LinkType.external ||
+                                                    input.prompt.linkType === LinkType.internalNewTab ? (
+                                                        <ExternalLinkAltIcon
                                                             style={{ verticalAlign: '-0.125em', marginLeft: '8px' }}
-                                                            icon={AcmIconVariant.openNewTab}
-                                                        ></AcmIcon>
+                                                        />
                                                     ) : null}
                                                 </AcmButton>
                                             </SplitItem>
@@ -941,13 +939,11 @@ export function AcmDataFormInputs(props: {
                                                     onClick={input.prompt.callback}
                                                 >
                                                     {input.prompt.text}
-                                                    {input.prompt.linkType === LinkType.external ? (
-                                                        <ExternalLinkAltIcon />
-                                                    ) : input.prompt.linkType === LinkType.internalNewTab ? (
-                                                        <AcmIcon
+                                                    {input.prompt.linkType === LinkType.external ||
+                                                    input.prompt.linkType === LinkType.internalNewTab ? (
+                                                        <ExternalLinkAltIcon
                                                             style={{ verticalAlign: '-0.125em', marginLeft: '8px' }}
-                                                            icon={AcmIconVariant.openNewTab}
-                                                        ></AcmIcon>
+                                                        />
                                                     ) : null}
                                                 </AcmButton>
                                             </SplitItem>
