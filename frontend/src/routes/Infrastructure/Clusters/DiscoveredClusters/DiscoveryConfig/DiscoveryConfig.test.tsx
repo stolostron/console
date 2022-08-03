99f9fd7d85728c3e12cb6639f9b04af132e0113c
Erin Murphy <42587690+eemurphy@users.noreply.github.com>
2022-08-03 09:29:36 -0400

    add 4.11 and remove 4.7 from discovery ocp versions (#1935)
    * add 4.11 and remove 4.7 from discovery ocp versions
    
    Signed-off-by: Erin Murphy erinmurp@redhat.com
    
    * edit tests to remove depricated 4.7 ocp version
    
    Signed-off-by: Erin Murphy erinmurp@redhat.com
    
    * edit tests to remove depricated 4.7 ocp version
    
    Signed-off-by: Erin Murphy erinmurp@redhat.com

--- frontend/src/routes/Infrastructure/Clusters/DiscoveredClusters/DiscoveryConfig/DiscoveryConfig.test.tsx
+++ frontend/src/routes/Infrastructure/Clusters/DiscoveredClusters/DiscoveryConfig/DiscoveryConfig.test.tsx
@@ -115,7 +115,7 @@ describe('discovery config page', () => {
         // Select Version
         expect(container.querySelectorAll(`[aria-labelledby^="discoveryVersions-label"]`)).toHaveLength(1)
         container.querySelector<HTMLButtonElement>(`[aria-labelledby^="discoveryVersions-label"]`)!.click()
-        await clickByText('4.7')
+        await clickByText('4.8')
 
         // Submit form
         const createDiscoveryConfigNock = nockCreate(discoveryConfig, discoveryConfig)
@@ -154,7 +154,7 @@ describe('discovery config page', () => {
         await clickByText('30 days')
 
         container.querySelector<HTMLButtonElement>(`[aria-labelledby^="discoveryVersions-label"]`)!.click()
-        await clickByText('4.8')
+        await clickByText('4.9')
 
         const replaceNock = nockReplace(discoveryConfigUpdated)
         await clickByText('Save')
