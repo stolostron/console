664c16252f684b65a2d0af67ddb4181f450bfbd1
Maggie Chen <magchen@redhat.com>
2022-08-05 16:19:49 -0400

    change helper text
    Signed-off-by: Maggie Chen <magchen@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateInfrastructure/CreateControlPlane.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateInfrastructure/CreateControlPlane.tsx
@@ -137,7 +137,7 @@ export function CreateControlPlane() {
         <Fragment>
             <PageHeader
                 title={t('Control plane type')}
-                description={t('Choose a control plane type for your on-premise machine.')}
+                description={t('Next, select a control plane type for your cluster.')}
                 breadcrumbs={breadcrumbs}
             />
             <ItemView
