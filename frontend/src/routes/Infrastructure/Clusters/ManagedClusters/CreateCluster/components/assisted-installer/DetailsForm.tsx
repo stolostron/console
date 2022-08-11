6eae24ce89e05f8fb04f8a11bc216412f6544485
Rastislav Wagner <rawagner@redhat.com>
2022-08-11 14:21:19 +0200

    Prefill form values based on credentials selection (#1983)
    Signed-off-by: Rastislav Wagner <rawagner@redhat.com>
    
    Signed-off-by: Rastislav Wagner <rawagner@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/components/assisted-installer/DetailsForm.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/components/assisted-installer/DetailsForm.tsx
@@ -218,6 +218,15 @@ const DetailsForm: React.FC<DetailsFormProps> = ({ control, handleChange, contro
         handleChange(control)
     }, [additionalLabels])
 
+    useEffect(() => {
+        control.active = {
+            ...control.active,
+            pullSecret: controlProps?.stringData?.pullSecret || '',
+            baseDnsDomain: controlProps?.stringData?.baseDomain || '',
+        }
+        handleChange(control)
+    }, [controlProps?.metadata.uid, controlProps?.stringData?.pullSecret, controlProps?.stringData?.baseDomain])
+
     return clusterImages ? (
         <FeatureGateContextProvider features={ACM_ENABLED_FEATURES}>
             <ACMFeatureSupportLevelProvider clusterImages={clusterImages}>
@@ -226,8 +235,6 @@ const DetailsForm: React.FC<DetailsFormProps> = ({ control, handleChange, contro
                     onValuesChanged={onValuesChanged}
                     clusterImages={clusterImages}
                     usedClusterNames={usedClusterNames}
-                    pullSecret={controlProps?.stringData?.pullSecret}
-                    defaultBaseDomain={controlProps?.stringData?.baseDomain}
                     extensionAfter={extensionAfter}
                 />
             </ACMFeatureSupportLevelProvider>
