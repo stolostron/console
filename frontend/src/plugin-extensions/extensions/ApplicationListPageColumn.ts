6ec812147101d9f34009abce0335cf79b5681311
Gowtham Shanmugasundaram <gshanmug@redhat.com>
2022-08-18 03:02:03 +0530

    ACM plugin extension for Application list page column
    Signed-off-by: Gowtham Shanmugasundaram <gshanmug@redhat.com>

---
+++ frontend/src/plugin-extensions/extensions/ApplicationListPageColumn.ts
@@ -0,0 +1,9 @@
+/* Copyright Contributors to the Open Cluster Management project */
+import { Extension, ExtensionDeclaration } from '@openshift-console/dynamic-plugin-sdk/lib/types'
+import { ApplicationListColumnProps } from '../properties'
+
+export type ApplicationListColumn = ExtensionDeclaration<'acm.application/list/column', ApplicationListColumnProps>
+
+// Type guards
+export const isApplicationListColumn = (e: Extension): e is ApplicationListColumn =>
+    e.type === 'acm.application/list/column'
