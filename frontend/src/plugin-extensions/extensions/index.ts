7f646f50796ad9a966765b3e68d13babc9ce2d95
Gowtham Shanmugasundaram <gshanmug@redhat.com>
2022-08-23 15:59:42 -0400

    ACM plugin extension for Application list page column (#2017)
    Signed-off-by: Gowtham Shanmugasundaram <gshanmug@redhat.com>
    
    Signed-off-by: Gowtham Shanmugasundaram <gshanmug@redhat.com>

--- frontend/src/plugin-extensions/extensions/index.ts
+++ frontend/src/plugin-extensions/extensions/index.ts
@@ -1,2 +1,3 @@
 /* Copyright Contributors to the Open Cluster Management project */
 export * from './ApplicationAction'
+export * from './ApplicationListPageColumn'
---
+++ frontend/src/plugin-extensions/properties/addListColumnProps.ts
@@ -0,0 +1,17 @@
+/* Copyright Contributors to the Open Cluster Management project */
+
+import { ITransform } from '@patternfly/react-table'
+
+export type ApplicationListColumnProps = {
+    /** the header of the column */
+    header: string
+
+    tooltip?: React.ReactNode
+
+    transforms?: ITransform[]
+
+    cellTransforms?: ITransform[]
+
+    /** component type*/
+    cell: React.ComponentType<{ resource?: any }>
+}
