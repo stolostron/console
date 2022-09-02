27b426b349474c2a8c68d81a5da8dd9c96cf431d
Jiri Tomasek <jtomasek@redhat.com>
2022-09-02 10:41:52 +0200

    Add resource counts to ClusterTemplatesPage tabs (#2069)
    - Add hooks for watching ClusterTemplates resources
    - Rename HelmChartRepositoriesPage to HelmChartRepositoriesTab
    
    Signed-off-by: Jiri Tomasek <jtomasek@redhat.com>
    
    Signed-off-by: Jiri Tomasek <jtomasek@redhat.com>

--- frontend/src/routes/Infrastructure/ClusterTemplates/ClusterTemplatesPage/ClusterTemplatesPage.tsx
+++ frontend/src/routes/Infrastructure/ClusterTemplates/ClusterTemplatesPage/ClusterTemplatesPage.tsx
@@ -1,29 +1,39 @@
 /* Copyright Contributors to the Open Cluster Management project */
+import React from 'react';
 import {
   HorizontalNav,
   ListPageCreate,
   ListPageCreateButton,
   ListPageHeader,
+  NavPage,
 } from '@openshift-console/dynamic-plugin-sdk';
 import { useHistory } from 'react-router-dom';
 import ClusterTemplatesTab from './ClusterTemplatesTab';
-import HelmRepositoriesPage from './HelmRepositoriesPage';
-
-const pages = [
-  {
-    href: '',
-    name: 'Templates',
-    component: ClusterTemplatesTab,
-  },
-  {
-    href: 'repositories',
-    name: 'HELM repositories',
-    component: HelmRepositoriesPage,
-  },
-];
+import HelmRepositoriesTab from './HelmRepositoriesTab';
+import { useClusterTemplatesCount } from '../hooks/useClusterTemplates';
+import { useHelmRepositoriesCount } from '../hooks/useHelmRepositories';
+import { getNavLabelWithCount } from '../utils';
 
 const ClusterTemplatesPage = () => {
   const history = useHistory();
+  const templatesCount = useClusterTemplatesCount();
+  const helmRepositoriesCount = useHelmRepositoriesCount();
+
+  const pages: NavPage[] = React.useMemo(
+    () => [
+      {
+        href: '',
+        name: getNavLabelWithCount('Templates', templatesCount),
+        component: ClusterTemplatesTab,
+      },
+      {
+        href: 'repositories',
+        name: getNavLabelWithCount('HELM repositories', helmRepositoriesCount),
+        component: HelmRepositoriesTab,
+      },
+    ],
+    [templatesCount, helmRepositoriesCount],
+  );
 
   const repositoriesPage = history.location.pathname.endsWith('/repositories');
   return (
