8165dd5590724b4b1b65df60ef03338aeb756b01
John Swanke <jswanke@redhat.com>
2022-09-28 15:42:54 -0400

    fix
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- frontend/src/lib/test-logging.ts
+++ frontend/src/lib/test-logging.ts
@@ -1,6 +1,6 @@
-/* Copyright Contributors to the Open Cluster Management project */
-
 /* istanbul ignore file */
+
+/* Copyright Contributors to the Open Cluster Management project */
 
 import React from 'react'
 
