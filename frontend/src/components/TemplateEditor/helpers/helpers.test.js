78e4290eb826797d7f971e41dc0bd1925c21afef
Kevin Cormier <kcormier@redhat.com>
2022-10-11 14:08:58 -0400

    Exclude folder and resource pool if not set
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/components/TemplateEditor/helpers/helpers.test.js
+++ frontend/src/components/TemplateEditor/helpers/helpers.test.js
@@ -4,6 +4,7 @@ import { if_eqFn } from './if_eq'
 import { if_gtFn } from './if_gt'
 import { if_neFn } from './if_ne'
 import { if_orFn } from './if_or'
+import { if_truthyFn } from './if_truthy'
 
 const opts = {
     fn: () => {
@@ -38,5 +39,17 @@ describe('handlebar helper tests', () => {
     })
     it('or false', () => {
         expect(if_orFn(undefined, undefined, opts)).toBe(false)
+    })
+    it('truthy true', () => {
+        expect(if_truthyFn('non-empty', opts)).toBe(true)
+    })
+    it('truthy false', () => {
+        expect(if_truthyFn('', opts)).toBe(false)
+    })
+    it('truthy true array', () => {
+        expect(if_truthyFn(['non-empty'], opts)).toBe(true)
+    })
+    it('truthy false array', () => {
+        expect(if_truthyFn([''], opts)).toBe(false)
     })
 })
