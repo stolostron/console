cc8711e1147cc7bb88b46fd451926090333ea8d9
randybrunopiverger <rbrunopi@redhat.com>
2022-10-14 15:09:50 -0700

    WIP
    api resource path, backend
    
    Co-authored-by: Kevin Cormier <kcormier@redhat.com>
    Signed-off-by: randybrunopiverger <rbrunopi@redhat.com>

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,102 +1,105 @@
 /* Copyright Contributors to the Open Cluster Management project */
 
 /* istanbul ignore file */
-import Router from 'find-my-way'
-import { Http2Server, Http2ServerRequest, Http2ServerResponse } from 'http2'
-import { loadSettings, stopSettingsWatch } from './lib/config'
-import { cors } from './lib/cors'
-import { delay } from './lib/delay'
-import { logger, stopLogger } from './lib/logger'
-import { startLoggingMemory } from './lib/memory'
-import { notFound, respondInternalServerError, respondOK } from './lib/respond'
-import { startServer, stopServer } from './lib/server'
-import { ServerSideEvents } from './lib/server-side-events'
-import { ansibleTower } from './routes/ansibletower'
-import { authenticated } from './routes/authenticated'
-import { configure } from './routes/configure'
-import { consoleLinks } from './routes/consoleLinks'
-import { events, startWatching, stopWatching } from './routes/events'
-import { liveness } from './routes/liveness'
-import { mchVersion } from './routes/mchVersion'
-import { login, loginCallback, logout } from './routes/oauth'
-import { proxy } from './routes/proxy'
-import { readiness } from './routes/readiness'
-import { search } from './routes/search'
-import { serve } from './routes/serve'
-import { username } from './routes/username'
+import Router from "find-my-way";
+import { Http2Server, Http2ServerRequest, Http2ServerResponse } from "http2";
+import { loadSettings, stopSettingsWatch } from "./lib/config";
+import { cors } from "./lib/cors";
+import { delay } from "./lib/delay";
+import { logger, stopLogger } from "./lib/logger";
+import { startLoggingMemory } from "./lib/memory";
+import { notFound, respondInternalServerError, respondOK } from "./lib/respond";
+import { startServer, stopServer } from "./lib/server";
+import { ansibleTower } from "./routes/ansibletower";
+import { apiPaths } from "./routes/apiPaths";
+import { authenticated } from "./routes/authenticated";
+import { configure } from "./routes/configure";
+import { consoleLinks } from "./routes/consoleLinks";
+import { startWatching, stopWatching } from "./resources/watch";
+import { liveness } from "./routes/liveness";
+import { mchVersion } from "./routes/mchVersion";
+import { login, loginCallback, logout } from "./routes/oauth";
+import { proxy } from "./routes/proxy";
+import { readiness } from "./routes/readiness";
+import { search } from "./routes/search";
+import { serve } from "./routes/serve";
+import { username } from "./routes/username";
 
 // Router defaults to max param length of 100 - We need to override to 500 to handle resources with very long names
 // If the route exceeds 500 chars the route will not be found from this fn: router.find()
-export const router = Router<Router.HTTPVersion.V2>({ maxParamLength: 500 })
-router.get(`/readinessProbe`, readiness)
-router.get(`/livenessProbe`, liveness)
-router.get(`/ping`, respondOK)
-router.all(`/api`, proxy)
-router.all(`/api/*`, proxy)
-router.all(`/apis`, proxy)
-router.all(`/apis/*`, proxy)
-router.all(`/version`, proxy)
-router.all(`/version/`, proxy)
-router.get(`/login`, login)
-router.get(`/login/callback`, loginCallback)
-router.get(`/logout`, logout)
-router.get(`/logout/`, logout)
-router.get(`/events`, events)
-router.post(`/proxy/search`, search)
-router.get(`/authenticated`, authenticated)
-router.post(`/ansibletower`, ansibleTower)
-router.get(`/*`, serve)
-router.get('/configure', configure)
-router.get('/console-links', consoleLinks)
-router.get('/username', username)
-router.get('/mchVersion', mchVersion)
+export const router = Router<Router.HTTPVersion.V2>({ maxParamLength: 500 });
+router.get(`/readinessProbe`, readiness);
+router.get(`/livenessProbe`, liveness);
+router.get(`/ping`, respondOK);
+router.all(`/api`, proxy);
+router.all(`/api/*`, proxy);
+router.all(`/apis`, proxy);
+router.all(`/apis/*`, proxy);
+router.all(`/apiPaths`, apiPaths);
+router.all(`/version`, proxy);
+router.all(`/version/`, proxy);
+router.get(`/login`, login);
+router.get(`/login/callback`, loginCallback);
+router.get(`/logout`, logout);
+router.get(`/logout/`, logout);
+router.post(`/proxy/search`, search);
+router.get(`/authenticated`, authenticated);
+router.post(`/ansibletower`, ansibleTower);
+router.get(`/*`, serve);
+router.get("/configure", configure);
+router.get("/console-links", consoleLinks);
+router.get("/username", username);
+router.get("/mchVersion", mchVersion);
 
-export async function requestHandler(req: Http2ServerRequest, res: Http2ServerResponse): Promise<void> {
-    if (process.env.NODE_ENV !== 'production') {
-        if (cors(req, res)) return
-        await delay(req, res)
-    }
+export async function requestHandler(
+  req: Http2ServerRequest,
+  res: Http2ServerResponse
+): Promise<void> {
+  if (process.env.NODE_ENV !== "production") {
+    if (cors(req, res)) return;
+    await delay(req, res);
+  }
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
-    if (req.url === '/multicloud') (req as any).url = '/'
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
-    else if (req.url.startsWith('/multicloud')) (req as any).url = req.url.substr(11)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+  if (req.url === "/multicloud") (req as any).url = "/";
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+  else if (req.url.startsWith("/multicloud"))
+    (req as any).url = req.url.substr(11);
 
-    const route = router.find(req.method as Router.HTTPMethod, req.url)
-    if (!route) {
-        logger.warn({ msg: 'route not found', url: req.url })
-        return notFound(req, res)
-    }
+  const route = router.find(req.method as Router.HTTPMethod, req.url);
+  if (!route) {
+    logger.warn({ msg: "route not found", url: req.url });
+    return notFound(req, res);
+  }
 
-    try {
-        const result: unknown = route.handler(req, res, route.params, route.store)
-        if (result instanceof Promise) await result
-    } catch (err) {
-        logger.error(err)
-        if (!res.headersSent) return respondInternalServerError(req, res)
-    }
+  try {
+    const result: unknown = route.handler(req, res, route.params, route.store);
+    if (result instanceof Promise) await result;
+  } catch (err) {
+    logger.error(err);
+    if (!res.headersSent) return respondInternalServerError(req, res);
+  }
 }
 
 export function start(): Promise<Http2Server | undefined> {
-    loadSettings()
-    startWatching()
-    return startServer({ requestHandler })
+  loadSettings();
+  startWatching();
+  return startServer({ requestHandler });
 }
 
 export async function stop(): Promise<void> {
-    if (process.env.NODE_ENV === 'development') {
-        setTimeout(() => {
-            logger.warn('process stop timeout. exiting...')
-            process.exit(1)
-        }, 0.5 * 1000).unref()
-    }
-    stopSettingsWatch()
-    await ServerSideEvents.dispose()
-    stopWatching()
-    await stopServer()
-    stopLogger()
+  if (process.env.NODE_ENV === "development") {
+    setTimeout(() => {
+      logger.warn("process stop timeout. exiting...");
+      process.exit(1);
+    }, 0.5 * 1000).unref();
+  }
+  stopSettingsWatch();
+  stopWatching();
+  await stopServer();
+  stopLogger();
 }
 
-if (process.env.LOG_MEMORY === 'true') {
-    startLoggingMemory()
+if (process.env.LOG_MEMORY === "true") {
+  startLoggingMemory();
 }
---
+++ backend/src/routes/apiPaths.ts
@@ -0,0 +1,73 @@
+/* Copyright Contributors to the Open Cluster Management project */
+
+import { constants, Http2ServerRequest, Http2ServerResponse } from "http2";
+import { jsonRequest } from "../lib/json-request";
+import { logger } from "../lib/logger";
+import { respondInternalServerError, unauthorized } from "../lib/respond";
+import { getToken, isAuthenticated } from "../lib/token";
+import { getServiceAccountToken } from "./liveness";
+
+export async function apiPaths(
+  req: Http2ServerRequest,
+  res: Http2ServerResponse
+): Promise<void> {
+  const token = getToken(req);
+  if (!token) return unauthorized(req, res);
+  const serviceAccountToken = getServiceAccountToken();
+
+  try {
+    const authResponse = await isAuthenticated(token);
+    if (authResponse.status === constants.HTTP_STATUS_OK) {
+      const paths = await jsonRequest<unknown>(
+        process.env.CLUSTER_API_URL + "/",
+        serviceAccountToken
+      ).then(async (response) => {
+        // filter...
+        // TODO: determine if will we fetch all the endpoint with /apis/<Version>
+        // or fetch one by one with additional perameters to guide search
+
+        // /api/<VERSION>
+        // /apis/<GROUP>
+        // /apis/<GROUP>/<VERSION>
+
+        const apiResourceLists = await Promise.all(
+          response.paths
+            .filter((path) => {
+              const pathArray = path.substring(1).split("/");
+              return (
+                pathArray.length &&
+                ((pathArray[0] === "api" && pathArray.length === 2) ||
+                  (pathArray[0] === "apis" && pathArray.length === 3))
+              );
+            })
+            .map(async (path) => {
+              return jsonRequest<unknown>(
+                process.env.CLUSTER_API_URL + path,
+                serviceAccountToken
+              );
+            })
+        );
+        return apiResourceLists;
+      });
+
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(paths));
+    } else {
+      res.writeHead(authResponse.status).end();
+      void authResponse.blob();
+    }
+  } catch (err) {
+    logger.error(err);
+    console.log(err);
+    respondInternalServerError(req, res);
+  }
+}
+
+function buildPathObject(jsonResponse: unknown) {
+  // groupVersion
+  // name:
+  // singular:
+
+  const jsonBody = {};
+  jsonRequest;
+}
