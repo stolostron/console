c70b51b9f35c2377865d691e9b7a02887a77a931
rhowingt@redhat.com <rhowingt@redhat.com>
2022-10-26 20:19:01 -0400

    init commit string externalization
    Signed-off-by: rhowingt@redhat.com <rhowingt@redhat.com>

---
+++ frontend/src/lib/wizardStrings.ts
@@ -0,0 +1,33 @@
+/* Copyright Contributors to the Open Cluster Management project */
+
+import { WizardStrings } from '@patternfly-labs/react-form-wizard'
+
+export const wizardStrings: WizardStrings = {
+    reviewLabel: 'Review',
+    unknownError: 'Unknown Error',
+    errorString: 'error',
+    stepsAriaLabel: 'steps',
+    contentAriaLabel: 'content',
+    actionAriaLabel: 'Action',
+    detailsAriaLabel: 'Details',
+    sortableMoveItemUpAriaLabel: 'Move item up',
+    sortableMoveItemDownAriaLabel: 'Move item down',
+    removeItemAriaLabel: 'Remove item',
+    deselectAllAriaLabel: 'Deselect all',
+    selectAllAriaLabel: 'Select all',
+    clearButtonTooltip: 'Clear',
+    pasteButtonTooltip: 'Paste',
+    backButtonText: 'Back',
+    cancelButtonText: 'Cancel',
+    nextButtonText: 'Next',
+    fixValidationErrorsMsg: 'Please fix validation errors',
+    submitText: 'Submit',
+    submittingText: 'Submitting',
+    moreInfo: 'More info',
+    hideSecretTooltip: 'Hide secret',
+    showSecretTooltip: 'Show secret',
+    spinnerButtonTooltip: 'Loading',
+    syncButtonTooltip: 'Refresh',
+    required: 'Required',
+    expandToFixValidationErrors: 'Expand to fix validation errors',
+}
