9d77be90334af9f5d753208a75262a497da82db7
Kevin Cormier <kcormier@redhat.com>
2022-11-02 01:27:23 -0400

    Maintain compatibility with OCP 4.10 (#2227)
    * Use custom extension to share context
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Workaround for 4.10 (no custom extensions)
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Test coverage
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

---
+++ frontend/src/lib/SharedContext.ts
@@ -0,0 +1,28 @@
+/* Copyright Contributors to the Open Cluster Management project */
+import { Context } from 'react'
+import { CodeRef, Extension, ExtensionDeclaration } from '@openshift-console/dynamic-plugin-sdk/lib/types'
+
+/** Add to MCE extensions in next release */
+// {
+//     "type": "acm.shared-context",
+//     "properties": {
+//         "id": "mce-data-context",
+//         "context": { "$codeRef": "context.PluginDataContext" },
+//     }
+// },
+
+/** Enables sharing of a React context across plugins. */
+export type SharedContext<T = any> = ExtensionDeclaration<
+    'acm.shared-context',
+    {
+        /** Unique identifier for this item. */
+        id: string
+        /** Context reference for sharing across plugins. */
+        context: CodeRef<Context<T>>
+    }
+>
+
+// Type guards
+
+export const isSharedContext = (e: Extension): e is SharedContext =>
+    e.type === 'acm.shared-context' && e.properties.id && e.properties.context
