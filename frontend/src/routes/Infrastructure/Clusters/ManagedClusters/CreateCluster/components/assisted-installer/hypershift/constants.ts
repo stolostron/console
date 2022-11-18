f4d5b7bd123d49b4a6aadad596a0c608e711ff92
Marek Libra <marek.libra@gmail.com>
2022-11-18 08:18:42 +0100

    Do not obfuscate default CIDRs
    Signed-off-by: Marek Libra <mlibra@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/components/assisted-installer/hypershift/constants.ts
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/components/assisted-installer/hypershift/constants.ts
@@ -1,3 +1,3 @@
 /* Copyright Contributors to the Open Cluster Management project */
-export const defaultServiceCIDR = '172d31d0d0/16'.replaceAll('d', '.')
-export const defaultPodCIDR = '10d132d0d0/14'.replaceAll('d', '.')
+export const defaultServiceCIDR = '172.31.0.0/16'
+export const defaultPodCIDR = '10.132.0.0/14'
