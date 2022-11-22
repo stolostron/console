7cd2e6ccc9ca032a05c564c7efd9185f8fc25979
Zack Layne <zlayne@redhat.com>
2022-11-21 20:58:09 -0500

    Update YAML page to match with OCP (#2291)
    * Update YAML page to match with OCP
    
    Signed-off-by: Zack Layne <zlayne@redhat.com>
    
    * update tests
    
    Signed-off-by: Zack Layne <zlayne@redhat.com>
    
    * more tests
    
    Signed-off-by: Zack Layne <zlayne@redhat.com>
    
    * update editor test
    
    Signed-off-by: Zack Layne <zlayne@redhat.com>
    
    Signed-off-by: Zack Layne <zlayne@redhat.com>

--- frontend/src/components/YamlEditor.tsx
+++ frontend/src/components/YamlEditor.tsx
@@ -1,13 +1,12 @@
 /* Copyright Contributors to the Open Cluster Management project */
-import { CodeEditor, Language } from '@patternfly/react-code-editor'
 import { global_BackgroundColor_dark_100 } from '@patternfly/react-tokens'
-import useResizeObserver from '@react-hook/resize-observer'
 import jsYaml from 'js-yaml'
 import { debounce } from 'lodash'
 import 'monaco-editor/esm/vs/basic-languages/yaml/yaml.contribution.js'
 import 'monaco-editor/esm/vs/editor/editor.all.js'
 import * as monacoEditor from 'monaco-editor/esm/vs/editor/editor.api'
-import { useCallback, useEffect, useRef } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import MonacoEditor, { monaco } from 'react-monaco-editor'
 import './YAMLEditor.css'
 
 /**
@@ -24,49 +23,86 @@ export const findResourceFieldLineNumber = (resourceYaml: any, fieldPath: string
 }
 
 export default function YAMLEditor(props: {
-    resourceYAML: any
-    editMode: boolean
-    width: string
-    height?: string
-    setEditedResourceYaml?: React.Dispatch<React.SetStateAction<string>>
+    resourceYAML: string
+    readOnly: boolean
+    height: number // in pixels - to be convested to string in memo hook
+    setResourceYaml?: React.Dispatch<React.SetStateAction<string>>
     defaultScrollToLine?: number
 }) {
-    const { resourceYAML, editMode, setEditedResourceYaml, width, height, defaultScrollToLine } = props
-    const pageRef = useRef(null)
+    const { resourceYAML, readOnly, height, setResourceYaml, defaultScrollToLine } = props
     const editorRef = useRef<monacoEditor.editor.IStandaloneCodeEditor | null>(null)
     const monacoRef = useRef<typeof monacoEditor | null>(null)
+    const [hasManagedFieldsFolded, setHasManagedFieldsFolded] = useState<boolean>(false)
 
-    useResizeObserver(pageRef, (entry) => {
-        const { width } = entry.contentRect
-        const { height } = entry.contentRect
-        editorRef?.current?.layout({ width, height })
-    })
+    const editorHeight: string = useMemo(() => {
+        return height < 100 ? '100px' : `${height}px`
+    }, [height])
 
     useEffect(() => {
+        if (process.env.NODE_ENV === 'test') return
+        /* istanbul ignore if */
         if (resourceYAML && defaultScrollToLine) {
             editorRef.current?.revealLineNearTop(defaultScrollToLine)
         }
-    }, [resourceYAML, editorRef, defaultScrollToLine])
+    }, [resourceYAML, defaultScrollToLine])
 
+    // By default we will collapse the managedFields section
+    useEffect(() => {
+        let managedFieldsStart = 0
+        let managedFieldsEnd = 0
+        process.env.NODE_ENV !== 'test' && editorRef.current?.setSelection(new monaco.Range(1, 0, 1, 0))
+        if (resourceYAML && !hasManagedFieldsFolded) {
+            const resourceLines = resourceYAML.split('\n')
+            resourceLines.forEach((line, i) => {
+                if (line === '  managedFields:') {
+                    managedFieldsStart = i + 1
+                } else if (managedFieldsStart > 0 && managedFieldsEnd === 0 && (line[2] !== ' ' || line[0] !== ' ')) {
+                    managedFieldsEnd = i
+                }
+            })
+            /* istanbul ignore if */
+            if (managedFieldsStart > 0 && managedFieldsEnd > 0) {
+                if (process.env.NODE_ENV === 'test') return
+                const top = editorRef.current?.getScrollTop()
+                editorRef.current?.setSelection(new monaco.Range(managedFieldsStart, 0, managedFieldsEnd, 0))
+                editorRef.current
+                    ?.getAction('editor.fold')
+                    .run()
+                    .then(() => {
+                        if (defaultScrollToLine) {
+                            editorRef.current?.revealLineNearTop(defaultScrollToLine)
+                        } else {
+                            editorRef.current?.setScrollTop(Math.abs(top ?? 0))
+                        }
+                        setHasManagedFieldsFolded(true)
+                    })
+                    .catch(() => console.error('Encountered an error while trying to fold the ManagedFields section.'))
+            }
+        }
+    }, [resourceYAML, defaultScrollToLine, hasManagedFieldsFolded])
+
+    /* istanbul ignore next */
     function onEditorDidMount(editor: monacoEditor.editor.IStandaloneCodeEditor, monaco: typeof monacoEditor) {
-        // create 'console' theme
-        monaco.editor.defineTheme('console', {
-            base: 'vs-dark',
-            inherit: true,
-            rules: [
-                // avoid pf tokens for `rules` since tokens are opaque strings that might not be hex values
-                { token: 'number', foreground: 'ace12e' },
-                { token: 'type', foreground: '73bcf7' },
-                { token: 'string', foreground: 'f0ab00' },
-                { token: 'keyword', foreground: 'cbc0ff' },
-            ],
-            colors: {
-                'editor.background': global_BackgroundColor_dark_100.value,
-                'editorGutter.background': '#292e34', // no pf token defined
-                'editorLineNumber.activeForeground': '#fff',
-                'editorLineNumber.foreground': '#f0f0f0',
-            },
-        })
+        if (process.env.NODE_ENV !== 'test') {
+            monaco.editor.defineTheme('console', {
+                base: 'vs-dark',
+                inherit: true,
+                rules: [
+                    // avoid pf tokens for `rules` since tokens are opaque strings that might not be hex values
+                    { token: 'number', foreground: 'ace12e' },
+                    { token: 'type', foreground: '73bcf7' },
+                    { token: 'string', foreground: 'f0ab00' },
+                    { token: 'keyword', foreground: 'cbc0ff' },
+                ],
+                colors: {
+                    'editor.background': global_BackgroundColor_dark_100.value,
+                    'editorGutter.background': '#292e34', // no pf token defined
+                    'editorLineNumber.activeForeground': '#fff',
+                    'editorLineNumber.foreground': '#f0f0f0',
+                },
+            })
+            monaco.editor.setTheme('console')
+        }
         editor.changeViewZones(
             (changeAccessor: {
                 addZone: (arg0: { afterLineNumber: number; heightInPx: number; domNode: HTMLDivElement }) => void
@@ -79,7 +115,6 @@ export default function YAMLEditor(props: {
                 })
             }
         )
-        monaco.editor.setTheme('console')
         editorRef.current = editor
         monacoRef.current = monaco
     }
@@ -88,24 +123,26 @@ export default function YAMLEditor(props: {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     const onChange = useCallback(
         debounce((value) => {
-            setEditedResourceYaml && setEditedResourceYaml(value)
+            setResourceYaml && setResourceYaml(value)
         }, 100),
         []
     )
 
     return (
-        <div ref={pageRef}>
-            <CodeEditor
-                width={width}
-                height={height ?? '100%'}
-                code={resourceYAML}
-                onChange={onChange}
-                language={Language.yaml}
-                onEditorDidMount={onEditorDidMount}
-                isReadOnly={!editMode}
-                isLineNumbersVisible={true}
-                isMinimapVisible={true}
+        <div
+            style={{
+                minHeight: '100px',
+                flex: 1,
+                position: 'relative',
+            }}
+        >
+            <MonacoEditor
+                language="yaml"
+                theme="console"
+                height={editorHeight}
+                value={resourceYAML}
                 options={{
+                    readOnly,
                     wordWrap: 'wordWrapColumn',
                     wordWrapColumn: 132,
                     scrollBeyondLastLine: true,
@@ -117,6 +154,8 @@ export default function YAMLEditor(props: {
                         horizontalScrollbarSize: 17,
                     },
                 }}
+                editorDidMount={onEditorDidMount}
+                onChange={onChange}
             />
         </div>
     )
