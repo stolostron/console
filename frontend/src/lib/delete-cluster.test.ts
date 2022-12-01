8bb88e8a825353e746f5c0858b8904e73d25ca52
Feng Xiang <fxiang@redhat.com>
2022-12-01 11:53:09 -0500

    Add more test coverage
    Signed-off-by: Feng Xiang <fxiang@redhat.com>

---
+++ frontend/src/lib/delete-cluster.test.ts
@@ -0,0 +1,103 @@
+/* Copyright Contributors to the Open Cluster Management project */
+
+import { detachCluster } from './delete-cluster'
+import { Cluster, ClusterStatus } from '../resources'
+import { nockIgnoreApiPaths } from './nock-util'
+
+const mockCluster1: Cluster = {
+    name: 'hypershift-cluster1',
+    displayName: 'hypershift-cluster1',
+    namespace: 'clusters',
+    uid: 'hypershift-cluster1-uid',
+    provider: undefined,
+    status: ClusterStatus.ready,
+    distribution: {
+        ocp: {
+            version: '4.11.12',
+            availableUpdates: [],
+            desiredVersion: '4.11.12',
+            upgradeFailed: false,
+        },
+        isManagedOpenShift: false,
+    },
+    labels: { abc: '123' },
+    nodes: undefined,
+    kubeApiServer: '',
+    consoleURL: '',
+    hive: {
+        isHibernatable: true,
+        clusterPool: undefined,
+        secrets: {
+            installConfig: '',
+        },
+    },
+    hypershift: {
+        agent: false,
+        hostingNamespace: 'clusters',
+        nodePools: [],
+        secretNames: ['feng-hs-bug-ssh-key', 'feng-hs-bug-pull-secret'],
+    },
+    isHive: false,
+    isManaged: true,
+    isCurator: true,
+    isHostedCluster: true,
+    isHypershift: true,
+    isSNOCluster: false,
+    owner: {},
+    kubeadmin: '',
+    kubeconfig: '',
+    isRegionalHubCluster: false,
+}
+
+const mockCluster2: Cluster = {
+    name: 'hypershift-cluster1',
+    displayName: 'hypershift-cluster1',
+    namespace: 'clusters',
+    uid: 'hypershift-cluster1-uid',
+    provider: undefined,
+    status: ClusterStatus.ready,
+    distribution: {
+        ocp: {
+            version: '4.11.12',
+            availableUpdates: [],
+            desiredVersion: '4.11.12',
+            upgradeFailed: false,
+        },
+        isManagedOpenShift: false,
+    },
+    labels: { abc: '123' },
+    nodes: undefined,
+    kubeApiServer: '',
+    consoleURL: '',
+    hive: {
+        isHibernatable: true,
+        clusterPool: undefined,
+        secrets: {
+            installConfig: '',
+        },
+    },
+    isHive: false,
+    isManaged: true,
+    isCurator: true,
+    isHostedCluster: true,
+    isHypershift: true,
+    isSNOCluster: false,
+    owner: {},
+    kubeadmin: '',
+    kubeconfig: '',
+    isRegionalHubCluster: false,
+}
+
+describe('detachCluster normal operation', () => {
+    nockIgnoreApiPaths()
+    it('detachCluster normal operation', () => {
+        detachCluster(mockCluster1)
+    })
+})
+
+describe('detachCluster no hypershift', () => {
+    nockIgnoreApiPaths()
+    it('detachCluster no hypershift', () => {
+        detachCluster(mockCluster2)
+    })
+})
