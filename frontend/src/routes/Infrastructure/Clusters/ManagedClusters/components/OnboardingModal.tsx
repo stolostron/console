11c1933df1f231a34f829076947fd2df38aeeed5
Feng Xiang <fxiang@redhat.com>
2022-12-12 16:21:32 -0500

    Fix unit test
    Signed-off-by: Feng Xiang <fxiang@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/components/OnboardingModal.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/components/OnboardingModal.tsx
@@ -8,7 +8,7 @@ import { AcmExpandableSection, AcmModal } from '../../../../../ui-components'
 import MainIcon from './OnboardingModalIcon.svg'
 import { ImportIcon, CloudTenantIcon, ConnectedIcon, ExternalLinkAltIcon } from '@patternfly/react-icons'
 import { NavigationPath } from '../../../../../NavigationPath'
-import './OnboardingModal.css'
+// import './OnboardingModal.css'
 import { DOC_BASE_PATH } from '../../../../../lib/doc-util'
 import { Cluster } from '../../../../../resources'
 
@@ -49,94 +49,99 @@ export function OnboardingModal(props: IOnboardingModalProps) {
     }
 
     return (
-        <AcmModal
-            variant={ModalVariant.medium}
-            title=" "
-            id="clustersOnboardingModal"
-            ouiaId="clustersOnboardingModal"
-            isOpen={true}
-            onClose={props.close}
-            footer={
-                <AcmExpandableSection
-                    label={t('Want to learn more?')}
-                    style={{
-                        backgroundColor: '#F0F0F0',
-                        width: '100%',
-                        height: '100%',
-                        padding: '16px 16px 16px 36px',
-                    }}
-                >
-                    <div>
-                        <Link key="gettingstarted" to={`${NavigationPath.managedClusters}?quickstart=host-inventory`}>
-                            {t('Getting Started with on-premise host inventory')}
-                        </Link>
-                    </div>
-                    <div>
-                        <Text
-                            component={TextVariants.a}
-                            isVisitedLink
-                            href={DOC_BASE_PATH}
-                            target="_blank"
-                            style={{
-                                cursor: 'pointer',
-                                display: 'inline-block',
-                            }}
-                        >
-                            {t('Learn more about Red Hat Advanced Cluster Management for Kubernetes')}{' '}
-                            <ExternalLinkAltIcon />
-                        </Text>
-                    </div>
-                </AcmExpandableSection>
-            }
-        >
-            <Grid>
-                <GridItem span={3}>
-                    <MainIcon />
-                </GridItem>
-                <GridItem span={9}>
-                    <div>
-                        <span style={{ color: '#393F44', fontSize: '24px' }}>
-                            <Trans
-                                i18nKey="Managing clusters <bold>just got easier</bold>"
-                                components={{ bold: <strong /> }}
-                            />
-                        </span>
-                    </div>
-                    <div style={{ color: '#6A6E73', fontSize: '14px', paddingTop: '8px' }}>
-                        {t(
-                            'Create and manage a fleet of clusters with ease using this all clusters view. To access a single cluster you can select it from the cluster list table.'
-                        )}
-                    </div>
-                    <div style={{ color: '#151515', fontSize: '16px', paddingTop: '48px' }}>
-                        {t('How would you like to create your cluster?')}
-                    </div>
-                </GridItem>
-            </Grid>
-            <div style={{ paddingTop: '24px' }} />
-            <Grid hasGutter>
-                {cards.map((card) => (
-                    <GridItem key={card.id} span={4}>
-                        <Link key={card.id} to={card.link} style={{ color: 'inherit', textDecoration: 'none' }}>
-                            <Card id={card.id} key={card.id} isSelectable isFlat>
-                                <CardBody style={{ minHeight: '160px', borderColor: '#D2D2D2', color: '#151515' }}>
-                                    <div
-                                        style={{
-                                            position: 'absolute',
-                                            top: '50%',
-                                            left: '50%',
-                                            transform: 'translate(-50%, -50%)',
-                                        }}
-                                    >
-                                        <div style={{ textAlign: 'center' }}>{card.icon}</div>
-                                        <div style={{ textAlign: 'center' }}>{card.text}</div>
-                                    </div>
-                                </CardBody>
-                            </Card>
-                        </Link>
+        <div className="onboardingmodalclass">
+            <AcmModal
+                variant={ModalVariant.medium}
+                title=" "
+                id="clustersOnboardingModal"
+                ouiaId="clustersOnboardingModal"
+                isOpen={true}
+                onClose={props.close}
+                footer={
+                    <AcmExpandableSection
+                        label={t('Want to learn more?')}
+                        style={{
+                            backgroundColor: '#F0F0F0',
+                            width: '100%',
+                            height: '100%',
+                            padding: '16px 16px 16px 36px',
+                        }}
+                    >
+                        <div>
+                            <Link
+                                key="gettingstarted"
+                                to={`${NavigationPath.managedClusters}?quickstart=host-inventory`}
+                            >
+                                {t('Getting Started with on-premise host inventory')}
+                            </Link>
+                        </div>
+                        <div>
+                            <Text
+                                component={TextVariants.a}
+                                isVisitedLink
+                                href={DOC_BASE_PATH}
+                                target="_blank"
+                                style={{
+                                    cursor: 'pointer',
+                                    display: 'inline-block',
+                                }}
+                            >
+                                {t('Learn more about Red Hat Advanced Cluster Management for Kubernetes')}{' '}
+                                <ExternalLinkAltIcon />
+                            </Text>
+                        </div>
+                    </AcmExpandableSection>
+                }
+            >
+                <Grid>
+                    <GridItem span={3}>
+                        <MainIcon />
                     </GridItem>
-                ))}
-            </Grid>
-            <div style={{ paddingTop: '24px' }} />
-        </AcmModal>
+                    <GridItem span={9}>
+                        <div>
+                            <span style={{ fontSize: '24px' }}>
+                                <Trans
+                                    i18nKey="Managing clusters <bold>just got easier</bold>"
+                                    components={{ bold: <strong /> }}
+                                />
+                            </span>
+                        </div>
+                        <div style={{ color: '#6A6E73', fontSize: '14px', paddingTop: '8px' }}>
+                            {t(
+                                'Create and manage a fleet of clusters with ease using this all clusters view. To access a single cluster you can select it from the cluster list table.'
+                            )}
+                        </div>
+                        <div style={{ color: '#151515', fontSize: '16px', paddingTop: '48px' }}>
+                            {t('How would you like to create your cluster?')}
+                        </div>
+                    </GridItem>
+                </Grid>
+                <div style={{ paddingTop: '24px' }} />
+                <Grid hasGutter>
+                    {cards.map((card) => (
+                        <GridItem key={card.id} span={4}>
+                            <Link key={card.id} to={card.link} style={{ color: 'inherit', textDecoration: 'none' }}>
+                                <Card id={card.id} key={card.id} isSelectable isFlat>
+                                    <CardBody style={{ minHeight: '160px', borderColor: '#D2D2D2', color: '#151515' }}>
+                                        <div
+                                            style={{
+                                                position: 'absolute',
+                                                top: '50%',
+                                                left: '50%',
+                                                transform: 'translate(-50%, -50%)',
+                                            }}
+                                        >
+                                            <div style={{ textAlign: 'center' }}>{card.icon}</div>
+                                            <div style={{ textAlign: 'center' }}>{card.text}</div>
+                                        </div>
+                                    </CardBody>
+                                </Card>
+                            </Link>
+                        </GridItem>
+                    ))}
+                </Grid>
+                <div style={{ paddingTop: '24px' }} />
+            </AcmModal>
+        </div>
     )
 }
