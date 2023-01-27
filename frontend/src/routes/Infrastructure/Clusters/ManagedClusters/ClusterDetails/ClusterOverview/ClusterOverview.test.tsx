d9ba68178e514c6aa08da8f17a0643cacdfe26d4
Omar Farag <ofarag@redhat.com>
2023-01-27 15:18:53 -0500

    Added unit test
    Signed-off-by: Omar Farag <ofarag@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/ClusterDetails/ClusterOverview/ClusterOverview.test.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/ClusterDetails/ClusterOverview/ClusterOverview.test.tsx
@@ -1,6 +1,6 @@
 /* Copyright Contributors to the Open Cluster Management project */
 
-import { render } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
 import {
@@ -21,8 +21,15 @@ import {
 } from '../../../../../../atoms'
 import { nockGet, nockIgnoreApiPaths, nockIgnoreRBAC, nockSearch } from '../../../../../../lib/nock-util'
 import { clickByText, waitForText } from '../../../../../../lib/test-util'
-import { Secret, SecretApiVersion, SecretKind } from '../../../../../../resources'
+import {
+  HostedClusterApiVersion,
+  HostedClusterKind,
+  Secret,
+  SecretApiVersion,
+  SecretKind,
+} from '../../../../../../resources'
 import { ClusterContext } from '../ClusterDetails'
+import { HypershiftImportCommand } from '../../components/HypershiftImportCommand'
 import {
   mockAWSHostedCluster,
   mockAWSHypershiftCluster,
@@ -32,6 +39,8 @@ import {
   mockRegionalHubCluster,
 } from '../ClusterDetails.sharedmocks'
 import { ClusterOverviewPageContent } from './ClusterOverview'
+import { HostedClusterK8sResource } from 'openshift-assisted-ui-lib/cim'
+import userEvent from '@testing-library/user-event'
 
 const mockHistoryPush = jest.fn()
 
@@ -426,5 +435,115 @@ describe('ClusterOverview with regional hub cluster information with hostedClust
     await waitForText('Hub, Hosted')
     await waitForText('release-2.7')
     await waitForText('2.7.0')
+  })
+})
+
+describe('ClusterOverview with AWS hypershift cluster', () => {
+  const mockHostedCluster1: HostedClusterK8sResource = {
+    apiVersion: HostedClusterApiVersion,
+    kind: HostedClusterKind,
+    metadata: {
+      name: 'aa.bb.cc',
+      namespace: 'aa.bb.cc',
+    },
+    spec: {
+      services: [],
+      dns: {
+        baseDomain: 'test.com',
+      },
+      pullSecret: { name: 'local-cluster-pull-secret' },
+      release: { image: 'quay.io/openshift-release-dev/ocp-release:4.10.15-x86_64' },
+      sshKey: { name: 'feng-hypershift-test-ssh-key' },
+      platform: {
+        agent: {
+          agentNamespace: 'aa.bb.cc',
+        },
+        type: 'Agent',
+      },
+    },
+  }
+
+  beforeEach(() => {
+    const getSecrets1 = {
+      req: {
+        apiVersion: 'v1',
+        kind: 'secrets',
+        metadata: {
+          namespace: 'clusters',
+          name: 'feng-hypershift-test-admin-kubeconfig',
+        },
+      },
+    }
+
+    const getSecrets2 = {
+      req: {
+        apiVersion: 'v1',
+        kind: 'secrets',
+        metadata: {
+          namespace: 'feng-hypershift-test',
+          name: 'feng-hypershift-test-kubeadmin-password',
+        },
+      },
+    }
+
+    nockIgnoreRBAC()
+    nockSearch(mockSearchQuery, mockSearchResponse)
+    nockIgnoreApiPaths()
+    nockGet(getSecrets1.req, getSecrets1.req) // get 'secrets' in 'clusters' namespace
+    nockGet(getSecrets2.req, getSecrets2.req)
+  })
+
+  it('should display error alert on import with invalid cluster name', async () => {
+    const { getByText } = render(
+      <RecoilRoot
+        initializeState={(snapshot) => {
+          snapshot.set(policyreportState, [])
+          snapshot.set(managedClustersState, [])
+          snapshot.set(clusterDeploymentsState, [])
+          snapshot.set(managedClusterInfosState, [])
+          snapshot.set(certificateSigningRequestsState, [])
+          snapshot.set(managedClusterAddonsState, [])
+          snapshot.set(clusterManagementAddonsState, [])
+          snapshot.set(clusterClaimsState, [])
+          snapshot.set(clusterCuratorsState, [])
+          snapshot.set(agentClusterInstallsState, [])
+          snapshot.set(agentsState, [])
+          snapshot.set(infraEnvironmentsState, [])
+          snapshot.set(hostedClustersState, [])
+          snapshot.set(nodePoolsState, [])
+        }}
+      >
+        <MemoryRouter>
+          <ClusterContext.Provider
+            value={{
+              cluster: mockAWSHypershiftCluster,
+              addons: undefined,
+              hostedCluster: mockAWSHostedCluster,
+            }}
+          >
+            {' '}
+            <HypershiftImportCommand selectedHostedClusterResource={mockHostedCluster1}></HypershiftImportCommand>
+            <ClusterOverviewPageContent />
+          </ClusterContext.Provider>
+        </MemoryRouter>
+      </RecoilRoot>
+    )
+
+    await new Promise((resolve) => setTimeout(resolve, 500)) // makes sure everything is finished
+    screen.logTestingPlaygroundURL()
+
+    userEvent.click(
+      screen.getByRole('button', {
+        name: /import cluster/i,
+      })
+    )
+    await waitFor(() =>
+      expect(
+        getByText(
+          "The cluster name is invalid. Change the name to the RFC 1123 standard which consists of lower case alphanumeric characters, '-', and must start and end with an alphanumeric character."
+        )
+      ).toBeInTheDocument()
+    )
+    //await waitFor(() => expect(queryAllByText("The cluster name is invalid. Change the name to the RFC 1123 standard which consists of lower case alphanumeric characters, '-', and must start and end with an alphanumeric character.")).toHaveLength(1))
   })
 })
