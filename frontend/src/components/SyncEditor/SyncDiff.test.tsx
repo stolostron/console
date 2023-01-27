db231ac8f57a6beee44a5a5beb164f1ae82b2227
John Swanke <jswanke@redhat.com>
2023-01-27 11:17:03 -0500

    Synceditor tests (#2507)
    * synceditor test
    
    Signed-off-by: John Swanke <jswanke@redhat.com>
    
    * syncdiff test
    
    Signed-off-by: John Swanke <jswanke@redhat.com>
    
    * fix test
    
    Signed-off-by: John Swanke <jswanke@redhat.com>
    
    * fix process test
    
    Signed-off-by: John Swanke <jswanke@redhat.com>
    
    * the rest
    
    Signed-off-by: John Swanke <jswanke@redhat.com>
    
    * oops
    
    Signed-off-by: John Swanke <jswanke@redhat.com>
    
    ---------
    
    Signed-off-by: John Swanke <jswanke@redhat.com>

---
+++ frontend/src/components/SyncEditor/SyncDiff.test.tsx
@@ -0,0 +1,54 @@
+/* Copyright Contributors to the Open Cluster Management project */
+import { SyncDiff, SyncDiffType } from './SyncDiff'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+const revealFn = jest.fn()
+describe('SyncDiff tests', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('happy path', async () => {
+    render(<SyncDiff {...props} />)
+
+    expect(
+      screen.getByRole('cell', {
+        name: /10/i,
+      })
+    ).toBeInTheDocument()
+
+    const cell = screen.getByRole('cell', {
+      name: /data : 1234/i,
+    })
+    expect(cell).toBeInTheDocument()
+
+    userEvent.click(cell)
+    expect(revealFn).toHaveBeenCalledTimes(1)
+  })
+})
+
+const props: { stateChanges: SyncDiffType; errorMessage: string } = {
+  stateChanges: {
+    warnings: [],
+    errors: [],
+    changes: [
+      {
+        type: 'N',
+        line: 10,
+        path: ['Secret', 0, 'metadata', 'labels', 'data'],
+        length: 1,
+        latest: ['data: 1234'],
+        reveal: revealFn,
+      },
+      {
+        type: 'E',
+        line: 14,
+        path: ['Secret', 0, 'metadata', 'labels', 'datax'],
+        length: 1,
+        latest: ['datax: 1234'],
+      },
+    ],
+  },
+  errorMessage: '',
+}
