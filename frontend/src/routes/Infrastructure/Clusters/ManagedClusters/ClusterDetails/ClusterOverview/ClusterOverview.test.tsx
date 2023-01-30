4ec468d0279657088a64c5e97101a9c48d2cbf83
Omar Farag <ofarag@redhat.com>
2023-01-30 12:53:13 -0500

    Added coverage
    Signed-off-by: Omar Farag <ofarag@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/ClusterDetails/ClusterOverview/ClusterOverview.test.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/ClusterDetails/ClusterOverview/ClusterOverview.test.tsx
@@ -41,6 +41,7 @@ import {
 import { ClusterOverviewPageContent } from './ClusterOverview'
 import { HostedClusterK8sResource } from 'openshift-assisted-ui-lib/cim'
 import userEvent from '@testing-library/user-event'
+import { AcmToastGroup, AcmToastProvider } from '../../../../../../ui-components'
 
 const mockHistoryPush = jest.fn()
 
@@ -443,8 +444,8 @@ describe('ClusterOverview with AWS hypershift cluster', () => {
     apiVersion: HostedClusterApiVersion,
     kind: HostedClusterKind,
     metadata: {
-      name: 'aa.bb.cc',
-      namespace: 'aa.bb.cc',
+      name: 'hostedCluster1',
+      namespace: 'hostedCluster1',
     },
     spec: {
       services: [],
@@ -456,10 +457,175 @@ describe('ClusterOverview with AWS hypershift cluster', () => {
       sshKey: { name: 'feng-hypershift-test-ssh-key' },
       platform: {
         agent: {
-          agentNamespace: 'aa.bb.cc',
+          agentNamespace: 'hostedCluster1',
         },
         type: 'Agent',
       },
+    },
+    status: {
+      conditions: [
+        {
+          lastTransitionTime: '2022-12-20T16:48:44Z',
+          message: '',
+          observedGeneration: 1,
+          reason: 'StatusUnknown',
+          status: 'Unknown',
+          type: 'ValidAWSIdentityProvider',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:48:44Z',
+          message: 'Condition not found in the CVO.',
+          observedGeneration: 3,
+          reason: 'StatusUnknown',
+          status: 'Unknown',
+          type: 'ClusterVersionProgressing',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:48:44Z',
+          message: 'Condition not found in the CVO.',
+          observedGeneration: 3,
+          reason: 'StatusUnknown',
+          status: 'Unknown',
+          type: 'ClusterVersionReleaseAccepted',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:51:09Z',
+          message:
+            'Kubernetes 1.25 and therefore OpenShift 4.12 remove several APIs which require admin consideration. Please see the knowledge article https://access.redhat.com/articles/6955381 for details and instructions.',
+          observedGeneration: 3,
+          reason: 'AdminAckRequired',
+          status: 'False',
+          type: 'ClusterVersionUpgradeable',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:48:44Z',
+          message: 'Condition not found in the CVO.',
+          observedGeneration: 3,
+          reason: 'StatusUnknown',
+          status: 'Unknown',
+          type: 'ClusterVersionAvailable',
+        },
+        {
+          lastTransitionTime: '2022-12-20T19:15:09Z',
+          message: 'Working towards 4.11.17: 508 of 560 done (90% complete)',
+          observedGeneration: 3,
+          reason: 'AsExpected',
+          status: 'True',
+          type: 'ClusterVersionSucceeding',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:48:55Z',
+          message:
+            '[capi-provider deployment has 1 unavailable replicas, cluster-api deployment has 1 unavailable replicas]',
+          observedGeneration: 3,
+          reason: 'UnavailableReplicas',
+          status: 'True',
+          type: 'Degraded',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:49:32Z',
+          message: '',
+          observedGeneration: 3,
+          reason: 'QuorumAvailable',
+          status: 'True',
+          type: 'EtcdAvailable',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:50:08Z',
+          message: 'Kube APIServer deployment is available',
+          observedGeneration: 3,
+          reason: 'AsExpected',
+          status: 'True',
+          type: 'KubeAPIServerAvailable',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:48:59Z',
+          message: '',
+          observedGeneration: 3,
+          reason: 'AsExpected',
+          status: 'True',
+          type: 'InfrastructureReady',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:50:08Z',
+          message: 'The hosted control plane is available',
+          observedGeneration: 3,
+          reason: 'AsExpected',
+          status: 'True',
+          type: 'Available',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:48:44Z',
+          message: 'Configuration passes validation',
+          observedGeneration: 3,
+          reason: 'AsExpected',
+          status: 'True',
+          type: 'ValidConfiguration',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:48:44Z',
+          message: 'HostedCluster is supported by operator configuration',
+          observedGeneration: 3,
+          reason: 'AsExpected',
+          status: 'True',
+          type: 'SupportedHostedCluster',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:48:55Z',
+          message: 'Configuration passes validation',
+          reason: 'HostedClusterAsExpected',
+          status: 'True',
+          type: 'ValidHostedControlPlaneConfiguration',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:50:10Z',
+          message: 'Ignition server deployment is available',
+          observedGeneration: 3,
+          reason: 'AsExpected',
+          status: 'True',
+          type: 'IgnitionEndpointAvailable',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:48:44Z',
+          message: 'Reconciliation active on resource',
+          observedGeneration: 3,
+          reason: 'ReconciliationActive',
+          status: 'True',
+          type: 'ReconciliationActive',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:48:44Z',
+          message: 'Release image is valid',
+          observedGeneration: 3,
+          reason: 'AsExpected',
+          status: 'True',
+          type: 'ValidReleaseImage',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:48:44Z',
+          message: 'HostedCluster is at expected version',
+          observedGeneration: 3,
+          reason: 'AsExpected',
+          status: 'False',
+          type: 'Progressing',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:48:46Z',
+          message: 'Reconciliation completed succesfully',
+          observedGeneration: 3,
+          reason: 'ReconciliatonSucceeded',
+          status: 'True',
+          type: 'ReconciliationSucceeded',
+        },
+        {
+          lastTransitionTime: '2022-12-20T16:48:46Z',
+          message: 'OIDC configuration is valid',
+          observedGeneration: 3,
+          reason: 'AsExpected',
+          status: 'True',
+          type: 'ValidOIDCConfiguration',
+        },
+      ],
     },
   }
 
@@ -494,56 +660,38 @@ describe('ClusterOverview with AWS hypershift cluster', () => {
   })
 
   it('should display error alert on import with invalid cluster name', async () => {
-    const { getByText } = render(
-      <RecoilRoot
-        initializeState={(snapshot) => {
-          snapshot.set(policyreportState, [])
-          snapshot.set(managedClustersState, [])
-          snapshot.set(clusterDeploymentsState, [])
-          snapshot.set(managedClusterInfosState, [])
-          snapshot.set(certificateSigningRequestsState, [])
-          snapshot.set(managedClusterAddonsState, [])
-          snapshot.set(clusterManagementAddonsState, [])
-          snapshot.set(clusterClaimsState, [])
-          snapshot.set(clusterCuratorsState, [])
-          snapshot.set(agentClusterInstallsState, [])
-          snapshot.set(agentsState, [])
-          snapshot.set(infraEnvironmentsState, [])
-          snapshot.set(hostedClustersState, [])
-          snapshot.set(nodePoolsState, [])
-        }}
-      >
+    const { queryAllByText } = render(
+      <RecoilRoot>
         <MemoryRouter>
           <ClusterContext.Provider
             value={{
-              cluster: mockAWSHypershiftCluster,
+              cluster: mockRegionalHubCluster,
               addons: undefined,
-              hostedCluster: mockAWSHostedCluster,
+              hostedCluster: mockHostedCluster1,
             }}
           >
-            {' '}
-            <HypershiftImportCommand selectedHostedClusterResource={mockHostedCluster1}></HypershiftImportCommand>
+            <AcmToastProvider>
+              <AcmToastGroup />
+              <HypershiftImportCommand selectedHostedClusterResource={mockHostedCluster1}></HypershiftImportCommand>
+            </AcmToastProvider>
             <ClusterOverviewPageContent />
           </ClusterContext.Provider>
         </MemoryRouter>
       </RecoilRoot>
     )
 
-    await new Promise((resolve) => setTimeout(resolve, 500)) // makes sure everything is finished
-    screen.logTestingPlaygroundURL()
-
     userEvent.click(
       screen.getByRole('button', {
         name: /import cluster/i,
       })
     )
+
     await waitFor(() =>
       expect(
-        getByText(
+        queryAllByText(
           "The cluster name is invalid. Change the name to the RFC 1123 standard which consists of lower case alphanumeric characters, '-', and must start and end with an alphanumeric character."
         )
-      ).toBeInTheDocument()
+      ).toHaveLength(1)
     )
-    //await waitFor(() => expect(queryAllByText("The cluster name is invalid. Change the name to the RFC 1123 standard which consists of lower case alphanumeric characters, '-', and must start and end with an alphanumeric character.")).toHaveLength(1))
   })
 })
