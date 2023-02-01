5a2564405c1d46a66c3f3c25d1238f3ca94aa1d9
John Swanke <jswanke@redhat.com>
2023-02-01 14:42:21 -0500

    lint
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- frontend/src/components/TemplateEditor/controls/ControlPanelComboBox.test.js
+++ frontend/src/components/TemplateEditor/controls/ControlPanelComboBox.test.js
@@ -10,167 +10,167 @@ import i18n from 'i18next'
 const t = i18n.t.bind(i18n)
 
 const propsPlain = {
-  controlId: 'masterType',
-  control: {
-    name: 'Instance type',
-    tooltip: 'The EC2 instance type for your compute machines.',
-    learnMore: 'https://aws.amazon.com/ec2/instance-types/',
-    id: 'masterType',
-    type: 'combobox',
-    available: [
-      'm5.10xlarge - 40 vCPU, 160 GiB RAM - General Purpose',
-      'm5.16xlarge - 64 vCPU, 256 GiB RAM - General Purpose',
-      'm5.2xlarge - 8 vCPU, 32 GiB RAM - General Purpose',
-      'm5.4xlarge - 16 vCPU, 64 GiB RAM - General Purpose',
-      'm5.large - 2 vCPU, 8 GiB RAM - General Purpose',
-      'm5.xlarge - 4 vCPU, 16 GiB RAM - General Purpose',
-    ],
-    active: 'm5.xlarge',
-    validation: {
-      constraint: '[A-Za-z0-9.]+',
-      notification: 'creation.ocp.cluster.valid.alphanumeric.period',
-      required: false,
-      tester: /^[A-Za-z0-9.]+$/,
+    controlId: 'masterType',
+    control: {
+        name: 'Instance type',
+        tooltip: 'The EC2 instance type for your compute machines.',
+        learnMore: 'https://aws.amazon.com/ec2/instance-types/',
+        id: 'masterType',
+        type: 'combobox',
+        available: [
+            'm5.10xlarge - 40 vCPU, 160 GiB RAM - General Purpose',
+            'm5.16xlarge - 64 vCPU, 256 GiB RAM - General Purpose',
+            'm5.2xlarge - 8 vCPU, 32 GiB RAM - General Purpose',
+            'm5.4xlarge - 16 vCPU, 64 GiB RAM - General Purpose',
+            'm5.large - 2 vCPU, 8 GiB RAM - General Purpose',
+            'm5.xlarge - 4 vCPU, 16 GiB RAM - General Purpose',
+        ],
+        active: 'm5.xlarge',
+        validation: {
+            constraint: '[A-Za-z0-9.]+',
+            notification: 'creation.ocp.cluster.valid.alphanumeric.period',
+            required: false,
+            tester: /^[A-Za-z0-9.]+$/,
+        },
+        cacheUserValueKey: 'create.cluster.master.type',
+        availableMap: {
+            'm5.large - 2 vCPU, 8 GiB RAM - General Purpose': 'm5.large',
+            'm5.xlarge - 4 vCPU, 16 GiB RAM - General Purpose': 'm5.xlarge',
+            'm5.2xlarge - 8 vCPU, 32 GiB RAM - General Purpose': 'm5.2xlarge',
+            'm5.4xlarge - 16 vCPU, 64 GiB RAM - General Purpose': 'm5.4xlarge',
+            'm5.10xlarge - 40 vCPU, 160 GiB RAM - General Purpose': 'm5.10xlarge',
+            'm5.16xlarge - 64 vCPU, 256 GiB RAM - General Purpose': 'm5.16xlarge',
+        },
+        fetchAvailable: {
+            setAvailableMap: jest.fn(),
+        },
+        hasValueDescription: true,
+        isInitialized: true,
     },
-    cacheUserValueKey: 'create.cluster.master.type',
-    availableMap: {
-      'm5.large - 2 vCPU, 8 GiB RAM - General Purpose': 'm5.large',
-      'm5.xlarge - 4 vCPU, 16 GiB RAM - General Purpose': 'm5.xlarge',
-      'm5.2xlarge - 8 vCPU, 32 GiB RAM - General Purpose': 'm5.2xlarge',
-      'm5.4xlarge - 16 vCPU, 64 GiB RAM - General Purpose': 'm5.4xlarge',
-      'm5.10xlarge - 40 vCPU, 160 GiB RAM - General Purpose': 'm5.10xlarge',
-      'm5.16xlarge - 64 vCPU, 256 GiB RAM - General Purpose': 'm5.16xlarge',
-    },
-    fetchAvailable: {
-      setAvailableMap: jest.fn(),
-    },
-    hasValueDescription: true,
-    isInitialized: true,
-  },
-  handleControlChange: jest.fn(),
-  i18n: t,
+    handleControlChange: jest.fn(),
+    i18n: t,
 }
 
 const propsMulti = {
-  controlId: 'placementrulecombo',
-  control: {
-    id: 'placementrulecombo',
-    type: 'combobox',
-    opaque: false,
-    placeholder: 'Select an existing placement configuration',
-    validation: {},
-    isInitialized: true,
-    forceUpdate: [Function],
-    setActive: [Function],
-    isLoading: false,
-    available: ['test-placement-1'],
-    fetchAvailable: jest.fn(),
-    availableData: {
-      'test-placement-1': {
-        apiVersion: 'apps.open-cluster-management.io/v1',
-        kind: 'PlacementRule',
-        metadata: [Object],
-        spec: [Object],
-        status: [Object],
-      },
-    },
     controlId: 'placementrulecombo',
-    isLoaded: true,
-    availableInfo: { 'test-placement-1': 'test-placement-1 deploys only to local cluster' },
-    info: '',
-  },
-  handleControlChange: jest.fn(),
-  i18n: t,
+    control: {
+        id: 'placementrulecombo',
+        type: 'combobox',
+        opaque: false,
+        placeholder: 'Select an existing placement configuration',
+        validation: {},
+        isInitialized: true,
+        forceUpdate: [Function],
+        setActive: [Function],
+        isLoading: false,
+        available: ['test-placement-1'],
+        fetchAvailable: jest.fn(),
+        availableData: {
+            'test-placement-1': {
+                apiVersion: 'apps.open-cluster-management.io/v1',
+                kind: 'PlacementRule',
+                metadata: [Object],
+                spec: [Object],
+                status: [Object],
+            },
+        },
+        controlId: 'placementrulecombo',
+        isLoaded: true,
+        availableInfo: { 'test-placement-1': 'test-placement-1 deploys only to local cluster' },
+        info: '',
+    },
+    handleControlChange: jest.fn(),
+    i18n: t,
 }
 
 describe('ControlPanelComboBox component', () => {
-  it('basic combo', async () => {
-    render(<ControlPanelComboBox {...propsPlain} />)
+    it('basic combo', async () => {
+        render(<ControlPanelComboBox {...propsPlain} />)
 
-    const input = screen.getByRole('combobox', {
-      name: /listbox input field/i,
-    })
-    // select item
-    expect(input).toHaveValue('m5.xlarge - 4 vCPU, 16 GiB RAM - General Purpose')
-    userEvent.click(input)
-    userEvent.click(screen.getByText(/m5\.2xlarge - 8 vcpu, 32 gib ram - general purpose/i))
-    expect(input).toHaveValue('m5.2xlarge - 8 vCPU, 32 GiB RAM - General Purpose')
+        const input = screen.getByRole('combobox', {
+            name: /listbox input field/i,
+        })
+        // select item
+        expect(input).toHaveValue('m5.xlarge - 4 vCPU, 16 GiB RAM - General Purpose')
+        userEvent.click(input)
+        userEvent.click(screen.getByText(/m5\.2xlarge - 8 vcpu, 32 gib ram - general purpose/i))
+        expect(input).toHaveValue('m5.2xlarge - 8 vCPU, 32 GiB RAM - General Purpose')
 
-    // clear
-    userEvent.click(
-      screen.getByRole('button', {
-        name: /clear selected item/i,
-      })
-    )
-    expect(input).toHaveValue('')
+        // clear
+        userEvent.click(
+            screen.getByRole('button', {
+                name: /clear selected item/i,
+            })
+        )
+        expect(input).toHaveValue('')
 
-    // enter new item
-    userEvent.click(input)
-    userEvent.type(input, 'hello{enter}')
-    expect(input).toHaveValue('hello')
+        // enter new item
+        userEvent.click(input)
+        userEvent.type(input, 'hello{enter}')
+        expect(input).toHaveValue('hello')
 
-    // key events
-    userEvent.click(input)
-    userEvent.type(input, 'hello2')
-    userEvent.tab()
-    userEvent.click(input)
-    userEvent.type(input, '{esc}')
-    expect(input).toHaveValue('')
-  })
-
-  it('searching', async () => {
-    render(<ControlPanelComboBox {...propsPlain} />)
-
-    const input = screen.getByRole('combobox', {
-      name: /listbox input field/i,
-    })
-    // search
-    userEvent.type(input, '256')
-    userEvent.click(screen.getByText(/m5\.16xlarge - 64 vcpu, gib ram - general purpose/i))
-    expect(input).toHaveValue('m5.16xlarge - 64 vCPU, 256 GiB RAM - General Purpose')
-  })
-
-  it('editing', async () => {
-    render(<ControlPanelComboBox {...propsPlain} />)
-
-    const input = screen.getByRole('combobox', {
-      name: /listbox input field/i,
+        // key events
+        userEvent.click(input)
+        userEvent.type(input, 'hello2')
+        userEvent.tab()
+        userEvent.click(input)
+        userEvent.type(input, '{esc}')
+        expect(input).toHaveValue('')
     })
 
-    // edit
-    fireEvent(
-      input,
-      new MouseEvent('click', {
-        bubbles: true,
-        cancelable: true,
-        x: 100,
-        y: 0,
-      })
-    )
-    userEvent.type(input, 'x')
-    await waitFor(() => expect(input).toHaveValue('m5.16xlarge'))
-  })
+    it('searching', async () => {
+        render(<ControlPanelComboBox {...propsPlain} />)
 
-  it('selection with description', async () => {
-    render(<ControlPanelComboBox {...propsMulti} />)
-
-    const input = screen.getByRole('combobox', {
-      name: /listbox input field/i,
+        const input = screen.getByRole('combobox', {
+            name: /listbox input field/i,
+        })
+        // search
+        userEvent.type(input, '256')
+        userEvent.click(screen.getByText(/m5\.16xlarge - 64 vcpu, gib ram - general purpose/i))
+        expect(input).toHaveValue('m5.16xlarge - 64 vCPU, 256 GiB RAM - General Purpose')
     })
 
-    userEvent.click(input)
-    userEvent.click(screen.getByText(/test-placement-1 deploys only to local cluster/i))
-    expect(input).toHaveValue('test-placement-1')
-  })
+    it('editing', async () => {
+        render(<ControlPanelComboBox {...propsPlain} />)
 
-  it('keyboard', async () => {
-    render(<ControlPanelComboBox {...propsMulti} />)
+        const input = screen.getByRole('combobox', {
+            name: /listbox input field/i,
+        })
 
-    const input = screen.getByRole('combobox', {
-      name: /listbox input field/i,
+        // edit
+        fireEvent(
+            input,
+            new MouseEvent('click', {
+                bubbles: true,
+                cancelable: true,
+                x: 100,
+                y: 0,
+            })
+        )
+        userEvent.type(input, 'x')
+        await waitFor(() => expect(input).toHaveValue('m5.16xlarge'))
     })
-    userEvent.click(input)
-    userEvent.type(screen.getByText(/test-placement-1 deploys only to local cluster/i), '{enter}')
-    expect(input).toHaveValue('test-placement-1')
-  })
+
+    it('selection with description', async () => {
+        render(<ControlPanelComboBox {...propsMulti} />)
+
+        const input = screen.getByRole('combobox', {
+            name: /listbox input field/i,
+        })
+
+        userEvent.click(input)
+        userEvent.click(screen.getByText(/test-placement-1 deploys only to local cluster/i))
+        expect(input).toHaveValue('test-placement-1')
+    })
+
+    it('keyboard', async () => {
+        render(<ControlPanelComboBox {...propsMulti} />)
+
+        const input = screen.getByRole('combobox', {
+            name: /listbox input field/i,
+        })
+        userEvent.click(input)
+        userEvent.type(screen.getByText(/test-placement-1 deploys only to local cluster/i), '{enter}')
+        expect(input).toHaveValue('test-placement-1')
+    })
 })
--- frontend/src/components/TemplateEditor/controls/ControlPanelTreeSelect.test.js
+++ frontend/src/components/TemplateEditor/controls/ControlPanelTreeSelect.test.js
@@ -10,128 +10,128 @@ import i18n from 'i18next'
 const t = i18n.t.bind(i18n)
 
 const props = {
-  controlId: 'workerType',
-  control: {
-    name: 'Instance type',
-    tooltip: 'The GCP machine type.',
-    learnMore: 'https://cloud.google.com/compute/docs/machine-types',
-    id: 'workerType',
-    type: 'treeselect',
-    available: [
-      {
-        label: 'Compute Optimized',
-        children: [
-          { value: 'c2-standard-4', description: '4 vCPU, 16 GiB RAM - Compute Optimized' },
-          { value: 'c2-standard-8', description: '8 vCPU, 32 GiB RAM - Compute Optimized' },
-          { value: 'c2-standard-16', description: '16 vCPU, 64 GiB RAM - Compute Optimized' },
-          { value: 'c2-standard-30', description: '30 vCPU, 120 GiB RAM - Compute Optimized' },
-          { value: 'c2-standard-60', description: '60 vCPU, 240 GiB RAM - Compute Optimized' },
-        ],
-      },
-      {
-        label: 'Memory Optimized',
-        children: [
-          { value: 'm2-ultramem-2084', description: '208 vCPU, 5888 GiB RAM - Memory Optimized' },
-          { value: 'm2-ultramem-4164', description: '416 vCPU, 11,776 GiB RAM - Memory Optimized' },
-          { value: 'm1-ultramem-40', description: '40 vCPU, 961 GiB RAM - Memory Optimized' },
-          { value: 'm1-ultramem-80', description: '80 vCPU, 1922 GiB RAM - Memory Optimized' },
-          { value: 'm1-ultramem-160', description: '160 vCPU, 3844 GiB RAM - Memory Optimized' },
-          { value: 'm1-megamem-96', description: '96 vCPU, 1433.6 GiB RAM - Memory Optimized' },
-        ],
-      },
-    ],
-    active: 'c2-standard-4',
-    validation: {
-      constraint: '[A-Za-z0-9-]+',
-      notification: 'creation.ocp.cluster.valid.alphanumeric.period',
-      required: false,
-      tester: /^[A-Za-z0-9-]+$/,
-    },
-    cacheUserValueKey: 'create.cluster.worker.type',
-    isInitialized: true,
-    sourcePathMap: {
-      'install-config': 'unknown[0].$synced.compute.$v[0].$v.platform.$v.gcp.$v.type.$v',
-      '<<main>>': 'MachinePool[0].$synced.spec.$v.platform.$v.gcp.$v.type.$v',
-    },
     controlId: 'workerType',
-    availableMap: {
-      'c2-standard-4': '4 vCPU, 16 GiB RAM - Compute Optimized',
-      'c2-standard-8': '8 vCPU, 32 GiB RAM - Compute Optimized',
-      'c2-standard-16': '16 vCPU, 64 GiB RAM - Compute Optimized',
-      'c2-standard-30': '30 vCPU, 120 GiB RAM - Compute Optimized',
-      'c2-standard-60': '60 vCPU, 240 GiB RAM - Compute Optimized',
-      'm2-ultramem-2084': '208 vCPU, 5888 GiB RAM - Memory Optimized',
-      'm2-ultramem-4164': '416 vCPU, 11,776 GiB RAM - Memory Optimized',
-      'm1-ultramem-40': '40 vCPU, 961 GiB RAM - Memory Optimized',
-      'm1-ultramem-80': '80 vCPU, 1922 GiB RAM - Memory Optimized',
-      'm1-ultramem-160': '160 vCPU, 3844 GiB RAM - Memory Optimized',
-      'm1-megamem-96': '96 vCPU, 1433.6 GiB RAM - Memory Optimized',
+    control: {
+        name: 'Instance type',
+        tooltip: 'The GCP machine type.',
+        learnMore: 'https://cloud.google.com/compute/docs/machine-types',
+        id: 'workerType',
+        type: 'treeselect',
+        available: [
+            {
+                label: 'Compute Optimized',
+                children: [
+                    { value: 'c2-standard-4', description: '4 vCPU, 16 GiB RAM - Compute Optimized' },
+                    { value: 'c2-standard-8', description: '8 vCPU, 32 GiB RAM - Compute Optimized' },
+                    { value: 'c2-standard-16', description: '16 vCPU, 64 GiB RAM - Compute Optimized' },
+                    { value: 'c2-standard-30', description: '30 vCPU, 120 GiB RAM - Compute Optimized' },
+                    { value: 'c2-standard-60', description: '60 vCPU, 240 GiB RAM - Compute Optimized' },
+                ],
+            },
+            {
+                label: 'Memory Optimized',
+                children: [
+                    { value: 'm2-ultramem-2084', description: '208 vCPU, 5888 GiB RAM - Memory Optimized' },
+                    { value: 'm2-ultramem-4164', description: '416 vCPU, 11,776 GiB RAM - Memory Optimized' },
+                    { value: 'm1-ultramem-40', description: '40 vCPU, 961 GiB RAM - Memory Optimized' },
+                    { value: 'm1-ultramem-80', description: '80 vCPU, 1922 GiB RAM - Memory Optimized' },
+                    { value: 'm1-ultramem-160', description: '160 vCPU, 3844 GiB RAM - Memory Optimized' },
+                    { value: 'm1-megamem-96', description: '96 vCPU, 1433.6 GiB RAM - Memory Optimized' },
+                ],
+            },
+        ],
+        active: 'c2-standard-4',
+        validation: {
+            constraint: '[A-Za-z0-9-]+',
+            notification: 'creation.ocp.cluster.valid.alphanumeric.period',
+            required: false,
+            tester: /^[A-Za-z0-9-]+$/,
+        },
+        cacheUserValueKey: 'create.cluster.worker.type',
+        isInitialized: true,
+        sourcePathMap: {
+            'install-config': 'unknown[0].$synced.compute.$v[0].$v.platform.$v.gcp.$v.type.$v',
+            '<<main>>': 'MachinePool[0].$synced.spec.$v.platform.$v.gcp.$v.type.$v',
+        },
+        controlId: 'workerType',
+        availableMap: {
+            'c2-standard-4': '4 vCPU, 16 GiB RAM - Compute Optimized',
+            'c2-standard-8': '8 vCPU, 32 GiB RAM - Compute Optimized',
+            'c2-standard-16': '16 vCPU, 64 GiB RAM - Compute Optimized',
+            'c2-standard-30': '30 vCPU, 120 GiB RAM - Compute Optimized',
+            'c2-standard-60': '60 vCPU, 240 GiB RAM - Compute Optimized',
+            'm2-ultramem-2084': '208 vCPU, 5888 GiB RAM - Memory Optimized',
+            'm2-ultramem-4164': '416 vCPU, 11,776 GiB RAM - Memory Optimized',
+            'm1-ultramem-40': '40 vCPU, 961 GiB RAM - Memory Optimized',
+            'm1-ultramem-80': '80 vCPU, 1922 GiB RAM - Memory Optimized',
+            'm1-ultramem-160': '160 vCPU, 3844 GiB RAM - Memory Optimized',
+            'm1-megamem-96': '96 vCPU, 1433.6 GiB RAM - Memory Optimized',
+        },
     },
-  },
-  handleChange: jest.fn(),
-  i18n: t,
+    handleChange: jest.fn(),
+    i18n: t,
 }
 
 describe('ControlPanelTreeSelect component', () => {
-  it('basic tree select', async () => {
-    const propz = { ...props }
-    const { rerender } = render(<ControlPanelTreeSelect {...propz} />)
+    it('basic tree select', async () => {
+        const propz = { ...props }
+        const { rerender } = render(<ControlPanelTreeSelect {...propz} />)
 
-    const input = screen.getByRole('combobox', {
-      name: /listbox input field/i,
+        const input = screen.getByRole('combobox', {
+            name: /listbox input field/i,
+        })
+        // // select item
+        expect(input).toHaveValue('c2-standard-4 - 4 vCPU, 16 GiB RAM - Compute Optimized')
+        userEvent.click(input)
+        userEvent.click(
+            screen.getByRole('button', {
+                name: /memory optimized/i,
+            })
+        )
+        userEvent.click(
+            screen.getByRole('button', {
+                name: /m2-ultramem-4164 - 416 vcpu, 11,776 gib ram - memory optimized/i,
+            })
+        )
+        expect(input).toHaveValue('m2-ultramem-4164  # 416 vCPU, 11,776 GiB RAM - Memory Optimized')
+
+        // clear
+        userEvent.click(
+            screen.getByRole('button', {
+                name: /clear selected item/i,
+            })
+        )
+        expect(input).toHaveValue('')
+
+        // enter new item
+        userEvent.click(input)
+        userEvent.type(input, 'hello{enter}')
+        expect(propz.handleChange).toHaveBeenCalledWith({ selectedItem: 'hello' })
+        propz.control.active = 'hello'
+        rerender(<ControlPanelTreeSelect {...propz} />)
+        expect(input).toHaveValue('hello')
+
+        // key events
+        userEvent.click(input)
+        userEvent.tab()
+        userEvent.click(input)
+        userEvent.type(input, '{esc}')
+        expect(input).toHaveValue('')
     })
-    // // select item
-    expect(input).toHaveValue('c2-standard-4 - 4 vCPU, 16 GiB RAM - Compute Optimized')
-    userEvent.click(input)
-    userEvent.click(
-      screen.getByRole('button', {
-        name: /memory optimized/i,
-      })
-    )
-    userEvent.click(
-      screen.getByRole('button', {
-        name: /m2-ultramem-4164 - 416 vcpu, 11,776 gib ram - memory optimized/i,
-      })
-    )
-    expect(input).toHaveValue('m2-ultramem-4164  # 416 vCPU, 11,776 GiB RAM - Memory Optimized')
 
-    // clear
-    userEvent.click(
-      screen.getByRole('button', {
-        name: /clear selected item/i,
-      })
-    )
-    expect(input).toHaveValue('')
+    it('searching', async () => {
+        render(<ControlPanelTreeSelect {...props} />)
 
-    // enter new item
-    userEvent.click(input)
-    userEvent.type(input, 'hello{enter}')
-    expect(propz.handleChange).toHaveBeenCalledWith({ selectedItem: 'hello' })
-    propz.control.active = 'hello'
-    rerender(<ControlPanelTreeSelect {...propz} />)
-    expect(input).toHaveValue('hello')
-
-    // key events
-    userEvent.click(input)
-    userEvent.tab()
-    userEvent.click(input)
-    userEvent.type(input, '{esc}')
-    expect(input).toHaveValue('')
-  })
-
-  it('searching', async () => {
-    render(<ControlPanelTreeSelect {...props} />)
-
-    const input = screen.getByRole('combobox', {
-      name: /listbox input field/i,
+        const input = screen.getByRole('combobox', {
+            name: /listbox input field/i,
+        })
+        // search
+        userEvent.type(input, 'm2-ultramem-4164')
+        userEvent.click(
+            screen.getByRole('button', {
+                name: /m2-ultramem-4164 - 416 vcpu, 11,776 gib ram - memory optimized/i,
+            })
+        )
+        expect(input).toHaveValue('m2-ultramem-4164  # 416 vCPU, 11,776 GiB RAM - Memory Optimized')
     })
-    // search
-    userEvent.type(input, 'm2-ultramem-4164')
-    userEvent.click(
-      screen.getByRole('button', {
-        name: /m2-ultramem-4164 - 416 vcpu, 11,776 gib ram - memory optimized/i,
-      })
-    )
-    expect(input).toHaveValue('m2-ultramem-4164  # 416 vCPU, 11,776 GiB RAM - Memory Optimized')
-  })
 })
