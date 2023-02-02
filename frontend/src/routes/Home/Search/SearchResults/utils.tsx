882194e2d99690eaad6ab337c05610060393f994
Zack Layne <zlayne@redhat.com>
2023-02-02 14:28:10 -0500

    Use accordion to present search results (#2527)
    Signed-off-by: Zack Layne <zlayne@redhat.com>

--- frontend/src/routes/Home/Search/SearchResults/utils.tsx
+++ frontend/src/routes/Home/Search/SearchResults/utils.tsx
@@ -1,10 +1,16 @@
 /* Copyright Contributors to the Open Cluster Management project */
 import { Card, CardBody, CardHeader, ExpandableSection } from '@patternfly/react-core'
+import { TFunction } from 'i18next'
 import { ReactNode, useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { NavigationPath } from '../../../../NavigationPath'
 import { ClosedDeleteModalProps, IDeleteModalProps } from '../components/Modals/DeleteResourceModal'
+import { GetUrlSearchParam } from '../searchDefinitions'
 
 export interface ISearchResult {
   kind: string
+  apiversion: string
+  apigroup?: string
   __type: string
 }
 
@@ -34,16 +40,33 @@ export function SearchResultExpandableCard(props: {
 
 export function GetRowActions(
   kind: string,
-  rowTitle: string,
   currentQuery: string,
   relatedResource: boolean,
-  setDeleteResource: React.Dispatch<React.SetStateAction<IDeleteModalProps>>
+  setDeleteResource: React.Dispatch<React.SetStateAction<IDeleteModalProps>>,
+  t: TFunction
 ) {
+  const history = useHistory()
   return kind !== 'cluster' && kind !== 'release' && kind !== 'policyreport'
     ? [
         {
+          id: 'edit',
+          title: t('Edit {{resourceKind}}', { resourceKind: kind }),
+          click: (item: any) => {
+            const searchParams = GetUrlSearchParam(item)
+            return history.push(`${NavigationPath.resourceYAML}${searchParams}`)
+          },
+        },
+        {
+          id: 'view-related',
+          title: t('View related resources'),
+          click: (item: any) => {
+            const searchParams = GetUrlSearchParam(item)
+            return history.push(`${NavigationPath.resourceRelated}${searchParams}`)
+          },
+        },
+        {
           id: 'delete',
-          title: rowTitle,
+          title: t('Delete {{resourceKind}}', { resourceKind: kind }),
           click: (item: any) => {
             setDeleteResource({
               open: true,
