ee806f0c942106f6ec9245d3926e56bd69656ea8
randybrunopiverger <rbrunopi@redhat.com>
2023-02-20 09:23:01 -0800

    test change
    Signed-off-by: randybrunopiverger <rbrunopi@redhat.com>

--- cypress/e2e/1-getting-started/test.cy.js
+++ cypress/e2e/1-getting-started/test.cy.js
@@ -13,23 +13,11 @@
 // please read our getting started guide:
 // https://on.cypress.io/introduction-to-cypress
 
-describe('example to-do app', () => {
+describe('testing visiting cluster', () => {
   it('displays two todo items by default', () => {
     // Cypress.Cookies.defaults({
     //   preserve: ['_csrf', '_oauth_proxy', 'acm-access-token-cookie'],
     // })
     cy.visit('/multicloud/infrastructure/clusters')
-
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
-    cy.get('.todo-list li').should('have.length', 2)
-
-    // We can go even further and check that the default todos each contain
-    // the correct text. We use the `first` and `last` functions
-    // to get just the first and last matched elements individually,
-    // and then perform an assertion with `should`.
-    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-    cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
   })
 })
