cb3bf60aa76706f3741a2ea23bb652c3470dec93
randybrunopiverger <rbrunopi@redhat.com>
2023-03-07 09:25:55 -0800

    class name change
    Signed-off-by: randybrunopiverger <rbrunopi@redhat.com>

--- frontend/src/ui-components/AcmCharts/AcmDonutChart/AcmDonutChart.tsx
+++ frontend/src/ui-components/AcmCharts/AcmDonutChart/AcmDonutChart.tsx
@@ -150,5 +150,5 @@ export const colorThemes = {
   criticalSuccess: [criticalColorClass, successColorClass],
   criticalLowSuccess: [criticalColorClass, moderateColorClass, successColorClass],
   criticalImportantModerateLow: [criticalColorClass, importantColorClass, moderateColorClass, lowColorClass],
-  criticalLowImportantUnknownSuccess: [criticalColorClass, lowColorClass, unknownColorClass, successColorClass],
+  criticalLowUnknownSuccess: [criticalColorClass, lowColorClass, unknownColorClass, successColorClass],
 }
