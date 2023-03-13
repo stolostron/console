e0373daf88b4fb21dc73986029073e4ad745cbf0
Zack Layne <zlayne@redhat.com>
2023-03-13 16:19:15 -0400

    remove commented lines
    Signed-off-by: Zack Layne <zlayne@redhat.com>

--- backend/test/routes/userpreference.test.ts
+++ backend/test/routes/userpreference.test.ts
@@ -51,30 +51,10 @@ describe('userpreference Route', function () {
     const res = await request('POST', '/userpreference/kube-admin', postBody)
     expect(res.statusCode).toEqual(200)
     expect(JSON.stringify(await parsePipedJsonBody(res))).toEqual(JSON.stringify(postBody))
-    // expect(JSON.stringify(await parseResponseJsonBody(res))).toEqual(
-    //   JSON.stringify({
-    //     apiVersion: 'console.open-cluster-management.io/v1',
-    //     kind: 'UserPreference',
-    //     metadata: {
-    //       name: 'kube-admin',
-    //     },
-    //     spec: {
-    //       savedSearches: [{ description: '', id: '1678205878189', name: 'testing', searchText: 'kind:Pod' }],
-    //     },
-    //   })
-    // )
   })
   it(`when bad things happen to Ansible TowerJobs 3`, async function () {
     nock(process.env.CLUSTER_API_URL).get('/apis').reply(400)
     const res = await request('POST', '/userpreference/kube-admin')
     expect(JSON.stringify(await parsePipedJsonBody(res))).toEqual(JSON.stringify({}))
   })
-  //   it('should handle GET request errors', async function () {
-  //     nock(process.env.CLUSTER_API_URL).get('/apis').reply(500)
-  //     nock(process.env.CLUSTER_API_URL)
-  //       .get('/apis/console.open-cluster-management.io/v1/userpreferences/kube-admin')
-  //       .reply(500)
-  //     const res = await request('GET', '/userpreference/kube-admin')
-  //     expect(res.statusCode).toEqual(500)
-  //   })
 })
