dcfb134b6adbb23e04b4b12c53259c132bb74f59
rawagner <rawagner@redhat.com>
2023-03-16 12:53:10 +0100

    CIM/Hypershift: Enable hostPrefix and stop using podCIDR (deprecated)
    Signed-off-by: rawagner <rawagner@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/components/assisted-installer/hypershift/constants.ts
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/components/assisted-installer/hypershift/constants.ts
@@ -1,3 +1,4 @@
 /* Copyright Contributors to the Open Cluster Management project */
 export const defaultServiceCIDR = '172.31.0.0/16'
 export const defaultPodCIDR = '10.132.0.0/14'
+export const defaultHostPrefix = '23'
