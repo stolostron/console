3b4d866993677defd8d6c49a8814183652f0eb2a
Omar Farag <ofarag@redhat.com>
2023-03-29 16:24:49 -0400

    immediately show hcp upgrade
    Signed-off-by: Omar Farag <ofarag@redhat.com>

--- frontend/src/resources/utils/get-cluster.test.ts
+++ frontend/src/resources/utils/get-cluster.test.ts
@@ -128,7 +128,7 @@ const mockHostedCluster: HostedClusterK8sResource = {
             baseDomain: 'dev06.red-chesterfield.com',
         },
         release: {
-            image: 'randomimage',
+            image: 'quay.io/openshift-release-dev/ocp-release:4.11.22-x86_64',
         },
         services: [],
         platform: {},
