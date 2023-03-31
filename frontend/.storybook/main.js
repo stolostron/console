d6cd497db33f81229c83afc018e7168848c44a15
Kevin Cormier <kcormier@redhat.com>
2023-03-31 14:43:43 -0400

    Translate storybook (#2731)
    * Enable translations in storybook
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Enable translation in storybook
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    ---------
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -2,45 +2,46 @@
 const sass = require('sass')
 
 module.exports = {
-    addons: [
-        '@storybook/addon-essentials',
-        '@storybook/addon-storysource',
-        '@storybook/addon-a11y',
-        '@storybook/addon-actions',
-    ],
-    stories: ['../src/ui-components/AcmPage/AcmPage.stories.tsx', '../src/ui-components/**/*.stories.tsx'],
-    webpackFinal: async (config) => {
-        config.module.rules.push(
-            {
-                test: /\.(ts|tsx)$/,
-                loader: 'ts-loader',
-                options: {
-                    transpileOnly: true,
-                },
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-storysource',
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+  ],
+  stories: ['../src/ui-components/AcmPage/AcmPage.stories.tsx', '../src/ui-components/**/*.stories.tsx'],
+  webpackFinal: async (config) => {
+    config.module.rules.push(
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: sass,
             },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            implementation: sass,
-                        },
-                    },
-                ],
-            }
-        )
-        config.resolve.extensions.push('.ts', '.tsx')
-        return config
-    },
-    typescript: {
-        check: false,
-        checkOptions: {},
-    },
-    framework: '@storybook/react',
-    core: {
-        builder: 'webpack5',
-    },
+          },
+        ],
+      }
+    )
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+  },
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+  staticDirs: [{ from: '../public', to: '/multicloud' }],
 }
--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,24 +1,67 @@
 /* Copyright Contributors to the Open Cluster Management project */
-
 export const parameters = {
-    layout: 'fullscreen',
-    backgrounds: {
-        default: 'pf-grey',
-        values: [
-            {
-                name: 'pf-grey',
-                value: '#f0f0f0',
-            },
-        ],
-    },
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'pf-grey',
+    values: [
+      {
+        name: 'pf-grey',
+        value: '#f0f0f0',
+      },
+    ],
+  },
 }
 
 import '@patternfly/react-core/dist/styles/base.css'
+import React, { Suspense, useEffect } from 'react'
+import { I18nextProvider } from 'react-i18next'
+import i18n from '../src/lib/i18n.ts'
+
+// wrap stories in the I18nextProvider component
+const withI18next = (Story, context) => {
+  const { locale } = context.globals
+
+  // When the locale global changes
+  // Set the new locale in i18n
+  useEffect(() => {
+    i18n.changeLanguage(locale)
+  }, [locale])
+
+  return (
+    // This catches the suspense from components not yet ready (still loading translations)
+    // Alternative: set useSuspense to false on i18next.options.react when initializing i18next
+    <Suspense fallback={<div>loading translations...</div>}>
+      <I18nextProvider i18n={i18n}>
+        <Story />
+      </I18nextProvider>
+    </Suspense>
+  )
+}
 
 export const decorators = [
-    (Story) => (
-        <div style={{ height: '100vh' }}>
-            <Story />
-        </div>
-    ),
+  (Story) => (
+    <div style={{ height: '100vh' }}>
+      <Story />
+    </div>
+  ),
+  withI18next,
 ]
+
+// Create a global variable called locale in storybook
+// and add a menu in the toolbar to change your locale
+export const globalTypes = {
+  locale: {
+    name: 'Locale',
+    description: 'Internationalization locale',
+    toolbar: {
+      icon: 'globe',
+      items: [
+        { value: 'en', title: 'English' },
+        { value: 'zh', title: 'Chinese (simplified)' },
+        { value: 'ja', title: 'Japanese' },
+        { value: 'ko', title: 'Korean' },
+      ],
+      showName: true,
+    },
+  },
+}
