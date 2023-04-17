6b44c91b5653521be936cde0e48db593c1b83eff
zlayne <zlayne@redhat.com>
2023-04-17 14:24:03 -0400

    Fix search share modal clipboard copy
    Signed-off-by: zlayne <zlayne@redhat.com>

--- frontend/src/routes/Home/Search/components/Modals/ShareSearchModal.test.tsx
+++ frontend/src/routes/Home/Search/components/Modals/ShareSearchModal.test.tsx
@@ -1,6 +1,7 @@
 /* Copyright Contributors to the Open Cluster Management project */
 // Copyright (c) 2021 Red Hat, Inc.
 // Copyright Contributors to the Open Cluster Management project
+import { screen } from '@testing-library/dom'
 import { render } from '@testing-library/react'
 import { ShareSearchModal } from './ShareSearchModal'
 
@@ -12,5 +13,7 @@ test('renders share modal with searchText', () => {
     />
   )
   expect(getByText('Share search')).toBeInTheDocument()
-  expect(getByText('http://localhost/?filters={"textsearch":"kind%3Apod%20name%3AtestPod"}')).toBeInTheDocument()
+  expect(
+    screen.getByDisplayValue(/http:\/\/localhost\/\?filters=\{"textsearch":"kind%3apod%20name%3atestpod"\}/i)
+  ).toBeInTheDocument()
 })
