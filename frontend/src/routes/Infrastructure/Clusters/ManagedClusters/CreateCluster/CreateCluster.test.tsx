95e2d8c1d1818c2f47e723b8a12fcc084d97f79c
Kevin Cormier <kcormier@redhat.com>
2023-04-26 13:40:14 -0400

    Fix test
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/CreateCluster.test.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/CreateCluster.test.tsx
@@ -124,6 +124,15 @@ const mockClusterCuratorInstall: ClusterCurator = {
       ],
       towerAuthSecret: 'toweraccess-install',
     },
+    upgrade: {
+      posthook: [
+        {
+          name: 'test-posthook-upgrade',
+          extra_vars: {},
+        },
+      ],
+      towerAuthSecret: 'toweraccess-upgrade',
+    },
     desiredCuration: 'install',
   },
 }
@@ -150,6 +159,26 @@ const mockProviderConnectionAnsibleCopied: ProviderConnection = {
   kind: ProviderConnectionKind,
   metadata: {
     name: 'toweraccess-install',
+    namespace: clusterName,
+    labels: {
+      'cluster.open-cluster-management.io/type': 'ans',
+      'cluster.open-cluster-management.io/copiedFromNamespace': 'test-ii',
+      'cluster.open-cluster-management.io/copiedFromSecretName': 'ansible-connection',
+      'cluster.open-cluster-management.io/backup': 'cluster',
+    },
+  },
+  stringData: {
+    host: 'test',
+    token: 'test',
+  },
+  type: 'Opaque',
+}
+
+const mockProviderConnectionAnsibleCopiedUpgrade: ProviderConnection = {
+  apiVersion: ProviderConnectionApiVersion,
+  kind: ProviderConnectionKind,
+  metadata: {
+    name: 'toweraccess-upgrade',
     namespace: clusterName,
     labels: {
       'cluster.open-cluster-management.io/type': 'ans',
@@ -684,6 +713,8 @@ describe('CreateCluster AWS', () => {
     await waitForText(`View ${mockClusterCurators[0].metadata.name!}`)
     await waitForText('Pre-install Ansible template')
     await waitForText(mockClusterCurators[0].spec!.install!.prehook![0].name!)
+    await waitForText('Post-upgrade Ansible template')
+    await waitForText(mockClusterCurators[0].spec!.upgrade!.posthook![0].name!)
 
     // clear template
     await clickByRole('button', { name: /clear selected item/i })
@@ -772,6 +803,7 @@ describe('CreateCluster AWS', () => {
       nockCreate(mockKlusterletAddonSecretAws),
       nockCreate(mockClusterDeploymentAwsAnsible),
       nockCreate(mockProviderConnectionAnsibleCopied),
+      nockCreate(mockProviderConnectionAnsibleCopiedUpgrade),
       nockCreate(mockClusterCuratorInstall),
     ]
 
