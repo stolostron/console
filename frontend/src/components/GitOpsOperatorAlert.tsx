785d5ecce49276d10718f1459be92fb48bbb7375
Kevin Cormier <kcormier@redhat.com>
2023-05-10 15:55:45 -0400

    Remove unnecessary variable rename
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/components/GitOpsOperatorAlert.tsx
+++ frontend/src/components/GitOpsOperatorAlert.tsx
@@ -3,12 +3,12 @@ import { useTranslation } from '../lib/acm-i18next'
 import { OperatorAlert } from './OperatorAlert'
 
 export function GitOpsOperatorAlert(props: { showAlert: boolean; className?: string }) {
-  const { showAlert: isVisible, className } = props
+  const { showAlert, className } = props
 
   const { t } = useTranslation()
   const component = 'alert'
   const message = t('OpenShift GitOps Operator is required to create ApplicationSets.')
   const operatorName = 'Red Hat OpenShift GitOps'
 
-  return <>{isVisible && <OperatorAlert {...{ component, message, operatorName, className }} />}</>
+  return <>{showAlert && <OperatorAlert {...{ component, message, operatorName, className }} />}</>
 }
