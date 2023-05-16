a612d745aaa01ad0e85102a09dd5a26cd91e1095
randybrunopiverger <rbrunopi@redhat.com>
2023-05-16 15:23:42 -0700

    test edit
    Signed-off-by: randybrunopiverger <rbrunopi@redhat.com>

--- frontend/src/ui-components/AcmEmptyState/AcmEmptyState.test.tsx
+++ frontend/src/ui-components/AcmEmptyState/AcmEmptyState.test.tsx
@@ -26,8 +26,4 @@ describe('AcmEmptyState', () => {
     expect(container.querySelector('button')).toBeNull()
     expect(await axe(container)).toHaveNoViolations()
   })
-  test('renders with isEmptyTableState', () => {
-    const { getByText } = render(<AcmEmptyState title="Empty state title" message="Empty state message" />)
-    expect(getByText('Empty state title')).toBeInTheDocument()
-  })
 })
