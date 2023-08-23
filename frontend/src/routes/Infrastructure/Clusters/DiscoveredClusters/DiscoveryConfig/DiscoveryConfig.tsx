8f61ce7461801c42b77655fac47915155929331b
Disaiah Bennett <lavontae.bennett@gmail.com>
2023-08-23 09:49:47 -0400

    update DiscoveryConfig OCP version options (#2993)
    Signed-off-by: Disaiah Bennett <dbennett@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/DiscoveredClusters/DiscoveryConfig/DiscoveryConfig.tsx
+++ frontend/src/routes/Infrastructure/Clusters/DiscoveredClusters/DiscoveryConfig/DiscoveryConfig.tsx
@@ -49,7 +49,7 @@ import {
 } from '../../../../../ui-components'
 import { CredentialsForm } from '../../../../Credentials/CredentialsForm'
 
-const discoveryVersions = ['4.10', '4.11', '4.12', '4.13']
+const discoveryVersions = ['4.11', '4.12', '4.13', '4.14']
 
 export default function DiscoveryConfigPage() {
   const { t } = useTranslation()
