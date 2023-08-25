52113d1807d30393c559250c45078f94b567fca5
Maggie Chen <magchen@redhat.com>
2023-08-25 13:30:31 -0400

    add key
    Signed-off-by: Maggie Chen <magchen@redhat.com>

--- frontend/src/routes/Applications/ApplicationDetails/ApplicationOverview/ApplicationOverview.tsx
+++ frontend/src/routes/Applications/ApplicationDetails/ApplicationOverview/ApplicationOverview.tsx
@@ -504,7 +504,7 @@ function createSourceCards(
   return appRepos?.map((appRepo) => {
     if (appRepo) {
       return (
-        <Card>
+        <Card key={appRepo.pathName}>
           <CardBody className="sub-card-container">
             <div className="sub-card-content">
               <ResourceLabels
