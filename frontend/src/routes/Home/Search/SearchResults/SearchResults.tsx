57368a6fb7921d166a4196cc801c672694b614b3
Jorge Padilla <jpadilla@redhat.com>
2023-09-06 16:03:36 -0400

    Update search definitions for subscription, clusterserviceversion, and clusteroperator
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- frontend/src/routes/Home/Search/SearchResults/SearchResults.tsx
+++ frontend/src/routes/Home/Search/SearchResults/SearchResults.tsx
@@ -71,13 +71,15 @@ function RenderAccordionItem(props: {
 
   const renderContent = useCallback(
     (kind: string, items: ISearchResult[]) => {
+      const kindAndGroup = kind.toLowerCase() === "subscription" ? `subscription.${items[0].apigroup}` : kind.toLowerCase()
+     
       return (
         <AcmTable
           items={items}
           emptyState={undefined} // table only shown for kinds with results
           columns={_.get(
             searchDefinitions,
-            `[${kind.toLowerCase()}].columns`,
+            `[${kindAndGroup}].columns`,
             searchDefinitions['genericresource'].columns
           )}
           keyFn={(item: any) => item._uid.toString()}
