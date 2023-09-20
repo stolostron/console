f48e4c4f767e3402d40f84457028fe53696d3151
John Swanke <jswanke@redhat.com>
2023-09-20 14:21:07 -0400

    ACM-7577 saved-warning-is-triggerec
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- frontend/src/wizards/common/LostChangesPrompt.tsx
+++ frontend/src/wizards/common/LostChangesPrompt.tsx
@@ -6,12 +6,12 @@ import isEqual from 'lodash/isEqual'
 import { useTranslation } from '../../lib/acm-i18next'
 import { useItem } from '@patternfly-labs/react-form-wizard'
 
-export function LostChangesPrompt(props: { initialData?: any; data?: any }) {
+export function LostChangesPrompt(props: { initialData?: any; data?: any; wasSubmitted?: boolean }) {
   const { t } = useTranslation()
   const resources = useItem()
-  const { data = resources, initialData } = props
+  const { data = resources, initialData, wasSubmitted } = props
   const [originalData] = useState(initialData || data)
-  const dirty = !isEqual(data, originalData)
+  const dirty = !wasSubmitted && !isEqual(data, originalData)
   useBeforeunload(dirty ? (event) => event.preventDefault() : undefined)
   return <Prompt when={dirty} message={t('changes.maybe.lost')} />
 }
