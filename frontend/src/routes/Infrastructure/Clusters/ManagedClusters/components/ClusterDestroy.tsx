19ca1ece51d34f2695efb86842eec41d277cd8a5
Rastislav Wagner <rawagner@redhat.com>
2023-10-11 11:48:28 +0200

    Download installation logs button is disabled when destroying cluster (#3068)
    Signed-off-by: rawagner <rawagner@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/components/ClusterDestroy.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/components/ClusterDestroy.tsx
@@ -1,6 +1,6 @@
 /* Copyright Contributors to the Open Cluster Management project */
 import { useContext } from 'react'
-import { LogsDownloadButton } from '@openshift-assisted/ui-lib/cim'
+import { LogsDownloadButton, AgentClusterInstallK8sResource } from '@openshift-assisted/ui-lib/cim'
 import { Cluster, ClusterStatus } from '../../../../../resources'
 import { AcmButton, AcmPageProcess, Provider } from '../../../../../ui-components'
 import { ExternalLinkAltIcon } from '@patternfly/react-icons'
@@ -13,6 +13,32 @@ import { ButtonVariant } from '@patternfly/react-core'
 import { ClusterContext } from '../../../../../routes/Infrastructure/Clusters/ManagedClusters/ClusterDetails/ClusterDetails'
 import { useSharedAtoms, useRecoilState } from '../../../../../shared-recoil'
 
+const getLoadingMsgI18nKey = (
+  cluster: Cluster | undefined,
+  agentClusterInstall: AgentClusterInstallK8sResource | undefined
+) => {
+  const isHybrid =
+    cluster?.provider &&
+    [Provider.hostinventory, Provider.nutanix].includes(cluster?.provider) &&
+    !cluster?.isHypershift
+  if (cluster?.status === ClusterStatus.destroying && isHybrid) {
+    /*
+      t('destroying.ai.inprogress.message')
+      t('destroying.ai.nologs.inprogress.message')
+    */
+    return agentClusterInstall?.status?.debugInfo?.logsURL
+      ? 'destroying.ai.inprogress.message'
+      : 'destroying.ai.nologs.inprogress.message'
+  }
+  /*
+    t('detaching.inprogress.message')
+    t('detaching.success.message')
+    t('destroying.inprogress.message')
+    t('destroying.success.message')
+  */
+  return `${cluster?.status}.inprogress.message`
+}
+
 export function ClusterDestroy(props: { isLoading: boolean; cluster: Cluster }) {
   const { t } = useTranslation()
   const history = useHistory()
@@ -24,12 +50,6 @@ export function ClusterDestroy(props: { isLoading: boolean; cluster: Cluster }) 
     !props.cluster?.isHypershift
   const { agentClusterInstall } = useContext(ClusterContext)
 
-  /*
-        t('detaching.inprogress.message')
-        t('detaching.success.message')
-        t('destroying.inprogress.message')
-        t('destroying.success.message')
-    */
   const { loadingTitle, successTitle } =
     props.cluster.status === ClusterStatus.detaching
       ? {
@@ -46,7 +66,7 @@ export function ClusterDestroy(props: { isLoading: boolean; cluster: Cluster }) 
       loadingTitle={loadingTitle}
       loadingMessage={
         <Trans
-          i18nKey={`${props.cluster?.status}.inprogress.message`}
+          i18nKey={getLoadingMsgI18nKey(props.cluster, agentClusterInstall)}
           values={{ clusterName: props.cluster?.displayName }}
           components={{ bold: <strong /> }}
         />
@@ -77,11 +97,13 @@ export function ClusterDestroy(props: { isLoading: boolean; cluster: Cluster }) 
                 {t('view.logs')}
               </AcmButton>
             ) : (
-              <LogsDownloadButton
-                id="cluster-logs-button"
-                agentClusterInstall={agentClusterInstall}
-                variant={ButtonVariant.link}
-              />
+              agentClusterInstall?.status?.debugInfo?.logsURL && (
+                <LogsDownloadButton
+                  id="cluster-logs-button"
+                  agentClusterInstall={agentClusterInstall}
+                  variant={ButtonVariant.link}
+                />
+              )
             ))}
         </>
       }
