db28c60071d658aecff40b53e10293c9e93d5398
fxiang1 <fxiang@redhat.com>
2023-10-12 17:41:46 -0400

    Fix unit test
    Signed-off-by: fxiang1 <fxiang@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateClusterCatalog/CreateAWSControlPlane.test.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateClusterCatalog/CreateAWSControlPlane.test.tsx
@@ -5,11 +5,62 @@ import { RecoilRoot } from 'recoil'
 import { clickByTestId } from '../../../../../lib/test-util'
 import { NavigationPath } from '../../../../../NavigationPath'
 import { CreateAWSControlPlane } from './CreateAWSControlPlane'
+import { nockIgnoreApiPaths, nockList } from '../../../../../lib/nock-util'
+import { multiClusterEnginesState } from '../../../../../atoms'
+import { MultiClusterEngine, MultiClusterEngineApiVersion, MultiClusterEngineKind } from '../../../../../resources'
 
-describe('CreateAWSControlPlane', () => {
+const mockMultiClusterEngine: MultiClusterEngine = {
+  apiVersion: MultiClusterEngineApiVersion,
+  kind: MultiClusterEngineKind,
+  spec: {
+    availabilityConfig: 'High',
+    imagePullSecret: 'multiclusterhub-operator-pull-secret',
+    overrides: {
+      components: [
+        { enabled: true, name: 'hypershift-local-hosting' },
+        {
+          enabled: true,
+          name: 'hypershift',
+        },
+      ],
+    },
+    targetNamespace: 'multicluster-engine',
+    tolerations: [],
+  },
+  metadata: {
+    name: 'multiclusterengine',
+    generation: 2,
+  },
+  status: {
+    conditions: [
+      {
+        reason: 'ManagedClusterAddOnLeaseUpdated',
+        status: 'True',
+      },
+    ],
+  },
+}
+
+const listMulticlusterengines1 = {
+  req: {
+    apiVersion: 'multicluster.openshift.io/v1',
+    kind: 'multiclusterengines',
+  },
+  res: [],
+}
+
+describe('CreateAWSControlPlane hosted', () => {
+  beforeEach(() => {
+    nockIgnoreApiPaths()
+  })
+
   const Component = () => {
     return (
-      <RecoilRoot>
+      <RecoilRoot
+        initializeState={(snapshot) => {
+          snapshot.set(multiClusterEnginesState, [mockMultiClusterEngine])
+        }}
+      >
         <MemoryRouter initialEntries={[NavigationPath.createAWSControlPlane]}>
           <Route path={NavigationPath.createAWSControlPlane}>
             <CreateAWSControlPlane />
@@ -25,6 +76,8 @@ describe('CreateAWSControlPlane', () => {
   })
 
   test('can click standalone', async () => {
+    nockList(listMulticlusterengines1.req, listMulticlusterengines1.res)
+    nockList(listMulticlusterengines1.req, listMulticlusterengines1.res)
     render(<Component />)
     await clickByTestId('standalone')
   })
