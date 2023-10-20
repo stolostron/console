685b92da004ae1198dc0029c4a38957230f96d98
Kevin Cormier <kcormier@redhat.com>
2023-10-20 17:02:43 -0400

    ACM-7782 translation scripts (#3133)
    * Copy scripts from OpenShift console
    * Customize scripts for ACM
    * Remove hard-coded counts from singular strings
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/i18next-parser.config.js
+++ frontend/i18next-parser.config.js
@@ -1,116 +1,129 @@
 /* Copyright Contributors to the Open Cluster Management project */
 // i18next-parser.config.js
-
+const { readFileSync } = require('fs')
 const { CustomJSONLexer } = require('./i18n-scripts/lexers/json.js')
 
 const JavascriptLexer = { lexer: 'JavascriptLexer', functions: ['t', 'i18n'] }
 const JsxLexer = { lexer: 'JsxLexer', functions: ['t', 'i18n'] }
 
+const GENERATE = process.env.MODE === 'generate'
+
+let enTranslations
+
 module.exports = {
-    contextSeparator: '_',
-    // Key separator used in your translation keys
+  contextSeparator: '_',
+  // Key separator used in your translation keys
 
-    createOldCatalogs: false,
-    // Save the \_old files
+  createOldCatalogs: false,
+  // Save the \_old files
 
-    defaultNamespace: 'translation',
-    // Default namespace used in your i18next config
+  defaultNamespace: 'translation',
+  // Default namespace used in your i18next config
 
-    defaultValue: '',
-    // Default value to give to empty keys
-    // You may also specify a function accepting the locale, namespace, and key as arguments
+  defaultValue: (locale, _namespace, key) => {
+    if (locale === 'en') {
+      return key
+    } else {
+      if (!enTranslations) {
+        enTranslations = JSON.parse(readFileSync('public/locales/en/translation.json', 'utf-8'))
+      }
+      return enTranslations[key]
+    }
+  },
+  // Default value to give to empty keys
+  // You may also specify a function accepting the locale, namespace, and key as arguments
 
-    indentation: 2,
-    // Indentation of the catalog files
+  indentation: 2,
+  // Indentation of the catalog files
 
-    keepRemoved: false,
-    // Keep keys from the catalog that are no longer in code
+  keepRemoved: false,
+  // Keep keys from the catalog that are no longer in code
 
-    keySeparator: false,
-    // Key separator used in your translation keys
-    // If you want to use plain english keys, separators such as `.` and `:` will conflict. You might want to set `keySeparator: false` and `namespaceSeparator: false`. That way, `t('Status: Loading...')` will not think that there are a namespace and three separator dots for instance.
+  keySeparator: false,
+  // Key separator used in your translation keys
+  // If you want to use plain english keys, separators such as `.` and `:` will conflict. You might want to set `keySeparator: false` and `namespaceSeparator: false`. That way, `t('Status: Loading...')` will not think that there are a namespace and three separator dots for instance.
 
-    // see below for more details
-    lexers: {
-        hbs: ['HandlebarsLexer'],
-        handlebars: ['HandlebarsLexer'],
+  // see below for more details
+  lexers: {
+    hbs: ['HandlebarsLexer'],
+    handlebars: ['HandlebarsLexer'],
 
-        htm: ['HTMLLexer'],
-        html: ['HTMLLexer'],
+    htm: ['HTMLLexer'],
+    html: ['HTMLLexer'],
 
-        mjs: [JavascriptLexer],
-        js: [JavascriptLexer], // if you're writing jsx inside .js files, change this to JsxLexer
-        ts: [JavascriptLexer],
-        jsx: [JsxLexer],
-        tsx: [JsxLexer],
-        json: [CustomJSONLexer],
+    mjs: [JavascriptLexer],
+    js: [JavascriptLexer], // if you're writing jsx inside .js files, change this to JsxLexer
+    ts: [JavascriptLexer],
+    jsx: [JsxLexer],
+    tsx: [JsxLexer],
+    json: [CustomJSONLexer],
 
-        default: [JavascriptLexer],
-    },
+    default: [JavascriptLexer],
+  },
 
-    lineEnding: 'auto',
-    // Control the line ending. See options at https://github.com/ryanve/eol
+  lineEnding: 'auto',
+  // Control the line ending. See options at https://github.com/ryanve/eol
 
-    locales: ['en'],
-    // An array of the locales in your applications
+  locales: GENERATE ? ['ja', 'ko', 'zh'] : ['en'],
+  // An array of the locales in your applications
 
-    namespaceSeparator: '~',
-    // Namespace separator used in your translation keys
-    // If you want to use plain english keys, separators such as `.` and `:` will conflict. You might want to set `keySeparator: false` and `namespaceSeparator: false`. That way, `t('Status: Loading...')` will not think that there are a namespace and three separator dots for instance.
+  namespaceSeparator: '~',
+  // Namespace separator used in your translation keys
+  // If you want to use plain english keys, separators such as `.` and `:` will conflict. You might want to set `keySeparator: false` and `namespaceSeparator: false`. That way, `t('Status: Loading...')` will not think that there are a namespace and three separator dots for instance.
 
-    output: 'public/locales/$LOCALE/translation.json',
-    // Supports $LOCALE and $NAMESPACE injection
-    // Supports JSON (.json) and YAML (.yml) file formats
-    // Where to write the locale files relative to process.cwd()
+  output: GENERATE ? 'public/locales/upload/$LOCALE-translation.json' : 'public/locales/$LOCALE/translation.json',
+  // Supports $LOCALE and $NAMESPACE injection
+  // Supports JSON (.json) and YAML (.yml) file formats
+  // Where to write the locale files relative to process.cwd()
 
-    pluralSeparator: '_',
-    // Plural separator used in your translation keys
-    // If you want to use plain english keys, separators such as `_` might conflict. You might want to set `pluralSeparator` to a different string that does not occur in your keys.
+  pluralSeparator: '_',
+  // Plural separator used in your translation keys
+  // If you want to use plain english keys, separators such as `_` might conflict. You might want to set `pluralSeparator` to a different string that does not occur in your keys.
 
-    input: ['./src/**/*.tsx', './src/**/*.ts', './src/**/*.js', './plugins/**/console-extensions.json'],
-    // An array of globs that describe where to look for source files
-    // relative to the location of the configuration file
+  input: ['./src/**/*.tsx', './src/**/*.ts', './src/**/*.js', './plugins/**/console-extensions.json'],
+  // An array of globs that describe where to look for source files
+  // relative to the location of the configuration file
 
-    sort: true,
-    // Whether or not to sort the catalog. Can also be a [compareFunction](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#parameters)
+  sort: true,
+  // Whether or not to sort the catalog. Can also be a [compareFunction](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#parameters)
 
-    skipDefaultValues: false,
-    // Whether to ignore default values
-    // You may also specify a function accepting the locale and namespace as arguments
+  skipDefaultValues: false,
+  // Whether to ignore default values
+  // You may also specify a function accepting the locale and namespace as arguments
 
-    useKeysAsDefaultValue: true,
-    // Whether to use the keys as the default value; ex. "Hello": "Hello", "World": "World"
-    // This option takes precedence over the `defaultValue` and `skipDefaultValues` options
-    // You may also specify a function accepting the locale and namespace as arguments
+  useKeysAsDefaultValue: false,
+  // Whether to use the keys as the default value; ex. "Hello": "Hello", "World": "World"
+  // This option takes precedence over the `defaultValue` and `skipDefaultValues` options
+  // You may also specify a function accepting the locale and namespace as arguments
 
-    verbose: false,
-    // Display info about the parsing including some stats
+  verbose: false,
+  // Display info about the parsing including some stats
 
-    failOnWarnings: false,
-    // Exit with an exit code of 1 on warnings
+  failOnWarnings: false,
+  // Exit with an exit code of 1 on warnings
 
-    failOnUpdate: false,
-    // Exit with an exit code of 1 when translations are updated (for CI purpose)
+  failOnUpdate: false,
+  // Exit with an exit code of 1 when translations are updated (for CI purpose)
 
-    customValueTemplate: null,
-    // If you wish to customize the value output the value as an object, you can set your own format.
-    // ${defaultValue} is the default value you set in your translation function.
-    // Any other custom property will be automatically extracted.
-    //
-    // Example:
-    // {
-    //   message: "${defaultValue}",
-    //   description: "${maxLength}", // t('my-key', {maxLength: 150})
-    // }
+  customValueTemplate: null,
+  // If you wish to customize the value output the value as an object, you can set your own format.
+  // ${defaultValue} is the default value you set in your translation function.
+  // Any other custom property will be automatically extracted.
+  //
+  // Example:
+  // {
+  //   message: "${defaultValue}",
+  //   description: "${maxLength}", // t('my-key', {maxLength: 150})
+  // }
 
-    resetDefaultValueLocale: null,
-    // The locale to compare with default values to determine whether a default value has been changed.
-    // If this is set and a default value differs from a translation in the specified locale, all entries
-    // for that key across locales are reset to the default value, and existing translations are moved to
-    // the `_old` file.
+  resetDefaultValueLocale: null,
+  // The locale to compare with default values to determine whether a default value has been changed.
+  // If this is set and a default value differs from a translation in the specified locale, all entries
+  // for that key across locales are reset to the default value, and existing translations are moved to
+  // the `_old` file.
 
-    i18nextOptions: { compatibilityJSON: 'v3' },
-    // If you wish to customize options in internally used i18next instance, you can define an object with any
-    // configuration property supported by i18next (https://www.i18next.com/overview/configuration-options).
-    // { compatibilityJSON: 'v3' } can be used to generate v3 compatible plurals.
+  i18nextOptions: { compatibilityJSON: 'v3' },
+  // If you wish to customize options in internally used i18next instance, you can define an object with any
+  // configuration property supported by i18next (https://www.i18next.com/overview/configuration-options).
+  // { compatibilityJSON: 'v3' } can be used to generate v3 compatible plurals.
 }
