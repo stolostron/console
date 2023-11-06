8d10fdafadf0918cfe697f40f14e86f70644c842
Maggie Chen <magchen@redhat.com>
2023-11-06 11:25:10 -0500

    ACM-8448: fix clusterset filter (#3157)
    * ACM-8448: fix clusterset filter
    
    Signed-off-by: Maggie Chen <magchen@redhat.com>
    
    * fix render
    
    Signed-off-by: Maggie Chen <magchen@redhat.com>
    
    ---------
    
    Signed-off-by: Maggie Chen <magchen@redhat.com>

--- frontend/src/wizards/Argo/ArgoWizard.tsx
+++ frontend/src/wizards/Argo/ArgoWizard.tsx
@@ -428,9 +428,9 @@ export function ArgoWizard(props: ArgoWizardProps) {
                     if (placement?.spec?.clusterSets) {
                       if (placement?.spec?.clusterSets.length > 0) {
                         return placement?.spec?.clusterSets.includes(clusterSet.metadata?.name!)
-                      } else {
-                        return clusterSet
                       }
+                    } else {
+                      return clusterSet
                     }
                   })
 
@@ -752,10 +752,13 @@ function ArgoWizardPlacementSection(props: {
   const namespaceClusterSetNames =
     props.clusterSetBindings
       .filter((clusterSetBinding) =>
-        props.clusterSets?.find((clusterSet) => clusterSet.metadata?.name === clusterSetBinding.spec?.clusterSet)
+        props.clusterSets?.find(
+          (clusterSet) =>
+            clusterSet.metadata?.name === clusterSetBinding.spec?.clusterSet &&
+            clusterSetBinding.metadata?.namespace === applicationSet?.metadata?.namespace
+        )
       )
       .map((clusterSetBinding) => clusterSetBinding.spec?.clusterSet ?? '') ?? []
-
   const { update } = useData()
   return (
     <Section label={t('Placement')}>
