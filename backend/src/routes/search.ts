96a4cc8ff59f8edc4ee54e05d31751db7035e11f
zlayne <zlayne@redhat.com>
2024-01-24 11:38:15 -0500

    review updates
    Signed-off-by: zlayne <zlayne@redhat.com>

--- backend/src/routes/search.ts
+++ backend/src/routes/search.ts
@@ -31,9 +31,8 @@ export async function search(req: Http2ServerRequest, res: Http2ServerResponse):
 
     const searchUrl = process.env.SEARCH_API_URL || searchService
     const endpoint =
-      process.env.globalSearchFeatureFlag === 'enabled' && process.env.globalSearchAPIEndpoint
-        ? process.env.globalSearchAPIEndpoint || '/federated'
-        : '/searchapi/graphql'
+      process.env.searchApiEnpoint ||
+      (process.env.globalSearchFeatureFlag === 'enabled' ? '/federated' : '/searchapi/graphql')
     const url = new URL(searchUrl + endpoint)
     headers.authorization = `Bearer ${token}`
     headers.host = url.hostname
