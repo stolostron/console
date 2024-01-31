b6d16e9716958b9fef1f951ec155c09e50d6d20f
zlayne <zlayne@redhat.com>
2024-01-31 11:50:29 -0500

    Fix flicker on saved search card load
    Signed-off-by: zlayne <zlayne@redhat.com>

--- frontend/src/ui-components/AcmExpandable/AcmExpandableWrapper/AcmExpandableWrapper.tsx
+++ frontend/src/ui-components/AcmExpandable/AcmExpandableWrapper/AcmExpandableWrapper.tsx
@@ -2,12 +2,13 @@
 
 import { css } from '@emotion/css'
 import { Grid, GridItem, gridSpans, Title } from '@patternfly/react-core'
-import useResizeObserver from '@react-hook/resize-observer'
-import { Children, useMemo, useRef, useState } from 'react'
+import { Children, useEffect, useMemo, useRef, useState } from 'react'
 import { useTranslation } from '../../../lib/acm-i18next'
 import { AcmButton } from '../../AcmButton/AcmButton'
+import { useViewport } from '../../AcmCharts'
 
 type AcmExpandableWrapperProps = {
+  id: string
   headerLabel?: string
   children: React.ReactNode
   withCount: boolean
@@ -31,26 +32,29 @@ const showAllButton = css({
 })
 
 export const AcmExpandableWrapper = (props: AcmExpandableWrapperProps) => {
-  const { children, headerLabel, withCount, expandable, minWidth = 300, maxItemsPerRow = 6 } = props
+  const { id, children, headerLabel, withCount, expandable, minWidth = 300, maxItemsPerRow = 6 } = props
   const ref = useRef(null)
+  const viewPort = useViewport()
   const [showAll, setShowAll] = useState<boolean>(false)
-  const [columnCount, setColumnCount] = useState(1)
+  const [columnCount, setColumnCount] = useState(0)
   const { t } = useTranslation()
 
   const itemCount = useMemo(() => {
     return Children.count(children)
   }, [children])
 
-  useResizeObserver(ref, (entry) => {
+  useEffect(() => {
+    const section = document.querySelector(`#${id}`)
     let columns = 1
-    while ((columns + 1) * minWidth < entry.contentRect.width) {
+    while ((columns + 1) * minWidth < (section?.clientWidth ?? 0)) {
       columns++
     }
     if (columns > maxItemsPerRow) {
       columns = maxItemsPerRow
     }
     setColumnCount(columns)
-  })
+  }, [id, maxItemsPerRow, minWidth, viewPort.viewWidth])
+
   const spanPerColumn = useMemo(() => Math.floor(12 / columnCount) as gridSpans, [columnCount])
 
   const visibleItems = useMemo(() => {
@@ -60,33 +64,42 @@ export const AcmExpandableWrapper = (props: AcmExpandableWrapperProps) => {
     return children
   }, [children, expandable, showAll, columnCount])
 
+  {
+    /* Do not display until column count is calculated to avoid flashing child items */
+  }
   return (
-    <div className={root}>
-      {headerLabel && (
-        <Title headingLevel="h4">
-          {headerLabel}
-          {withCount && <span className={headerCount}> {`( ${itemCount} total )`}</span>}
-        </Title>
-      )}
-      <div ref={ref}>
-        <Grid hasGutter className={wrapperContainer}>
-          {Children.map(visibleItems, (child, idx) => {
-            return (
-              <GridItem span={spanPerColumn}>
-                <div key={`item-${idx}`}>{child}</div>
-              </GridItem>
-            )
-          })}
-        </Grid>
-      </div>
-      {expandable && itemCount > columnCount && (
-        <AcmButton className={showAllButton} variant={'secondary'} onClick={() => setShowAll(!showAll)}>
-          {showAll
-            ? t('Show less')
-            : t('Show all ({{count}})', {
-                count: itemCount,
+    <div id={id} ref={ref}>
+      {columnCount > 0 ? (
+        <div className={root}>
+          {headerLabel && (
+            <Title headingLevel="h4">
+              {headerLabel}
+              {withCount && <span className={headerCount}> {`( ${itemCount} total )`}</span>}
+            </Title>
+          )}
+          <div>
+            <Grid hasGutter className={wrapperContainer}>
+              {Children.map(visibleItems, (child, idx) => {
+                return (
+                  <GridItem span={spanPerColumn}>
+                    <div key={`item-${idx}`}>{child}</div>
+                  </GridItem>
+                )
               })}
-        </AcmButton>
+            </Grid>
+          </div>
+          {expandable && itemCount > columnCount && (
+            <AcmButton className={showAllButton} variant={'secondary'} onClick={() => setShowAll(!showAll)}>
+              {showAll
+                ? t('Show less')
+                : t('Show all ({{count}})', {
+                    count: itemCount,
+                  })}
+            </AcmButton>
+          )}
+        </div>
+      ) : (
+        <></>
       )}
     </div>
   )
