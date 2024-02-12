61cc2993084ff56577d58814a7a96b22f3031c06
Kevin Cormier <kcormier@redhat.com>
2024-02-12 15:53:55 -0500

    Remove DELETED event after broadcast to current clients
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- backend/src/routes/events.ts
+++ backend/src/routes/events.ts
@@ -488,7 +488,7 @@ function deleteResource(resource: IResource) {
   const existing = cache[uid]
   if (existing) ServerSideEvents.removeEvent(existing.eventID)
 
-  ServerSideEvents.pushEvent({
+  const deletedID = ServerSideEvents.pushEvent({
     data: {
       type: 'DELETED',
       object: {
@@ -498,6 +498,8 @@ function deleteResource(resource: IResource) {
       },
     },
   })
+  // after deletion has been broadcast to current clients, no need to retain
+  ServerSideEvents.removeEvent(deletedID)
 
   delete cache[uid]
 }
