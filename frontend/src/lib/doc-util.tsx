3261534b83377f909cd097fb7052451e81083830
rawagner <rawagner@redhat.com>
2024-04-04 15:21:23 +0200

    MGMT-16178: Update nutanix post-install doc
    Signed-off-by: rawagner <rawagner@redhat.com>

--- frontend/src/lib/doc-util.tsx
+++ frontend/src/lib/doc-util.tsx
@@ -62,7 +62,7 @@ export const DOC_LINKS = {
   ENABLE_OBSERVABILITY: `${DOC_HOME}/html/observability/observing-environments-intro#enabling-observability-service`,
   RHV_DEPRECATION: `${OCP_DOC_BASE_PATH}/4.13/html/release_notes/ocp-4-13-release-notes#ocp-4-13-rhv-deprecations`,
   ACCESSING_CONSOLE: `${DOC_HOME}/html/web_console/web-console#accessing-your-console`,
-  NUTANIX_POST_INSTALL: `https://access.redhat.com/documentation/en-us/assisted_installer_for_openshift_container_platform/2023/html/assisted_installer_for_openshift_container_platform/assembly_installing-on-nutanix#nutanix-post-installation-configuration_assembly_installing-on-nutanix`,
+  NUTANIX_POST_INSTALL: `https://access.redhat.com/documentation/en-us/assisted_installer_for_openshift_container_platform/2024/html/installing_openshift_container_platform_with_the_assisted_installer/assembly_installing-on-nutanix#nutanix-post-installation-configuration_installing-on-nutanix`,
 }
 
 export function ViewDocumentationLink(props: { doclink: string }): JSX.Element {
