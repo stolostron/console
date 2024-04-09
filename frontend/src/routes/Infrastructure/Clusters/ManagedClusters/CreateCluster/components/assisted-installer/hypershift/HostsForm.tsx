df41291577c646fa864c89a5e3301073aadcc2af
Julie <jgyselo@redhat.com>
2024-04-09 10:34:45 +0200

    Add infrastructure availability policy to the review step
    Signed-off-by: Julie <jgyselo@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/components/assisted-installer/hypershift/HostsForm.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/components/assisted-installer/hypershift/HostsForm.tsx
@@ -85,6 +85,13 @@ const HostsForm: React.FC<HostsFormProps> = ({ control, handleChange }) => {
       desc: control.active.olmCatalogPlacement === 'management' ? t('Management') : t('Guest'),
     },
     {
+      term: t('Infrastructure availability policy'),
+      desc:
+        control.active.infrastructureAvailabilityPolicy === 'HighlyAvailable'
+          ? t('Highly available')
+          : t('Single replica'),
+    },
+    {
       term: t('Node pools'),
       desc: control.active.nodePools.length,
     },
