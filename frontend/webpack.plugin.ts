72ba1c58df936d40b9c7b462cfb6e9dd0caa43bc
Kevin Cormier <kcormier@redhat.com>
2024-04-15 16:32:04 -0400

    Update dynamic plugin packages and minimum versions
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/webpack.plugin.ts
+++ frontend/webpack.plugin.ts
@@ -69,7 +69,7 @@ module.exports = function (env: any, argv: { hot?: boolean; mode: string | undef
       ],
     },
     plugins: [
-      new ConsoleRemotePlugin(),
+      new ConsoleRemotePlugin({ validateSharedModules: false }),
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production'),
         'process.env.REACT_APP_BACKEND_PATH': JSON.stringify('/multicloud'),
