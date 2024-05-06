9b551dfb81278048e3aa7a521cf659bd9b3a28f8
fxiang1 <fxiang@redhat.com>
2024-05-06 15:50:41 -0400

    Fix sonar bug
    Signed-off-by: fxiang1 <fxiang@redhat.com>

--- frontend/src/routes/Applications/CreateApplication/Subscription/transformers/transform-resources-to-controls.js
+++ frontend/src/routes/Applications/CreateApplication/Subscription/transformers/transform-resources-to-controls.js
@@ -155,7 +155,7 @@ const discoverChannelFromSource = (
       isDeprecatedPRControl.active = true
       const ruleName = _.get(templateObject, getSourcePath('Subscription[0].spec.placement.placementRef.name'))?.$v
       const rule = templateObject[PlacementRuleKind]
-        ? templateObject[PlacementRuleKind].find((rule) => ruleName === rule?.$raw?.metadata?.name)
+        ? templateObject[PlacementRuleKind].find((ruleItem) => ruleName === ruleItem?.$raw?.metadata?.name)
         : undefined
       deprecatedRuleControl.active = YAML.stringify(filterDeep(rule?.$raw))
     }
