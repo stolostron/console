df4f743b3f07390d3d9a65e6e321f9dfea46c32b
Kevin Cormier <kcormier@redhat.com>
2024-06-19 13:38:28 -0400

    Fix Storybook
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/ui-components/AcmPage/AcmPage.stories.tsx
+++ frontend/src/ui-components/AcmPage/AcmPage.stories.tsx
@@ -13,7 +13,6 @@ import {
 import '@patternfly/react-core/dist/styles/base.css'
 import { Meta } from '@storybook/react'
 import { Fragment, useState } from 'react'
-import { MemoryRouter } from 'react-router-dom-v5-compat'
 import { AlertGroupStory } from '../AcmAlert/AcmAlert.stories'
 import { AcmAutoRefreshSelect } from '../AcmAutoRefreshSelect/AcmAutoRefreshSelect'
 import { DescriptionList as DescriptionListStory } from '../AcmDescriptionList/AcmDescriptionList.stories'
@@ -56,130 +55,126 @@ export const Page = (args: {
   const [isActionDowndownOpen, setActionDowndownOpen] = useState(false)
   const [secondaryTab, setSecondaryTab] = useState('table')
   return (
-    <MemoryRouter>
-      <AcmPage
-        header={
-          <AcmPageHeader
-            breadcrumb={args.showBreadcrumb ? [{ text: 'AcmHeader' }, { text: 'AcmPage' }] : undefined}
-            title={args.title}
-            titleTooltip={
-              args.showTooltip && (
-                <>
-                  View all resources
-                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                  <a href="#" style={{ display: 'block', marginTop: '4px' }}>
-                    Learn more
-                  </a>
-                </>
-              )
-            }
-            popoverPosition="bottom"
-            popoverAutoWidth={true}
-            label={args.label}
-            labelColor={args.labelColor as AcmPageHeaderProps['labelColor']}
-            description={args.description}
-            switches={args.showSwitch && <Switch label="YAML on" labelOff="YAML off" />}
-            controls={
-              args.showControls && (
-                <Fragment>
-                  <AcmAutoRefreshSelect refetch={() => null} />
-                  <AcmRefreshTime
-                    timestamp={'Wed Jan 06 2021 00:00:00 GMT+0000 (Coordinated Universal Time)'}
-                    reloading={true}
-                  />
-                </Fragment>
-              )
-            }
-            actions={
-              args.showActions && (
-                <Dropdown
-                  isOpen={isActionDowndownOpen}
-                  toggle={
-                    <DropdownToggle onToggle={() => setActionDowndownOpen(!isActionDowndownOpen)}>
-                      Actions
-                    </DropdownToggle>
-                  }
-                  dropdownItems={[
-                    <DropdownItem component="button" key="1">
-                      Action 1
-                    </DropdownItem>,
-                    <DropdownItem component="button" key="2">
-                      Action 2
-                    </DropdownItem>,
-                  ]}
-                  onSelect={() => setActionDowndownOpen(!isActionDowndownOpen)}
+    <AcmPage
+      header={
+        <AcmPageHeader
+          breadcrumb={args.showBreadcrumb ? [{ text: 'AcmHeader' }, { text: 'AcmPage' }] : undefined}
+          title={args.title}
+          titleTooltip={
+            args.showTooltip && (
+              <>
+                View all resources
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <a href="#" style={{ display: 'block', marginTop: '4px' }}>
+                  Learn more
+                </a>
+              </>
+            )
+          }
+          popoverPosition="bottom"
+          popoverAutoWidth={true}
+          label={args.label}
+          labelColor={args.labelColor as AcmPageHeaderProps['labelColor']}
+          description={args.description}
+          switches={args.showSwitch && <Switch label="YAML on" labelOff="YAML off" />}
+          controls={
+            args.showControls && (
+              <Fragment>
+                <AcmAutoRefreshSelect refetch={() => null} />
+                <AcmRefreshTime
+                  timestamp={'Wed Jan 06 2021 00:00:00 GMT+0000 (Coordinated Universal Time)'}
+                  reloading={true}
                 />
-              )
-            }
-            navigation={
-              args.showNavigation && (
-                <AcmSecondaryNav>
-                  <AcmSecondaryNavItem isActive={secondaryTab === 'table'} onClick={() => setSecondaryTab('table')}>
-                    Table
-                  </AcmSecondaryNavItem>
-                  <AcmSecondaryNavItem isActive={secondaryTab === 'form'} onClick={() => setSecondaryTab('form')}>
-                    Form
-                  </AcmSecondaryNavItem>
-                  <AcmSecondaryNavItem
-                    isActive={secondaryTab === 'descriptionList'}
-                    onClick={() => setSecondaryTab('descriptionList')}
-                  >
-                    Details
-                  </AcmSecondaryNavItem>
-                  <AcmSecondaryNavItem isActive={secondaryTab === 'loading'} onClick={() => setSecondaryTab('loading')}>
-                    Loading
-                  </AcmSecondaryNavItem>
-                  <AcmSecondaryNavItem isActive={secondaryTab === 'alerts'} onClick={() => setSecondaryTab('alerts')}>
-                    Alerts
-                  </AcmSecondaryNavItem>
-                </AcmSecondaryNav>
-              )
-            }
-          />
-        }
-      >
-        {/* Each tab needs it's own AcmPageContent so it has its own ErrorBoundary and AlertGroup */}
-        {secondaryTab === 'table' ? (
-          <AcmPageContent id="table">
-            <PageSection>
-              <TableStory />
-            </PageSection>
-          </AcmPageContent>
-        ) : secondaryTab === 'form' ? (
-          <AcmPageContent id="form">
-            <PageSection>
-              <Card isLarge>
-                <CardBody>
-                  <FormStory />
-                </CardBody>
-              </Card>
-            </PageSection>
-          </AcmPageContent>
-        ) : secondaryTab === 'descriptionList' ? (
-          <AcmPageContent id="descriptionList">
-            <PageSection>
-              <DescriptionListStory />
-            </PageSection>
-          </AcmPageContent>
-        ) : secondaryTab === 'loading' ? (
-          <AcmPageContent id="loading">
-            <PageSection isFilled>
-              <Bullseye>
-                <LoadingPageStory />
-              </Bullseye>
-            </PageSection>
-          </AcmPageContent>
-        ) : secondaryTab === 'alerts' ? (
-          <AcmPageContent id="alerts">
-            <PageSection>
-              <AlertGroupStory />
-            </PageSection>
-          </AcmPageContent>
-        ) : (
-          <div />
-        )}
-      </AcmPage>
-    </MemoryRouter>
+              </Fragment>
+            )
+          }
+          actions={
+            args.showActions && (
+              <Dropdown
+                isOpen={isActionDowndownOpen}
+                toggle={
+                  <DropdownToggle onToggle={() => setActionDowndownOpen(!isActionDowndownOpen)}>Actions</DropdownToggle>
+                }
+                dropdownItems={[
+                  <DropdownItem component="button" key="1">
+                    Action 1
+                  </DropdownItem>,
+                  <DropdownItem component="button" key="2">
+                    Action 2
+                  </DropdownItem>,
+                ]}
+                onSelect={() => setActionDowndownOpen(!isActionDowndownOpen)}
+              />
+            )
+          }
+          navigation={
+            args.showNavigation && (
+              <AcmSecondaryNav>
+                <AcmSecondaryNavItem isActive={secondaryTab === 'table'} onClick={() => setSecondaryTab('table')}>
+                  Table
+                </AcmSecondaryNavItem>
+                <AcmSecondaryNavItem isActive={secondaryTab === 'form'} onClick={() => setSecondaryTab('form')}>
+                  Form
+                </AcmSecondaryNavItem>
+                <AcmSecondaryNavItem
+                  isActive={secondaryTab === 'descriptionList'}
+                  onClick={() => setSecondaryTab('descriptionList')}
+                >
+                  Details
+                </AcmSecondaryNavItem>
+                <AcmSecondaryNavItem isActive={secondaryTab === 'loading'} onClick={() => setSecondaryTab('loading')}>
+                  Loading
+                </AcmSecondaryNavItem>
+                <AcmSecondaryNavItem isActive={secondaryTab === 'alerts'} onClick={() => setSecondaryTab('alerts')}>
+                  Alerts
+                </AcmSecondaryNavItem>
+              </AcmSecondaryNav>
+            )
+          }
+        />
+      }
+    >
+      {/* Each tab needs it's own AcmPageContent so it has its own ErrorBoundary and AlertGroup */}
+      {secondaryTab === 'table' ? (
+        <AcmPageContent id="table">
+          <PageSection>
+            <TableStory />
+          </PageSection>
+        </AcmPageContent>
+      ) : secondaryTab === 'form' ? (
+        <AcmPageContent id="form">
+          <PageSection>
+            <Card isLarge>
+              <CardBody>
+                <FormStory />
+              </CardBody>
+            </Card>
+          </PageSection>
+        </AcmPageContent>
+      ) : secondaryTab === 'descriptionList' ? (
+        <AcmPageContent id="descriptionList">
+          <PageSection>
+            <DescriptionListStory />
+          </PageSection>
+        </AcmPageContent>
+      ) : secondaryTab === 'loading' ? (
+        <AcmPageContent id="loading">
+          <PageSection isFilled>
+            <Bullseye>
+              <LoadingPageStory />
+            </Bullseye>
+          </PageSection>
+        </AcmPageContent>
+      ) : secondaryTab === 'alerts' ? (
+        <AcmPageContent id="alerts">
+          <PageSection>
+            <AlertGroupStory />
+          </PageSection>
+        </AcmPageContent>
+      ) : (
+        <div />
+      )}
+    </AcmPage>
   )
 }
 Page.args = {
