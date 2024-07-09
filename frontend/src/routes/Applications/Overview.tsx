37db2a638acad344c445912244b97906003be4f5
John Swanke <jswanke@redhat.com>
2024-07-09 09:51:21 -0400

    get rid of Default OpenShift
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- frontend/src/routes/Applications/Overview.tsx
+++ frontend/src/routes/Applications/Overview.tsx
@@ -685,7 +685,6 @@ export default function ApplicationsOverview() {
             label: 'OpenShift',
             value: 'openshiftapps',
           },
-          { label: t('Default OpenShift'), value: 'openshift-default' },
           {
             label: t('Subscription'),
             value: `${getApiVersionResourceGroup(ApplicationApiVersion)}/${ApplicationKind}`,
@@ -695,21 +694,7 @@ export default function ApplicationsOverview() {
           return selectedValues.some((value) => {
             if (isOCPAppResource(item)) {
               const isFlux = isFluxApplication(item.label)
-              switch (value) {
-                case 'openshiftapps':
-                  return (
-                    !isFlux &&
-                    !item.metadata?.namespace?.startsWith('openshift-') &&
-                    item.metadata?.namespace !== 'openshift'
-                  )
-                case 'openshift-default':
-                  return (
-                    !isFlux &&
-                    (item.metadata?.namespace?.startsWith('openshift-') || item.metadata?.namespace === 'openshift')
-                  )
-                case 'fluxapps':
-                  return isFlux
-              }
+              return (value === 'fluxapps' && isFlux) || (value === 'openshiftapps' && !isFlux)
             } else {
               return selectedValues.includes(`${getApiVersionResourceGroup(item.apiVersion)}/${item.kind}`)
             }
