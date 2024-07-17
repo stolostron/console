5826fe39e9b1308cd058fb4b806d765936f70a85
Kevin Cormier <kcormier@redhat.com>
2024-07-17 15:43:03 -0400

    ACM-12772 Fix form match expressions required fields (#3668)
    * Update to @patternfly-labs/react-form-wizard@1.32.0
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Revert "Remove required for AppSet labels (#3601)"
    
    This reverts commit 3e4f63e3fba847545de183cfbcc5d02bcbdc8cde.
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    ---------
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/wizards/Placement/MatchExpression.tsx
+++ frontend/src/wizards/Placement/MatchExpression.tsx
@@ -28,12 +28,14 @@ export function MatchExpression(props: { labelValuesMap?: Record<string, string[
           path="key"
           options={Object.keys(labelValuesMap)}
           isCreatable
+          required
           onValueChange={(_value, item) => set(item as object, 'values', [])}
         />
       ) : (
         <WizTextInput
           label={t('Label')}
           path="key"
+          required
           onValueChange={(_value, item) => set(item as object, 'values', [])}
         />
       )}
@@ -46,6 +48,7 @@ export function MatchExpression(props: { labelValuesMap?: Record<string, string[
           { label: t('exists'), value: 'Exists' },
           { label: t('does not exist'), value: 'DoesNotExist' },
         ]}
+        required
         onValueChange={(value, item) => {
           switch (value) {
             case 'Exists':
@@ -66,6 +69,7 @@ export function MatchExpression(props: { labelValuesMap?: Record<string, string[
                 placeholder={t('Select the values')}
                 path="values"
                 isCreatable
+                required
                 hidden={(labelSelector) => !['In', 'NotIn'].includes(labelSelector?.operator)}
                 options={values}
               />
@@ -76,6 +80,7 @@ export function MatchExpression(props: { labelValuesMap?: Record<string, string[
         <WizStringsInput
           label={t('Values')}
           path="values"
+          required
           hidden={(labelSelector) => !['In', 'NotIn'].includes(labelSelector?.operator)}
         />
       )}
