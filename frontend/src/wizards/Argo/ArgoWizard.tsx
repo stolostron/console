3e2630d6222e6ab425e2e75f58ad3e122aef02e6
Kevin Cormier <kcormier@redhat.com>
2024-08-20 15:24:16 -0400

    react-form-wizard Select -> WizSelect
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/wizards/Argo/ArgoWizard.tsx
+++ frontend/src/wizards/Argo/ArgoWizard.tsx
@@ -22,9 +22,9 @@ import {
   EditMode,
   WizItemSelector,
   Section,
-  Select,
   Step,
   WizardCancel,
+  WizSelect,
   WizardSubmit,
   WizCheckbox,
   WizTextInput,
@@ -487,7 +487,7 @@ export function ArgoWizard(props: ArgoWizardProps) {
                 validation={validateAppSetName}
                 disabled={disableForm}
               />
-              <Select
+              <WizSelect
                 id="namespace"
                 path="metadata.namespace"
                 label={t('Argo server')}
@@ -547,7 +547,7 @@ export function ArgoWizard(props: ArgoWizardProps) {
                   }
                 }}
               />
-              <Select
+              <WizSelect
                 path="spec.generators.0.clusterDecisionResource.requeueAfterSeconds"
                 label={t('Requeue time')}
                 options={requeueTimes}
@@ -669,7 +669,7 @@ export function ArgoWizard(props: ArgoWizardProps) {
                 inputValueToPathValue={checkboxPrunePropagationPolicyToSyncOptions}
                 pathValueToInputValue={checkboxSyncOptionsToPrunePropagationPolicy}
               >
-                <Select
+                <WizSelect
                   label={t('Propagation policy')}
                   options={[
                     { label: t('foreground'), value: 'foreground' },
@@ -950,7 +950,7 @@ function ArgoWizardPlacementSection(props: {
         </WizItemSelector>
       ) : (
         <WizItemSelector selectKey="kind" selectValue="ApplicationSet">
-          <Select
+          <WizSelect
             path="spec.generators.0.clusterDecisionResource.labelSelector.matchLabels.cluster\.open-cluster-management\.io/placement"
             label={t('Existing placement')}
             placeholder={t('Select the existing placement')}
