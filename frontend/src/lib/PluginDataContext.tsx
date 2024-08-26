5660c36001fe0e3e44684ad6f8391a18e265f1ab
John Swanke <jswanke@redhat.com>
2024-08-26 14:18:35 -0400

    all done
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- frontend/src/lib/PluginDataContext.tsx
+++ frontend/src/lib/PluginDataContext.tsx
@@ -8,6 +8,7 @@ import * as recoil from 'recoil'
 import * as selectors from '../selectors'
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import * as reactQuery from '@tanstack/react-query'
+import { getBackendUrl } from '../resources'
 
 export type PluginData = {
   recoil: typeof recoil
@@ -38,19 +39,21 @@ export const PluginDataContext = createContext<PluginData>(defaultContext)
 export const usePluginDataContextValue = () => {
   const [loaded, setLoaded] = useState(false)
   const [startLoading, setStartLoading] = useState(false)
+  const backendUrl = getBackendUrl()
 
   const contextValue = useMemo(
     () => ({
       recoil,
       atoms,
       selectors,
+      backendUrl,
       reactQuery,
       loaded,
       startLoading,
       setLoaded,
       load: () => setStartLoading(true),
     }),
-    [loaded, startLoading]
+    [backendUrl, loaded, startLoading]
   )
   return contextValue
 }
