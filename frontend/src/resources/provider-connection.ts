9ced124c04e63bb04bd924c4e3da59dace2e2cde
vsanghishetty <vishali.kamenani@ymail.com>
2024-09-03 14:03:46 -0400

    adds data token cid csecret and auth methods
    Signed-off-by: vsanghishetty <vishali.kamenani@ymail.com>

--- frontend/src/resources/provider-connection.ts
+++ frontend/src/resources/provider-connection.ts
@@ -54,9 +54,6 @@ export interface ProviderConnectionStringData {
   clusterOSImage?: string
   disconnectedAdditionalTrustBundle?: string
 
-  // OpenShift Cluster Manager
-  ocmAPIToken?: string
-
   // OpenStack
   ['clouds.yaml']?: string
   cloud?: string
@@ -82,6 +79,13 @@ export interface ProviderConnectionStringData {
 
   host?: string
   token?: string
+
+  auth_method?: 'service-account' | 'offline-token'
+  // OpenShift Cluster Manager Service Account credentials
+  client_id?: string
+  client_secret?: string
+  // OpenShift Cluster Manager API Token
+  ocmAPIToken?: string
 }
 
 export interface ProviderConnection {
@@ -114,7 +118,9 @@ export function unpackProviderConnection(secret: ProviderConnection | Secret) {
     ...secret,
   } as ProviderConnection
   if (providerConnection.data) {
-    if (!providerConnection.stringData) providerConnection.stringData = {}
+    if (!providerConnection.stringData) {
+      providerConnection.stringData = {} as ProviderConnectionStringData
+    }
     const data = providerConnection.data as Record<string, string>
     const stringData = providerConnection.stringData as Record<string, string>
     for (const key in providerConnection.data) {
@@ -126,7 +132,9 @@ export function unpackProviderConnection(secret: ProviderConnection | Secret) {
 }
 
 export function packProviderConnection(providerConnection: ProviderConnection) {
-  if (!providerConnection.data) providerConnection.data = {}
+  if (!providerConnection.data) {
+    providerConnection.data = {} as ProviderConnectionStringData
+  }
   const data = providerConnection.data as Record<string, string>
   const stringData = providerConnection.stringData as Record<string, string>
   if (stringData !== undefined) {
