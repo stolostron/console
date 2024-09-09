fcd579edd5d90162ab7d31126b6ae9cdd15e49c0
John Swanke <jswanke@redhat.com>
2024-09-09 13:13:10 -0400

    sonar
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- frontend/src/routes/Home/Overview/OverviewPageBeta.tsx
+++ frontend/src/routes/Home/Overview/OverviewPageBeta.tsx
@@ -171,7 +171,7 @@ export default function OverviewPageBeta(props: { selectedClusterLabels: Record<
 
   const { criticalUpdateCount, warningUpdateCount, infoUpdateCount, clustersWithRiskPredictors } = useMemo(() => {
     const reducedUpgradeRiskPredictions = upgradeRiskPredictions.reduce((acc: any[], curr: any) => {
-      if (curr && curr?.body && curr.body.predictions) {
+      if (curr?.body && curr.body.predictions) {
         return [...acc, ...curr.body.predictions]
       }
       return acc
