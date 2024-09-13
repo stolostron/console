4cd11424338d6245faf23390ffa18ffb96e4f40d
vsanghishetty <vishali.kamenani@ymail.com>
2024-09-13 11:29:24 -0400

    Implement function to retrieve the last selected language from localStorage
    Signed-off-by: vsanghishetty <vishali.kamenani@ymail.com>

---
+++ frontend/src/resources/utils/getLastLanguage.ts
@@ -0,0 +1,5 @@
+/* Copyright Contributors to the Open Cluster Management project */
+import { LAST_LANGUAGE_LOCAL_STORAGE_KEY } from './constants'
+
+export const getLastLanguage = (): string => localStorage.getItem(LAST_LANGUAGE_LOCAL_STORAGE_KEY) ?? navigator.language
+console.log('GET LAST LANG : :', getLastLanguage())
