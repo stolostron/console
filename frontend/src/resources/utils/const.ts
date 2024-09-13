8eacce011ceee89f0524a18b508030b98f63e13c
vsanghishetty <vishali.kamenani@ymail.com>
2024-09-13 06:52:28 -0400

    adds local storage key
    Signed-off-by: vsanghishetty <vishali.kamenani@ymail.com>

--- frontend/src/resources/utils/const.ts
+++ frontend/src/resources/utils/const.ts
@@ -1,12 +1,4 @@
 // /* Copyright Contributors to the Open Cluster Management project */
-export const supportedLocales = {
-  en: 'English',
-  es: 'Español - Spanish',
-  fr: 'Français - French',
-  ko: '한국어 - Korean',
-  ja: '日本語 - Japanese',
-  'zh-CN': '中文 - Chinese (Simplified)',
-}
 
-export const LAST_LANGUAGE_LOCAL_STORAGE_KEY = 'bridge/last-language'
+export const LAST_LANGUAGE_LOCAL_STORAGE_KEY = 'i18nextLng'
 console.log(LAST_LANGUAGE_LOCAL_STORAGE_KEY)
