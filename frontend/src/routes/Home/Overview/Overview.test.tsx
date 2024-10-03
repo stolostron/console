f17d49d10786a0e0fb25b63682cef24669336c57
zlayne <zlayne@redhat.com>
2024-10-03 15:13:36 -0400

    update tests
    Signed-off-by: zlayne <zlayne@redhat.com>

--- frontend/src/routes/Home/Overview/Overview.test.tsx
+++ frontend/src/routes/Home/Overview/Overview.test.tsx
@@ -1,14 +1,14 @@
 /* Copyright Contributors to the Open Cluster Management project */
+import { MockedProvider } from '@apollo/client/testing'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { render } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom-v5-compat'
-import Overview from './Overview'
+import { RecoilRoot } from 'recoil'
+import { nockAggegateRequest, nockCreate, nockGet, nockIgnoreApiPaths } from '../../../lib/nock-util'
 import { PluginContext } from '../../../lib/PluginContext'
 import { PluginDataContext } from '../../../lib/PluginDataContext'
-import { render } from '@testing-library/react'
 import { clickByText, waitForNocks, waitForText } from '../../../lib/test-util'
-import { RecoilRoot } from 'recoil'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { MockedProvider } from '@apollo/client/testing'
-import { nockAggegateRequest, nockCreate, nockGet, nockIgnoreApiPaths, nockPostRequest } from '../../../lib/nock-util'
+import Overview from './Overview'
 import {
   getAddonRequest,
   getAddonResponse,
@@ -29,7 +29,6 @@ it('should render overview page with extension', async () => {
   const apiPathNock = nockIgnoreApiPaths()
   const getAddonNock = nockGet(getAddonRequest, getAddonResponse)
   const getManageedClusterAccessRequeset = nockCreate(mockGetSelfSubjectAccessRequest, mockGetSelfSubjectAccessResponse)
-  const metricNock = nockPostRequest('/metrics?overview-classic', {})
   nockAggegateRequest('statuses', statusAggregate.req, statusAggregate.res)
   render(
     <RecoilRoot>
@@ -77,14 +76,13 @@ it('should render overview page with extension', async () => {
   await clickByText('Test tab')
 
   await waitForText('Test extension content')
-  await waitForNocks([metricNock, getAddonNock, getManageedClusterAccessRequeset, apiPathNock])
+  await waitForNocks([getAddonNock, getManageedClusterAccessRequeset, apiPathNock])
 })
 
 it('should render overview page layout when extension tab crashes', async () => {
   const apiPathNock = nockIgnoreApiPaths()
   const getAddonNock = nockGet(getAddonRequest, getAddonResponse)
   const getManageedClusterAccessRequeset = nockCreate(mockGetSelfSubjectAccessRequest, mockGetSelfSubjectAccessResponse)
-  const metricNock = nockPostRequest('/metrics?overview-classic', {})
   nockAggegateRequest('statuses', statusAggregate.req, statusAggregate.res)
   render(
     <RecoilRoot>
@@ -134,5 +132,5 @@ it('should render overview page layout when extension tab crashes', async () => 
   await clickByText('Test tab')
 
   await waitForText('Overview')
-  await waitForNocks([metricNock, getAddonNock, getManageedClusterAccessRequeset, apiPathNock])
+  await waitForNocks([getAddonNock, getManageedClusterAccessRequeset, apiPathNock])
 })
