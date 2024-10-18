931e26bfe175c5c940a6c110af13b8047041dad5
John Swanke <jswanke@redhat.com>
2024-10-18 16:17:13 -0400

    ACM-14986-ArgoCD-apps-remain-on-Ul-forever-after-deletion (#3976)
    * ACM-14986-ArgoCD-apps-remain-on-Ul-forever-after-deletion
    
    Signed-off-by: John Swanke <jswanke@redhat.com>
    
    * fix
    
    Signed-off-by: John Swanke <jswanke@redhat.com>
    
    * fix
    
    Signed-off-by: John Swanke <jswanke@redhat.com>
    
    * oops
    
    Signed-off-by: John Swanke <jswanke@redhat.com>
    
    * Make wrapPromise generic and remove "reason" arg
    Change reject argument to Error
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * tweaks
    
    Signed-off-by: John Swanke <jswanke@redhat.com>
    
    ---------
    
    Signed-off-by: John Swanke <jswanke@redhat.com>
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    Co-authored-by: Kevin Cormier <kcormier@redhat.com>

--- backend/src/routes/aggregators/applications.ts
+++ backend/src/routes/aggregators/applications.ts
@@ -4,6 +4,7 @@ import { getOCPApps, isSystemApp, discoverSystemAppNamespacePrefixes } from './a
 import { getArgoApps } from './applicationsArgo'
 import { IResource } from '../../resources/resource'
 import { FilterSelections, ITransformedResource } from '../../lib/pagination'
+import { logger } from '../../lib/logger'
 
 export enum AppColumns {
   'name' = 0,
@@ -105,36 +106,78 @@ export function startAggregatingApplications() {
   void searchAPILoop()
 }
 
-async function searchAPILoop(): Promise<void> {
+// timeout failsafe to make sure search loop keeps running
+const SEARCH_TIMEOUT = 5 * 60 * 1000
+const promiseTimeout = <T>(promise: Promise<T>, delay: number) => {
+  let timeoutID: string | number | NodeJS.Timeout
+  const promises = [
+    new Promise<void>((_resolve, reject) => {
+      timeoutID = setTimeout(() => reject(new Error(`timeout of ${delay} exceeded`)), delay)
+    }),
+    promise.then((data) => {
+      clearTimeout(timeoutID)
+      return data
+    }),
+  ]
+  return Promise.race(promises)
+}
+
+async function searchAPILoop() {
   let pass = 1
   while (!stopping) {
-    await aggregateSearchAPIApplications(pass)
+    try {
+      await promiseTimeout(aggregateSearchAPIApplications(pass), SEARCH_TIMEOUT * 2).catch((e) =>
+        logger.error(`searchAPILoop exception ${e}`)
+      )
+    } catch (e) {
+      logger.error(`searchAPILoop exception ${e}`)
+    }
     pass++
   }
 }
 
 export function aggregateKubeApplications() {
   // ACM Apps
-  applicationCache['subscription'] = generateTransforms(
-    structuredClone(getKubeResources('Application', 'app.k8s.io/v1beta1'))
-  )
+  try {
+    applicationCache['subscription'] = generateTransforms(
+      structuredClone(getKubeResources('Application', 'app.k8s.io/v1beta1'))
+    )
+  } catch (e) {
+    logger.error(`aggregateKubeApplications subscription exception ${e}`)
+  }
 
   // AppSets
-  applicationCache['appset'] = generateTransforms(
-    structuredClone(getKubeResources('ApplicationSet', 'argoproj.io/v1alpha1'))
-  )
+  try {
+    applicationCache['appset'] = generateTransforms(
+      structuredClone(getKubeResources('ApplicationSet', 'argoproj.io/v1alpha1'))
+    )
+  } catch (e) {
+    logger.error(`aggregateKubeApplications appset exception ${e}`)
+  }
 }
 
+let argoAppSet = new Set<string>()
 export async function aggregateSearchAPIApplications(pass: number) {
   // Argo Apps
-  const argoAppSet = await getArgoApps(applicationCache, pass)
+  logger.info(`search begin ArgoCD`)
+  await promiseTimeout(getArgoApps(applicationCache, pass), SEARCH_TIMEOUT)
+    .then((data) => {
+      if (data) argoAppSet = data
+    })
+    .catch((e) => logger.error(`aggregateSearchAPIApplications ArgoCD exception ${e}`))
 
   // OCP Apps/FLUX
-  await getOCPApps(applicationCache, argoAppSet, MODE.ExcludeSystemApps, pass)
+  logger.info(`search begin Openshift/Flux`)
+  await promiseTimeout(getOCPApps(applicationCache, argoAppSet, MODE.ExcludeSystemApps, pass), SEARCH_TIMEOUT).catch(
+    (e) => logger.error(`aggregateSearchAPIApplications OCP/Flux exception ${e}`)
+  )
 
   // system apps -- because system apps shouldn't change much, don't do it every time
   if (pass <= 3 || pass % 3 === 0) {
-    await getOCPApps(applicationCache, argoAppSet, MODE.OnlySystemApps, pass)
+    logger.info(`search begin System`)
+    await promiseTimeout(getOCPApps(applicationCache, argoAppSet, MODE.OnlySystemApps, pass), SEARCH_TIMEOUT).catch(
+      (e) => logger.error(`aggregateSearchAPIApplications OCP/Flux exception ${e}`)
+    )
   }
 }
 
