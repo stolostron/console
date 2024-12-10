9a76e603a8438ddb569b1c4a1ff8e151a3237298
Yi Rae Kim <yikim@redhat.com>
2024-12-10 12:14:52 -0500

    Fix critical console bugs related to policies with exceptional templates (#4140)
    Ref: https://issues.redhat.com/browse/ACM-16238
    
    Signed-off-by: yiraeChristineKim <yikim@redhat.com>

--- frontend/src/routes/Governance/common/util.tsx
+++ frontend/src/routes/Governance/common/util.tsx
@@ -482,7 +482,7 @@ export function getPolicyRemediation(policy: Policy | undefined, propagatedPolic
   const remediationSet = new Set()
 
   templates?.forEach((template: PolicyTemplate) => {
-    const templateRemediation = template.objectDefinition.spec.remediationAction
+    const templateRemediation = template.objectDefinition.spec?.remediationAction
     remediationSet.add(templateRemediation)
     if (remediationSet.size === 3) {
       return
@@ -559,7 +559,7 @@ export function getPolicyRemediation(policy: Policy | undefined, propagatedPolic
 
 export function getPolicyTempRemediation(propagatedPolicy: Policy, template: PolicyTemplate | undefined): string {
   const propagatedRA = propagatedPolicy.spec.remediationAction
-  const objectRemediation = template?.objectDefinition.spec.remediationAction
+  const objectRemediation = template?.objectDefinition?.spec?.remediationAction
   return propagatedRA || objectRemediation || ''
 }
 
