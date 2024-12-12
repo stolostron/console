75c0324ea5ef2df7aa49fe3961e38de53c3f828c
Kevin Cormier <kcormier@redhat.com>
2024-12-12 14:07:52 -0500

    Implement AcmTimestamp and SimpleTimestamp
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

---
+++ frontend/src/lib/AcmTimestamp.tsx
@@ -0,0 +1,44 @@
+/* Copyright Contributors to the Open Cluster Management project */
+
+import { css } from '@emotion/css'
+import { SimpleTimestamp } from './SimpleTimestamp'
+import { PluginContext } from './PluginContext'
+import { useContext } from 'react'
+import { TimestampProps } from '@openshift-console/dynamic-plugin-sdk'
+
+type AcmTimestampProps = TimestampProps & {
+  showIcon?: boolean
+}
+
+const AcmTimestamp: React.FC<AcmTimestampProps> = ({
+  timestamp,
+  simple,
+  omitSuffix,
+  className = '',
+  showIcon = false,
+}) => {
+  const {
+    ocpApi: { Timestamp },
+  } = useContext(PluginContext)
+
+  return Timestamp ? (
+    <Timestamp
+      timestamp={timestamp}
+      simple={simple}
+      omitSuffix={omitSuffix}
+      className={`${className}${
+        !showIcon
+          ? ` ${css({
+              '.co-icon-and-text__icon': {
+                display: 'none',
+              },
+            })}`
+          : ''
+      }`}
+    />
+  ) : (
+    <SimpleTimestamp timestamp={timestamp} />
+  )
+}
+
+export default AcmTimestamp
