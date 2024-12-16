3c8a1778f87884914b43cf8cc210516c1ade04eb
Zack Layne <zlayne@redhat.com>
2024-12-16 18:52:22 -0500

    [ACM-16080] Dynamic VM actions in search results and details page (#4145)
    * [ACM-16080] Dynamic VM actions in search results and details page
    
    Signed-off-by: zlayne <zlayne@redhat.com>
    
    * add test for detailspage
    
    Signed-off-by: zlayne <zlayne@redhat.com>
    
    ---------
    
    Signed-off-by: zlayne <zlayne@redhat.com>

--- frontend/src/routes/Search/Details/DetailsPage.test.tsx
+++ frontend/src/routes/Search/Details/DetailsPage.test.tsx
@@ -5,40 +5,12 @@
 import { render, screen, waitFor } from '@testing-library/react'
 import { MemoryRouter, Route, Routes } from 'react-router-dom-v5-compat'
 import { RecoilRoot } from 'recoil'
+import { settingsState } from '../../../atoms'
 import { nockGet, nockIgnoreApiPaths, nockIgnoreRBAC, nockPostRequest } from '../../../lib/nock-util'
 import { waitForNocks } from '../../../lib/test-util'
 import { NavigationPath } from '../../../NavigationPath'
 import Search from '../Search'
 import { getResourceParams } from './DetailsPage'
-
-jest.mock('react-router-dom-v5-compat', () => {
-  const originalModule = jest.requireActual('react-router-dom-v5-compat')
-  return {
-    __esModule: true,
-    ...originalModule,
-    useLocation: () => ({
-      pathname: '/multicloud/search/resources',
-      search:
-        '?cluster=local-cluster&kind=Pod&apiversion=v1&namespace=testNamespace&name=testLocalPod&_hubClusterResource=true',
-      state: {
-        from: '/multicloud/search',
-        fromSearch: '?filters={%22textsearch%22:%22kind%3APod%22}',
-      },
-    }),
-    useNavigate: () => jest.fn(),
-  }
-})
-Object.defineProperty(window, 'location', {
-  value: {
-    pathname: '/multicloud/search/resources',
-    search:
-      '?cluster=local-cluster&kind=Pod&apiversion=v1&namespace=testNamespace&name=testLocalPod&_hubClusterResource=true',
-    state: {
-      from: '/multicloud/search',
-      fromSearch: '?filters={%22textsearch%22:%22kind%3APod%2',
-    },
-  },
-})
 
 const mockLocalClusterPod = {
   kind: 'Pod',
@@ -91,12 +63,47 @@ const mockLocalClusterPod = {
 
 describe('DetailsPage', () => {
   beforeEach(async () => {
+    // jest.resetAllMocks()
     nockIgnoreRBAC()
     nockIgnoreApiPaths()
+  })
+  afterEach(() => {
+    jest.resetAllMocks()
+    // Object.defineProperty(window, 'location', {
+    //   value: {},
+    // })
   })
   const metricNock = nockPostRequest('/metrics?search-details', {})
 
   it('should render local-cluster resource details correctly', async () => {
+    jest.mock('react-router-dom-v5-compat', () => {
+      const originalModule = jest.requireActual('react-router-dom-v5-compat')
+      return {
+        __esModule: true,
+        ...originalModule,
+        useLocation: () => ({
+          pathname: '/multicloud/search/resources',
+          search:
+            '?cluster=local-cluster&kind=Pod&apiversion=v1&namespace=testNamespace&name=testLocalPod&_hubClusterResource=true',
+          state: {
+            from: '/multicloud/search',
+            fromSearch: '?filters={%22textsearch%22:%22kind%3APod%22}',
+          },
+        }),
+        useNavigate: () => jest.fn(),
+      }
+    })
+    Object.defineProperty(window, 'location', {
+      value: {
+        pathname: '/multicloud/search/resources',
+        search:
+          '?cluster=local-cluster&kind=Pod&apiversion=v1&namespace=testNamespace&name=testLocalPod&_hubClusterResource=true',
+        state: {
+          from: '/multicloud/search',
+          fromSearch: '?filters={%22textsearch%22:%22kind%3APod%2',
+        },
+      },
+    })
     render(
       <RecoilRoot>
         <MemoryRouter initialEntries={[NavigationPath.resources]}>
@@ -147,5 +154,89 @@ describe('DetailsPage', () => {
     })
     const res = getResourceParams()
     expect(res).toMatchSnapshot()
+  })
+
+  it('should render VirtualMachine resource details correctly', async () => {
+    jest.mock('react-router-dom-v5-compat', () => {
+      const originalModule = jest.requireActual('react-router-dom-v5-compat')
+      return {
+        __esModule: true,
+        ...originalModule,
+        useLocation: () => ({
+          pathname: '/multicloud/search/resources',
+          search:
+            '?cluster=local-cluster&kind=VirtualMachine&apiversion=kubevirt.io/v1&namespace=openshift-cnv&name=test-vm&_hubClusterResource=true',
+          state: {
+            from: '/multicloud/search',
+            fromSearch: '?filters={%22textsearch%22:%22kind%3AVirtualMachine%22}',
+          },
+        }),
+        useNavigate: () => jest.fn(),
+      }
+    })
+    Object.defineProperty(window, 'location', {
+      value: {
+        pathname: '/multicloud/search/resources',
+        search:
+          '?cluster=local-cluster&kind=VirtualMachine&apiversion=kubevirt.io/v1&namespace=openshift-cnv&name=test-vm&_hubClusterResource=true',
+        state: {
+          from: '/multicloud/search',
+          fromSearch: '?filters={%22textsearch%22:%22kind%3AVirtualMachine%2',
+        },
+      },
+    })
+    render(
+      <RecoilRoot
+        initializeState={(snapshot) => {
+          snapshot.set(settingsState, { VIRTUAL_MACHINE_ACTIONS: 'enabled' })
+        }}
+      >
+        <MemoryRouter initialEntries={[NavigationPath.resources]}>
+          <Routes>
+            <Route path={`${NavigationPath.search}/*`} element={<Search />} />
+          </Routes>
+        </MemoryRouter>
+      </RecoilRoot>
+    )
+
+    // Wait for delete resource requests to finish
+    await waitForNocks([
+      metricNock,
+      nockGet({
+        apiVersion: 'kubevirt.io/v1',
+        kind: 'VirtualMachine',
+        metadata: {
+          creationTimestamp: '2024-10-02T20:02:14Z',
+          name: 'test-vm',
+          namespace: 'openshift-cnv',
+        },
+        spec: {
+          running: true,
+          template: {
+            metadata: {
+              creationTimestamp: null,
+            },
+            spec: {},
+          },
+          status: {
+            created: true,
+            desiredGeneration: 9,
+            observedGeneration: 9,
+            printableStatus: 'Running',
+            ready: true,
+            runStrategy: 'Always',
+          },
+        },
+      }),
+    ])
+
+    // Test that the component has rendered correctly with data
+    await waitFor(() =>
+      expect(
+        screen.getByRole('heading', {
+          name: /test-vm/i,
+        })
+      ).toBeTruthy()
+    )
   })
 })
