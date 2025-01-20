3eaaf4f675f6c0d9c6066c7cde7b461437f061de
Nitin Dhevar <nitin.dhanabalan@gmail.com>
2025-01-20 15:37:04 -0500

    fixing fuzzy search highlight issues
    Signed-off-by: Nitin Dhevar <nitin.dhanabalan@gmail.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/ManagedClusters.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/ManagedClusters.tsx
@@ -730,9 +730,14 @@ export function useClusterNamespaceColumn(): IAcmTableColumn<Cluster> {
       'Standalone clusters will display the namespace used by the ManagedCluster resource. Hosted clusters will display the hosting namespace when the status is "Pending import" and the ManagedCluster namespace when the status is "Ready".'
     ),
     sort: 'namespace',
-    cell: (cluster, search) => {
-      return <HighlightSearchText text={cluster.namespace ?? '-'} searchText={search} isTruncate />
-    },
+    search: (cluster) => [cluster.namespace as string, cluster.hive.clusterPoolNamespace as string],
+    cell: (cluster, search) => (
+      <>
+        <span style={{ whiteSpace: 'nowrap' }}>
+          <HighlightSearchText text={cluster.namespace} searchText={search} isTruncate />
+        </span>
+      </>
+    ),
     exportContent: (cluster) => cluster.namespace,
   }
 }
