ff0c017cfd70b7dfb442576bbe1a2354b74fb535
Kevin Cormier <kcormier@redhat.com>
2025-01-24 09:41:44 -0500

    Revert "Make monaco globally accessible for testing (#4170)" (#4225)
    This reverts commit fc39a0f64ab480bc6c99cd5e1fab0713275811df.
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -106,7 +106,7 @@ module.exports = function (env: any, argv: { hot?: boolean; mode: string | undef
             files: ['./src/**/*.{ts,tsx,js,jsx}'],
           },
         }),
-      new MonacoWebpackPlugin({ languages: ['yaml'], globalAPI: true }),
+      new MonacoWebpackPlugin({ languages: ['yaml'] }),
       isProduction &&
         new CopyPlugin({
           patterns: [{ from: 'public', globOptions: { ignore: ['**/*.html', '**/translation.json'] } }],
