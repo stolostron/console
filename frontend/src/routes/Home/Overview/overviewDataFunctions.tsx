955dd291ecf7592106f9b2c77bd8eacd50344a77
zlayne <zlayne@redhat.com>
2025-02-07 13:29:32 -0500

    Fix overview page addon filtering
    Signed-off-by: zlayne <zlayne@redhat.com>

--- frontend/src/routes/Home/Overview/overviewDataFunctions.tsx
+++ frontend/src/routes/Home/Overview/overviewDataFunctions.tsx
@@ -3,6 +3,7 @@ import { PrometheusResponse } from '@openshift-console/dynamic-plugin-sdk'
 import { CheckCircleIcon, ExclamationCircleIcon, UnknownIcon } from '@patternfly/react-icons'
 import { TFunction } from 'react-i18next'
 
+import { Icon } from '@patternfly/react-core'
 import { IResultStatuses } from '../../../lib/useAggregates'
 import { NavigationPath } from '../../../NavigationPath'
 import { Addon, AddonStatus, Cluster, Policy, PolicyReport, PolicyReportResults } from '../../../resources'
@@ -447,23 +448,23 @@ export function getAddonHealth(
     {
       key: t('Degraded'),
       value: addonData['Degraded'].count,
-      link: `${NavigationPath.managedClusters}?addons=Degraded`,
+      link: `${NavigationPath.managedClusters}?add-ons=Degraded`,
     },
     {
       key: t('Progressing'),
       value: addonData['Progressing'].count,
-      link: `${NavigationPath.managedClusters}?addons=Progressing`,
+      link: `${NavigationPath.managedClusters}?add-ons=Progressing`,
     },
     {
       key: t('Unknown'),
       value: addonData['Unknown'].count,
-      link: `${NavigationPath.managedClusters}?addons=Unknown`,
+      link: `${NavigationPath.managedClusters}?add-ons=Unknown`,
     },
     {
       key: t('Available'),
       value: addonData['Available'].count,
       isPrimary: true,
-      link: `${NavigationPath.managedClusters}?addons=Available`,
+      link: `${NavigationPath.managedClusters}?add-ons=Available`,
     },
   ]
 }
