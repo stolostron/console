d10fbef52a9cd6917b7383fcec875f600930a05b
jgyselov <jgyselov@redhat.com>
2025-02-27 10:59:11 +0100

    Update Assisted Installer UI Lib to 2.14.3-cim
    Signed-off-by: jgyselov <jgyselov@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/components/assisted-installer/utils.test.ts
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/components/assisted-installer/utils.test.ts
@@ -87,7 +87,7 @@ describe('networking patch utils', () => {
       apiVip: '10.10.10.10',
       ingressVip: '10.10.10.10',
     })
-    expect(patches.length).toBe(6)
+    expect(patches.length).toBe(4)
   })
 })
 
