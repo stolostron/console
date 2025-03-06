71f0b0184f30d1aea319178806a7fd1040b9d233
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2025-03-06 16:19:31 +0100

    Fix VAP policies getting shown in parameter resources table (#4315)
    VAP policies getting shown in parameter resources table of VAPB template details page. VAP policy should not appear in the list.
    Ref: https://issues.redhat.com/browse/ACM-18105
    Signed-off-by: yiraeChristineKim <yikim@redhat.com>
    
    Co-authored-by: yiraeChristineKim <yikim@redhat.com>

--- frontend/src/routes/Governance/policies/policy-details/PolicyTemplateDetail/PolicyTemplateDetailHooks.tsx
+++ frontend/src/routes/Governance/policies/policy-details/PolicyTemplateDetail/PolicyTemplateDetailHooks.tsx
@@ -67,7 +67,14 @@ export function useFetchVapb() {
   return { vapbItems: data?.searchResult?.[0]?.items, loading, err: error?.message }
 }
 
-const availableKindList = ['ValidatingAdmissionPolicyBinding', 'Assign', 'AssignImage', 'AssignMetadata', 'ModifySet']
+const availableKindList = [
+  'ValidatingAdmissionPolicyBinding',
+  'ValidatingAdmissionPolicy',
+  'Assign',
+  'AssignImage',
+  'AssignMetadata',
+  'ModifySet',
+]
 const availableApiGroups = ['admissionregistration.k8s.io', 'mutations.gatekeeper.sh']
 
 export function useFetchOnlyRelatedResources() {
