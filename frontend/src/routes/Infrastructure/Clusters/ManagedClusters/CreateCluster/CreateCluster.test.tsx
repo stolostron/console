8a4bd4d947c222f3bcb0fb9c2e2dee547d8f1ab7
Julie Gyselova <jgyselov@redhat.com>
2025-03-27 10:55:06 +0100

    Bump AI Lib to 2.14.3-cim (#4364)
    * Update Assisted Installer UI Lib to 2.14.4-cim
    
    Signed-off-by: jgyselov <jgyselov@redhat.com>
    
    * Tweak tests
    
    Signed-off-by: jgyselov <jgyselov@redhat.com>
    
    ---------
    
    Signed-off-by: jgyselov <jgyselov@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/CreateCluster.test.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/CreateCluster.test.tsx
@@ -1163,7 +1163,7 @@ describe('CreateCluster on premise', () => {
       await typeByTestId('form-input-name-field', clusterName)
       await typeByTestId('form-input-baseDnsDomain-field', baseDomain)
 
-      await waitForText('OpenShift 4.8.15') // single value of combobox
+      await waitForText('OpenShift 4.8.15-x86_64') // single value of combobox
       await typeByTestId('additionalLabels', 'myLabelKey=myValue')
       await clickByTestId('form-input-pullSecret-field')
 
