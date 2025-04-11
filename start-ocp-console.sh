fed09126255db0b9ba8ec8494c780263a8ea8b76
Kevin Cormier <kcormier@redhat.com>
2025-04-11 16:37:34 -0400

    Fix startup script for 4.19 (#4400)
    * Specify KUBEVIRT_PORT with npm run plugins to include kubevirt-plugin
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Make scripts compatible with OCP 4.19
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Document KUBEVIRT_PORT variable
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Delete commented code
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    ---------
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- start-ocp-console.sh
+++ start-ocp-console.sh
@@ -6,25 +6,25 @@ source ./port-defaults.sh
 source ./oauth-client-name.sh
 
 CONSOLE_VERSION=${CONSOLE_VERSION:=4.17}
+KUBEVIRT_PORT=${KUBEVIRT_PORT:=""}
 CONSOLE_IMAGE="quay.io/openshift/origin-console:${CONSOLE_VERSION}"
 
 mkdir -p ocp-console
 oc get oauthclient $OAUTH_CLIENT_NAME -o jsonpath='{.secret}' > ocp-console/console-client-secret
-
-INSTALLATION_NAMESPACE_MCE=`oc get multiclusterengine -A -o jsonpath='{.items[0].spec.targetNamespace}'`
-oc get secrets -n $INSTALLATION_NAMESPACE_MCE --field-selector type=kubernetes.io/service-account-token -o json | \
-    jq '.items[0].data."ca.crt"' -r | python -m base64 -d > ocp-console/ca.crt
+oc extract secret/off-cluster-token -n openshift-console --to ocp-console --confirm
 
 echo "Starting local OpenShift console..."
 
 BRIDGE_BASE_ADDRESS="http://localhost:${CONSOLE_PORT}"
+
 BRIDGE_USER_AUTH="openshift"
 BRIDGE_K8S_MODE="off-cluster"
-BRIDGE_K8S_AUTH="bearer-token"
 BRIDGE_CA_FILE="/tmp/ca.crt"
 BRIDGE_USER_AUTH_OIDC_CLIENT_ID=$OAUTH_CLIENT_NAME
 BRIDGE_USER_AUTH_OIDC_CLIENT_SECRET_FILE="/tmp/console-client-secret"
 BRIDGE_USER_AUTH_OIDC_CA_FILE="/tmp/ca.crt"
+BRIDGE_K8S_MODE_OFF_CLUSTER_SERVICE_ACCOUNT_BEARER_TOKEN_FILE="/tmp/token"
+
 BRIDGE_K8S_MODE_OFF_CLUSTER_SKIP_VERIFY_TLS=true
 BRIDGE_K8S_MODE_OFF_CLUSTER_ENDPOINT=$(oc whoami --show-server)
 # The monitoring operator is not always installed (e.g. for local OpenShift). Tolerate missing config maps.
@@ -51,8 +51,11 @@ echo "Console URL: http://localhost:${CONSOLE_PORT}"
 
 function getBridgePlugins {
     local host=$1
-    local endpoint="https://${host}:${BACKEND_PORT}"
-    echo "mce=http://${host}:${MCE_PORT},acm=http://${host}:${ACM_PORT}"
+    local kubevirt=""
+    if [ -n "$KUBEVIRT_PORT" ]; then
+        kubevirt=",kubevirt-plugin=http://${host}:${KUBEVIRT_PORT}"
+    fi
+    echo "mce=http://${host}:${MCE_PORT},acm=http://${host}:${ACM_PORT}${kubevirt}"
 }
 
 function getBridgePluginProxy {
@@ -67,8 +70,7 @@ if [ -x "$(command -v podman)" ]; then
         # Use host networking on Linux since host.containers.internal is unreachable in some environments.
         HOST="localhost"
         podman run \
-            -v $PWD/ocp-console/console-client-secret:/tmp/console-client-secret:Z \
-            -v $PWD/ocp-console/ca.crt:/tmp/ca.crt:Z \
+            -v $PWD/ocp-console:/tmp:Z \
             --pull always --rm -p "$CONSOLE_PORT":9000 \
             --rm --network=host \
             --env-file <(set | grep BRIDGE) \
@@ -78,8 +80,7 @@ if [ -x "$(command -v podman)" ]; then
     else
         HOST="host.containers.internal"
         podman run \
-            -v $PWD/ocp-console/console-client-secret:/tmp/console-client-secret \
-            -v $PWD/ocp-console/ca.crt:/tmp/ca.crt \
+            -v $PWD/ocp-console:/tmp \
             --pull always --rm -p "$CONSOLE_PORT":9000 \
             --env-file <(set | grep BRIDGE) \
             --env BRIDGE_PLUGINS="$(getBridgePlugins $HOST)" \
@@ -90,8 +91,7 @@ if [ -x "$(command -v podman)" ]; then
 else
     HOST="host.docker.internal"
     docker run \
-        -v $PWD/ocp-console/console-client-secret:/tmp/console-client-secret \
-        -v $PWD/ocp-console/ca.crt:/tmp/ca.crt \
+        -v $PWD/ocp-console:/tmp \
         --pull always --rm -p "$CONSOLE_PORT":9000 \
         --env-file <(set | grep BRIDGE) \
         --env BRIDGE_PLUGINS="$(getBridgePlugins $HOST)" \
