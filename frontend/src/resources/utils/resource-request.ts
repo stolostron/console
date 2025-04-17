63cc234bd69c7a8d538b82789d84a4283fb10a7c
John Swanke <jswanke@redhat.com>
2025-04-17 09:22:29 -0400

    fix tests
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- frontend/src/resources/utils/resource-request.ts
+++ frontend/src/resources/utils/resource-request.ts
@@ -66,7 +66,7 @@ export function isRequestAbortedError(error: any) {
 
 export function getBackendUrl() {
   if (process.env.NODE_ENV === 'test') {
-    return process.env.JEST_DEFAULT_HOST
+    return process.env.JEST_DEFAULT_HOST ?? ''
   }
   if (process.env.MODE === 'plugin') {
     const proxyPath = process.env.PLUGIN_PROXY_PATH
