6889cffa9690dcebe48f18d9fe359b2d4f03d535
Kevin Cormier <kcormier@redhat.com>
2025-04-17 17:04:55 -0400

    TEMP Typescript workarounds
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/routes/Search/Details/DetailsPage.tsx
+++ frontend/src/routes/Search/Details/DetailsPage.tsx
@@ -236,7 +236,7 @@ export default function DetailsPage() {
       )
     }
     if (vmActionsEnabled && kind.toLowerCase() === 'virtualmachine') {
-      const printableStatus = resource?.status?.printableStatus ?? ''
+      const printableStatus = (resource as any)?.status?.printableStatus ?? ''
       actions.unshift(
         ...createVMDropdownItems([
           printableStatus === 'Stopped'
