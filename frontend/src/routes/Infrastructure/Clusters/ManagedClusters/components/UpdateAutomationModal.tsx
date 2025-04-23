0ce7ca8aa0edfdc7c30e5705051536d929729032
Nitin Dhevar <nitin.dhanabalan@gmail.com>
2025-04-23 16:19:34 -0400

    updating translation text
    Signed-off-by: Nitin Dhevar <nitin.dhanabalan@gmail.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/components/UpdateAutomationModal.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/components/UpdateAutomationModal.tsx
@@ -256,7 +256,7 @@ export function UpdateAutomationModal(props: {
                 variant="warning"
                 title={t('{{count}} cluster cannot be edited ', { count: nonUpdatableCount })}
                 message={t(
-                  'Automation is only supported for Red Hat OpenShift clusters that are not provisioned by a managed Kubernetes service. The automation template can be updated only for ready clusters that do not have an upgrade in progress.'
+                  'Automation is only supported for Red Hat OpenShift clusters that are not provisioned by a managed Kubernetes service and managed Azure Red Hat OpenShift cluster. The automation template can be updated only for ready clusters that do not have an upgrade in progress.'
                 )}
                 isInline
                 noClose
