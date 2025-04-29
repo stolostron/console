15b07647e4e57e6f708faead7e2ba11719880a75
Enrique Mingorance Cano <emingora@redhat.com>
2025-04-29 10:23:12 +0200

    mocking AccessControl response
    Signed-off-by: Enrique Mingorance Cano <emingora@redhat.com>

--- backend/src/lib/server-side-events.ts
+++ backend/src/lib/server-side-events.ts
@@ -344,6 +344,8 @@ export class ServerSideEvents {
           break
         case 'Search':
         case 'Secret':
+        // // TODO: is other the proper case for AccessControl?
+        case 'AccessControl':
           other.push(event)
           break
         default:
---
+++ backend/src/routes/mockResponses.ts
@@ -0,0 +1,158 @@
+/* Copyright Contributors to the Open Cluster Management project */
+/* eslint-disable no-constant-condition */
+
+export const accessControlResponse = {
+    metadata: { _continue: '', resourceVersion: '1', continue: "" },
+    items: [
+        {
+            apiVersion: 'clusterview.open-cluster-management.io/v11',
+            kind: 'AccessControl',
+            metadata: {
+                annotations: {},
+                creationTimestamp: '2025-04-28T07:08:26Z',
+                finalizers: [{}],
+                generation: 3,
+                labels: [{}],
+                managedFields: [{}],
+                name: 'd9316f95-7138-4c45-8a92-38fa25923111',
+                namespace: 'jg-test-1',
+                resourceVersion: '227021',
+                uid: '99b9ba60-f9be-490c-97c8-2a12cc3aed11'
+            },
+            data: {
+                id: '2b84f22b-e4b3-4d8b-9d0d-f20dbb87cb20',
+                namespaces: ["ns1", "ns2"],
+                clusters: ["cluster1", "cluster2"],
+                users: ["user1", "user2"],
+                roles: ["role1", "role2"],
+                creationTimestamp: '2025-04-01T04:56:17Z',
+                isActive: 'true',
+            },
+        },
+        {
+            apiVersion: 'clusterview.open-cluster-management.io/v11',
+            kind: 'AccessControl',
+            metadata: {
+                annotations: {},
+                creationTimestamp: '2025-04-28T07:08:26Z',
+                finalizers: [{}],
+                generation: 3,
+                labels: [{}],
+                managedFields: [{}],
+                name: 'd9316f95-7138-4c45-8a92-38fa25923112',
+                namespace: 'jg-test-1',
+                resourceVersion: '227021',
+                uid: '99b9ba60-f9be-490c-97c8-2a12cc3aed12'
+            },
+            data: {
+                id: '2b84f22b-e4b3-4d8b-9d0d-f20dbb87cb21',
+                namespaces: ["*"],
+                clusters: ["*"],
+                users: ["*"],
+                roles: ["*"],
+                creationTimestamp: '2025-04-02T04:56:17Z',
+                isActive: 'true',
+            },
+        },
+        {
+            apiVersion: 'clusterview.open-cluster-management.io/v11',
+            kind: 'AccessControl',
+            metadata: {
+                annotations: {},
+                creationTimestamp: '2025-04-28T07:08:26Z',
+                finalizers: [{}],
+                generation: 3,
+                labels: [{}],
+                managedFields: [{}],
+                name: 'd9316f95-7138-4c45-8a92-38fa25923113',
+                namespace: 'jg-test-1',
+                resourceVersion: '227021',
+                uid: '99b9ba60-f9be-490c-97c8-2a12cc3aed13'
+            },
+            data: {
+                id: '2b84f22b-e4b3-4d8b-9d0d-f20dbb87cb22',
+                namespaces: ["ns1"],
+                clusters: ["cluster1"],
+                users: ["user1"],
+                roles: ["role1", "role2"],
+                creationTimestamp: '2025-04-03T04:56:17Z',
+                isActive: 'true',
+            },
+        },
+        {
+            apiVersion: 'clusterview.open-cluster-management.io/v11',
+            kind: 'AccessControl',
+            metadata: {
+                annotations: {},
+                creationTimestamp: '2025-04-28T07:08:26Z',
+                finalizers: [{}],
+                generation: 3,
+                labels: [{}],
+                managedFields: [{}],
+                name: 'd9316f95-7138-4c45-8a92-38fa25923114',
+                namespace: 'jg-test-1',
+                resourceVersion: '227021',
+                uid: '99b9ba60-f9be-490c-97c8-2a12cc3aed14'
+            },
+            data: {
+                id: '2b84f22b-e4b3-4d8b-9d0d-f20dbb87cb23',
+                namespaces: ["ns1", "ns2"],
+                clusters: ["cluster1", "cluster2"],
+                groups: ["group1", "group2"],
+                roles: ["role1", "role2"],
+                creationTimestamp: '2025-04-04T04:56:17Z',
+                isActive: 'true',
+            },
+        },
+        {
+            apiVersion: 'clusterview.open-cluster-management.io/v11',
+            kind: 'AccessControl',
+            metadata: {
+                annotations: {},
+                creationTimestamp: '2025-04-28T07:08:26Z',
+                finalizers: [{}],
+                generation: 3,
+                labels: [{}],
+                managedFields: [{}],
+                name: 'd9316f95-7138-4c45-8a92-38fa25923115',
+                namespace: 'jg-test-1',
+                resourceVersion: '227021',
+                uid: '99b9ba60-f9be-490c-97c8-2a12cc3aed15'
+            },
+            data: {
+                id: '2b84f22b-e4b3-4d8b-9d0d-f20dbb87cb24',
+                namespaces: ["ns1", "ns2"],
+                clusters: ["cluster1", "cluster2"],
+                users: ["user1", "user2"],
+                roles: ["role1", "role2"],
+                creationTimestamp: '2025-04-05T04:56:17Z',
+                isActive: 'false',
+            },
+        },
+        {
+            apiVersion: 'clusterview.open-cluster-management.io/v11',
+            kind: 'AccessControl',
+            metadata: {
+                annotations: {},
+                creationTimestamp: '2025-04-28T07:08:26Z',
+                finalizers: [{}],
+                generation: 3,
+                labels: [{}],
+                managedFields: [{}],
+                name: 'd9316f95-7138-4c45-8a92-38fa25923116',
+                namespace: 'jg-test-1',
+                resourceVersion: '227021',
+                uid: '99b9ba60-f9be-490c-97c8-2a12cc3aed16'
+            },
+            data: {
+                id: '2b84f22b-e4b3-4d8b-9d0d-f20dbb87cb25',
+                namespaces: ["*"],
+                clusters: ["*"],
+                groups: ["*"],
+                roles: ["*"],
+                creationTimestamp: '2025-04-06T04:56:17Z',
+                isActive: 'true',
+            },
+        },
+    ]
+}
