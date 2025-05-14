142bc34b870960467e0a4b227daa88b9a9eab227
Nitin Dhevar <nitin.dhanabalan@gmail.com>
2025-05-14 17:05:47 -0400

    Removing compat and eslint recommended
    Signed-off-by: Nitin Dhevar <nitin.dhanabalan@gmail.com>

--- frontend/packages/eslint-config/index.mjs
+++ frontend/packages/eslint-config/index.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'eslint/config'
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat'
+import { fixupPluginRules } from '@eslint/compat'
 import react from 'eslint-plugin-react'
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
 import jest from 'eslint-plugin-jest'
@@ -8,32 +8,9 @@ import prettier from 'eslint-plugin-prettier'
 import jsxA11Y from 'eslint-plugin-jsx-a11y'
 import globals from 'globals'
 import tsParser from '@typescript-eslint/parser'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-})
 
 export default defineConfig([
   {
-    extends: fixupConfigRules(
-      compat.extends(
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:jest/recommended',
-        'plugin:react-hooks/recommended',
-        'prettier',
-        'plugin:jsx-a11y/recommended'
-      )
-    ),
-
     plugins: {
       react,
       '@typescript-eslint': fixupPluginRules(typescriptEslint),
