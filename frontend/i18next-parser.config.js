9ce0b561577130f0a012a4dde672fa965291cca2
vsanghishetty <vishali.kamenani@ymail.com>
2025-05-15 13:46:33 -0400

    updates frontend/i18next-parser.config.js to find translation strings in new extension files
    Signed-off-by: vsanghishetty <vishali.kamenani@ymail.com>

--- frontend/i18next-parser.config.js
+++ frontend/i18next-parser.config.js
@@ -80,7 +80,16 @@ module.exports = {
   // Plural separator used in your translation keys
   // If you want to use plain english keys, separators such as `_` might conflict. You might want to set `pluralSeparator` to a different string that does not occur in your keys.
 
-  input: ['./src/**/*.tsx', './src/**/*.ts', './src/**/*.js', './plugins/**/console-extensions.json'],
+  input: [
+    './src/**/*.tsx',
+    './src/**/*.ts',
+    './src/**/*.js',
+    './plugins/**/console-extensions.json',
+    './plugins/**/*.tsx',
+    './plugins/**/*.ts',
+    './plugins/**/*.js',
+    './plugins/**/console-plugin-metadata.ts'
+  ],
   // An array of globs that describe where to look for source files
   // relative to the location of the configuration file
 
