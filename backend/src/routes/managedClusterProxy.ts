1694f8639b3c9113cf4c3e9b3db69968de31f290
Kevin Cormier <kcormier@redhat.com>
2025-05-22 09:08:44 -0400

    Fix host/origin for managedClusterProxy
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- backend/src/routes/managedClusterProxy.ts
+++ backend/src/routes/managedClusterProxy.ts
@@ -10,8 +10,6 @@ import { getMultiClusterEngine } from '../lib/multi-cluster-engine'
 import proxy from 'http2-proxy'
 import { TLSSocket } from 'tls'
 
-const REMOVE_HEADERS = [constants.HTTP2_HEADER_HOST, 'origin']
-
 export async function managedClusterProxy(req: Http2ServerRequest, res: Http2ServerResponse): Promise<void>
 export async function managedClusterProxy(req: Http2ServerRequest, socket: TLSSocket, head: Buffer): Promise<void>
 export async function managedClusterProxy(
@@ -58,12 +56,9 @@ export async function managedClusterProxy(
 
     req.url = `/${managedCluster}/${apiPath}`
 
-    for (const header of REMOVE_HEADERS) {
-      if (req.headers[header]) {
-        delete req.headers[header]
-      }
-    }
     req.headers[constants.HTTP2_HEADER_AUTHORIZATION] = `Bearer ${managedClusterToken}`
+    req.headers[constants.HTTP2_HEADER_HOST] = proxyHost
+    req.headers['origin'] = `https://${proxyHost}`
 
     logger.info(`HOSTNAME: ${proxyHost} PORT: ${proxyPort}`)
 
@@ -93,74 +88,5 @@ export async function managedClusterProxy(
     } else {
       resOrSocket.destroy()
     }
-  }
-}
-
-export async function managedClusterProxyWS(req: Http2ServerRequest, socket: TLSSocket, head: Buffer): Promise<void> {
-  const token = await getAuthenticatedToken(req, socket)
-  if (!token) return
-
-  const serviceAccountToken = getServiceAccountToken()
-
-  // expected path is /managedclusterproxy/<managedClusterName>/<apiPath>
-  const path = req.url
-  const splitPath = path.split('/')
-  const managedCluster = splitPath[2]
-  const apiPath = splitPath.slice(3).join('/')
-
-  const hasAuth = await canAccess(
-    {
-      kind: 'ManagedClusterAction',
-      apiVersion: 'action.open-cluster-management.io/v1beta1',
-      metadata: { namespace: managedCluster },
-    },
-    'create',
-    token
-  )
-
-  if (!hasAuth) {
-    logger.error({ msg: 'Unauthorized request...' })
-    socket.destroy()
-  }
-
-  try {
-    const managedClusterToken: string = await getManagedClusterToken(managedCluster, serviceAccountToken)
-    const mce = await getMultiClusterEngine()
-    const proxyService = `cluster-proxy-addon-user.${mce?.spec?.targetNamespace || 'multicluster-engine'}.svc.cluster.local`
-    const proxyHost = process.env.CLUSTER_PROXY_ADDON_USER_HOST || proxyService
-    const proxyPort = process.env.CLUSTER_PROXY_ADDON_USER_HOST ? 443 : 9092
-
-    req.url = `/${managedCluster}/${apiPath}`
-
-    for (const header of REMOVE_HEADERS) {
-      if (req.headers[header]) {
-        const value = Array.isArray(req.headers[header]) ? req.headers[header].join(', ') : req.headers[header]
-        delete req.headers[header]
-      }
-    }
-    req.headers[constants.HTTP2_HEADER_AUTHORIZATION] = `Bearer ${managedClusterToken}`
-
-    logger.info(`HOSTNAME: ${proxyHost} PORT: ${proxyPort}`)
-
-    await proxy.ws(
-      req,
-      socket,
-      head,
-      {
-        protocol: 'https',
-        hostname: proxyHost,
-        port: proxyPort,
-        ca: getServiceCACertificate(),
-      },
-      (err) => {
-        if (err) {
-          logger.error(err)
-          throw err
-        }
-      }
-    )
-  } catch (err) {
-    logger.error(err)
-    socket.destroy()
   }
 }
