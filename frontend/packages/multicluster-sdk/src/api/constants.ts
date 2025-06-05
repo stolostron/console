4f046d05f4f1ffaf190b473e71548e9f99f55dbb
Ugo Palatucci <upalatuc@redhat.com>
2025-06-05 10:56:49 +0200

    Add event stream with cluster support, remove fetchRetry from sdk
    Signed-off-by: Ugo Palatucci <upalatuc@redhat.com>

---
+++ frontend/packages/multicluster-sdk/src/api/constants.ts
@@ -0,0 +1,2 @@
+/* Copyright Contributors to the Open Cluster Management project */
+export const BASE_K8S_API_PATH = '/api/kubernetes'
--- frontend/packages/multicluster-sdk/src/api/utils/api-resource-list.ts
+++ frontend/packages/multicluster-sdk/src/api/utils/api-resource-list.ts
@@ -1,33 +1,15 @@
 /* Copyright Contributors to the Open Cluster Management project */
 
-import { fetchGet, getBackendUrl } from './fetchRetry'
-
-const versionUrl = '/apiPaths'
-
-export interface APIResourceNames {
-  [kind: string]: APIResourceMeta
-}
-
-export interface APIResourceMeta {
-  [kind: string]: { pluralName: string }
-}
-
-export interface IRequestResult<ResultType = unknown> {
-  promise: Promise<ResultType>
-  abort: () => void
-}
-
-export function getRequest<ResultT>(url: string | Promise<string>): IRequestResult<ResultT> {
-  const abortController = new AbortController()
-  return {
-    promise: Promise.resolve(url).then((url) =>
-      fetchGet<ResultT>(url, abortController.signal).then((result: { data: any }) => result.data)
-    ),
-    abort: () => abortController.abort(),
-  }
-}
-
-export function getApiPaths() {
-  const url = getBackendUrl() + versionUrl
-  return getRequest<APIResourceNames>(url)
+export function getBackendUrl() {
+  return Promise.resolve('/api/proxy/plugin/acm/console/multicloud')
+  // if (process.env.NODE_ENV === 'test') {
+  //   return process.env.JEST_DEFAULT_HOST ?? ''
+  // }
+  // if (process.env.MODE === 'plugin') {
+  //   const proxyPath = process.env.PLUGIN_PROXY_PATH
+  //   const value = proxyPath ? `${proxyPath}${process.env.REACT_APP_BACKEND_PATH}` : ''
+  //   return value
+  // }
+  // /* istanbul ignore next */
+  // return process.env.REACT_APP_BACKEND_PATH ?? ''
 }
