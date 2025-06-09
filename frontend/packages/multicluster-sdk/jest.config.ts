455cd6edd1412a5c81fd66800a57a917a605d8f9
Nitin Dhevar <nitin.dhanabalan@gmail.com>
2025-06-09 10:13:40 -0400

    Setting up jest testing for multicluster-sdk package
    Signed-off-by: Nitin Dhevar <nitin.dhanabalan@gmail.com>

---
+++ frontend/packages/multicluster-sdk/jest.config.ts
@@ -0,0 +1,46 @@
+/* Copyright Contributors to the Open Cluster Management project */
+
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest/presets/js-with-ts',
+  testEnvironment: 'jsdom',
+  testResultsProcessor: 'jest-sonar-reporter',
+  watchAll: false,
+  slowTestThreshold: 30,
+  watchPathIgnorePatterns: ['<rootDir>/../node_modules', '<rootDir>/../.eslintcache', '<rootDir>/../coverage'],
+  moduleFileExtensions: ['js', 'json', 'jsx', 'node', 'ts', 'tsx', 'css'],
+  transform: {
+    '^.+\\.[jt]sx?$': [
+      'ts-jest',
+      {
+        isolatedModules: true,
+        tsconfig: '<rootDir>/tsconfig.dev.json',
+      },
+    ],
+    '^.+\\.yaml?$': '<rootDir>/jest-raw-loader.js',
+    // '^.+\\.hbs$': 'jest-raw-loader',
+    '^.+\\.hbs$': '<rootDir>/jest-raw-loader.js',
+    // '\\.(css|less)$': 'jest-raw-loader',
+    '\\.(css|less|scss)$': '<rootDir>/jest-raw-loader.js',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!d3*|internmap|robust-predicates|react-monaco-editor|@openshift-assisted|lodash-es|@patternfly/react-tokens|@patternfly/react-icons|@patternfly/react-user-feedback|@patternfly/react-icons|@patternfly-labs/react-form-wizard|@juggle/resize-observer|@react-hook/*|uuid|@openshift-console/dynamic-plugin-sdk*|screenfull)',
+  ],
+  ci: true,
+  collectCoverage: true,
+  coverageDirectory: './coverage',
+  coverageReporters: ['text-summary', 'html', ['lcov', { projectRoot: '../' }]],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{tsx,ts,jsx,js}',
+    '<rootDir>/src/*.{tsx,ts,jsx,js}',
+    '!<rootDir>/src/**/*.test.{tsx,ts,jsx,js}',
+    '!<rootDir>/src/*.test.{tsx,ts,jsx,js}',
+    '!<rootDir>/node_modules/**',
+  ],
+  reporters: ['default'],
+  verbose: true,
+  bail: false,
+}
+
+export default config
