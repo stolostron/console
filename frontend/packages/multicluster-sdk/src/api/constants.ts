da77be27dcfc7187b49e5325827a6e042c0ae0df
Ugo Palatucci <upalatuc@redhat.com>
2025-06-10 18:15:53 +0200

    treeview
    Signed-off-by: Ugo Palatucci <upalatuc@redhat.com>

--- frontend/packages/multicluster-sdk/src/api/constants.ts
+++ frontend/packages/multicluster-sdk/src/api/constants.ts
@@ -1,2 +1,3 @@
 /* Copyright Contributors to the Open Cluster Management project */
 export const BASE_K8S_API_PATH = '/api/kubernetes'
+export const BASE_FLEET_SEARCH_PATH = '/multicloud/search/resources'
