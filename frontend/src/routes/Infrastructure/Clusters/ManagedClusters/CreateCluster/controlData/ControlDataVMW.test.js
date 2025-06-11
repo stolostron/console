5f89d61c25aca4d04c92a1d95b9c20239e567354
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2025-06-11 16:37:37 +0200

    [release-2.13] ACM-21378 Update vSphere install config syntax (#4597)
    * Update vSphere install-config format
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Fix tests
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    * Add test coverage
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    
    ---------
    
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>
    Co-authored-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/controlData/ControlDataVMW.test.js
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/CreateCluster/controlData/ControlDataVMW.test.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 Red Hat, Inc.
 // Copyright Contributors to the Open Cluster Management project
 'use strict'
-import { getControlDataVMW } from './ControlDataVMW'
+import { clusterPath, datastorePath, folderPath, getControlDataVMW, resourcePoolPath } from './ControlDataVMW'
 import i18next from 'i18next'
 
 const t = i18next.t.bind(i18next)
@@ -18,5 +18,30 @@ describe('Cluster creation control data for VMW', () => {
 
   it('generates correctly for MCE', () => {
     expect(getControlDataVMW(t, handleModalToggle, true, false, false)).toMatchSnapshot()
+  })
+})
+
+describe('vSphere Handlebars helpers', () => {
+  it('clusterPath can generate absolute value', () => {
+    expect(clusterPath('cluster-name', 'datacenter-name', {})).toBe('/datacenter-name/host/cluster-name')
+  })
+  it('clusterPath accepts user-supplied absolute value', () => {
+    expect(clusterPath('/custom-datacenter/path/cluster-name', 'datacenter-name', {})).toBe(
+      '/custom-datacenter/path/cluster-name'
+    )
+  })
+  it('clusterPath can generate relative value', () => {
+    expect(clusterPath('/custom-datacenter/path/cluster-name', 'datacenter-name', true, {})).toBe('cluster-name')
+  })
+  it('datastorePath can generate absolute value', () => {
+    expect(datastorePath(['my-datastore'], ['datacenter-name'], {})).toBe('/datacenter-name/datastore/my-datastore')
+  })
+  it('folderPath can generate absolute value', () => {
+    expect(folderPath(['folder/subfolder'], ['datacenter-name'], {})).toBe('/datacenter-name/vm/folder/subfolder')
+  })
+  it('resourcePoolPath can generate absolute value', () => {
+    expect(resourcePoolPath(['pool'], 'datacenter-name', ['the-cluster'], {})).toBe(
+      '/datacenter-name/host/the-cluster/Resources/pool'
+    )
   })
 })
