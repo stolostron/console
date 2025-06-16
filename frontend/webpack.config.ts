110f87dffc3c25e4b24b0d28c3ca72e310336aea
Yi Rae Kim <yikim@redhat.com>
2025-06-16 03:36:49 -0400

    Add API to get CPU, memory, and storage usage (#4576)
    We should display fields such as CPU usage, Memory usage and Disk usage. To display CPU, memory, and storage usage in the UI, we need to implement a backend API that provides this resource usage data.
    Ref: https://issues.redhat.com/browse/ACM-21179
    
    Signed-off-by: yiraeChristineKim <yikim@redhat.com>

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -178,6 +178,7 @@ module.exports = function (env: any, argv: { hot?: boolean; mode: string | undef
         '/multicloud/virtualmachinesnapshots',
         '/multicloud/virtualmachinerestores',
         '/multicloud/multiclusterhub/components',
+        '/multicloud/vmResourceUsage',
       ].map((backendPath) => ({
         path: backendPath,
         target: `https://localhost:${process.env.BACKEND_PORT}`,
