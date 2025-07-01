eaf987da6d4e3409ec45f34a95190d0102abdba5
Oksana Bazylieva <obazylie@redhat.com>
2025-06-30 20:13:56 -0400

    ACM-21518 Live Migration - UI Implementation - Wizard and modal
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>

---
+++ frontend/src/wizards/Migration/useMigrationFormState.ts
@@ -0,0 +1,54 @@
+/* Copyright Contributors to the Open Cluster Management project */
+import { useState } from 'react'
+
+export function useMigrationFormState() {
+  const [srcCluster] = useState('sno-2-b9657')
+  const [srcNs] = useState('Namespace')
+
+  const [dstCluster, setDstCluster] = useState('')
+  const [dstNamespace, setDstNamespace] = useState('')
+
+  const [srcNetwork, setSrcNetwork] = useState('network1')
+  const [dstNetwork, setDstNetwork] = useState('')
+  const [srcStorage, setSrcStorage] = useState('')
+  const [dstStorage, setDstStorage] = useState('')
+  const [srcCompute, setSrcCompute] = useState('')
+  const [dstCompute, setDstCompute] = useState('')
+  const [storageUsed, setStorageUsed] = useState(111)
+  const [storageReserved, setStorageReserved] = useState(6)
+  const [storageTotal, setStorageTotal] = useState(238)
+
+  const [openDstCluster, setOpenDstCluster] = useState(false)
+  const [openDstNamespace, setOpenDstNamespace] = useState(false)
+
+  return {
+    srcCluster,
+    srcNs,
+    dstCluster,
+    setDstCluster,
+    dstNamespace,
+    setDstNamespace,
+    srcNetwork,
+    setSrcNetwork,
+    dstNetwork,
+    setDstNetwork,
+    srcStorage,
+    setSrcStorage,
+    dstStorage,
+    setDstStorage,
+    srcCompute,
+    setSrcCompute,
+    dstCompute,
+    setDstCompute,
+    openDstCluster,
+    setOpenDstCluster,
+    openDstNamespace,
+    setOpenDstNamespace,
+    storageUsed,
+    setStorageUsed,
+    storageReserved,
+    setStorageReserved,
+    storageTotal,
+    setStorageTotal,
+  }
+}
