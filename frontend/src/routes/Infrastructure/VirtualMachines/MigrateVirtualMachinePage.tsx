f3496676af71be299db0b03b35ced4aad9b0951a
Oksana Bazylieva <obazylie@redhat.com>
2025-06-30 20:13:56 -0400

    ACM-21518 Live Migration - UI Implementation - Wizard and modal
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>

--- frontend/src/routes/Infrastructure/VirtualMachines/MigrateVirtualMachinePage.tsx
+++ frontend/src/routes/Infrastructure/VirtualMachines/MigrateVirtualMachinePage.tsx
@@ -1,35 +1,21 @@
 /* Copyright Contributors to the Open Cluster Management project */
-import { useParams, useNavigate } from 'react-router-dom-v5-compat'
-import { AcmModal, AcmButton } from '../../../ui-components'
+import { useNavigate } from 'react-router-dom-v5-compat'
+import { AcmModal } from '../../../ui-components'
 import { NavigationPath } from '../../../NavigationPath'
-import { useTranslation } from '../../../lib/acm-i18next'
+import { VMWizardPage } from '../../../wizards/Migration/VMWizardPage'
 
 export default function MigrateVirtualMachinePage() {
-  const { id } = useParams()
   const navigate = useNavigate()
-  const { t } = useTranslation()
 
   return (
     <AcmModal
-      title="Migrate Virtual Machine"
-      isOpen={true}
-      width={'80%'}
+      hasNoBodyWrapper
+      isOpen
+      width="65%"
+      showClose={false}
       onClose={() => navigate(NavigationPath.virtualMachines)}
-      actions={[
-        <AcmButton
-          key="migrate"
-          onClick={() => {
-            console.log('trigger migration')
-          }}
-        >
-          Migrate {id}
-        </AcmButton>,
-        <AcmButton key="cancel" variant="secondary" onClick={() => navigate(NavigationPath.virtualMachines)}>
-          {t('Cancel')}
-        </AcmButton>,
-      ]}
     >
-      <p>Proceed with migration of VM {id} ?</p>
+      <VMWizardPage />
     </AcmModal>
   )
 }
