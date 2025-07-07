abd7d37dc42551eddd51bbc2a2499e2707680bc3
Nitin Dhevar <nitin.dhanabalan@gmail.com>
2025-07-07 15:59:29 -0400

    Fixing jest test errors in managedClusterProxy
    Signed-off-by: Nitin Dhevar <nitin.dhanabalan@gmail.com>

--- backend/test/routes/managedClusterProxy.test.ts
+++ backend/test/routes/managedClusterProxy.test.ts
@@ -1,21 +1,12 @@
 /* Copyright Contributors to the Open Cluster Management project */
-jest.mock('../../src/lib/token', () => ({
-  getAuthenticatedToken: jest.fn(() => {
-    return 'testtoken'
-  }),
-}))
 import { Http2ServerRequest, Http2ServerResponse } from 'http2'
 import { managedClusterProxy } from '../../src/routes/managedClusterProxy'
 import proxy from 'http2-proxy'
 import { jest } from '@jest/globals'
 import { TLSSocket } from 'tls'
-
 let isHttp2Response: boolean = true
-
-jest.mock('../../src/lib/token', () => ({
-  isHttp2ServerResponse: jest.fn(() => isHttp2Response),
-}))
-
+const proxyWeb = proxy.web as jest.Mock
+const proxyWs = proxy.ws as jest.Mock
 jest.mock('http2-proxy', () => ({
   web: jest.fn((_req, _resOrSocket, _proxyOptions, proxyHandler: (err: Error | null) => void) =>
     process.nextTick(() => proxyHandler(null))
@@ -24,21 +15,21 @@ jest.mock('http2-proxy', () => ({
     process.nextTick(() => proxyHandler(null))
   ),
 }))
-
-const proxyWeb = proxy.web as jest.Mock
-const proxyWs = proxy.ws as jest.Mock
-
+jest.mock('../../src/lib/token', () => ({
+  isHttp2ServerResponse: jest.fn(() => isHttp2Response),
+  getAuthenticatedToken: jest.fn(() => {
+    return 'testtoken'
+  }),
+}))
 describe('ManagedClusterProxy tests', () => {
   const req = { url: '/managedclusterproxy/testcluster/testapi/', headers: {} } as Http2ServerRequest
   const res = { destroy: jest.fn() } as unknown as Http2ServerResponse
   const socket = { destroy: jest.fn() } as unknown as TLSSocket
   const head = Buffer.from('')
-
   beforeEach(() => {
     jest.clearAllMocks()
     isHttp2Response = true
   })
-
   it('test proxy call to web server', async () => {
     isHttp2Response = true
     await managedClusterProxy(req, res)
