c7682033c6179faaa76b4ce59c52ee23476e003f
vsanghishetty <vishali.kamenani@ymail.com>
2025-07-10 14:25:59 -0400

    removes VMRedirect
    Signed-off-by: vsanghishetty <vishali.kamenani@ymail.com>

--- frontend/plugins/mce/console-extensions.ts
+++ frontend/plugins/mce/console-extensions.ts
@@ -137,18 +137,6 @@ const virtualMachinesRoute: EncodedExtension<RoutePage> = {
   },
 }
 
-// Virtual Machines Detail Route - type: 'console.page/route'
-const virtualMachinesDetailRoute: EncodedExtension<RoutePage> = {
-  type: 'console.page/route',
-  properties: {
-    path: '/multicloud/infrastructure/virtualmachines/:cluster/:namespace/:name', // with parameters
-    component: { $codeRef: 'vmRedirect.default' }, // points to VMRedirect
-  },
-  flags: {
-    disallowed: ['KUBEVIRT_DYNAMIC_ACM'],
-  },
-}
-
 // Credentials Navigation Item - type: 'console.navigation/href'
 const credentialsNavItem: EncodedExtension<HrefNavItem> = {
   type: 'console.navigation/href',
@@ -181,7 +169,6 @@ export const extensions: EncodedExtension[] = [
   hostInventoryNavItem,
   hostInventoryRoute,
   virtualMachinesNavItem,
-  virtualMachinesDetailRoute,
   virtualMachinesRoute,
   credentialsNavItem,
   credentialsRoute,
