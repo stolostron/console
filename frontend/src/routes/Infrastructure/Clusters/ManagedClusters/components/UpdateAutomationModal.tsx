679c11ed5f958f5273b11361d38812f7d4df9c65
John Swanke <jswanke@redhat.com>
2025-07-10 11:49:10 -0400

    ACM-21701-Remove-automation Text-truncation (#4741)
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/components/UpdateAutomationModal.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/components/UpdateAutomationModal.tsx
@@ -219,7 +219,8 @@ export function UpdateAutomationModal(props: {
     <AcmModal
       title={t('Update automation template')}
       isOpen={props.open}
-      variant={ModalVariant.medium}
+      variant={ModalVariant.small}
+      style={{ display: 'table !important' }}
       onClose={() => {
         setSelectedCuratorTemplate(undefined)
         props.close()
