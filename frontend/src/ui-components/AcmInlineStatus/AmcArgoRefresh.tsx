27b50e20f198177d451c6332efc36178ca700fe8
Erik Jan de Wit <erikjan.dewit@gmail.com>
2025-07-15 15:12:17 +0200

    fix the refresh
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- frontend/src/ui-components/AcmInlineStatus/AmcArgoRefresh.tsx
+++ frontend/src/ui-components/AcmInlineStatus/AmcArgoRefresh.tsx
@@ -4,36 +4,52 @@ import { Button, Icon, Label } from '@patternfly/react-core'
 import { SyncAltIcon } from '@patternfly/react-icons'
 import { useContext, useState } from 'react'
 import { useTranslation } from '../../lib/acm-i18next'
-import { IResource } from '../../resources'
+import { Application, IResource } from '../../resources'
 import { replaceResource } from '../../resources/utils'
 import { AcmToastContext } from '../AcmAlert'
 
-const refreshAppk8s = async (app: IResource): Promise<IResource<IResource>> =>
-  replaceResource({
-    ...app,
-    metadata: {
-      ...app.metadata,
-      annotations: { ...app.metadata?.annotations, 'argocd.argoproj.io/refresh': 'refreshing' },
-    },
-  }).promise
+const refreshAppK8s = async (application: any) =>
+  Promise.all(
+    (application.appSetApps ?? []).map(
+      (app: IResource) =>
+        replaceResource({
+          ...app,
+          operation: {
+            info: [
+              {
+                name: 'Reason',
+                value: 'Initiated by user in openshift console',
+              },
+            ],
+            initiatedBy: {
+              automated: false,
+              username: 'OpenShift-Console',
+            },
+            sync: {
+              ...(app as any).spec?.syncPolicy,
+            },
+          },
+        }).promise
+    )
+  )
+
 type AmcArgoRefreshProps = {
-  app: IResource
+  app: Application
 }
 export function AmcArgoRefresh({ app }: AmcArgoRefreshProps) {
   const [isRefreshing, setIsRefreshing] = useState(false)
   const { addAlert } = useContext(AcmToastContext)
 
   const { t } = useTranslation()
-  const handleRefresh = () => {
+  const handleRefresh = async () => {
     setIsRefreshing(true)
-    refreshAppk8s(app).then(() => {
-      setIsRefreshing(false)
-      addAlert({
-        title: t('ArgoCD app refreshed'),
-        message: t('{{name}} was successfully refreshed.', { name: app.metadata?.name }),
-        type: 'success',
-        autoClose: true,
-      })
+    await refreshAppK8s(app)
+    setIsRefreshing(false)
+    addAlert({
+      title: t('ArgoCD app refreshed'),
+      message: t('{{name}} was successfully refreshed.', { name: app.metadata?.name }),
+      type: 'success',
+      autoClose: true,
     })
   }
   return (
