9c267be97c06b97680e62c3eef810d94e63de4e0
vsanghishetty <vishali.kamenani@ymail.com>
2025-07-17 17:13:18 -0400

    add resourceRoutes to AcmExtension type definition
    Signed-off-by: vsanghishetty <vishali.kamenani@ymail.com>

--- frontend/src/plugin-extensions/types.ts
+++ frontend/src/plugin-extensions/types.ts
@@ -1,12 +1,13 @@
 /* Copyright Contributors to the Open Cluster Management project */
 import { ResolvedExtension } from '@openshift-console/dynamic-plugin-sdk'
-import { ActionExtensionProps, ListColumnExtensionProps } from './properties'
+import { ActionExtensionProps, ListColumnExtensionProps, ResourceRouteExtensionProps } from './properties'
 import { OverviewTab } from './extensions'
 
 export type AcmExtension = Partial<{
   applicationAction: ActionExtensionProps[]
   applicationListColumn: ListColumnExtensionProps[]
   overviewTab: ResolvedExtension<OverviewTab>[]
+  resourceRoutes: ResourceRouteExtensionProps[]
   virtualMachineAction: ActionExtensionProps[]
   virtualMachineListColumn: ListColumnExtensionProps[]
 }>
