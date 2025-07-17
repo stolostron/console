02f67f4da1f038c07c3de67b49982414b033e144
vsanghishetty <vishali.kamenani@ymail.com>
2025-07-17 17:15:59 -0400

    moved to index.ts
    Signed-off-by: vsanghishetty <vishali.kamenani@ymail.com>

--- frontend/packages/multicluster-sdk/src/components/index.ts
+++ frontend/packages/multicluster-sdk/src/components/index.ts
@@ -1,3 +1,8 @@
 /* Copyright Contributors to the Open Cluster Management project */
 export { default as FleetResourceEventStream } from './FleetResourceEventStream/EventStream'
 export { FleetResourceLink } from './FleetResourceLink'
+export {
+  getResourceRouteHandler,
+  findResourceRouteHandler,
+  useResourceRouteExtensions,
+} from '../internal/fleetResourceHelpers'
--- frontend/src/plugin-extensions/extensions/index.ts
+++ frontend/src/plugin-extensions/extensions/index.ts
@@ -2,3 +2,4 @@
 export * from './actionExtension'
 export * from './listColumnExtension'
 export * from './OverviewTab'
+export * from './resourceRouteExtension'
