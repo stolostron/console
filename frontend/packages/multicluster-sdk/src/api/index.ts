bde89e59413b871588458c513bbd91e70d737589
vsanghishetty <vishali.kamenani@ymail.com>
2025-07-23 14:54:38 -0400

    merge conflict fix
    Signed-off-by: vsanghishetty <vishali.kamenani@ymail.com>

--- frontend/packages/multicluster-sdk/src/api/index.ts
+++ frontend/packages/multicluster-sdk/src/api/index.ts
@@ -1,6 +1,7 @@
 /* Copyright Contributors to the Open Cluster Management project */
 export * from './constants'
 export * from './useFleetClusterNames'
+export * from './search/useFleetSearchPoll'
 export * from './useHubClusterName'
 export * from './useIsFleetAvailable'
 export * from './useFleetK8sAPIPath'
