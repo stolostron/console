5754780ce848ed10b871b79291847422914b70ab
vsanghishetty <vishali.kamenani@ymail.com>
2025-07-24 05:45:30 -0400

    reverts searchPath handle in different PR
    Signed-off-by: vsanghishetty <vishali.kamenani@ymail.com>

--- frontend/packages/multicluster-sdk/src/api/utils/searchPaths.ts
+++ frontend/packages/multicluster-sdk/src/api/utils/searchPaths.ts
@@ -1,27 +1,23 @@
 /* Copyright Contributors to the Open Cluster Management project */
 export const getURLSearchParam = (resource: any) => {
-  const params = new URLSearchParams()
+  let searchString = `cluster=${resource.cluster}`
 
-  if (resource.cluster) {
-    params.append('cluster', resource.cluster)
-  }
   if (resource.kind) {
-    params.append('kind', resource.kind)
+    searchString = `${searchString}&kind=${resource.kind}`
   }
   if (resource.apigroup && resource.apiversion) {
-    params.append('apiversion', `${resource.apigroup}/${resource.apiversion}`)
+    searchString = `${searchString}&apiversion=${resource.apigroup}/${resource.apiversion}`
   } else if (!resource.apigroup && resource.apiversion) {
-    params.append('apiversion', resource.apiversion)
+    searchString = `${searchString}&apiversion=${resource.apiversion}`
   }
   if (resource.namespace) {
-    params.append('namespace', resource.namespace)
+    searchString = `${searchString}&namespace=${resource.namespace}`
   }
   if (resource.name) {
-    params.append('name', resource.name)
+    searchString = `${searchString}&name=${resource.name}`
   }
   if (resource._hubClusterResource && resource._hubClusterResource === 'true') {
-    params.append('_hubClusterResource', 'true')
+    searchString = `${searchString}&_hubClusterResource=true`
   }
-
-  return `?${params.toString()}`
+  return `?${encodeURIComponent(searchString)}`
 }
