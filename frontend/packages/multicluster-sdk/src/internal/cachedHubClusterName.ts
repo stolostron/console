4295ab73c67a7f87ba8f685d1e3f369bf4563b9c
Nitin Dhevar <46418326+nitin-dhevar@users.noreply.github.com>
2025-07-29 13:39:17 -0400

    Fixing multiple api requests to clusterhubname issue (#4820)
    Signed-off-by: Nitin Dhevar <nitin.dhanabalan@gmail.com>

--- frontend/packages/multicluster-sdk/src/internal/cachedHubClusterName.ts
+++ frontend/packages/multicluster-sdk/src/internal/cachedHubClusterName.ts
@@ -4,15 +4,25 @@ import { consoleFetchJSON } from '@openshift-console/dynamic-plugin-sdk'
 
 const getHubClusterNameUrl = '/hub'
 let cachedHubClusterName: string | undefined = undefined
+let fetchPromise: Promise<string | undefined> | undefined = undefined
 
 export const fetchHubClusterName = async (): Promise<string | undefined> => {
   if (cachedHubClusterName) {
     return cachedHubClusterName
   }
-  const url = getBackendUrl() + getHubClusterNameUrl
-  const data = await consoleFetchJSON(url, 'GET')
-  cachedHubClusterName = data.localHubName
-  return cachedHubClusterName
+
+  if (fetchPromise) {
+    return fetchPromise
+  }
+
+  fetchPromise = (async () => {
+    const url = getBackendUrl() + getHubClusterNameUrl
+    const data = await consoleFetchJSON(url, 'GET')
+    cachedHubClusterName = data.localHubName
+    return cachedHubClusterName
+  })()
+
+  return fetchPromise
 }
 
 export const getCachedHubClusterName = (): string | undefined => {
