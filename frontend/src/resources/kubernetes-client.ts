a213db96bcc483544e63b757ba24825558c7bfb8
Kurtis Wang <66376528+kurwang@users.noreply.github.com>
2025-08-04 11:37:32 -0400

    adding backend resources for serviceaccount, clusterrole, and roles (#4832)
    * add changes to rbac.ts using kubernetes-client.ts
    
    Signed-off-by: kurwang <madeof50gold@gmail.com>
    
    * adding subject and roleref interface to kubernetes-client.ts
    
    Signed-off-by: kurwang <madeof50gold@gmail.com>
    
    * adding missing copyright and fix files from npm run check:fix
    
    Signed-off-by: kurwang <madeof50gold@gmail.com>
    
    ---------
    
    Signed-off-by: kurwang <madeof50gold@gmail.com>

---
+++ frontend/src/resources/kubernetes-client.ts
@@ -0,0 +1,25 @@
+/* Copyright Contributors to the Open Cluster Management project */
+export interface PolicyRule {
+  verbs: string[]
+  apiGroups: string[]
+  resources: string[]
+  resourceNames?: string[]
+  nonResourceURLs?: string[]
+}
+
+export interface LocalObjectReference {
+  name: string
+}
+
+export interface Subject {
+  kind: string
+  apiGroup?: string
+  name: string
+  namespace?: string
+}
+
+export interface RoleRef {
+  apiGroup: string
+  kind: string
+  name: string
+}
