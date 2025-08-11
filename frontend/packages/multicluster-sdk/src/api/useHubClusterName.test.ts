a7e66658c9dbcd9e59d266ad88df1de9f4975218
John Swanke <jswanke@redhat.com>
2025-08-11 13:14:24 -0400

    reset
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- frontend/packages/multicluster-sdk/src/api/useHubClusterName.test.ts
+++ frontend/packages/multicluster-sdk/src/api/useHubClusterName.test.ts
@@ -2,6 +2,7 @@
 import { renderHook } from '@testing-library/react-hooks'
 import * as internal from '../internal/cachedHubConfiguration'
 import { useHubClusterName } from './useHubClusterName'
+import { useIsFleetAvailable } from './useIsFleetAvailable'
 
 jest.mock('../internal/cachedHubConfiguration')
 
@@ -51,13 +52,28 @@ describe('useHubClusterName', () => {
     mockUseIsFleetAvailable.mockReturnValue(false)
     const { result } = renderHook(() => useHubClusterName())
 
+    expect(result.current).toEqual(['local-cluster', true, undefined])
+  })
+
+  it('should fetch hub cluster name if not cached', async () => {
+    void (internal.getCachedHubClusterName as jest.Mock).mockReturnValue(undefined)
+    const fetchMock = jest.spyOn(internal, 'fetchHubClusterName').mockResolvedValue('local-cluster')
+    mockUseIsFleetAvailable.mockReturnValue(true)
+    const { result, waitForNextUpdate } = renderHook(() => useHubClusterName())
+    expect(result.current).toEqual([undefined, false, undefined])
+    await waitForNextUpdate()
+    expect(fetchMock).toHaveBeenCalled()
+    expect(result.current).toEqual(['local-cluster', true, undefined])
+  })
+
+  it('should return error if fleet is not available', async () => {
+    void (internal.getCachedHubClusterName as jest.Mock).mockReturnValue('local-cluster')
+    mockUseIsFleetAvailable.mockReturnValue(false)
+    const { result } = renderHook(() => useHubClusterName())
     expect(result.current).toEqual([
       undefined,
       false,
       'A version of RHACM that is compatible with the multicluster SDK is not available',
     ])
-
-    // getCachedHubClusterName should not be called when fleet is not available
-    expect(mockGetCachedHubClusterName).not.toHaveBeenCalled()
   })
 })
