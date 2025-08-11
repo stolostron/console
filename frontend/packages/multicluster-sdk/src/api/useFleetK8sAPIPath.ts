359ebe070cd08ec994369fee7aaf55a870d1a5f7
John Swanke <jswanke@redhat.com>
2025-08-11 16:15:34 -0400

    ACM-21206-SDK---useFleetK8sAPIPath
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- frontend/packages/multicluster-sdk/src/api/useFleetK8sAPIPath.ts
+++ frontend/packages/multicluster-sdk/src/api/useFleetK8sAPIPath.ts
@@ -1,13 +1,18 @@
 /* Copyright Contributors to the Open Cluster Management project */
-import { k8sListItems } from '@openshift-console/dynamic-plugin-sdk'
 import { UseFleetK8sAPIPath } from '../types'
 import { getBackendUrl } from './apiRequests'
-import { BASE_K8S_API_PATH, LOCAL_CLUSTER_LABEL, MANAGED_CLUSTER_API_PATH } from '../internal/constants'
-import { ManagedClusterModel } from '../internal/models'
-import { useHubClusterName } from './useHubClusterName'
+import { BASE_K8S_API_PATH, MANAGED_CLUSTER_API_PATH } from '../internal/constants'
+import { useHubConfigurationItem } from '../internal/useHubConfigurationItem'
+import { getCachedHubConfiguration } from '../internal/cachedHubConfiguration'
 
+/**
+ * Hook that provides the k8s API path for the fleet.
+ *
+ * @param cluster - The cluster name.
+ * @returns Array with `k8sAPIPath`, `loaded` and `error` values.
+ */
 export const useFleetK8sAPIPath: UseFleetK8sAPIPath = (cluster) => {
-  const [hubClusterName, loaded, error] = useHubClusterName()
+  const [hubClusterName, loaded, error] = useHubConfigurationItem('localHubName')
 
   if (!loaded) return [undefined, false, error]
 
@@ -16,18 +21,15 @@ export const useFleetK8sAPIPath: UseFleetK8sAPIPath = (cluster) => {
   return [`${getBackendUrl()}/${MANAGED_CLUSTER_API_PATH}/${cluster}`, loaded, error]
 }
 
-let cachedHubClusterName: string | undefined = undefined
+/**
+ * Function that provides the k8s API path for the fleet.
+ *
+ * @param cluster - The cluster name.
+ * @returns The k8s API path for the fleet.
+ */
 
 export const getFleetK8sAPIPath = async (cluster?: string) => {
-  if (!cachedHubClusterName) {
-    const hubClusters = await k8sListItems({
-      model: ManagedClusterModel,
-      queryParams: { labelSelector: { matchLabels: { [LOCAL_CLUSTER_LABEL]: true } } },
-    })
-
-    cachedHubClusterName = hubClusters?.[0]?.metadata?.name
-  }
-
+  const cachedHubClusterName = getCachedHubConfiguration()?.['localHubName']
   if (cluster && cachedHubClusterName !== cluster) {
     return `${getBackendUrl()}/${MANAGED_CLUSTER_API_PATH}/${cluster}`
   } else {
