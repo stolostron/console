05c8dee488a26c38a780a3ce52d0e0771ed1660b
Oksana Bazylieva <obazylie@redhat.com>
2025-09-02 13:11:06 -0400

    Adjust tests for GroupYaml file
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>

--- frontend/src/routes/UserManagement/Identities/Groups/GroupYaml.tsx
+++ frontend/src/routes/UserManagement/Identities/Groups/GroupYaml.tsx
@@ -1,29 +1,16 @@
 /* Copyright Contributors to the Open Cluster Management project */
-import { Page, PageSection } from '@patternfly/react-core'
-import { useParams } from 'react-router-dom-v5-compat'
+import { PageSection } from '@patternfly/react-core'
 import { useTranslation } from '../../../../lib/acm-i18next'
-import { useQuery } from '../../../../lib/useQuery'
-import { listGroups } from '../../../../resources/rbac'
-import { useMemo } from 'react'
 import { dump } from 'js-yaml'
 import YamlEditor from '../../../../components/YamlEditor'
-import { AcmButton, AcmLoadingPage } from '../../../../ui-components'
+import { AcmLoadingPage } from '../../../../ui-components'
 import { useYamlEditorHeight } from '../../../../hooks/useYamlEditorHeight'
-import { ErrorPage } from '../../../../components/ErrorPage'
-import { NavigationPath } from '../../../../NavigationPath'
-import { ResourceError, ResourceErrorCode } from '../../../../resources/utils'
-import { useNavigate } from 'react-router-dom-v5-compat'
+import { useGroupDetailsContext } from './GroupPage'
 
 const GroupYaml = () => {
   const { t } = useTranslation()
-  const { id = undefined } = useParams()
-  const { data: groups, loading } = useQuery(listGroups)
-  const navigate = useNavigate()
+  const { group, loading } = useGroupDetailsContext()
 
-  const group = useMemo(
-    () => (id ? groups?.find((u) => u.metadata.uid === id || u.metadata.name === id) : undefined),
-    [groups, id]
-  )
   const baseHeight = useYamlEditorHeight()
   const customHeight = Math.min(baseHeight, 450)
 
@@ -36,20 +23,9 @@ const GroupYaml = () => {
       )
     case !group:
       return (
-        <Page>
-          <ErrorPage
-            error={new ResourceError(ResourceErrorCode.NotFound)}
-            actions={
-              <AcmButton
-                role="link"
-                onClick={() => navigate(NavigationPath.identitiesGroups)}
-                style={{ marginRight: '10px' }}
-              >
-                {t('button.backToGroups')}
-              </AcmButton>
-            }
-          />
-        </Page>
+        <PageSection>
+          <div>{t('Group not found')}</div>
+        </PageSection>
       )
     default:
       return (
