f6fea1c91b4e106e71066ad27e991c6ddd902057
Oksana Bazylieva <obazylie@redhat.com>
2025-09-02 14:45:01 -0400

    Refactor: create reusable RBACResourceYaml component
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>

--- frontend/src/routes/UserManagement/Identities/Groups/GroupYaml.tsx
+++ frontend/src/routes/UserManagement/Identities/Groups/GroupYaml.tsx
@@ -1,39 +1,11 @@
 /* Copyright Contributors to the Open Cluster Management project */
-import { PageSection } from '@patternfly/react-core'
-import { useTranslation } from '../../../../lib/acm-i18next'
-import { dump } from 'js-yaml'
-import YamlEditor from '../../../../components/YamlEditor'
-import { AcmLoadingPage } from '../../../../ui-components'
-import { useYamlEditorHeight } from '../../../../hooks/useYamlEditorHeight'
 import { useGroupDetailsContext } from './GroupPage'
+import { RBACResourceYaml } from '../../../../components/RBACResourceYaml'
 
 const GroupYaml = () => {
-  const { t } = useTranslation()
   const { group, loading } = useGroupDetailsContext()
 
-  const baseHeight = useYamlEditorHeight()
-  const customHeight = Math.min(baseHeight, 450)
-
-  switch (true) {
-    case loading:
-      return (
-        <PageSection>
-          <AcmLoadingPage />
-        </PageSection>
-      )
-    case !group:
-      return (
-        <PageSection>
-          <div>{t('Group not found')}</div>
-        </PageSection>
-      )
-    default:
-      return (
-        <PageSection>
-          <YamlEditor resourceYAML={dump(group, { indent: 2 })} readOnly={true} height={customHeight} />
-        </PageSection>
-      )
-  }
+  return <RBACResourceYaml resource={group} loading={loading} resourceType="Group" />
 }
 
 export { GroupYaml }
--- frontend/src/routes/UserManagement/Identities/Users/UserYaml.tsx
+++ frontend/src/routes/UserManagement/Identities/Users/UserYaml.tsx
@@ -1,39 +1,11 @@
 /* Copyright Contributors to the Open Cluster Management project */
-import { PageSection } from '@patternfly/react-core'
-import { useTranslation } from '../../../../lib/acm-i18next'
-import { dump } from 'js-yaml'
-import YamlEditor from '../../../../components/YamlEditor'
-import { AcmLoadingPage } from '../../../../ui-components'
-import { useYamlEditorHeight } from '../../../../hooks/useYamlEditorHeight'
 import { useUserDetailsContext } from './UserPage'
+import { RBACResourceYaml } from '../../../../components/RBACResourceYaml'
 
 const UserYaml = () => {
-  const { t } = useTranslation()
   const { user, loading } = useUserDetailsContext()
 
-  const baseHeight = useYamlEditorHeight()
-  const customHeight = Math.min(baseHeight, 450)
-
-  switch (true) {
-    case loading:
-      return (
-        <PageSection>
-          <AcmLoadingPage />
-        </PageSection>
-      )
-    case !user:
-      return (
-        <PageSection>
-          <div>{t('User not found')}</div>
-        </PageSection>
-      )
-    default:
-      return (
-        <PageSection>
-          <YamlEditor resourceYAML={dump(user, { indent: 2 })} readOnly={true} height={customHeight} />
-        </PageSection>
-      )
-  }
+  return <RBACResourceYaml resource={user} loading={loading} resourceType="User" />
 }
 
 export { UserYaml }
