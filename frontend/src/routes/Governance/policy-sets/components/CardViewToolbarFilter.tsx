d3a7ee96afad22790da3c38b30c69e542f4fa06b
vsanghishetty <vishali.kamenani@ymail.com>
2025-09-23 14:50:14 -0400

    replace Badge JSX with string to prevent object display
    Signed-off-by: vsanghishetty <vishali.kamenani@ymail.com>

--- frontend/src/routes/Governance/policy-sets/components/CardViewToolbarFilter.tsx
+++ frontend/src/routes/Governance/policy-sets/components/CardViewToolbarFilter.tsx
@@ -1,6 +1,6 @@
 /* Copyright Contributors to the Open Cluster Management project */
 
-import { Badge, SelectOption, SelectGroup } from '@patternfly/react-core'
+import { SelectOption, SelectGroup } from '@patternfly/react-core'
 import { AcmSelectBase, SelectVariant } from '../../../../components/AcmSelectBase'
 import { FilterIcon } from '@patternfly/react-icons'
 import { useCallback, useMemo, useState } from 'react'
@@ -8,7 +8,6 @@ import { useRecoilValue, useSharedAtoms } from '../../../../shared-recoil'
 import { NavigationPath } from '../../../../NavigationPath'
 import { PolicySet } from '../../../../resources/policy-set'
 import { useTranslation } from '../../../../lib/acm-i18next'
-import { filterOption, filterOptionBadge } from '../../../../ui-components/AcmTable/filterStyles'
 
 export default function CardViewToolbarFilter(props: {
   preSelectedFilters: string[]
@@ -60,16 +59,16 @@ export default function CardViewToolbarFilter(props: {
             label: t('No status'),
             complianceValue: undefined,
           },
-        ].map(({ key, label, complianceValue }) => (
-          <SelectOption key={key} value={key}>
-            <div className={filterOption}>
-              {label}
-              <Badge className={filterOptionBadge} key={`${key}-count`} isRead>
-                {policySets.filter((policySet: PolicySet) => policySet?.status?.compliant === complianceValue).length}
-              </Badge>
-            </div>
-          </SelectOption>
-        ))}
+        ].map(({ key, label, complianceValue }) => {
+          const count = policySets.filter(
+            (policySet: PolicySet) => policySet?.status?.compliant === complianceValue
+          ).length
+          return (
+            <SelectOption key={key} value={key}>
+              {`${label} (${count})`}
+            </SelectOption>
+          )
+        })}
       </SelectGroup>,
     ],
     [policySets, t]
