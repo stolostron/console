690d8923acbe6f4c173224d0deeaaed41ba85e70
Kevin Cormier <kcormier@redhat.com>
2025-09-30 15:52:29 -0400

    Enable connection pooling for all agents
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- backend/src/lib/agent.ts
+++ backend/src/lib/agent.ts
@@ -2,11 +2,21 @@
 import { Agent } from 'https'
 import { getCACertificate, getServiceCACertificate } from './serviceAccountToken'
 import { HttpsProxyAgent } from 'https-proxy-agent'
+import { AgentOptions } from 'http'
+
+const COMMON_AGENT_OPTIONS: Partial<AgentOptions> = {
+  keepAlive: true, // Reuse connections
+  keepAliveMsecs: 30000, // 30 seconds keep alive
+  timeout: 30000, // 30 second socket timeout
+}
 
 let defaultAgent: Agent
 export function getDefaultAgent() {
   if (!defaultAgent) {
-    defaultAgent = new Agent({ ca: getCACertificate() })
+    defaultAgent = new Agent({
+      ca: getCACertificate(),
+      ...COMMON_AGENT_OPTIONS,
+    })
   }
   return defaultAgent
 }
@@ -14,7 +24,7 @@ export function getDefaultAgent() {
 let serviceAgent: Agent
 export function getServiceAgent() {
   if (!serviceAgent) {
-    serviceAgent = new Agent({ ca: getServiceCACertificate() })
+    serviceAgent = new Agent({ ca: getServiceCACertificate(), ...COMMON_AGENT_OPTIONS })
   }
   return serviceAgent
 }
@@ -22,7 +32,7 @@ export function getServiceAgent() {
 let proxyAgent: HttpsProxyAgent<string>
 export function getProxyAgent() {
   if (!proxyAgent && process.env.HTTPS_PROXY) {
-    proxyAgent = new HttpsProxyAgent(process.env.HTTPS_PROXY)
+    proxyAgent = new HttpsProxyAgent(process.env.HTTPS_PROXY, COMMON_AGENT_OPTIONS)
   }
   return proxyAgent
 }
