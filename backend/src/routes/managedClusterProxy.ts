842c6b303197d725cf461053041d270508f2656d
Kevin Cormier <kcormier@redhat.com>
2025-10-10 10:21:51 -0400

    Sonar issues
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- backend/src/routes/managedClusterProxy.ts
+++ backend/src/routes/managedClusterProxy.ts
@@ -5,7 +5,7 @@ import { respondInternalServerError } from '../lib/respond'
 import { getAuthenticatedToken, isHttp2ServerResponse } from '../lib/token'
 import { getMultiClusterEngine } from '../lib/multi-cluster-engine'
 import proxy from 'http2-proxy'
-import { TLSSocket } from 'tls'
+import { TLSSocket } from 'node:tls'
 import { getServiceCACertificate } from '../lib/serviceAccountToken'
 
 export async function managedClusterProxy(req: Http2ServerRequest, res: Http2ServerResponse): Promise<void>
