ceefee92af83a049b272cf5d84a8385fdb17e7f5
Kevin Cormier <kcormier@redhat.com>
2025-10-29 10:36:57 -0400

    v1alpha1 is no longer served for Placement/PlacementDecision
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- backend/src/routes/events.ts
+++ backend/src/routes/events.ts
@@ -179,8 +179,6 @@ const definitions: IWatchOptions[] = [
   },
   { kind: 'ManagedCluster', apiVersion: 'cluster.open-cluster-management.io/v1' },
   { kind: 'Placement', apiVersion: 'cluster.open-cluster-management.io/v1beta1' },
-  { kind: 'Placement', apiVersion: 'cluster.open-cluster-management.io/v1alpha1' },
-  { kind: 'PlacementDecision', apiVersion: 'cluster.open-cluster-management.io/v1alpha1' },
   { kind: 'PlacementDecision', apiVersion: 'cluster.open-cluster-management.io/v1beta1' },
   { kind: 'ManagedClusterSetBinding', apiVersion: 'cluster.open-cluster-management.io/v1beta2' },
   { kind: 'ManagedClusterSet', apiVersion: 'cluster.open-cluster-management.io/v1beta2' },
