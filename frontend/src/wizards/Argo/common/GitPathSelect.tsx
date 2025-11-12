bd70b7470df6fb53fdc5f53f63ae1e1a4dcf4c21
Randy Bruno Piverger <21374229+Randy424@users.noreply.github.com>
2025-11-11 19:27:19 -0800

    Correction to git revision and path behavior onChange
    Signed-off-by: Randy Bruno Piverger <21374229+Randy424@users.noreply.github.com>

--- frontend/src/wizards/Argo/common/GitPathSelect.tsx
+++ frontend/src/wizards/Argo/common/GitPathSelect.tsx
@@ -1,10 +1,12 @@
 /* Copyright Contributors to the Open Cluster Management project */
 
-import { useItem, WizAsyncSelect } from '@patternfly-labs/react-form-wizard'
-import { useCallback } from 'react'
+import { ItemContext, useData, useItem, WizAsyncSelect } from '@patternfly-labs/react-form-wizard'
+import { useCallback, useContext } from 'react'
+import set from 'set-value'
 import { useTranslation } from '../../../lib/acm-i18next'
 import { Channel, getGitPathList } from '../ArgoWizard'
 import { getGitChannelPaths } from '../../../resources'
+import { usePrevious } from '../../../components/usePrevious'
 
 type GitPathSelectProps = {
   channels: Channel[]
@@ -14,6 +16,12 @@ export const GitPathSelect = ({ channels }: GitPathSelectProps) => {
   const { t } = useTranslation()
   const repoURL = useItem('repoURL')
   const revision = useItem('targetRevision')
+  const path = useItem('path')
+  const item = useContext(ItemContext)
+  const { update } = useData()
+
+  const previousRepoURL = usePrevious(repoURL)
+  const previousRevision = usePrevious(revision)
 
   const gitPathsAsyncCallback = useCallback(() => {
     const channel = channels?.find((channel) => channel?.spec?.pathname === repoURL)
@@ -33,6 +41,15 @@ export const GitPathSelect = ({ channels }: GitPathSelectProps) => {
       repoURL
     )
   }, [channels, repoURL, revision])
+
+  // Clear path when repoURL or revision changes (update during render)
+  const repoChanged = previousRepoURL !== repoURL && previousRepoURL !== undefined
+  const revisionChanged = previousRevision !== revision && previousRevision !== undefined
+
+  if ((repoChanged || revisionChanged) && path) {
+    set(item, 'path', undefined, { preservePaths: false })
+    update()
+  }
 
   return (
     <WizAsyncSelect
