4f8e04cb18be82ba2b7352234391371027afc270
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2025-12-05 00:04:39 +0100

    [release-2.15] [ACM-26304] Fix missing select options in react-form-wizard (#5281)
    * Fix missing select options in react-form-wizard
    
    Signed-off-by: zlayne <zlayne@redhat.com>
    
    * fix test
    
    Signed-off-by: zlayne <zlayne@redhat.com>
    
    * update tests
    
    Signed-off-by: zlayne <zlayne@redhat.com>
    
    * fix empty options array handling
    
    Signed-off-by: zlayne <zlayne@redhat.com>
    
    ---------
    
    Signed-off-by: zlayne <zlayne@redhat.com>
    Co-authored-by: zlayne <zlayne@redhat.com>

--- frontend/packages/react-form-wizard/src/inputs/WizSingleSelect.tsx
+++ frontend/packages/react-form-wizard/src/inputs/WizSingleSelect.tsx
@@ -89,7 +89,13 @@ export function WizSingleSelect(props: WizSingleSelectProps) {
               selected={value}
               onSelect={(_event, value) => onSelect(value?.toString() ?? '')}
             >
-              <SelectListOptions value={value} options={filteredOptions} isCreatable={isCreatable} footer={footer} />
+              <SelectListOptions
+                value={value}
+                allOptions={options}
+                options={filteredOptions}
+                isCreatable={isCreatable}
+                footer={footer}
+              />
             </PfSelect>
           </InputGroupItem>
         </InputGroup>
