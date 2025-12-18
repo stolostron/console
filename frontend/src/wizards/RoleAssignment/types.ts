ad27a7f05a1b77b1d5266fdb90877696c7c9de54
Oksana Bazylieva <obazylie@redhat.com>
2025-12-17 19:16:05 -0500

    Fix types
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>

--- frontend/src/wizards/RoleAssignment/types.ts
+++ frontend/src/wizards/RoleAssignment/types.ts
@@ -1,16 +1,18 @@
 /* Copyright Contributors to the Open Cluster Management project */
 import { RoleAssignmentPreselected } from '../../routes/UserManagement/RoleAssignments/model/role-assignment-preselected'
 
+export type ScopeType = 'Global access' | 'Select cluster sets' | 'Select clusters'
+
+export interface RoleAssignmentFormData {
+  scope: ScopeType
+  selectedClusterSets?: any[]
+  selectedClusters?: any[]
+}
+
 export interface RoleAssignmentWizardModalProps {
   isOpen: boolean
   onClose: () => void
-  onSubmit?: (data: any) => void
+  onSubmit?: (data: RoleAssignmentFormData) => void
   isEditing?: boolean
   preselected?: RoleAssignmentPreselected
-}
-
-export interface RoleAssignmentFormData {
-  scope: string
-  selectedClusterSets?: any[]
-  selectedClusters?: any[]
 }
