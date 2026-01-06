dc262a3d954b4802d4b0229e1649b74175f9f414
Kevin Cormier <kcormier@redhat.com>
2026-01-06 14:47:29 -0500

    Remove deprecated option
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/packages/multicluster-sdk/jest.config.ts
+++ frontend/packages/multicluster-sdk/jest.config.ts
@@ -19,7 +19,6 @@ const config: Config.InitialOptions = {
     '^.+\\.[jt]sx?$': [
       'ts-jest',
       {
-        isolatedModules: true,
         tsconfig: '<rootDir>/tsconfig.dev.json',
       },
     ],
