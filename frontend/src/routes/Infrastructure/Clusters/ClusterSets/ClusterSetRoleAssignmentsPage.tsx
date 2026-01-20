3ddef732a85902259e69d34974f75cb9b4e2de69
Enrique Mingorance Cano <emingora@redhat.com>
2026-01-20 11:19:42 +0100

    new tab on the cluster set
    Signed-off-by: Enrique Mingorance Cano <emingora@redhat.com>

---
+++ frontend/src/routes/Infrastructure/Clusters/ClusterSets/ClusterSetRoleAssignmentsPage.tsx
@@ -0,0 +1,20 @@
+/* Copyright Contributors to the Open Cluster Management project */
+import { PageSection } from '@patternfly/react-core'
+import { generatePath, Navigate, useParams } from 'react-router-dom-v5-compat'
+import { NavigationPath } from '../../../../NavigationPath'
+import { useRecoilValue, useSharedAtoms } from '../../../../shared-recoil'
+import { ClusterRoleAssignments } from '../ClusterRoleAssignments'
+
+export default function ClusterSetRoleAssignmentsPage() {
+  const { id = '' } = useParams()
+  const { isFineGrainedRbacEnabledState } = useSharedAtoms()
+  const isFineGrainedRbacEnabled = useRecoilValue(isFineGrainedRbacEnabledState)
+
+  return isFineGrainedRbacEnabled ? (
+    <PageSection hasBodyWrapper={false}>
+      <ClusterRoleAssignments />
+    </PageSection>
+  ) : (
+    <Navigate to={generatePath(NavigationPath.clusterSetDetails, { id })} replace />
+  )
+}
