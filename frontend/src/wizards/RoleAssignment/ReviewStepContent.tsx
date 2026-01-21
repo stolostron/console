27580ae9862d447adf27339f82b6ece76218b091
Enrique Mingorance Cano <emingora@redhat.com>
2026-01-21 10:47:56 +0100

    TS error fixed at ReviewStepContent
    Signed-off-by: Enrique Mingorance Cano <emingora@redhat.com>

--- frontend/src/wizards/RoleAssignment/ReviewStepContent.tsx
+++ frontend/src/wizards/RoleAssignment/ReviewStepContent.tsx
@@ -11,6 +11,7 @@ import { GranularityStepContent } from './GranularityStepContent'
 import { RoleAssignmentWizardFormData, RoleAssignmentWizardModalProps } from './types'
 import { useMemo } from 'react'
 import { RoleAssignmentPreselected } from '../../routes/UserManagement/RoleAssignments/model/role-assignment-preselected'
+import { ManagedClusterSet } from '../../resources'
 
 interface ReviewStepContentProps {
   formData: RoleAssignmentWizardFormData
@@ -90,7 +91,9 @@ export const ReviewStepContent = ({ formData, preselected }: ReviewStepContentPr
                             </div>
                             <div>
                               {formData.selectedClusterSets && formData.selectedClusterSets.length > 0
-                                ? formData.selectedClusterSets.map((cs) => cs.metadata?.name || cs).join(', ')
+                                ? formData.selectedClusterSets
+                                    .map((cs) => (cs as ManagedClusterSet).metadata?.name || (cs as string))
+                                    .join(', ')
                                 : t('None selected')}
                             </div>
                           </div>
