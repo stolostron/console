249b07bf5770e379466fd1eecd12693237b37038
Oksana Bazylieva <61097730+oksanabaza@users.noreply.github.com>
2026-01-26 11:54:54 -0500

    ACM-28836 RBAC UI - Creation wizard - Update Review step with prev values when edit RA (#5512)
    * Show changes in review step when editing role assignments
    
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>
    
    * Add isEditing prop
    
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>
    
    * Disable save button when no changes in role assignment edit mode
    
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>
    
    * Display preselected cluster names in ReviewStepContent when selectedClusters is empty
    
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>
    
    * ACM-28836 emingora's proposal
    
    Signed-off-by: Enrique Mingorance Cano <emingora@redhat.com>
    
    * Add warning message when no changes detected
    
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>
    
    * Add identity to ReviewStepContentHook
    
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>
    
    * Fix cluster sorting in review step
    
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>
    
    * Fix tests
    
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>
    
    * Fix sonar issue
    
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>
    
    * Review comments
    
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>
    
    * Add identity to ReviewStepContentHook test
    
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>
    
    * fix sonar issue
    
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>
    
    * Update switch(true) case in RoleAssignmentWizardModal
    
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>
    
    ---------
    
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>
    Signed-off-by: Enrique Mingorance Cano <emingora@redhat.com>
    Co-authored-by: Enrique Mingorance Cano <emingora@redhat.com>

--- frontend/src/wizards/RoleAssignment/ReviewStepContent.tsx
+++ frontend/src/wizards/RoleAssignment/ReviewStepContent.tsx
@@ -1,5 +1,6 @@
 /* Copyright Contributors to the Open Cluster Management project */
 import {
+  Alert,
   DescriptionList,
   DescriptionListDescription,
   DescriptionListGroup,
@@ -7,48 +8,48 @@ import {
   Title,
 } from '@patternfly/react-core'
 import { useTranslation } from '../../lib/acm-i18next'
-import { GranularityStepContent } from './GranularityStepContent'
-import { RoleAssignmentWizardFormData, RoleAssignmentWizardModalProps } from './types'
-import { useMemo } from 'react'
-import { RoleAssignmentPreselected } from '../../routes/UserManagement/RoleAssignments/model/role-assignment-preselected'
 import { ManagedClusterSet } from '../../resources'
+import { GranularityStepContent } from './GranularityStepContent'
+import { useReviewStepContent } from './ReviewStepContentHook'
+import { RoleAssignmentWizardFormData, RoleAssignmentWizardModalProps } from './types'
 
 interface ReviewStepContentProps {
   formData: RoleAssignmentWizardFormData
   preselected?: RoleAssignmentWizardModalProps['preselected']
+  isEditing?: boolean
+  hasChanges?: boolean
 }
 
-export const ReviewStepContent = ({ formData, preselected }: ReviewStepContentProps) => {
+export const ReviewStepContent = ({ formData, preselected, isEditing, hasChanges }: ReviewStepContentProps) => {
   const { t } = useTranslation()
 
-  const getClusterNames = ({
-    selectedClusters,
-    clusterNames,
-  }: {
-    selectedClusters: RoleAssignmentWizardFormData['selectedClusters']
-    clusterNames: RoleAssignmentPreselected['clusterNames']
-  }): string | null => {
-    switch (true) {
-      case selectedClusters && selectedClusters.length > 0:
-        return selectedClusters.map((c) => c.metadata?.name || c.name || c).join(', ')
-      case clusterNames && clusterNames.length > 0:
-        return clusterNames.join(', ')
-      default:
-        return null
-    }
-  }
-  const clusterNames = useMemo(
-    () =>
-      getClusterNames({
-        selectedClusters: formData.selectedClusters,
-        clusterNames: preselected?.clusterNames,
-      }),
-    [formData.selectedClusters, preselected?.clusterNames]
-  )
+  const { clusterNames, clustersDisplay, namespacesDisplay, roleDisplay, identityDisplay } = useReviewStepContent({
+    oldData: {
+      clusterNames: preselected?.clusterNames,
+      namespaces: preselected?.namespaces,
+      role: preselected?.roles?.[0],
+      subject: preselected?.subject,
+    },
+    newData: {
+      clusterNames: formData.selectedClusters,
+      namespaces: formData.scope.namespaces,
+      role: formData.roles?.[0],
+      subject: formData.subject,
+    },
+    isEditing,
+  })
 
   return (
     <div>
       <GranularityStepContent title={t('Review')} titleSize="lg" description={''} />
+      {!hasChanges && isEditing && (
+        <Alert
+          variant="danger"
+          title={t('No changes have been made. Please modify or cancel to exit.')}
+          isInline
+          style={{ marginBottom: '16px' }}
+        />
+      )}
       {formData.subject && (
         <div style={{ marginBottom: '16px' }}>
           <DescriptionList>
@@ -58,11 +59,7 @@ export const ReviewStepContent = ({ formData, preselected }: ReviewStepContentPr
               </Title>
               <DescriptionListDescription>
                 <div style={{ margin: '0 16px' }}>
-                  <strong>
-                    {formData.subject.kind === 'User'
-                      ? formData.subject.user?.join(', ') || t('Not selected')
-                      : formData.subject.group?.join(', ') || t('Not selected')}
-                  </strong>
+                  <strong>{identityDisplay}</strong>
                 </div>
               </DescriptionListDescription>
             </DescriptionListGroup>
@@ -116,17 +113,14 @@ export const ReviewStepContent = ({ formData, preselected }: ReviewStepContentPr
                             <div>
                               <strong>{clusterNames ? t('Clusters') : t('Access level')}</strong>{' '}
                             </div>
-                            <div>{clusterNames || t('Full access to all clusters in selected cluster sets')}</div>
+                            <div>
+                              {clusterNames
+                                ? clustersDisplay
+                                : t('Full access to all clusters in selected cluster sets')}
+                            </div>
                           </div>
                           <div style={{ marginTop: '8px' }}>
-                            <div>
-                              <strong>{t('Projects')}</strong>{' '}
-                            </div>
-                            <div>
-                              {formData.scope.namespaces && formData.scope.namespaces.length > 0
-                                ? formData.scope.namespaces.join(', ')
-                                : t('Full access')}
-                            </div>
+                            <strong>{t('Projects')}</strong> {namespacesDisplay}
                           </div>
                         </>
                       )
@@ -134,13 +128,10 @@ export const ReviewStepContent = ({ formData, preselected }: ReviewStepContentPr
                       return (
                         <>
                           <div>
-                            <strong>{t('Clusters')}:</strong> {clusterNames || t('None selected')}
+                            <strong>{t('Clusters')}:</strong> {clustersDisplay}
                           </div>
                           <div style={{ marginTop: '8px' }}>
-                            <strong>{t('Projects')}:</strong>{' '}
-                            {formData.scope.namespaces && formData.scope.namespaces.length > 0
-                              ? formData.scope.namespaces.join(', ')
-                              : t('Full access')}
+                            <strong>{t('Projects')}:</strong> {namespacesDisplay}
                           </div>
                         </>
                       )
@@ -161,11 +152,7 @@ export const ReviewStepContent = ({ formData, preselected }: ReviewStepContentPr
               {t('Role')}
             </Title>
             <DescriptionListDescription>
-              <div style={{ margin: '0 16px' }}>
-                <strong>
-                  <strong>{formData.roles?.[0] ?? t('No role selected')}</strong>
-                </strong>
-              </div>
+              <div style={{ margin: '0 16px' }}>{roleDisplay}</div>
             </DescriptionListDescription>
           </DescriptionListGroup>
         </DescriptionList>
