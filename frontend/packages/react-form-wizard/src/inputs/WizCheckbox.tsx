cdc15fe3f18875914bf6e35a853e4e625975bffe
Feng Xiang <fxiang@redhat.com>
2026-01-26 18:58:35 -0500

    ACM-28831 Disable AppSet auto sync when the option is unchecked (#5513)
    * Disable AppSet auto sync when the option is unchecked
    
    Signed-off-by: fxiang1 <fxiang@redhat.com>
    
    * Changes based on the review comments
    
    Signed-off-by: fxiang1 <fxiang@redhat.com>
    
    * Fix unit tests
    
    Signed-off-by: fxiang1 <fxiang@redhat.com>
    
    ---------
    
    Signed-off-by: fxiang1 <fxiang@redhat.com>

--- frontend/packages/react-form-wizard/src/inputs/WizCheckbox.tsx
+++ frontend/packages/react-form-wizard/src/inputs/WizCheckbox.tsx
@@ -56,6 +56,7 @@ export function WizCheckbox(props: WizCheckboxProps) {
             id={id}
             isChecked={getIsChecked(value)}
             onChange={onChange}
+            isDisabled={props.disabled}
             label={
               <>
                 {props.label} <LabelHelp id={id} labelHelp={props.labelHelp} labelHelpTitle={props.labelHelpTitle} />
