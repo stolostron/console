a4214cd1ece603cf3a656d42d674eb6672ef75fc
Enrique Mingorance Cano <emingora@redhat.com>
2026-01-30 11:10:03 +0100

    RoleAssignmentWizardModal detected hasChanges for empty and undefined arrays. Fixed
    Signed-off-by: Enrique Mingorance Cano <emingora@redhat.com>

--- frontend/src/wizards/RoleAssignment/RoleAssignmentWizardModal.tsx
+++ frontend/src/wizards/RoleAssignment/RoleAssignmentWizardModal.tsx
@@ -251,10 +251,11 @@ export const RoleAssignmentWizardModal = ({
 
     const roleChanged = preselected?.roles?.[0] !== formData.roles?.[0]
     const clustersChanged =
-      JSON.stringify(preselected?.clusterNames?.toSorted()) !==
-      JSON.stringify(formData.selectedClusters?.map((c) => c.metadata?.name || c.name || c).toSorted())
+      JSON.stringify(preselected?.clusterNames?.toSorted() ?? []) !==
+      JSON.stringify(formData.selectedClusters?.map((c) => c.metadata?.name || c.name || c).toSorted() ?? [])
     const namespacesChanged =
-      JSON.stringify(preselected?.namespaces?.toSorted()) !== JSON.stringify(formData.scope.namespaces?.toSorted())
+      JSON.stringify((preselected?.namespaces ?? []).toSorted()) !==
+      JSON.stringify((formData.scope.namespaces ?? []).toSorted())
 
     const identityKindChanged = preselected?.subject?.kind !== formData.subject?.kind
     const identityValueChanged = (() => {
