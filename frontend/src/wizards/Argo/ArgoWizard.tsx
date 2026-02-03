c38f6e24b468d1e0ad9da165ddb2f28fd408799a
fxiang1 <fxiang@redhat.com>
2026-02-03 11:28:19 -0500

    Fix PF6 margin issue with Argo server create modal
    Signed-off-by: fxiang1 <fxiang@redhat.com>

--- frontend/src/wizards/Argo/ArgoWizard.tsx
+++ frontend/src/wizards/Argo/ArgoWizard.tsx
@@ -387,6 +387,7 @@ export function ArgoWizard(props: ArgoWizardProps) {
         aria-describedby="modal-wizard-description"
         onClose={handleModalToggle}
         hasNoBodyWrapper
+        className="add-argo-server-modal"
       >
         <CreateArgoResources handleModalToggle={handleModalToggle} clusterSets={props.clusterSets} />
       </Modal>
