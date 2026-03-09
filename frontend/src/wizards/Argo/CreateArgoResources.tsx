0cd3649ad4639e5e6f5af2a4ef25caa0a5812979
John Swanke <jswanke@redhat.com>
2026-03-09 14:10:08 -0400

    init
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- frontend/src/wizards/Argo/CreateArgoResources.tsx
+++ frontend/src/wizards/Argo/CreateArgoResources.tsx
@@ -41,6 +41,7 @@ export function WizardSyncEditor() {
       variant="toolbar"
       resources={resources}
       schema={schema}
+      filters={['*.metadata.managedFields']}
       onEditorChange={(changes: { resources: any[] }): void => {
         update(changes?.resources)
       }}
