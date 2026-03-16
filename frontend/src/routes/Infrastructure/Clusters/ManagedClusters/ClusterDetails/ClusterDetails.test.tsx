b79571a7183422753128a4913e901bc9e292c5ca
Oksana Bazylieva <obazylie@redhat.com>
2026-03-16 14:01:33 -0400

    Remove obsolete cluster-version nock after mocking useClusterVersion
    Signed-off-by: Oksana Bazylieva <obazylie@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/ClusterDetails/ClusterDetails.test.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/ClusterDetails/ClusterDetails.test.tsx
@@ -4,7 +4,6 @@ import { AgentClusterInstallK8sResource, HostedClusterK8sResource } from '@opens
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import _ from 'lodash'
 import cloneDeep from 'lodash/cloneDeep'
-import nock from 'nock'
 import { Scope } from 'nock/types'
 import { generatePath, MemoryRouter, Route, Routes } from 'react-router-dom-v5-compat'
 import { RecoilRoot } from 'recoil'
@@ -1349,17 +1348,6 @@ describe('ClusterDetails', () => {
       { data: { searchResult: { items: [], __typename: 'SearchResult' } } }
     )
 
-    nock(process.env.JEST_DEFAULT_HOST as string)
-      .get('/cluster-version')
-      .reply(
-        200,
-        { version: '4.21.0' },
-        {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, OPTIONS',
-          'Access-Control-Allow-Credentials': 'true',
-        }
-      )
     nockIgnoreOperatorCheck()
 
     render(<Component />)
