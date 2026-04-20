f69fef3a88df4dfcc037d955ddbc2887f18760a0
Kevin Cormier <kcormier@redhat.com>
2026-04-20 11:55:25 -0400

    Convert to TypeScript and functional components
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- frontend/src/components/TemplateEditor/controls/Checkbox.tsx
+++ frontend/src/components/TemplateEditor/controls/Checkbox.tsx
@@ -2,66 +2,70 @@
 'use strict'
 
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Checkbox, Radio } from '@patternfly/react-core'
-import ControlPanelFormGroup from './ControlPanelFormGroup'
+import ControlPanelFormGroup from './FormGroup'
+import { ControlPanelBaseProps } from '../types'
 
-class ControlPanelCheckbox extends React.Component {
-  static propTypes = {
-    control: PropTypes.object,
-    controlId: PropTypes.string,
-    handleChange: PropTypes.func,
-    i18n: PropTypes.func,
+type Props = ControlPanelBaseProps & {
+  handleChange: () => void
+}
+
+const ControlPanelCheckbox = (props: Props) => {
+  const { controlId, control, controlData, handleChange, i18n } = props
+  const {
+    name,
+    active,
+    type,
+    tip,
+    disabled = false,
+  } = control as {
+    name?: string
+    active?: boolean | string
+    type?: string
+    tip?: React.ReactNode
+    disabled?: boolean
   }
 
-  constructor(props) {
-    super(props)
-    this.state = {}
+  const onChange = () => {
+    control.active = !active
+    handleChange()
   }
 
-  setControlRef = (control, ref) => {
+  const setControlRef = (ref: HTMLDivElement | null) => {
     control.ref = ref
   }
 
-  render() {
-    const { controlId, control, controlData, handleChange, i18n } = this.props
-    const { name, active, type, tip, disabled = false } = control
-
-    const onChange = () => {
-      control.active = !active
-      handleChange()
-    }
-
-    const Input = type === 'radio' ? Radio : Checkbox
-    return (
-      <React.Fragment>
-        <div
-          style={{ flexDirection: 'column', alignItems: 'flex-start' }}
-          className="creation-view-controls-checkbox"
-          ref={this.setControlRef.bind(this, control)}
-        >
-          <div style={{ display: 'flex', alignItems: 'baseline' }}>
-            <Input
-              aria-label={name}
-              id={controlId}
-              isChecked={typeof active === 'boolean' ? active : active === 'true'}
-              isDisabled={disabled}
-              onChange={onChange}
-              data-testid={`checkbox-${controlId}`}
-            />
-            <ControlPanelFormGroup
-              i18n={i18n}
-              controlId={controlId}
-              control={control}
-              controlData={controlData}
-              showTip={false}
-            />
-          </div>
-          <div style={{ fontSize: '14px', fontWeight: 'normal' }}>{tip}</div>
+  const Input = type === 'radio' ? Radio : Checkbox
+  return (
+    <React.Fragment>
+      <div
+        style={{ flexDirection: 'column', alignItems: 'flex-start' }}
+        className="creation-view-controls-checkbox"
+        ref={setControlRef}
+      >
+        <div style={{ display: 'flex', alignItems: 'baseline' }}>
+          <Input
+            aria-label={name}
+            id={controlId}
+            isChecked={typeof active === 'boolean' ? active : active === 'true'}
+            isDisabled={disabled}
+            onChange={onChange}
+            data-testid={`checkbox-${controlId}`}
+          />
+          <ControlPanelFormGroup
+            i18n={i18n}
+            controlId={controlId}
+            control={control}
+            controlData={controlData}
+            showTip={false}
+          >
+            {null}
+          </ControlPanelFormGroup>
         </div>
-      </React.Fragment>
-    )
-  }
+        <div style={{ fontSize: '14px', fontWeight: 'normal' }}>{tip}</div>
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default ControlPanelCheckbox
