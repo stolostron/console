edad22581702561232977aa30d48effb8d485350
John Swanke <jswanke@redhat.com>
2026-06-22 13:03:53 -0400

    tweak
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- frontend/src/routes/Governance/policies/CreatePolicyAutomation.test.tsx
+++ frontend/src/routes/Governance/policies/CreatePolicyAutomation.test.tsx
@@ -75,8 +75,6 @@ describe('Create Policy Automation Wizard', () => {
       const jobDropdown = await screen.findByPlaceholderText('Select the ansible job')
       jobDropdown.click()
       await new Promise((resolve) => setTimeout(resolve, 500))
-      screen.logTestingPlaygroundURL()
-
       const jobOption = await screen.findByRole('option', { name: 'test-job-pre-install' })
       jobOption.click()
       screen.getByPlaceholderText(/select the schedule/i).click()
