3bb86b03ae1f90aad44088633138448cb027dee1
Nitin Dhevar <46418326+nitin-dhevar@users.noreply.github.com>
2026-08-20 13:24:29 -0400

    Updating rosa hcp wizard prerequisites page. (#6735)
    * Updating rosa hcp wizard prerequisites page
    
    Signed-off-by: Nitin Dhevar <nitin.dhanabalan@gmail.com>
    
    * Adding fixes for coderabbit flagged issues
    
    Signed-off-by: Nitin Dhevar <nitin.dhanabalan@gmail.com>
    
    ---------
    
    Signed-off-by: Nitin Dhevar <nitin.dhanabalan@gmail.com>

---
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/components/rosahcp/PrerequisitesPage/StepEnableAutoImport.test.tsx
@@ -0,0 +1,35 @@
+/* Copyright Contributors to the Open Cluster Management project */
+
+import { render, screen } from '@testing-library/react'
+import { axe } from 'jest-axe'
+import { StepEnableAutoImport } from './StepEnableAutoImport'
+
+describe('StepEnableAutoImport', () => {
+  test('should render the title', () => {
+    render(<StepEnableAutoImport />)
+
+    expect(
+      screen.getByRole('heading', {
+        name: 'Enable auto import so that provisioned clusters are automatically registered with ACM.',
+      })
+    ).toBeInTheDocument()
+  })
+
+  test('should render the edit ClusterManager command', () => {
+    render(<StepEnableAutoImport />)
+
+    expect(screen.getByDisplayValue('oc edit ClusterManager cluster-manager')).toBeInTheDocument()
+  })
+
+  test('should render the ClusterRoleBinding apply command', () => {
+    render(<StepEnableAutoImport />)
+
+    expect(screen.getByDisplayValue(/cluster-manager-registration-capi/)).toBeInTheDocument()
+  })
+
+  test('should have no accessibility violations', async () => {
+    const { container } = render(<StepEnableAutoImport />)
+
+    expect(await axe(container)).toHaveNoViolations()
+  })
+})
