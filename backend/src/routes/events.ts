0193a19f6e964b8f4eb99831e5a93855aa210bed
Enrique Mingorance Cano <emingora@redhat.com>
2026-09-16 14:27:50 +0200

    references to #6609 reverted back from events.ts
    Signed-off-by: Enrique Mingorance Cano <emingora@redhat.com>

--- backend/src/routes/events.ts
+++ backend/src/routes/events.ts
@@ -680,14 +680,10 @@ export async function cacheResource(resource: IResource) {
       isHubSelfManaged = true
     }
   }
-
-  if (
-    resource.kind === 'ManagedClusterAddOn' &&
-    resource.apiVersion.startsWith('addon.open-cluster-management.io/') &&
-    (resource.metadata?.name === 'observability-controller' ||
-      resource.metadata?.name == 'multicluster-observability-addon')
-  ) {
-    isObservabilityInstalled = true
+  if (resource.kind === 'ManagedClusterAddOn') {
+    if (resource?.metadata?.name === 'observability-controller') {
+      isObservabilityInstalled = true
+    }
   }
 }
 
