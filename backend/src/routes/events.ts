85256469b7eddd227ec8d6463133ee0c826ac1a7
Enrique Mingorance Cano <emingora@redhat.com>
2026-09-16 13:29:53 +0200

    PR #5863 removed from events.ts
    Signed-off-by: Enrique Mingorance Cano <emingora@redhat.com>

--- backend/src/routes/events.ts
+++ backend/src/routes/events.ts
@@ -653,7 +653,7 @@ export async function cacheResource(resource: IResource) {
     const latestExisting = cache[uid]
     if (latestExisting === existing) {
       // if no other cacheResource call updated the cache while we were awaiting, we can replace the cache entry and event
-      if (eventID > 0) ServerSideEvents.removeEvent(eventID)
+      ServerSideEvents.removeEvent(eventID)
       break
     }
     // if a deleteResource ran while we were awaiting, we will exit the loop because the resource is no longer existing
@@ -699,10 +699,7 @@ async function deleteResource(resource: IResource) {
   const uid = resource.metadata.uid
 
   const existing = cache[uid]
-  if (existing) {
-    const eventID = await existing.eventID
-    if (eventID > 0) ServerSideEvents.removeEvent(eventID)
-  }
+  if (existing) ServerSideEvents.removeEvent(await existing.eventID)
 
   const deletedID = await ServerSideEvents.pushEvent({
     data: {
