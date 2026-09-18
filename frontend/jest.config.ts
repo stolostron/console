53b7c04979f3c2ab023390bf88b58185dbf602f8
Enrique Mingorance Cano <emingora@redhat.com>
2026-09-18 17:17:51 +0200

    React Router v8-compatible (#6850)
    Signed-off-by: Enrique Mingorance Cano <emingora@redhat.com>

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -22,7 +22,6 @@ const config: Config.InitialOptions = {
     '@openshift-assisted/ui-lib/cim': '<rootDir>/node_modules/@openshift-assisted/ui-lib/build/cjs/cim',
     '@openshift-assisted/locales/([a-z]{2,3}/translation.json)':
       '<rootDir>/node_modules/@openshift-assisted/locales/lib/$1/translation.json',
-    '^react-router-dom-v5-compat$': '<rootDir>/node_modules/react-router',
   },
   watchAll: false,
   slowTestThreshold: 30,
