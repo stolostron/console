61ca6e87150a0c33597c0aed2252cd2c02e968da
Enrique Mingorance Cano <emingora@redhat.com>
2026-09-22 07:12:10 +0200

    prettier error fixed
    Signed-off-by: Enrique Mingorance Cano <emingora@redhat.com>

--- frontend/src/routes/Infrastructure/Clusters/ManagedClusters/ClusterDetails/ClusterDetails.tsx
+++ frontend/src/routes/Infrastructure/Clusters/ManagedClusters/ClusterDetails/ClusterDetails.tsx
@@ -188,7 +188,7 @@ export default function ClusterDetailsPage() {
   }, [namespace])
 
   const { isSearchAvailable } = useContext(PluginContext)
-  
+
   const clusterDetailsContext = useMemo<ClusterDetailsContext>(
     () => ({
       cluster,
